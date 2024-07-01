f920592f423bded50230a4fec4e83ddd02142c0a
nany625 <jimmy19980625@gmail.com>
2024-07-01 11:43:48 +0800

    Create solution.c

---
+++ 1200 A DP Problem/solution.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        char buffer[256];
+        scanf("%s", buffer);
+        int i = 0, CoefficientOfX = 0, Const = 0, coefficient = 0;
+        bool plus = true, EmptyCoefficient = true;
+        while(buffer[i] != '=') {
+            if(isdigit(buffer[i])) {
+                coefficient = coefficient * 10 + buffer[i] - '0';
+                EmptyCoefficient = false;
+            } else if(buffer[i] == '+') {
+                if(plus)
+                    Const += coefficient;
+                else {
+                    Const -= coefficient;
+                    plus = true;
+                }
+                coefficient = 0;
+                EmptyCoefficient = true;
+            } else if(buffer[i] == '-') {
+                if(plus) {
+                    Const += coefficient;
+                    plus = false;
+                } else
+                    Const -= coefficient;
+                coefficient = 0;
+                EmptyCoefficient = true;
+            } else if(buffer[i] == 'x') {
+                if(plus)
+                    CoefficientOfX += EmptyCoefficient + coefficient;
+                else
+                    CoefficientOfX -= EmptyCoefficient + coefficient;
+                coefficient = 0;
+                EmptyCoefficient = true;
+            }
+            ++i;
+        }
+        if(plus)
+            Const += coefficient;
+        else
+            Const -= coefficient;
+        plus = true;
+        coefficient = 0;
+        EmptyCoefficient = true;
+        ++i;
+        while(i < strlen(buffer)) {
+            if(isdigit(buffer[i])) {
+                coefficient = coefficient * 10 + buffer[i] - '0';
+                EmptyCoefficient = false;
+            } else if(buffer[i] == '+') {
+                if(plus)
+                    Const -= coefficient;
+                else {
+                    Const += coefficient;
+                    plus = true;
+                }
+                coefficient = 0;
+                EmptyCoefficient = true;
+            } else if(buffer[i] == '-') {
+                if(plus) {
+                    Const -= coefficient;
+                    plus = false;
+                } else
+                    Const += coefficient;
+                coefficient = 0;
+                EmptyCoefficient = true;
+            } else if(buffer[i] == 'x') {
+                if(plus)
+                    CoefficientOfX -= EmptyCoefficient + coefficient;
+                else
+                    CoefficientOfX += EmptyCoefficient + coefficient;
+                coefficient = 0;
+                EmptyCoefficient = true;
+            }
+            ++i;
+        }
+        if(plus)
+            Const -= coefficient;
+        else
+            Const += coefficient;
+        if(CoefficientOfX == 0 && Const == 0)
+            puts("IDENTITY");
+        else if(CoefficientOfX == 0 && Const != 0)
+            puts("IMPOSSIBLE");
+        else
+            printf("%.f\n", floor((double)Const / CoefficientOfX * (-1)));
+    }
+    return 0;
+}
