09f56c4f6fc213a00e9b8664ab71c53a57310185
nany625 <jimmy19980625@gmail.com>
2024-07-02 03:24:31 +0800

    Create solution.c

---
+++ 10800 Not That Kind of Graph/solution.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int N;
+	scanf("%d", &N);
+	for(int i = 1; i <= N; ++i) {
+		printf("Case #%d:\n", i);
+		char stockPrice[51];
+		scanf("%s", stockPrice);
+		int min, max, curr = 0;
+		if(stockPrice[0] == 'R')
+			min = max = curr++;
+		else if(stockPrice[0] == 'F')
+			min = max = --curr;
+		else if(stockPrice[0] == 'C')
+			min = max = curr;
+		int len = strlen(stockPrice);
+		for(int j = 1; j < len; ++j) {
+			if(stockPrice[j] == 'R'){
+				if(max < curr)
+					max = curr;
+				++curr;
+			} else if(stockPrice[j] == 'F') {
+				--curr;
+				if(min > curr)
+					min = curr;
+			} else if(stockPrice[j] == 'C') {
+				if(min > curr)
+					min = curr;
+				if(max < curr)
+					max = curr;
+			}
+		}
+		char graph[max - min + 1][len + 1];
+		int start = max;
+		for(int j = 0; j < len; ++j) {
+			for(int k = 0; k < max - min + 1; ++k) {
+				graph[k][j] = ' ';
+				graph[k][j + 1] = '\0';
+			}
+			if(stockPrice[j] == 'R')
+				graph[start--][j] = '/';
+			else if(stockPrice[j] == 'F')
+				graph[++start][j] = '\\';
+			else if(stockPrice[j] == 'C')
+				graph[start][j] = '_';
+		}
+		for(int j = 0; j < max - min + 1; ++j) {
+		    len = strlen(graph[j]) - 1;
+			while(graph[j][len] == ' ')
+				graph[j][len--] = '\0';
+			printf("| %s\n", graph[j]);
+		}
+		putchar('+');
+		len = strlen(stockPrice) + 2;
+		while(len--)
+			putchar('-');
+		puts("\n");
+	}
+	return 0;
+}
