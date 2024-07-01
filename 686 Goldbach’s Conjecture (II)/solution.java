81a520012e61bbcabd53cc920bbc30f0ef24eb44
nany625 <jimmy19980625@gmail.com>
2024-07-01 15:07:25 +0800

    Create solution.java

---
+++ 686 Goldbach’s Conjecture (II)/solution.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main {
+    static boolean[] map = new boolean[32768];
+    static short[] primes = new short[3512];
+    static int count = 1;
+	public static void main(String[] args) {
+	    try {
+            map[2] = true;
+    		primes[0] = 2;
+    		for(short i = 3; count < primes.length; i += 2) {
+    	        if(isPrime(i)) {
+    	            map[i] = true;
+    	            primes[count++] = i;
+    	        }
+    	    }
+    	    StreamTokenizer st = new StreamTokenizer(System.in);
+    	    int n;
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0) {
+    		    count = map[n - 2] ? 1 : 0;
+    			int limit = n / 2;
+    			for(int i = 1; primes[i] <= limit; ++i) {
+    				if(map[n - primes[i]])
+    					++count;
+    			}
+    			System.out.println(count);
+            }
+	    } catch(IOException e) {}
+	}
+	
+	static boolean isPrime(short n) {
+	    short limit = (short)Math.sqrt(n);
+	    for(int i = 1; i < count && primes[i] <= limit; ++i) {
+	        if(n % primes[i] == 0)
+        		return false;
+        }
+        return true;
+	}
+}
