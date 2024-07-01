3ac4784fcd1d8308c437216e3633f74df9ac6464
nany625 <jimmy19980625@gmail.com>
2024-07-01 13:23:26 +0800

    Update solution.java

--- 340 Master-Mind Hints/solution.java
+++ 340 Master-Mind Hints/solution.java
@@ -1,47 +1,52 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-		int games = 0, N;
-		while((N = sc.nextInt()) != 0) {
-			System.out.println("Game " + (++games) + ":");
-			char[] secretCode = new char[N];
-			for(int i = 0; i < N; ++i)
-				secretCode[i] = sc.next().charAt(0);
-			char[] guess = new char[N];
-			boolean lastGuess;
-			do {
-				lastGuess = true;
-				for(int i = 0; i < N; ++i) {
-					guess[i] = sc.next().charAt(0);
-					if(guess[i] != '0')
-						lastGuess = false;
-				}
-				if(!lastGuess) {
-				    boolean[] check = new boolean[N];
-					int a = 0, b = 0;
-					for(int i = 0; i < N; ++i) {
-						if(secretCode[i] == guess[i]) {
-							++a;
-							check[i] = true;
-							guess[i] = '0';
-						}
-					}
-					for(int i = 0; i < N; ++i) {
-						for(int j = 0; j < N; ++j) {
-							if(secretCode[i] == guess[j] && !check[i] && guess[j] != '0') {
-								++b;
-								check[i] = true;
-								guess[j] = '0';
-								break;
-							}
-						}
-					}
-					System.out.println("    (" + a + "," + b + ")");
-				}
-			} while(!lastGuess);
-		}
-		sc.close();
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StreamTokenizer st = new StreamTokenizer(br);
+            int games = 0, N;
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0) {
+                System.out.println("Game " + (++games) + ":");
+    			char[] secretCode = new char[N];
+    			for(int i = 0; i < N; ++i) {
+    				secretCode[i] = (char)br.read();
+    				br.read();
+    			}
+    			char[] guess = new char[N];
+    			boolean lastGuess;
+    			do {
+    				lastGuess = true;
+    				for(int i = 0; i < N; ++i) {
+    					guess[i] = (char)br.read();
+    					br.read();
+    					if(guess[i] != '0')
+    						lastGuess = false;
+    				}
+    				if(!lastGuess) {
+    				    boolean[] check = new boolean[N];
+    					int a = 0, b = 0;
+    					for(int i = 0; i < N; ++i) {
+    						if(secretCode[i] == guess[i]) {
+    							++a;
+    							check[i] = true;
+    							guess[i] = '0';
+    						}
+    					}
+    					for(int i = 0; i < N; ++i) {
+    						for(int j = 0; j < N; ++j) {
+    							if(secretCode[i] == guess[j] && !check[i] && guess[j] != '0') {
+    								++b;
+    								check[i] = true;
+    								guess[j] = '0';
+    								break;
+    							}
+    						}
+    					}
+    					System.out.println("    (" + a + "," + b + ")");
+    				}
+    			} while(!lastGuess);
+            }
+        } catch(IOException e) {};
     }
 }
