d6c597ac944d7b246946d770b9353500682bba74
nany625 <jimmy19980625@gmail.com>
2024-07-02 04:52:13 +0800

    Create solution.c

---
+++ 13185 DPA Numbers I/solution.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+char table[999];
+
+void sumOfDivisors(int n) {
+    if(table[n] == '2')
+        return;
+    int sum = 1, i, num = n + 2;
+    float limit = sqrt(num);
+    for(i = 2; i < limit && sum <= num; ++i) {
+        if(num % i == 0)
+            sum += i + num / i;
+    }
+    if(i == limit)
+        sum += i;
+    if(sum < num)
+        table[n] = '0';
+    else if(sum == num)
+        table[n] = '1';
+    else {
+        for(i = num; i <= 1000; i += num)
+            table[i - 2] = '2';
+    }
+}
+
+int main() {
+    for(int i = 0; i < 999; ++i)
+        sumOfDivisors(i);
+	int t;
+	scanf("%d", &t);
+	while(t--) {
+		int n;
+		scanf("%d", &n);
+		if(table[n - 2] == '0')
+			puts("deficient");
+		else if(table[n - 2] == '1')
+			puts("perfect");
+		else
+			puts("abundant");
+	}
+	return 0;
+}
