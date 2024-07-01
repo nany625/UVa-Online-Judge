3d27bab522fcb81314e7a793bb104a9b53be34eb
nany625 <jimmy19980625@gmail.com>
2024-07-01 18:24:06 +0800

    Create solution.c

---
+++ 10161 Ant on a Chessboard/solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int N;
+	while(scanf("%d", &N) && N != 0) {
+		int circle = ceil(sqrt(N)), corner = 1 + circle * (circle - 1);
+		if(circle % 2 == 1) {
+			if(N < corner)
+				printf("%d %d\n", circle, circle - (corner - N));
+			else
+				printf("%d %d\n", circle - (N - corner), circle);
+		} else {
+			if(N < corner)
+				printf("%d %d\n", circle - (corner - N), circle);
+			else
+				printf("%d %d\n", circle, circle - (N - corner));
+		}
+	}
+	return 0;
+}
