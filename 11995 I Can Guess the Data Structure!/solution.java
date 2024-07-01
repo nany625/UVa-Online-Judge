92075c02bb685079ee1d3c5d3d94d1906fa6cac8
nany625 <jimmy19980625@gmail.com>
2024-07-02 04:19:56 +0800

    Create solution.java

---
+++ 11995 I Can Guess the Data Structure!/solution.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+	    try {
+	        StreamTokenizer st = new StreamTokenizer(System.in);
+	        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+	            int n = (int)st.nval;
+	            short max = 1;
+	            LinkedList<Short> stack = new LinkedList<>();
+    			LinkedList<Short> queue = new LinkedList<>();
+    			LinkedList<Short> priorityQueue = new LinkedList<>();
+    			boolean isStack = true, isQueue = true, isPriorityQueue = true;
+    			while(n-- > 0) {
+    			    st.nextToken();
+    				int command = (int)st.nval;
+    				st.nextToken();
+    				short x = (short)st.nval;
+    				if(command == 1){
+    					stack.add(x);
+    					queue.add(x);
+    					priorityQueue.add(x);
+    					max = (short)Math.max(max, x);
+    				} else if(command == 2) {
+    					if(!priorityQueue.contains(x))
+    						isStack = isQueue = isPriorityQueue = false;
+    					else {
+    						if(isStack && x != stack.removeLast())
+    							isStack = false;
+    						if(isQueue && x != queue.removeFirst())
+    							isQueue = false;
+    						priorityQueue.remove(Short.valueOf(x));
+    						if(x == max) {
+    							max = 1;
+    							for(Short s : priorityQueue)
+    								max = (short)Math.max(max, s);
+    						} else
+    							isPriorityQueue = false;	
+    					}
+    				}
+    			}
+    			if(!isStack && !isQueue && !isPriorityQueue)
+    				System.out.println("impossible");
+    			else if((isStack && isQueue) || (isQueue && isPriorityQueue) || (isStack && isPriorityQueue))
+    				System.out.println("not sure");
+    			else if(isStack)
+    				System.out.println("stack");
+    			else if(isQueue)
+    				System.out.println("queue");
+    			else if(isPriorityQueue)
+    				System.out.println("priority queue");
+	        }
+	    } catch(IOException e) {}
+	}
+}
