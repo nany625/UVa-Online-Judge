91153e8b00f19de5aaf97c936d5b17eb632c610e
nany625 <jimmy19980625@gmail.com>
2024-07-01 14:31:05 +0800

    Update solution.java

--- 294 Divisors/solution.java
+++ 294 Divisors/solution.java
@@ -1,42 +1,45 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
     static short[] primes = new short[3401];
     static int count = 1;
-    static {
-        primes[0] = 2;
-        for(short i = 3; count < primes.length; i += 2) {
-            if(isPrime(i))
-                primes[count++] = i;
-        }
-    };
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-		while(N-- > 0) {
-			int L = sc.nextInt(), U = sc.nextInt(), maxCount = 0, ans = L;
-			for(int i = L; i <= U; ++i) {
-    			int factorCount = 1, j = 0, temp = i;
-    			while(j < primes.length && temp > 1) {
-    			    int currPow = 0;
-    			    while(temp % primes[j] == 0) {
-    			        ++currPow;
-    			        temp /= primes[j];
-    			    }
-    			    factorCount *= currPow + 1;
-    			    ++j;
-    			}
-    			if(maxCount < factorCount) {
-    				maxCount = factorCount;
-    				ans = i;
-    			}
+	public static void main(String[] args) {
+	    try {
+	        primes[0] = 2;
+            for(short i = 3; count < primes.length; i += 2) {
+                if(isPrime(i))
+                    primes[count++] = i;
+            }
+            StreamTokenizer st = new StreamTokenizer(System.in);
+            st.nextToken();
+            int N = (int)st.nval;
+            while(N-- > 0) {
+                st.nextToken();
+    			int L = (int)st.nval;
+    			st.nextToken();
+    			int U = (int)st.nval, maxCount = 0, ans = L;
+    			for(int i = L; i <= U; ++i) {
+        			int factorCount = 1, j = 0, temp = i;
+        			while(j < primes.length && temp > 1) {
+        			    int currPow = 0;
+        			    while(temp % primes[j] == 0) {
+        			        ++currPow;
+        			        temp /= primes[j];
+        			    }
+        			    factorCount *= currPow + 1;
+        			    ++j;
+        			}
+        			if(maxCount < factorCount) {
+        				maxCount = factorCount;
+        				ans = i;
+        			}
+        		}
+    			System.out.println("Between " + L + " and " + U + ", " + ans + " has a maximum of " + maxCount + " divisors.");
     		}
-			System.out.println("Between " + L + " and " + U + ", " + ans + " has a maximum of " + maxCount + " divisors.");
-		}
-		sc.close();
-    }
-    
-    static boolean isPrime(short n) {
+	    } catch(IOException e) {}
+	}
+	
+	static boolean isPrime(short n) {
         short limit = (short)Math.sqrt(n);
         for(int i = 1; i < count && primes[i] <= limit; ++i) {
             if(n % primes[i] == 0)
