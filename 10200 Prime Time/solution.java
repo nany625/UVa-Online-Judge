7ff6121993a76b81f02badcb512ce7fee80b06ed
nany625 <jimmy19980625@gmail.com>
2024-07-01 18:41:56 +0800

    Create solution.java

---
+++ 10200 Prime Time/solution.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class Main {
+    static short[] primes = new short[1230], ppCount = new short[10001];
+	static int primesCount = 1;
+	public static void main(String[] args) {
+	    try {
+	        primes[0] = 2;
+    		for(short i = 3; primesCount < primes.length; i += 2) {
+    			if(isPrime(i))
+    				primes[primesCount++] = i;
+    		}
+    		for(short i = 0; i < 40; ++i)
+    			ppCount[i] = (short)(i + 1);
+    		ppCount[40] = ppCount[41] = 40;
+    		for(int i = 42; i <= 10000; ++i) {
+    		    ppCount[i] = ppCount[i - 1];
+    			if(i % 41 == 0 || (i + 1) % 41 == 0)
+    				continue;
+    			if(isPrime(i * i + i + 41))
+    				++ppCount[i];
+    		}
+	        StreamTokenizer st = new StreamTokenizer(System.in);
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+                int a = (int)st.nval;
+                st.nextToken();
+                int b = (int)st.nval;
+    		    System.out.printf("%.2f%n", 100.0 * (ppCount[b] - ppCount[a] + (a == 0 || ppCount[a] > ppCount[a - 1] ? 1 : 0)) / (b - a + 1) + 0.0000001);
+            }
+	    } catch(IOException e) {}
+	}
+	
+	static boolean isPrime(int n) {
+	    short limit = (short)Math.sqrt(n);
+		for(int i = 1; i < primesCount && primes[i] <= limit; ++i) {
+			if(n % primes[i] == 0)
+				return false;
+		}
+		return true;
+	}
+}
