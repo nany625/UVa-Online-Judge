6d110565e4db2d2f241b9ca68766c64a3c60c529
nany625 <jimmy19980625@gmail.com>
2024-07-01 15:15:39 +0800

    Create solution.java

---
+++ 748 Exponentiation/solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) {
+	    try {
+    	    StreamTokenizer st = new StreamTokenizer(System.in);
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+    		    double R = st.nval;
+    		    st.nextToken();
+    		    int n = (int)st.nval;
+    		    BigDecimal ans = BigDecimal.ONE;
+    			while(n-- > 0)
+    				ans = ans.multiply(new BigDecimal("" + R));
+    			StringBuilder sb = new StringBuilder(ans.toPlainString());
+    			int i = 0;
+    			while(sb.charAt(i) == '0')
+    				sb.delete(i, i + 1);
+    			i = sb.length() - 1;
+    			while(sb.charAt(i) == '0') {
+    				sb.delete(i, i + 1);
+    				--i;
+    			}
+    			if(sb.charAt(sb.length() - 1) == '.')
+    				sb.delete(sb.length() - 1, sb.length());
+    			System.out.println(sb);
+            }
+	    } catch(IOException e) {}
+	}
+}
