41b45db7d7ac460b9d04f2d1ddb5e36ca3808a2b
nany625 <jimmy19980625@gmail.com>
2024-07-02 04:36:42 +0800

    Create solution.c

---
+++ 12455 Bars/solution.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isSubsetSum(short *array, int size, int sum) {
+	bool subsetSum[sum + 1];
+	subsetSum[0] = true;
+	for(int i = 1; i <= sum; ++i)
+	    subsetSum[i] = false;
+	for(int i = 0; i < size; ++i) {
+	    for(int j = sum; j >= array[i]; --j) {
+	        if(subsetSum[j - array[i]])
+	            subsetSum[j] = true;
+	    }
+	}
+	return subsetSum[sum];
+}
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while(t--) {
+		int n, p;
+		scanf("%d %d", &n, &p);
+		short barsLen[p];
+		for(int i = 0; i < p; ++i)
+			scanf("%hd", &barsLen[i]);
+		printf("%s\n", isSubsetSum(barsLen, p, n) ? "YES" : "NO");
+	}
+	return 0;
+}
