5891235d7a3c9e9f4909cf24bf47dae1961b421a
nany625 <jimmy19980625@gmail.com>
2024-07-02 06:13:47 +0800

    Create solution.java

---
+++ 100 The 3n + 1 problem/solution.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static ArrayList<Short> lenMap = new ArrayList<>();
+	public static void main(String[] args) {
+	    try {
+	        lenMap.add((short)0);
+		    lenMap.add((short)1);
+            StreamTokenizer st = new StreamTokenizer(System.in);
+    		while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+    		    int min = (int)st.nval;
+    		    st.nextToken();
+    		    int max = (int)st.nval;
+    		    System.out.print(min + " " + max + " ");
+    		    if(min > max) {
+    		        int temp = min;
+    		        min = max;
+    		        max = temp;
+    		    }
+    		    for(int i = lenMap.size(); i <= min; ++i)
+    		        Collatz(i);
+    		    short maxLen = lenMap.get(min);
+    		    for(int i = min + 1; i <= max; ++i) {
+    		        if(i >= lenMap.size())
+                        Collatz(i);
+                    maxLen = maxLen > lenMap.get(i) ? maxLen : lenMap.get(i);
+                }
+                System.out.println(maxLen);
+    		}
+        } catch(IOException e) {};
+	}
+	
+	static void Collatz(int n) {
+	    if(n % 2 == 0) {
+	        lenMap.add((short)(lenMap.get(n / 2) + 1));
+	        return;
+	    }
+	    int size = lenMap.size();
+	    lenMap.add((short)1);
+	    long temp = n;
+	    while(temp != 1) {
+	        if(temp < n) {
+	            lenMap.set(size, (short)(lenMap.get(size) + lenMap.get((int)temp) - 1));
+	            return;
+	        }
+	        temp = temp % 2 == 1 ? 3 * temp + 1 : temp / 2;
+            lenMap.set(size, (short)(lenMap.get(size) + 1));
+	    }
+	}
+}
