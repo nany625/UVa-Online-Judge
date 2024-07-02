c1ad97c2e4c507cfd6d7eb17463f184dc9934ce6
nany625 <jimmy19980625@gmail.com>
2024-07-02 11:17:31 +0800

    Create solution.c

---
+++ 406 Prime Cuts/solution.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#define MAX_SIZE 169
+
+short primes[MAX_SIZE] = {1, 2}, pos[1000] = {1, 2};
+int count = 2;
+
+bool isPrime(short n) {
+    short limit = sqrt(n);
+    for(int i = 2; i < count && primes[i] <= limit; ++i) {
+        if(n % primes[i] == 0)
+            return false;
+    }
+    return true;
+}
+
+void printPrimes(int first, int last) {
+	for(int i = first; i <= last; ++i)
+		printf(" %hd", primes[i]);
+}
+
+int main() {
+	for(short i = 3; i <= 1000; ++i) {
+	    pos[i - 1] = pos[i - 2];
+	    if(i % 2 == 1 && count < MAX_SIZE) {
+	        if(isPrime(i)) {
+    			primes[count++] = i;
+    			++pos[i - 1];
+    		}
+	    }
+		
+	}
+	int N, C;
+    while(scanf("%d %d", &N, &C) == 2) {
+    	printf("%d %d:", N, C);
+    	if(pos[N - 1] % 2 == 0 && 2 * C < pos[N - 1])
+    		printPrimes(pos[N - 1] / 2 - C, pos[N - 1] / 2 + C - 1);
+    	else if(pos[N - 1] % 2 == 1 && 2 * C - 1 < pos[N - 1])
+    		printPrimes((pos[N - 1] + 1) / 2 - C, (pos[N - 1] - 3) / 2 + C);
+    	else
+    		printPrimes(0, pos[N - 1] - 1);
+    	puts("\n");
+    }
+	return 0;
+}
