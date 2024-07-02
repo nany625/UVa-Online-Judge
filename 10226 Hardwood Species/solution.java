77c44e45f83e8206d49a867996d24e0b7ec24bf9
nany625 <jimmy19980625@gmail.com>
2024-07-02 15:03:24 +0800

    Update solution.java

--- 10226 Hardwood Species/solution.java
+++ 10226 Hardwood Species/solution.java
@@ -12,46 +12,25 @@ public class Main {
             while(cases-- > 0) {
     		    if(blankLine)
                     System.out.println();
-                ArrayList<Species> tree = new ArrayList<>();
+                HashMap<String, Integer> tree = new HashMap<>();
+                ArrayList<String> treeKeys = new ArrayList<>();
                 String species;
     	        while((species = br.readLine()) != null && !species.equals("")) {
-                    int pos = binarySearch(tree, species);
-                    if(pos < tree.size() && tree.get(pos).name.equals(species))
-                        ++tree.get(pos).count;
-                    else
-                        tree.add(pos, new Species(species, 1));
+                    if(tree.containsKey(species))
+                    	tree.replace(species, tree.get(species) + 1);
+                    else {
+                    	tree.put(species, 1);
+                    	treeKeys.add(species);
+                    }
                 }
+                Collections.sort(treeKeys);
                 int treeSize = 0;
-                for(Species s : tree)
-                    treeSize += s.count;
-                for(Species s : tree)
-                    System.out.printf("%s %.4f\n", s.name, 100.0 * s.count / treeSize);
+                for(Integer i : tree.values())
+                    treeSize += i;
+                for(String s : treeKeys)
+                    System.out.printf("%s %.4f\n", s, 100.0 * tree.get(s) / treeSize);
                 blankLine = true;
     		}
         } catch(IOException e) {};
 	}
-	
-	static int binarySearch(ArrayList<Species> tree, String key) {
-        int left = 0, right = tree.size() - 1;
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            int cmp = tree.get(mid).name.compareTo(key);
-            if(cmp == 0)
-                return mid;
-            if(cmp < 0)
-                left = mid + 1;
-            else
-                right = mid - 1;
-        }
-        return left;
-    }
-}
-
-class Species {
-    String name;
-    int count;
-    Species(String name, int count) {
-        this.name = name;
-        this.count = count;
-    }
 }
