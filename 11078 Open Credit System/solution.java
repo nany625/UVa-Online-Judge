aff83f03de480fdab895cc4755550f5450c802bc
nany625 <jimmy19980625@gmail.com>
2024-07-03 05:54:35 +0800

    Update solution.java

--- 11078 Open Credit System/solution.java
+++ 11078 Open Credit System/solution.java
@@ -1,27 +1,27 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-	        StreamTokenizer st = new StreamTokenizer(System.in);
+	public static void main(String[] args) throws IOException {
+	    StreamTokenizer st = new StreamTokenizer(System.in);
+	    st.nextToken();
+	    int T = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+	    while(T-- > 0) {
+            st.nextToken();
+	        int n = (int)st.nval;
 	        st.nextToken();
-	        int T = (int)st.nval;
-	        while(T-- > 0) {
-	            st.nextToken();
-	            int n = (int)st.nval;
-	            st.nextToken();
-    		    int senior = (int)st.nval;
-    		    st.nextToken();
-    		    int junior = (int)st.nval;
-    		    int maxScore = Math.max(senior, junior), maxDist = senior - junior;
-    		    while(n-- > 2) {
-    		        st.nextToken();
-    		        junior = (int)st.nval;
-    		        maxDist = Math.max(maxDist, maxScore - junior);
-    		        maxScore = Math.max(maxScore, junior);
-    		    }
-        		System.out.println(maxDist);
-	        }
-	    } catch(IOException e) {}
+    	    int senior = (int)st.nval;
+    	    st.nextToken();
+    	    int junior = (int)st.nval;
+    	    int maxScore = Math.max(senior, junior), maxDist = senior - junior;
+    	    while(n-- > 2) {
+    	        st.nextToken();
+    	        junior = (int)st.nval;
+    	        maxDist = Math.max(maxDist, maxScore - junior);
+    	        maxScore = Math.max(maxScore, junior);
+    	    }
+    		output.append(maxDist + "\n");
+	    }
+	    System.out.print(output);
 	}
 }
