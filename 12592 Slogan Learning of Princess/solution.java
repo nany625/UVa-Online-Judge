0ee25a8d57a619b499c25a207f9ab8a3117f8311
nany625 <jimmy19980625@gmail.com>
2024-07-03 07:30:48 +0800

    Update solution.java

--- 12592 Slogan Learning of Princess/solution.java
+++ 12592 Slogan Learning of Princess/solution.java
@@ -2,18 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            StreamTokenizer st = new StreamTokenizer(br);
-            st.nextToken();
-            int N = (int)st.nval;
-    		HashMap<String, String> slogans = new HashMap<>();
-    		for(int i = 0; i < N; ++i)
-    		    slogans.put(br.readLine(), br.readLine());
-    		st.nextToken();
-            int Q = (int)st.nval;
-    		while(Q-- > 0)
-    			System.out.println(slogans.get(br.readLine()));
-        } catch(IOException e) {};
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+    	HashMap<String, String> slogans = new HashMap<>();
+    	for(int i = 0; i < N; ++i)
+    	    slogans.put(br.readLine(), br.readLine());
+        int Q = Integer.parseInt(br.readLine());
+        StringBuilder output = new StringBuilder();
+    	while(Q-- > 0)
+    		output.append(slogans.get(br.readLine()) + "\n");
+        System.out.print(output);
 	}
 }
