56bdeebac74f35436023c1740fc2bc73676826c1
nany625 <jimmy19980625@gmail.com>
2024-07-03 05:36:06 +0800

    Update solution.java

--- 10340 All in All/solution.java
+++ 10340 All in All/solution.java
@@ -1,26 +1,26 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-	        StreamTokenizer st = new StreamTokenizer(System.in);
-	        while(st.nextToken() == StreamTokenizer.TT_WORD) {
-	            String s = st.sval;
-	            st.nextToken();
-	            String t = st.sval;
-	            int i = 0, j = 0, lens = s.length(), lent = t.length();
-	            while(j < lent) {
-    		        if(s.charAt(i) == t.charAt(j))
-    		            ++i;
-    		        if(i == lens) {
-    		            System.out.println("Yes");
-    		            break;
-    		        }
-    		        ++j;
-    		    }
-    		    if(i < lens)
-    		        System.out.println("No");
-	        }
-	    } catch(IOException e) {}
+	public static void main(String[] args) throws IOException {
+	    StreamTokenizer st = new StreamTokenizer(System.in);
+	    StringBuilder output = new StringBuilder();
+	    while(st.nextToken() == StreamTokenizer.TT_WORD) {
+	        String s = st.sval;
+	        st.nextToken();
+	        String t = st.sval;
+            int i = 0, j = 0, lens = s.length(), lent = t.length();
+	        while(j < lent) {
+		        if(s.charAt(i) == t.charAt(j))
+		            ++i;
+    	        if(i == lens) {
+    	            output.append("Yes\n");
+    	            break;
+    	        }
+    	        ++j;
+    	    }
+    	    if(i < lens)
+    	        output.append("No\n");
+	    }
+	    System.out.print(output);
 	}
 }
