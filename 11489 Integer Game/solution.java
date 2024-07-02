259765babcf366f43e8323cbe1e5cf591f48944d
nany625 <jimmy19980625@gmail.com>
2024-07-03 07:06:27 +0800

    Update solution.java

--- 11489 Integer Game/solution.java
+++ 11489 Integer Game/solution.java
@@ -1,22 +1,22 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-	        StreamTokenizer st = new StreamTokenizer(br);
-	        st.nextToken();
-	        for(int i = 1; i <= (int)st.nval; ++i) {
-	            String N = br.readLine();
-    			int len = N.length();
-    			short[] modThreeCount = new short[3];
-    			for(int j = 0; j < len; ++j)
-    				++modThreeCount[(N.charAt(j) - '0') % 3];
-    			short result = (short)((modThreeCount[1] + modThreeCount[2] * 2) % 3);
-    			if(result == 0)
-    				System.out.println("Case " + i + (modThreeCount[0] % 2 == 1 ? ": S" : ": T"));
-    			else
-    				System.out.println("Case " + i + (modThreeCount[result] != 0 && modThreeCount[0] % 2 == 0 ? ": S" : ": T"));
-    		}
-	    } catch(IOException e) {}
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int T = Integer.parseInt(br.readLine());
+	    StringBuilder output = new StringBuilder();
+        for(int i = 1; i <= T; ++i) {
+	        String N = br.readLine();
+    		int len = N.length();
+    		short[] modThreeCount = new short[3];
+    		for(int j = 0; j < len; ++j)
+    			++modThreeCount[(N.charAt(j) - '0') % 3];
+    		short result = (short)((modThreeCount[1] + modThreeCount[2] * 2) % 3);
+    		if(result == 0)
+    			output.append("Case " + i + (modThreeCount[0] % 2 == 1 ? ": S\n" : ": T\n"));
+    		else
+    			output.append("Case " + i + (modThreeCount[result] != 0 && modThreeCount[0] % 2 == 0 ? ": S\n" : ": T\n"));
+    	}
+	    System.out.print(output);
 	}
 }
