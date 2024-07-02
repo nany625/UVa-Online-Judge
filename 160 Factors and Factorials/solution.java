8dfc8087ddba01cd3220ded5b9dd820232e57bb7
nany625 <jimmy19980625@gmail.com>
2024-07-03 07:59:47 +0800

    Update solution.java

--- 160 Factors and Factorials/solution.java
+++ 160 Factors and Factorials/solution.java
@@ -1,35 +1,35 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-	        char[] primes = {
-                2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 
-                31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
-                73, 79, 83, 89, 97
-            };
-            StreamTokenizer st = new StreamTokenizer(System.in);
-            int N;
-            while((st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int) st.nval) != 0)) {
-                System.out.printf("%3d! =", N);
-        		char[] count = new char[25];
-        		for(int i = 2; i <= N; ++i) {
-        		    int j = 0, temp = i;
-        		    while(temp > 1) {
-        		        if(temp % primes[j] == 0) {
-        		            ++count[j];
-        		            temp /= primes[j];
-        		        } else
-        		            ++j;
-        		    }
-        		}
-        		for(int i = 0; i < 25 && count[i] > 0; ++i) {
-            		if(i == 15)
-            		    System.out.print("\n      ");
-            		System.out.printf("%3d", (int)count[i]);
-        		}
-        		System.out.println();
-            }
-	    } catch(IOException e) {}
+	public static void main(String[] args) throws IOException {
+	    char[] primes = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 
+            31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
+            73, 79, 83, 89, 97
+        };
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        int N;
+        StringBuilder output = new StringBuilder();
+        while((st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int) st.nval) != 0)) {
+            output.append(String.format("%3d! =", N));
+    		char[] count = new char[25];
+    		for(int i = 2; i <= N; ++i) {
+        	    int j = 0, temp = i;
+    		    while(temp > 1) {
+    		        if(temp % primes[j] == 0) {
+        	            ++count[j];
+        	            temp /= primes[j];
+    		        } else
+    		            ++j;
+        	    }
+        	}
+    		for(int i = 0; i < 25 && count[i] > 0; ++i) {
+        		if(i == 15)
+        		    output.append("\n      ");
+        		output.append(String.format("%3d", (int)count[i]));
+    		}
+        	output.append("\n");
+        }
+	    System.out.print(output);
 	}
 }
