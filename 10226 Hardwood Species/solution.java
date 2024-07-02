13bbd1599b462733b7a7eca8a5a8b4ca1b43c53f
nany625 <jimmy19980625@gmail.com>
2024-07-02 17:24:31 +0800

    Update solution.java

--- 10226 Hardwood Species/solution.java
+++ 10226 Hardwood Species/solution.java
@@ -2,37 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            StreamTokenizer st = new StreamTokenizer(br);
-            st.nextToken();
-            int cases = (int)st.nval;
-            br.readLine();
-            boolean blankLine = false;
-            StringBuilder output = new StringBuilder();
-            while(cases-- > 0) {
-    		    if(blankLine)
-                    sb.append('\n');
-                HashMap<String, Integer> tree = new HashMap<>();
-                ArrayList<String> treeKeys = new ArrayList<>();
-                String species;
-    	        while((species = br.readLine()) != null && !species.isEmpty()) {
-                    if(tree.containsKey(species))
-                    	tree.replace(species, tree.get(species) + 1);
-                    else {
-                    	tree.put(species, 1);
-                    	treeKeys.add(species);
-                    }
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cases = Integer.parseInt(br.readLine());
+        br.readLine();
+        StringBuilder output = new StringBuilder();
+        boolean firstCase = true;
+        while(cases-- > 0) {
+    		if(!firstCase)
+                output.append('\n');
+            HashMap<String, Integer> tree = new HashMap<>();
+            ArrayList<String> treeKeys = new ArrayList<>();
+            String species;
+    	    while((species = br.readLine()) != null && !species.isEmpty()) {
+                if(tree.containsKey(species))
+                	tree.replace(species, tree.get(species) + 1);
+                else {
+                	tree.put(species, 1);
+                	treeKeys.add(species);
                 }
-                Collections.sort(treeKeys);
-                int treeSize = 0;
-                for(Integer i : tree.values())
-                    treeSize += i;
-                for(String s : treeKeys)
-                    output.append(String.format("%s %.4f\n", s, 100.0 * tree.get(s) / treeSize));
-                blankLine = true;
-    		}
-    		System.out.print(output);
-        } catch(IOException e) {};
+            }
+            Collections.sort(treeKeys);
+            int treeSize = 0;
+            for(Integer i : tree.values())
+                treeSize += i;
+            for(String s : treeKeys)
+                output.append(String.format("%s %.4f\n", s, 100.0 * tree.get(s) / treeSize));
+            firstCase = false;
+        }
+    	System.out.print(output);
 	}
 }
