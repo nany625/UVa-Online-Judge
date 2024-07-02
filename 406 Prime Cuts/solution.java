db5bf59b5d0637d26b06e26a09d4911f7058eab6
nany625 <jimmy19980625@gmail.com>
2024-07-02 11:22:55 +0800

    Create solution.java

---
+++ 406 Prime Cuts/solution.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class Main {
+    static short[] primes = new short[169], pos = new short[1000];
+    static int count = 2;
+	public static void main(String[] args) {
+	    try {
+	        primes[0] = pos[0] = 1;
+    	    primes[1] = pos[1] = 2;
+    	    for(short i = 3; i <= 1000; ++i) {
+    	        pos[i - 1] = pos[i - 2];
+    	        if(i % 2 == 1 && count < primes.length) {
+    	            if(isPrime(i)) {
+    	                primes[count++] = i;
+    	                ++pos[i - 1];
+    	            }
+    	        }
+    	    }
+	        StreamTokenizer st = new StreamTokenizer(System.in);
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+    	        int N = (int)st.nval;
+    	        st.nextToken();
+    	        int C = (int)st.nval;
+    	        System.out.print(N + " " + C + ":");
+    	        if(pos[N - 1] % 2 == 0 && 2 * C < pos[N - 1])
+            		printPrimes(pos[N - 1] / 2 - C, pos[N - 1] / 2 + C - 1);
+            	else if(pos[N - 1] % 2 == 1 && 2 * C - 1 < pos[N - 1])
+            		printPrimes((pos[N - 1] + 1) / 2 - C, (pos[N - 1] - 3) / 2 + C);
+            	else
+            		printPrimes(0, pos[N - 1] - 1);
+    	        System.out.println("\n");
+            }
+        } catch(IOException e) {};
+	}
+	
+	static boolean isPrime(short n) {
+	    short limit = (short)Math.sqrt(n);
+        for(int i = 2; i < count && primes[i] <= limit; ++i) {
+            if(n % primes[i] == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    static void printPrimes(int first, int last) {
+    	for(int i = first; i <= last; ++i)
+    		System.out.print(" " + primes[i]);
+    }
+}
