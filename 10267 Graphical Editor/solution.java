0be2bc55fcd5f94f24ac9cfe8844aa1760822d1c
nany625 <jimmy19980625@gmail.com>
2024-07-02 19:24:44 +0800

    Update solution.java

--- 10267 Graphical Editor/solution.java
+++ 10267 Graphical Editor/solution.java
@@ -6,7 +6,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    String command;
-	    char C;
 		int M = 0, N = 0, X, X1, X2, Y, Y1, Y2;
 		StringBuilder output = new StringBuilder();
     	while((command = br.readLine()) != null && command.charAt(0) != 'X') {
@@ -20,43 +19,38 @@ public class Main {
     		else if(command.charAt(0) == 'L') {
     	        X = Integer.parseInt(tokens[1]);
     	        Y = Integer.parseInt(tokens[2]);
-    	        C = tokens[3].charAt(0);
-    			table[Y - 1][X - 1] = C;
+    			table[Y - 1][X - 1] = tokens[3].charAt(0);
     		} else if(command.charAt(0) == 'V') {
     		    X = Integer.parseInt(tokens[1]);
     	        Y1 = Integer.parseInt(tokens[2]);
     	        Y2 = Integer.parseInt(tokens[3]);
-    	        C = tokens[4].charAt(0);
     			if(Y1 > Y2) {
     			    int temp = Y1;
     			    Y1 = Y2;
     			    Y2 = temp;
     			}
     			for(int i = Y1 - 1; i <= Y2 - 1; ++i)
-    				table[i][X - 1] = C;
+    				table[i][X - 1] = tokens[4].charAt(0);
     		} else if(command.charAt(0) == 'H') {
     		    X1 = Integer.parseInt(tokens[1]);
     	        X2 = Integer.parseInt(tokens[2]);
     	        Y = Integer.parseInt(tokens[3]);
-    	        C = tokens[4].charAt(0);
     			if(X1 > X2) {
     			    int temp = X1;
     			    X1 = X2;
     			    X2 = temp;
     			}
-    			Arrays.fill(table[Y - 1], X1 - 1, X2, C);
+    			Arrays.fill(table[Y - 1], X1 - 1, X2, tokens[4].charAt(0));
     		} else if(command.charAt(0) == 'K') {
     		    X1 = Integer.parseInt(tokens[1]);
     	        Y1 = Integer.parseInt(tokens[2]);
     	        X2 = Integer.parseInt(tokens[3]);
     	        Y2 = Integer.parseInt(tokens[4]);
-    	        C = tokens[5].charAt(0);
-    			fill(Y1 - 1, Y2 - 1, X1 - 1, X2 - 1, C);
+    			fill(Y1 - 1, Y2 - 1, X1 - 1, X2 - 1, tokens[5].charAt(0));
     		} else if(command.charAt(0) == 'F') {
     	        X = Integer.parseInt(tokens[1]);
     	        Y = Integer.parseInt(tokens[2]);
-    	        C = tokens[3].charAt(0);
-    			dfs(Y - 1, N, X - 1, M, table[Y - 1][X - 1], C);
+    			dfs(Y - 1, N, X - 1, M, table[Y - 1][X - 1], tokens[3].charAt(0));
     		} else if(command.charAt(0) == 'S') {
     			output.append(tokens[1] + "\n");
     			for(int i = 0; i < N; ++i)
