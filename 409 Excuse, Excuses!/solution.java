10fb8789f336c6eea446cf8cc34dd4ce76c925b2
nany625 <jimmy19980625@gmail.com>
2024-07-02 11:41:17 +0800

    Create solution.java

---
+++ 409 Excuse, Excuses!/solution.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+	        int set = 0;
+	        StreamTokenizer st = new StreamTokenizer(br);
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+    	        int K = (int)st.nval;
+    	        st.nextToken();
+    	        int E = (int)st.nval;
+    	        System.out.println("Excuse Set #" + (++set));
+    	        ArrayList<String> keywords = new ArrayList<>();
+    		    while(K-- > 0) {
+    		        st.nextToken();
+    		        String keyword = st.sval;
+    		        if(keyword.equals(keyword.toLowerCase()))
+    		            keywords.add(keyword);
+    		    }
+    		    Collections.sort(keywords);
+    		    Excuse[] excuses = new Excuse[E];
+    		    short max = 0;
+    		    for(int i = 0; i < E; ++i) {
+    		        excuses[i] = new Excuse(br.readLine(), (short)0);
+    		        int start = 0, len = excuses[i].excuse.length();
+    		        while(start < len) {
+    		            if(Character.isLetter(excuses[i].excuse.charAt(start))) {
+    		                int end = start + 1;
+    		                while(end < excuses[i].excuse.length() && Character.isLetter(excuses[i].excuse.charAt(end)))
+    		                    ++end;
+    		                String token = excuses[i].excuse.substring(start, end);
+    		                if(binarySearch(keywords, token))
+    		                    ++excuses[i].count;
+    		                start = end;
+    		            }
+    		            ++start;
+    		        }
+    		        max = (short)Math.max(max, excuses[i].count);
+    		    }
+    		    for(Excuse e : excuses) {
+    		        if(e.count == max)
+    		            System.out.println(e.excuse);
+    		    }
+    		    System.out.println();
+            }
+        } catch(IOException e) {};
+	}
+	
+	static boolean binarySearch(ArrayList<String> arrayList, String key) {
+        int left = 0, right = arrayList.size() - 1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            int cmp = arrayList.get(mid).compareToIgnoreCase(key);
+            if(cmp == 0)
+                return true;
+            if(cmp < 0)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+}
+
+class Excuse {
+    String excuse;
+    short count;
+    Excuse(String excuse, short count) {
+        this.excuse = excuse;
+        this.count = count;
+    }
+}
