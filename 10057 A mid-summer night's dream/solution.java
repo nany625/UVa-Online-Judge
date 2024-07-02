7034ac7de0729434d69b4a357d01472c778ba4a5
nany625 <jimmy19980625@gmail.com>
2024-07-02 18:10:03 +0800

    Update solution.java

--- 10057 A mid-summer night's dream/solution.java
+++ 10057 A mid-summer night's dream/solution.java
@@ -2,24 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-            StreamTokenizer st = new StreamTokenizer(System.in);
-            while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-    		    int n = (int)st.nval;
-    	        int[] X = new int[n];
-    	        for(int i = 0; i < n; ++i) {
-    	            st.nextToken();
-    	            X[i] = (int)st.nval;
-    	        }
-    	        Arrays.sort(X);
-    	        int low = (n - 1) / 2, high = n / 2;
-                while(low > 0 && X[low] == X[low - 1])
-                    --low;
-                while(high < n - 1 && X[high] == X[high + 1])
-                    ++high;
-                System.out.println(X[(n - 1) / 2] + " " + (high - low + 1) + " " + (X[n / 2] - X[(n - 1) / 2] + 1));
-    		}
-        } catch(IOException e) {};
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+    	    int n = (int)st.nval;
+            int[] X = new int[n];
+            for(int i = 0; i < n; ++i) {
+                st.nextToken();
+                X[i] = (int)st.nval;
+            }
+            Arrays.sort(X);
+            int low = (n - 1) / 2, high = n / 2;
+            while(low > 0 && X[low] == X[low - 1])
+                --low;
+            while(high < n - 1 && X[high] == X[high + 1])
+                ++high;
+            output.append(String.format("%d %d %d\n", X[(n - 1) / 2], high - low + 1, X[n / 2] - X[(n - 1) / 2] + 1));
+    	}
+    	System.out.print(output);
 	}
 }
