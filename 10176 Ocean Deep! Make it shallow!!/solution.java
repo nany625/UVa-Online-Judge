89fe763fc54d924c55f6ce7e4bc33e6045fcaa46
nany625 <jimmy19980625@gmail.com>
2024-07-02 18:39:07 +0800

    Update solution.java

--- 10176 Ocean Deep! Make it shallow!!/solution.java
+++ 10176 Ocean Deep! Make it shallow!!/solution.java
@@ -1,21 +1,22 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            int remainder = 0;
-            String line;
-            while((line = br.readLine()) != null) {
-                int len = line.length();
-    		    for(int i = 0; i < len; ++i) {
-    		        if(line.charAt(i) != '#')
-    		            remainder = (2 * remainder + line.charAt(i) - '0') % 131071;
-    		        else {
-    		            System.out.println(remainder == 0 ? "YES" : "NO");
-    		            remainder = 0;
-    		        }
-    		    }
-            }
-	    } catch(IOException e) {}
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int remainder = 0;
+        String line;
+        StringBuilder output = new StringBuilder();
+        while((line = br.readLine()) != null) {
+            int len = line.length();
+    	    for(int i = 0; i < len; ++i) {
+    	        if(line.charAt(i) != '#')
+    	            remainder = (2 * remainder + line.charAt(i) - '0') % 131071;
+    	        else {
+    	            output.append(remainder == 0 ? "YES\n" : "NO\n");
+    	            remainder = 0;
+    	        }
+    	    }
+        }
+        System.out.print(output);
 	}
 }
