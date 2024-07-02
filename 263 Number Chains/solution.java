218d6eac7dd8da2d6c8fdd5afa6308e0aa642e3d
nany625 <jimmy19980625@gmail.com>
2024-07-02 10:48:03 +0800

    Update solution.java

--- 263 Number Chains/solution.java
+++ 263 Number Chains/solution.java
@@ -3,22 +3,23 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String number;
-            while(!(number = br.readLine()).equals("0")) {
+	    try {
+            StreamTokenizer st = new StreamTokenizer(System.in);
+            int number;
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER && (number = (int)st.nval) != 0) {
                 System.out.println("Original number was " + number);
-    			HashMap<String, Boolean> numbers = new HashMap<>();
+    			ArrayList<Integer> numbers = new ArrayList<>();
     			do {
-    				numbers.put(number, true);
-    				char[] newNumber = number.toCharArray();
+    				numbers.add(number);
+    				char[] newNumber = ("" + number).toCharArray();
     				Arrays.sort(newNumber);
     				int asc = Integer.parseInt(new String(newNumber)), len = newNumber.length;
     				for(int i = 0; i < len / 2; ++i)
     				    swap(newNumber, i, len - 1 - i);
     				int desc = Integer.parseInt(new String(newNumber));
     				System.out.println(desc + " - " + asc + " = " + (desc - asc));
-    				number = "" + (desc - asc);
-    			} while(!numbers.containsKey(number));
+    				number = desc - asc;
+    			} while(!numbers.contains(number));
     			System.out.println("Chain length " + numbers.size());
     			System.out.println();
     		}
