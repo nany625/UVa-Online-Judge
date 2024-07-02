090882927a7f3b12ba60d2c9bf3d1b509e101aed
nany625 <jimmy19980625@gmail.com>
2024-07-02 17:46:41 +0800

    Update solution.java

--- 10010 Where's Waldorf?/solution.java
+++ 10010 Where's Waldorf?/solution.java
@@ -2,45 +2,45 @@ import java.io.*;
 
 public class Main {
     static int[] dRow = {0, 1, 0, -1, 1, 1, -1, -1}, dCol = {1, 0, -1, 0, 1, -1, -1, 1};
-	public static void main(String[] args) {
-	    try {
-    	    StreamTokenizer st = new StreamTokenizer(System.in);
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int cases = (int)st.nval;
+        boolean firstCase = true;
+        StringBuilder output = new StringBuilder();
+        while(cases-- > 0) {
+            if(!firstCase)
+			    output.append("\n");
             st.nextToken();
-            int cases = (int)st.nval;
-            boolean blankLine = false;
-            while(cases-- > 0) {
-                if(blankLine)
-				    System.out.println();
+            int m = (int)st.nval;
+            st.nextToken();
+            int n = (int)st.nval;
+            String[] grid = new String[m];
+            for(int i = 0; i < m; ++i) {
                 st.nextToken();
-                int m = (int)st.nval;
-                st.nextToken();
-                int n = (int)st.nval;
-                String[] grid = new String[m];
-                for(int i = 0; i < m; ++i) {
-                    st.nextToken();
-				    grid[i] = st.sval;
-                }
-                st.nextToken();
-                int k = (int)st.nval;
-                while(k-- > 0) {
-                    st.nextToken();
-                    String word = st.sval;
-				    int wordLen = word.length();
-                    boolean found = false;
-    				for(int row = 0; row < m && !found; ++row) {
-    					for(int col = 0; col < n && !found; ++col) {
-    						for(int dir = 0; dir < 8 && !found; ++dir) {
-                                if(searchInDirection(grid, m, n, word, wordLen, row, col, dir)) {
-                                    System.out.println((row + 1) + " " + (col + 1));
-                                    found = true;
-                                }
-                            }
-    					}
-    				}
-                    blankLine = true;
-                }
+			    grid[i] = st.sval;
             }
-	    } catch(IOException e) {}
+            st.nextToken();
+            int k = (int)st.nval;
+            while(k-- > 0) {
+                st.nextToken();
+                String word = st.sval;
+			    int wordLen = word.length();
+                boolean found = false;
+    			for(int row = 0; row < m && !found; ++row) {
+    				for(int col = 0; col < n && !found; ++col) {
+    					for(int dir = 0; dir < 8 && !found; ++dir) {
+                            if(searchInDirection(grid, m, n, word, wordLen, row, col, dir)) {
+                                output.append(String.format("%d %d\n", row + 1, col + 1));
+                                found = true;
+                            }
+                        }
+    				}
+    			}
+                firstCase = false;
+            }
+        }
+        System.out.print(output);
 	}
 	
 	static boolean searchInDirection(String[] grid, int m, int n, String word, int wordLen, int row, int col, int dir) {
