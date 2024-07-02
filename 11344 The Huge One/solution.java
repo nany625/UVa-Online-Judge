9436079df0208652fd8ad1274711624042eee2df
nany625 <jimmy19980625@gmail.com>
2024-07-03 06:45:17 +0800

    Update solution.java

--- 11344 The Huge One/solution.java
+++ 11344 The Huge One/solution.java
@@ -1,31 +1,32 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-	        StreamTokenizer st = new StreamTokenizer(br);
-	        st.nextToken();
-	        int N = (int)st.nval;
-	        while(N-- > 0) {
-    		    String M = br.readLine();
-    		    st.nextToken();
-    		    int len = M.length(), S = (int)st.nval;
-    		    boolean wonderful = true;
-    		    while(S-- > 0) {
-    		        st.nextToken();
-                    short divisor = (short)st.nval;
-                    if(wonderful) {
-                        int remainder = 0;
-                        for(int i = 0; i < len; ++i) {
-                            int dividend = 10 * remainder + M.charAt(i) - '0';
-                            remainder = dividend % divisor;
-                        }
-                        if(remainder != 0)
-                            wonderful = false;
-                    }	
-                }
-    		    System.out.println(M + (wonderful ? " - Wonderful." : " - Simple."));
-    		}
-	    } catch(IOException e) {}
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    StreamTokenizer st = new StreamTokenizer(br);
+	    st.nextToken();
+	    int N = (int)st.nval;
+	    StringBuilder output = new StringBuilder();
+	    while(N-- > 0) {
+    	    String M = br.readLine();
+    	    st.nextToken();
+    	    int len = M.length(), S = (int)st.nval;
+    	    boolean wonderful = true;
+		    while(S-- > 0) {
+    	        st.nextToken();
+                short divisor = (short)st.nval;
+                if(wonderful) {
+                    int remainder = 0;
+                    for(int i = 0; i < len; ++i) {
+                        int dividend = 10 * remainder + M.charAt(i) - '0';
+                        remainder = dividend % divisor;
+                    }
+                    if(remainder != 0)
+                        wonderful = false;
+                }	
+            }
+    	    output.append(M + (wonderful ? " - Wonderful.\n" : " - Simple.\n"));
+    	}
+	    System.out.print(output);
 	}
 }
