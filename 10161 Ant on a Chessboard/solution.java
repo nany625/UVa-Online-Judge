33483e9cee1441670cf35e50d085f77b120dd901
nany625 <jimmy19980625@gmail.com>
2024-07-02 18:35:32 +0800

    Update solution.java

--- 10161 Ant on a Chessboard/solution.java
+++ 10161 Ant on a Chessboard/solution.java
@@ -1,24 +1,24 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-	        StreamTokenizer st = new StreamTokenizer(System.in);
-            int N;
-            while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0) {
-                int circle = (int)Math.ceil(Math.sqrt(N)), corner = 1 + circle * (circle - 1);
-                if(circle % 2 == 1) {
-                    if(N < corner)
-                        System.out.println(circle + " " + (circle - (corner - N)));
-                    else
-                        System.out.println((circle - (N - corner)) + " " + circle);
-                } else{
-        			if(N < corner)
-                        System.out.println((circle - (corner - N)) + " " + circle);
-                    else
-                        System.out.println(circle + " " + (circle - (N - corner)));
-        		}
-            }
-	    } catch(IOException e) {}
+	public static void main(String[] args) throws IOException {
+	    StreamTokenizer st = new StreamTokenizer(System.in);
+        int N;
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0) {
+            int circle = (int)Math.ceil(Math.sqrt(N)), corner = 1 + circle * (circle - 1);
+            if(circle % 2 == 1) {
+                if(N < corner) 
+                    output.append(String.format("%d %d\n", circle, circle - (corner - N)));
+                else
+                    output.append(String.format("%d %d\n", circle - (N - corner), circle));
+            } else {
+    			if(N < corner)
+                    output.append(String.format("%d %d\n", circle - (corner - N), circle));
+                else
+                    output.append(String.format("%d %d\n", circle, circle - (N - corner)));
+    		}
+        }
+        System.out.print(output);
 	}
 }
