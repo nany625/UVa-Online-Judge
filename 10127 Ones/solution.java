bb000c50150a411436b16646972239a982d59f88
nany625 <jimmy19980625@gmail.com>
2024-07-02 18:21:10 +0800

    Update solution.java

--- 10127 Ones/solution.java
+++ 10127 Ones/solution.java
@@ -1,17 +1,17 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-	        StreamTokenizer st = new StreamTokenizer(System.in);
-            while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-                int n = (int)st.nval, x = 0, remainder = 0;
-                do {
-                    remainder = (10 * remainder + 1) % n;
-                    ++x;
-                } while(remainder != 0);
-                System.out.println(x);
-            }
-	    } catch(IOException e) {}
+	public static void main(String[] args) throws IOException {
+	    StreamTokenizer st = new StreamTokenizer(System.in);
+	    StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int n = (int)st.nval, x = 0, remainder = 0;
+            do {
+                remainder = (10 * remainder + 1) % n;
+                ++x;
+            } while(remainder != 0);
+            output.append(x + "\n");
+        }
+        System.out.print(output);
 	}
 }
