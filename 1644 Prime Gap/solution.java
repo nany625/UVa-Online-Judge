2cb93e1c16bc6ec4c5bf753e04e5ab0d66ad628f
nany625 <jimmy19980625@gmail.com>
2024-07-03 08:01:55 +0800

    Update solution.java

--- 1644 Prime Gap/solution.java
+++ 1644 Prime Gap/solution.java
@@ -2,25 +2,25 @@ import java.io.*;
 
 public class Main {
     static String[] firstGrid = new String[6], secondGrid = new String[6];
-	public static void main(String[] args) {
-	    try {
-    	    StreamTokenizer st = new StreamTokenizer(System.in);
-            int k;
-            while(st.nextToken() == StreamTokenizer.TT_NUMBER && (k = (int)st.nval) != 0) {
-                if(isPrime(k))
-    		        System.out.println(0);
-    		    else {
-    		    	k -= (k + 1) % 2;
-    	            int i = 0, j = 0;
-    	            while(!isPrime(k - i))
-    	                i += 2;
-    	            do {
-    	                j += 2;
-    	            } while(!isPrime(k + j));
-    	            System.out.println(i + j);
-    		    }
-            }
-	    } catch(IOException e) {}
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        int k;
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (k = (int)st.nval) != 0) {
+            if(isPrime(k))
+    	        output.append("0\n");
+		    else {
+    	    	k -= (k + 1) % 2;
+                int i = 0, j = 0;
+	            while(!isPrime(k - i))
+                    i += 2;
+	            do {
+                    j += 2;
+	            } while(!isPrime(k + j));
+	            output.append((i + j) + "\n");
+		    }
+        }
+	    System.out.print(output);
 	}
 	
 	static boolean isPrime(int n) {
