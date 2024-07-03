1b50383e1a66b91a874fb131d04b77bbf589d47b
nany625 <jimmy19980625@gmail.com>
2024-07-03 09:18:11 +0800

    Update solution.java

--- 406 Prime Cuts/solution.java
+++ 406 Prime Cuts/solution.java
@@ -3,34 +3,34 @@ import java.io.*;
 public class Main {
     static short[] primes = new short[169], pos = new short[1000];
     static int count = 2;
-	public static void main(String[] args) {
-	    try {
-	        primes[0] = pos[0] = 1;
-    	    primes[1] = pos[1] = 2;
-    	    for(short i = 3; i <= 1000; ++i) {
-    	        pos[i - 1] = pos[i - 2];
-    	        if(i % 2 == 1 && count < primes.length) {
-    	            if(isPrime(i)) {
-    	                primes[count++] = i;
-    	                ++pos[i - 1];
-    	            }
-    	        }
-    	    }
-	        StreamTokenizer st = new StreamTokenizer(System.in);
-            while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-    	        int N = (int)st.nval;
-    	        st.nextToken();
-    	        int C = (int)st.nval;
-    	        System.out.print(N + " " + C + ":");
-    	        if(pos[N - 1] % 2 == 0 && 2 * C < pos[N - 1])
-            		printPrimes(pos[N - 1] / 2 - C, pos[N - 1] / 2 + C - 1);
-            	else if(pos[N - 1] % 2 == 1 && 2 * C - 1 < pos[N - 1])
-            		printPrimes((pos[N - 1] + 1) / 2 - C, (pos[N - 1] - 3) / 2 + C);
-            	else
-            		printPrimes(0, pos[N - 1] - 1);
-    	        System.out.println("\n");
-            }
-        } catch(IOException e) {};
+	public static void main(String[] args) throws IOException {
+        primes[0] = pos[0] = 1;
+	    primes[1] = pos[1] = 2;
+	    for(short i = 3; i <= 1000; ++i) {
+	        pos[i - 1] = pos[i - 2];
+	        if(i % 2 == 1 && count < primes.length) {
+	            if(isPrime(i)) {
+	                primes[count++] = i;
+	                ++pos[i - 1];
+	            }
+	        }
+	    }
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int N = (int)st.nval;
+            st.nextToken();
+            int C = (int)st.nval;
+            output.append(N + " " + C + ":");
+            if(pos[N - 1] % 2 == 0 && 2 * C < pos[N - 1])
+        		output.append(printPrimes(pos[N - 1] / 2 - C, pos[N - 1] / 2 + C - 1));
+        	else if(pos[N - 1] % 2 == 1 && 2 * C - 1 < pos[N - 1])
+        		output.append(printPrimes((pos[N - 1] + 1) / 2 - C, (pos[N - 1] - 3) / 2 + C));
+        	else
+        		output.append(printPrimes(0, pos[N - 1] - 1));
+    	    output.append("\n\n");
+        }
+        System.out.print(output);
 	}
 	
 	static boolean isPrime(short n) {
@@ -42,8 +42,10 @@ public class Main {
         return true;
     }
     
-    static void printPrimes(int first, int last) {
+    static StringBuilder printPrimes(int first, int last) {
+        StringBuilder ans = new StringBuilder();
     	for(int i = first; i <= last; ++i)
-    		System.out.print(" " + primes[i]);
+    		ans.append(" " + primes[i]);
+    	return ans;
     }
 }
