ccfa95d6579509d1b132b5917833b60e9f55aff3
nany625 <jimmy19980625@gmail.com>
2024-07-03 08:18:44 +0800

    Update solution.java

--- 264 Count on Cantor/solution.java
+++ 264 Count on Cantor/solution.java
@@ -1,19 +1,19 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-            StreamTokenizer st = new StreamTokenizer(System.in);
-            while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-                int number = (int)st.nval;
-                int i = (int)Math.ceil((-1 + Math.sqrt(1 + 8 * number)) / 2);
-                int a = i + 1 - number + (i - 1) * i / 2;
-                int b = number - (i - 1) * i / 2;
-                if(i % 2 == 1)
-                    System.out.println("TERM " + number + " IS " + a + "/" + b);
-    			else
-    			    System.out.println("TERM " + number + " IS " + b + "/" + a);
-    		}
-        } catch(IOException e) {};
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int number = (int)st.nval;
+            int i = (int)Math.ceil((-1 + Math.sqrt(1 + 8 * number)) / 2);
+            int a = i + 1 - number + (i - 1) * i / 2;
+            int b = number - (i - 1) * i / 2;
+            if(i % 2 == 1)
+                output.append(String.format("TERM %d IS %d/%d\n", number, a, b));
+    		else
+			    output.append(String.format("TERM %d IS %d/%d\n", number, b, a));
+		}
+        System.out.print(output);
 	}
 }
