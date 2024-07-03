503fe669eb310114cbc062556fc83af8d84544a4
nany625 <jimmy19980625@gmail.com>
2024-07-03 09:55:03 +0800

    Update solution.java

--- 494 Kindergarten Counting Game/solution.java
+++ 494 Kindergarten Counting Game/solution.java
@@ -1,24 +1,25 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String s;
-            while((s = br.readLine()) != null) {
-    		    s.trim();
-    		    int count = 0, i = 0, len = s.length();
-    			while(i < len) {
-    				if(Character.isLetter(s.charAt(i))) {
-    					int j = i + 1;
-    					while(j < len && Character.isLetter(s.charAt(j)))
-    						++j;
-    					++count;
-    					i = j;
-    				}
-    				++i;
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        StringBuilder output = new StringBuilder();
+        while((s = br.readLine()) != null) {
+    	    s.trim();
+    	    int count = 0, i = 0, len = s.length();
+    		while(i < len) {
+    			if(Character.isLetter(s.charAt(i))) {
+    				int j = i + 1;
+    				while(j < len && Character.isLetter(s.charAt(j)))
+    					++j;
+					++count;
+    				i = j;
     			}
-    			System.out.println(count);
+    			++i;
     		}
-        } catch(IOException e) {};
+    		output.append(count + "\n");
+    	}
+        System.out.print(output);
 	}
 }
