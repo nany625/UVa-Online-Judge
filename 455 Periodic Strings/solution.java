4668fb8a268d29a65534ee88a811cf6cf4b9aa96
nany625 <jimmy19980625@gmail.com>
2024-07-03 09:34:20 +0800

    Update solution.java

--- 455 Periodic Strings/solution.java
+++ 455 Periodic Strings/solution.java
@@ -1,34 +1,33 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-	        StreamTokenizer st = new StreamTokenizer(System.in);
-            st.nextToken();
-            int N = (int)st.nval;
-            boolean blankLine = false;
-            while(N-- > 0) {
-    	        if(blankLine)
-    				System.out.println();
-    	        st.nextToken();
-    	        String s = st.sval;
-    	        int len = s.length();
-    			boolean found = false;
-    			for(int k = 1; k <= len / 2 && !found; ++k) {
-    				if(len % k == 0) {
-    					int i = k;
-    					while(i < len && s.substring(0, k).equals(s.substring(i, i + k)))
-    						i += k;
-    					if(i >= len) {
-    						System.out.println(k);
-    						found = true;
-    					}
-    				}
-    			}
-    			if(!found)
-    			    System.out.println(len);
-    			blankLine = true;
-            }
-        } catch(IOException e) {};
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        boolean firstCase = true;
+        StringBuilder output = new StringBuilder();
+        while(N-- > 0) {
+            if(!firstCase)
+				output.append("\n");
+            br.readLine();
+	        String s = br.readLine();
+            int len = s.length();
+			boolean found = false;
+    		for(int k = 1; k <= len / 2 && !found; ++k) {
+				if(len % k == 0) {
+    				int i = k;
+					while(i < len && s.substring(0, k).equals(s.substring(i, i + k)))
+    					i += k;
+    				if(i >= len) {
+						output.append(k + "\n");
+    					found = true;
+					}
+				}
+			}
+			if(!found)
+			    output.append(len + "\n");
+			firstCase = false;
+        }
+        System.out.print(output);
 	}
 }
