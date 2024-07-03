3f08d39c9624d902dff4960a6fd9fefb5f103690
nany625 <jimmy19980625@gmail.com>
2024-07-03 10:17:11 +0800

    Update solution.java

--- 496 Simply Subsets/solution.java
+++ 496 Simply Subsets/solution.java
@@ -2,38 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String line;
-            while((line = br.readLine()) != null) {
-    		    String[] A = line.split("\\s+");
-    		    String[] B = br.readLine().split("\\s+");
-    		    ArrayList<String> setA = new ArrayList<>();
-    			ArrayList<String> setB = new ArrayList<>();
-    			ArrayList<String> union = new ArrayList<>();
-    			for(String s : A) {
-    				if(!setA.contains(s))
-    					setA.add(s);
-    				if(!union.contains(s))
-    					union.add(s);
-    			}
-    			for(String s : B) {
-    				if(!setB.contains(s))
-    					setB.add(s);
-    				if(!union.contains(s))
-    					union.add(s);
-    			}
-    			if(union.size() == setA.size() && union.size() == setB.size())
-    				System.out.println("A equals B");
-    			else if(union.size() == setA.size() && union.size() != setB.size())
-    				System.out.println("B is a proper subset of A");
-    			else if(union.size() == setB.size() && union.size() != setA.size())
-    				System.out.println("A is a proper subset of B");
-    			else if(union.size() == setA.size() + setB.size())
-    				System.out.println("A and B are disjoint");
-    			else
-    				System.out.println("I'm confused!");
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        StringBuilder output = new StringBuilder();
+        while((line = br.readLine()) != null) {
+    	    String[] A = line.split("\\s+");
+    	    String[] B = br.readLine().split("\\s+");
+    	    ArrayList<String> setA = new ArrayList<>();
+    		ArrayList<String> setB = new ArrayList<>();
+    		ArrayList<String> union = new ArrayList<>();
+    		for(String s : A) {
+    			if(!setA.contains(s))
+    				setA.add(s);
+    			if(!union.contains(s))
+    				union.add(s);
     		}
-        } catch(IOException e) {};
+    		for(String s : B) {
+    			if(!setB.contains(s))
+    				setB.add(s);
+    			if(!union.contains(s))
+    				union.add(s);
+    		}
+    		if(union.size() == setA.size() && union.size() == setB.size())
+    			output.append("A equals B\n");
+    		else if(union.size() == setA.size() && union.size() != setB.size())
+    			output.append("B is a proper subset of A\n");
+    		else if(union.size() == setB.size() && union.size() != setA.size())
+    			output.append("A is a proper subset of B\n");
+    		else if(union.size() == setA.size() + setB.size())
+    			output.append("A and B are disjoint\n");
+    		else
+    			output.append("I'm confused!\n");
+    	}
+        System.out.print(output);
 	}
 }
