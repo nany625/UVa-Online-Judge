2c5019200e094d5c641f96822a3083768866eb3e
nany625 <jimmy19980625@gmail.com>
2024-07-03 08:28:12 +0800

    Update solution.java

--- 299 Train Swapping/solution.java
+++ 299 Train Swapping/solution.java
@@ -1,25 +1,25 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-            StreamTokenizer st = new StreamTokenizer(System.in);
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int N = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(N-- > 0) {
             st.nextToken();
-            int N = (int)st.nval;
-            while(N-- > 0) {
+            int L = (int)st.nval, swap = 0;
+            int[] a = new int[L];
+	        for(int i = 0; i < L; ++i) {
                 st.nextToken();
-                int L = (int)st.nval, swap = 0;
-    	        int[] a = new int[L];
-    	        for(int i = 0; i < L; ++i) {
-    	            st.nextToken();
-    	            a[i] = (int)st.nval;
-    	            for(int j = 0; j < i; ++j) {
-                        if(a[j] > a[i])
-                            ++swap;
-                    }
-    	        }
-    	        System.out.println("Optimal train swapping takes " + swap + " swaps.");
-    		}
-        } catch(IOException e) {};
+	            a[i] = (int)st.nval;
+                for(int j = 0; j < i; ++j) {
+                    if(a[j] > a[i])
+                        ++swap;
+                }
+	        }
+	        output.append("Optimal train swapping takes " + swap + " swaps.\n");
+		}
+        System.out.print(output);
 	}
 }
