b8c3b48142e9a28e63a74321cbbdab591c3f5385
nany625 <jimmy19980625@gmail.com>
2024-07-03 17:29:10 +0800

    Create solution.c

---
+++ 11005 Cheapest Base/solution.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+	int cases;
+	scanf("%d", &cases);
+	for(int i = 1; i <= cases; ++i) {
+		if(i > 1)
+			puts("");
+		printf("Case %d:\n", i);
+		short costs[36];
+		for(int j = 0; j < 36; ++j)
+			scanf("%hd", &costs[j]);
+		int queries;
+		scanf("%d", &queries);
+		while(queries--) {
+			int number;
+			scanf("%d", &number);
+			printf("Cheapest base(s) for number %d:", number);
+			short totalCosts[35] = {}, cheapest = 0;
+			for(int base = 2; base <= 36; ++base) {
+				int temp = number;
+				do {
+					totalCosts[base - 2] += costs[temp % base];
+					temp /= base;
+				} while(temp > 0);
+				if(cheapest > totalCosts[base - 2] || cheapest == 0)
+					cheapest = totalCosts[base - 2];
+			}
+			for(int base = 2; base <= 36; ++base) {
+				if(totalCosts[base - 2] == cheapest)
+					printf(" %d", base);
+			}
+			puts("");
+		}
+	}
+	return 0;
+}
