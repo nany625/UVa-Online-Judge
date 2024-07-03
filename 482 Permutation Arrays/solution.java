914ae4d48045d716649e5e2d1767694bea07af94
nany625 <jimmy19980625@gmail.com>
2024-07-03 09:50:38 +0800

    Update solution.java

--- 482 Permutation Arrays/solution.java
+++ 482 Permutation Arrays/solution.java
@@ -2,26 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            StreamTokenizer st = new StreamTokenizer(br);
-            st.nextToken();
-            int cases = (int)st.nval;
-            boolean blankLine = false;
-            while(cases-- > 0) {
-    		    if(blankLine)
-    		        System.out.println();
-    			br.readLine();
-    			TreeMap<Integer, String> map = new TreeMap<>();
-    			String[] p = br.readLine().split("\\s+");
-    			String[] x = br.readLine().split("\\s+");
-    			int len = p.length;
-    			for(int i = 0; i < len; ++i)
-    				map.put(Integer.parseInt(p[i]), x[i]);
-    			for(String s : map.values())
-    				System.out.println(s);
-    			blankLine = true;
-    		}
-        } catch(IOException e) {};
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cases = Integer.parseInt(br.readLine());
+        boolean firstCase = true;
+        StringBuilder output = new StringBuilder();
+        while(cases-- > 0) {
+    	    if(!firstCase)
+		        output.append("\n");
+    		br.readLine();
+			HashMap<Integer, String> map = new HashMap<>();
+    		String[] p = br.readLine().split("\\s+");
+			String[] x = br.readLine().split("\\s+");
+    		int len = p.length;
+			for(int i = 0; i < len; ++i)
+    			map.put(Integer.parseInt(p[i]), x[i]);
+			for(int i = 1; i <= len; ++i)
+    			output.append(map.get(i) + "\n");
+    		firstCase = false;
+    	}
+        System.out.print(output);
 	}
 }
