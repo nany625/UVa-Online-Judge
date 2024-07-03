3d612690edcb419b56d1b18ff2a312818de0f4b4
nany625 <jimmy19980625@gmail.com>
2024-07-03 17:50:54 +0800

    Create solution.c

---
+++ 12015 Google is Feeling Lucky/solution.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+	
+typedef struct {
+    char url[101];
+    short relevance;
+} Webpage;
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    for(int i = 1; i <= T; ++i) {
+        printf("Case #%d:\n", i);
+        Webpage webpages[10];
+        short max = 0;
+        for(int j = 0; j < 10; ++j) {
+            scanf("%s %hd", webpages[j].url, &webpages[j].relevance);
+            max = max > webpages[j].relevance ? max : webpages[j].relevance;
+        }
+        for(int j = 0; j < 10; ++j) {
+            if(webpages[j].relevance == max)
+                printf("%s\n", webpages[j].url);
+        }
+    }
+    return 0;
+}
