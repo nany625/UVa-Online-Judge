259cba62a0d42901c7f277fe56b4c6ae1483e53a
nany625 <jimmy19980625@gmail.com>
2024-07-03 10:09:38 +0800

    Update solution.java

--- 495 Fibonacci Freeze/solution.java
+++ 495 Fibonacci Freeze/solution.java
@@ -3,31 +3,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-		    char[][] fib = new char[5001][];
-    		fib[0] = new char[1];
-    		fib[0][0] = '0';
-    		fib[1] = new char[1];
-    		fib[1][0] = '1';
-    		short[] digits = new short[5001];
-    		digits[0] = digits[1] = 1;
-    		for(int i = 2; i <= 5000; ++i) {
-    		    fib[i] = new char[digits[i - 1] + 1];
-                Arrays.fill(fib[i], '0');
-                short j;
-                for(j = 0; j < digits[i - 1]; ++j) {
-                	fib[i][j + 1] += (fib[i][j] + fib[i - 1][j] + fib[i - 2][j] - 3 * '0') / 10;
-                	fib[i][j] = (char)((fib[i][j] + fib[i - 1][j] + fib[i - 2][j] - 3 * '0') % 10 + '0');
-                }
-                digits[i] = (short)(j + (fib[i][j] - '0'));
+	public static void main(String[] args) throws IOException {
+	    char[][] fib = new char[5001][];
+		fib[0] = new char[1];
+		fib[0][0] = '0';
+		fib[1] = new char[1];
+		fib[1][0] = '1';
+		short[] digits = new short[5001];
+		digits[0] = digits[1] = 1;
+		for(int i = 2; i <= 5000; ++i) {
+		    fib[i] = new char[digits[i - 1] + 1];
+            Arrays.fill(fib[i], '0');
+            short j;
+            for(j = 0; j < digits[i - 1]; ++j) {
+            	fib[i][j + 1] += (fib[i][j] + fib[i - 1][j] + fib[i - 2][j] - 3 * '0') / 10;
+            	fib[i][j] = (char)((fib[i][j] + fib[i - 1][j] + fib[i - 2][j] - 3 * '0') % 10 + '0');
             }
-            StreamTokenizer st = new StreamTokenizer(br);
-            while((st.nextToken()) == st.TT_NUMBER) {
-                int number = (int)st.nval;
-                System.out.println("The Fibonacci number for " + number + " is " + new StringBuilder(new String(fib[number], 0, digits[number])).reverse());
-            }
-		} catch(IOException e) {}
+            digits[i] = (short)(j + (fib[i][j] - '0'));
+        }
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while((st.nextToken()) == st.TT_NUMBER) {
+            int number = (int)st.nval;
+            output.append("The Fibonacci number for " + number + " is " + new StringBuilder(new String(fib[number], 0, digits[number])).reverse() + "\n");
+        }
+		System.out.print(output);
 	}
 }
 
@@ -37,21 +37,20 @@ import java.util.*;
 import java.math.*;
 
 public class Main {
-	public static void main(String[] args) {
-		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-		    ArrayList<BigInteger> fib = new ArrayList<>();
-    		fib.add(BigInteger.ZERO);
-    		fib.add(BigInteger.ONE);
-    		StreamTokenizer st = new StreamTokenizer(br);
-    		while((st.nextToken()) == st.TT_NUMBER) {
-    		    int number = (int)st.nval;
-    		    System.out.print("The Fibonacci number for " + number + " is ");
-        		if(number >= fib.size()) {
-        			for(int i = fib.size(); i <= number; ++i)
-    			        fib.add(fib.get(i - 1).add(fib.get(i - 2)));
-        		}
-    		    System.out.println(fib.get(number));
+	public static void main(String[] args) throws IOException {
+        ArrayList<BigInteger> fib = new ArrayList<>();
+		fib.add(BigInteger.ZERO);
+		fib.add(BigInteger.ONE);
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		StringBuilder output = new StringBuilder();
+		while((st.nextToken()) == st.TT_NUMBER) {
+		    int number = (int)st.nval;
+    		if(number >= fib.size()) {
+    			for(int i = fib.size(); i <= number; ++i)
+			        fib.add(fib.get(i - 1).add(fib.get(i - 2)));
     		}
-		} catch(IOException e) {}
+		    output.append("The Fibonacci number for " + number + " is " + fib.get(number) + "\n");
+		}
+		System.out.print(output);
 	}
 }
