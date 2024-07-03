9e82e30ee9485b94bac29d9671efeb65f986aecd
nany625 <jimmy19980625@gmail.com>
2024-07-03 11:18:43 +0800

    Update solution.java

--- 993 Product of digits/solution.java
+++ 993 Product of digits/solution.java
@@ -2,39 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-    	    StreamTokenizer st = new StreamTokenizer(System.in);
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int sets = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(sets-- > 0) {
             st.nextToken();
-            int sets = (int)st.nval;
-            while(sets-- > 0) {
-                st.nextToken();
-                int N = (int)st.nval;
-                if(N == 0) {
-                    System.out.println(0);
-				    continue;
-                }
-                ArrayList<Integer> factors = new ArrayList<>();
-    			int curr = 9;
-    			while(curr > 1) {
-    				if(N % curr == 0) {
-    					factors.add(curr);
-    					N /= curr;
-    				} else
-    					--curr;
-    			}
-    			if(N == 1) {
-    				if(factors.isEmpty())
-    					System.out.println(1);
-    				else {
-    					for(curr = factors.size() - 1; curr >= 0; --curr)
-    						System.out.print(factors.get(curr));
-    					System.out.println();
-    				}
-    			} else
-    				System.out.println(-1);
+            int N = (int)st.nval;
+            if(N == 0) {
+                output.append("0\n");
+			    continue;
             }
-	    } catch(IOException e) {}
+            ArrayList<Integer> factors = new ArrayList<>();
+    		int curr = 9;
+    		while(curr > 1) {
+    			if(N % curr == 0) {
+    				factors.add(curr);
+    				N /= curr;
+    			} else
+    				--curr;
+    		}
+    		if(N == 1) {
+    			if(factors.isEmpty())
+    				output.append("1\n");
+    			else {
+    				for(curr = factors.size() - 1; curr >= 0; --curr)
+    					output.append(factors.get(curr));
+    				output.append("\n");
+    			}
+    		} else
+    			output.append("-1\n");
+        }
+	    System.out.print(output);
 	}
 	
 	static int binarySearch(int[] array, int key) {
