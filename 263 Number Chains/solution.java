c9240b8ae8702d608099fa13e1140a44c13525c9
nany625 <jimmy19980625@gmail.com>
2024-07-03 09:26:11 +0800

    Update solution.java

--- 263 Number Chains/solution.java
+++ 263 Number Chains/solution.java
@@ -2,27 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-	    try {
-            StreamTokenizer st = new StreamTokenizer(System.in);
-            int number;
-            while(st.nextToken() == StreamTokenizer.TT_NUMBER && (number = (int)st.nval) != 0) {
-                System.out.println("Original number was " + number);
-    			ArrayList<Integer> numbers = new ArrayList<>();
-    			do {
-    				numbers.add(number);
-    				char[] newNumber = ("" + number).toCharArray();
-    				Arrays.sort(newNumber);
-    				int asc = Integer.parseInt(new String(newNumber)), len = newNumber.length;
-    				for(int i = 0; i < len / 2; ++i)
-    				    swap(newNumber, i, len - 1 - i);
-    				int desc = Integer.parseInt(new String(newNumber));
-    				System.out.println(desc + " - " + asc + " = " + (desc - asc));
-    				number = desc - asc;
-    			} while(!numbers.contains(number));
-    			System.out.println("Chain length " + numbers.size() + "\n");
-    		}
-        } catch(IOException e) {};
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        int number;
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (number = (int)st.nval) != 0) {
+            output.append("Original number was " + number + "\n");
+    		ArrayList<Integer> numbers = new ArrayList<>();
+			do {
+    			numbers.add(number);
+				char[] newNumber = ("" + number).toCharArray();
+    			Arrays.sort(newNumber);
+    			int asc = Integer.parseInt(new String(newNumber)), len = newNumber.length;
+				for(int i = 0; i < len / 2; ++i)
+    			    swap(newNumber, i, len - 1 - i);
+    			int desc = Integer.parseInt(new String(newNumber));
+				output.append(desc + " - " + asc + " = " + (desc - asc) + "\n");
+    			number = desc - asc;
+    		} while(!numbers.contains(number));
+			output.append("Chain length " + numbers.size() + "\n\n");
+    	}
+        System.out.print(output);
 	}
 	
 	static void swap(char[] array, int i, int j) {
