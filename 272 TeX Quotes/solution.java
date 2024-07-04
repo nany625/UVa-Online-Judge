1ad8792188187f8a3846b211d3dd22cdf0e78971
nany625 <jimmy19980625@gmail.com>
2024-07-04 09:05:27 +0800

    Create solution.java

---
+++ 272 TeX Quotes/solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        boolean odd = true;
+        StringBuilder output = new StringBuilder();
+        while((line = br.readLine()) != null) {
+            int len = line.length();
+            for(int i = 0; i < len; ++i) {
+                if(line.charAt(i) == '"' && odd) {
+                    output.append("``");
+                    odd = false;
+                } else if(line.charAt(i) == '"' && !odd) {
+                    output.append("''");
+                    odd = true;
+                } else
+                    output.append(line.charAt(i));
+            }
+            output.append("\n");
+        }
+        System.out.print(output);
+	}
+}
