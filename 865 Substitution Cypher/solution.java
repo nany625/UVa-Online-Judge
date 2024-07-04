03128dd7ebbb29df798f9ebc5684ff559eebdefa
nany625 <jimmy19980625@gmail.com>
2024-07-04 19:39:52 +0800

    Create solution.java

---
+++ 865 Substitution Cypher/solution.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int cases = Integer.parseInt(br.readLine());
+	    br.readLine();
+	    boolean firstCase = true;
+        StringBuilder output = new StringBuilder();
+    	while(cases-- > 0) {
+    	    if(!firstCase)
+    	        output.append("\n");
+    	    String plaintext = br.readLine(), substitution = br.readLine();
+    	    output.append(substitution + "\n" + plaintext + "\n");
+    	    int len = plaintext.length();
+    	    HashMap<Character, Character> cypher = new HashMap<>();
+    	    for(int i = 0; i < len; ++i)
+    	        cypher.put(plaintext.charAt(i), substitution.charAt(i));
+    	    String text;
+    	    while((text = br.readLine()) != null && !text.isEmpty()) {
+    	        len = text.length();
+    	        for(int i = 0; i < len; ++i)
+    	            output.append(cypher.containsKey(text.charAt(i)) ? (char)cypher.get(text.charAt(i)) : text.charAt(i));
+    	        output.append("\n");
+    	    }
+    	    firstCase = false;
+    	}
+        System.out.print(output);
+	}
+}
