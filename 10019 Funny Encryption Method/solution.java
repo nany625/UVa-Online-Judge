e1540f5696965476de4d9002cda693e599a9d2b9
nany625 <jimmy19980625@gmail.com>
2024-07-05 12:16:24 +0800

    Create solution.java

---
+++ 10019 Funny Encryption Method/solution.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    StreamTokenizer st = new StreamTokenizer(System.in);
+	    st.nextToken();
+	    int T = (int)st.nval;
+	    StringBuilder output = new StringBuilder();
+	    while(T-- > 0) {
+	        st.nextToken();
+	        int N = (int)st.nval;
+	        int temp = N, count = 0;
+	        while(temp > 0) {
+    	        count += temp % 2;
+    	        temp /= 2;
+    	    }
+    	    output.append(count + " ");
+    	    count = 0;
+    	    while(N > 0) {
+    	        temp = N % 10;
+    	        while(temp > 0) {
+        	        count += temp % 2;
+        	        temp /= 2;
+        	    }
+        	    N /= 10;
+    	    }
+    	    output.append(count + "\n");
+	    }
+        System.out.print(output);
+	}
+}
