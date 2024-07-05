600b7abaab9c187ad2f674bbd6e4570fefd415b9
nany625 <jimmy19980625@gmail.com>
2024-07-05 15:08:39 +0800

    Update solution.c

--- 10050 Hartals/solution.c
+++ 10050 Hartals/solution.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
 	int T;
 	scanf("%d", &T);
 	while(T--) {
-	    int N, P;
-	    scanf("%d %d", &N, &P);
-	    int h[P];
+	    short N;
+	    int P;
+	    scanf("%hd %d", &N, &P);
+	    short h[P];
 	    for(int i = 0; i < P; ++i)
-	        scanf("%d", &h[i]);
+	        scanf("%hd", &h[i]);
 	    int hartals = 0;
 	    do {
 	        if((N - 6) % 7 != 0 && N % 7 != 0) {
