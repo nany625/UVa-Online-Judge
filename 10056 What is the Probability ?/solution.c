c84e26d4dd222e2b362a1baf49f300bf6b39e3e8
nany625 <jimmy19980625@gmail.com>
2024-07-05 15:41:56 +0800

    Create solution.c

---
+++ 10056 What is the Probability ?/solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	int S;
+	scanf("%d", &S);
+	while(S--) {
+		int N, I;
+		double p;
+		scanf("%d %lf %d", &N, &p, &I);
+		printf("%.4f\n", p > 0 ? p * pow(1 - p, I - 1) / (1 - pow(1 - p, N)) : p);
+	}
+	return 0;
+}
