e40353ab328b3a9f2faad792513d9972ff122b33
nany625 <jimmy19980625@gmail.com>
2024-07-05 18:30:05 +0800

    Update solution.java

--- 10226 Hardwood Species/solution.java
+++ 10226 Hardwood Species/solution.java
@@ -1,3 +1,4 @@
+// #解法一
 import java.io.*;
 import java.util.*;
 
@@ -27,6 +28,39 @@ public class Main {
             for(Integer i : tree.values())
                 treeSize += i;
             for(String s : treeKeys)
+                output.append(String.format("%s %.4f\n", s, 100.0 * tree.get(s) / treeSize));
+            firstCase = false;
+        }
+    	System.out.print(output);
+	}
+}
+
+// #解法二
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cases = Integer.parseInt(br.readLine());
+        br.readLine();
+        StringBuilder output = new StringBuilder();
+        boolean firstCase = true;
+        while(cases-- > 0) {
+    		if(!firstCase)
+                output.append('\n');
+            TreeMap<String, Integer> tree = new TreeMap<>();
+            String species;
+    	    while((species = br.readLine()) != null && !species.isEmpty()) {
+                if(tree.containsKey(species))
+                	tree.replace(species, tree.get(species) + 1);
+                else
+                	tree.put(species, 1);
+            }
+            int treeSize = 0;
+            for(Integer i : tree.values())
+                treeSize += i;
+            for(String s : tree.keySet())
                 output.append(String.format("%s %.4f\n", s, 100.0 * tree.get(s) / treeSize));
             firstCase = false;
         }
