c0c41399a825273650dfd6f40250b25f0a06c66f
nany625 <jimmy19980625@gmail.com>
2024-07-05 11:23:11 +0800

    Create solution.c

---
+++ 10008 What's Cryptanalysis?/solution.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct {
+    char alpha, count;
+} Frequency;
+
+int compare(const void *a, const void *b) {
+    Frequency *f1 = (Frequency*)a;
+    Frequency *f2 = (Frequency*)b;
+    if(f1->count != f2->count)
+        return f2->count - f1->count;
+    return f1->alpha - f2->alpha;
+}
+
+int main() {
+	Frequency frequencies[26];
+	for(int i = 0; i < 26; ++i) {
+	    frequencies[i].alpha = 'A' + i;
+	    frequencies[i].count = 0;
+	}
+	int n;
+	scanf("%d", &n);
+	getchar();
+	char text[31];
+	while(n--) {
+	    fgets(text, sizeof(text), stdin);
+	    int len = strlen(text);
+	    for(int i = 0; i < len; ++i) {
+	        if(isupper(text[i]))
+	            ++frequencies[text[i] - 'A'].count;
+	        else if(islower(text[i]))
+	            ++frequencies[text[i] - 'a'].count;
+	    }
+	}
+	qsort(frequencies, 26, sizeof(Frequency), compare);
+	for(int i = 0; i < 26 && frequencies[i].count > 0; ++i)
+	    printf("%c %d\n", frequencies[i].alpha, frequencies[i].count);
+	return 0;
+}
