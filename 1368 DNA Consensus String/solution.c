1dc237b14f58f9abdc55e193ec3ced938f7a3668
nany625 <jimmy19980625@gmail.com>
2024-07-05 08:14:56 +0800

    Create solution.c

---
+++ 1368 DNA Consensus String/solution.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int T;
+	scanf("%d", &T);
+	char nucleotides[5] = "ACGT";
+	while(T--) {
+		int m, n;
+		scanf("%d %d", &m, &n);
+		char DNA[m][n + 1];
+		for(int i = 0; i < m; ++i)
+			scanf("%s", DNA[i]);
+		int dist = m * n;
+		for(int i = 0; i < n; ++i) {
+            int nucleotidesCount[4] = {};
+            for(int j = 0; j < m; ++j) {
+                if(DNA[j][i] == 'A')
+                    ++nucleotidesCount[0];
+                else if(DNA[j][i] == 'C')
+                    ++nucleotidesCount[1];
+                else if(DNA[j][i] == 'G')
+                    ++nucleotidesCount[2];
+                else if(DNA[j][i] == 'T')
+                    ++nucleotidesCount[3];
+            }
+            int max = 0, temp = -1;
+            for(int j = 0; j < 4; ++j) {
+                if(max < nucleotidesCount[j]) {
+                    max = nucleotidesCount[j];
+                    temp = j;
+                }
+            }
+            putchar(nucleotides[temp]);
+            dist -= max;
+        }
+        printf("\n%d\n",dist);
+	}
+	return 0;
+}
