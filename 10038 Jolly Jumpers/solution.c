fe9ea96a06a5d0389545a9f50529b77ae5c39e2d
nany625 <jimmy19980625@gmail.com>
2024-07-05 13:22:24 +0800

    Update solution.c

--- 10038 Jolly Jumpers/solution.c
+++ 10038 Jolly Jumpers/solution.c
@@ -35,15 +35,17 @@ void DualPivotPartition(int *array, int *partitionIndices, int left, int right) 
 	partitionIndices[1] = high;
 }
 
-void DualPivotQuickSort(int *array, int left, int right) {
+void DualPivotQuickSort(int *array, int left, int right, bool *Jolly) {
     if(left < right) {
         int partitionIndices[2];
         DualPivotPartition(array, partitionIndices, left, right);
-		if(array[partitionIndices[0]] != partitionIndices[0] + 1 || array[partitionIndices[1]] != partitionIndices[1] + 1)
+		if(array[partitionIndices[0]] != partitionIndices[0] + 1 || array[partitionIndices[1]] != partitionIndices[1] + 1) {
+		    *Jolly = false;
 		    return;
-		DualPivotQuickSort(array, left, partitionIndices[0] - 1);
-		DualPivotQuickSort(array, partitionIndices[0] + 1, partitionIndices[1] - 1);
-		DualPivotQuickSort(array, partitionIndices[1] + 1, right);
+		}
+		DualPivotQuickSort(array, left, partitionIndices[0] - 1, Jolly);
+		DualPivotQuickSort(array, partitionIndices[0] + 1, partitionIndices[1] - 1, Jolly);
+		DualPivotQuickSort(array, partitionIndices[1] + 1, right, Jolly);
     }
 }
 
@@ -58,8 +60,8 @@ int main() {
 	        diff[i] = abs(prev - curr);
 	        prev = curr;
 	    }
-	    n <= 47 ? InsertionSort(diff, n) : DualPivotQuickSort(diff, 0, n - 1);
-        bool Jolly = true;
+	    bool Jolly = true;
+	    n <= 47 ? InsertionSort(diff, n) : DualPivotQuickSort(diff, 0, n - 1, &Jolly);
 	    for(int i = 0; i < n && Jolly; ++i) {
 	        if(diff[i] != i + 1)
 	            Jolly = false;
