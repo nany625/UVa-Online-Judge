35756941a9ada0d1556429c12fb861728901e00e
nany625 <jimmy19980625@gmail.com>
2024-07-05 13:10:01 +0800

    Create solution.c

---
+++ 10038 Jolly Jumpers/solution.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void InsertionSort(int *array, int size) {
+    for(int i = 1; i < size; ++i) {
+        int j = i - 1, insertionNum = array[i];
+        while(j >= 0 && insertionNum < array[j]) {
+            array[j + 1] = array[j];
+            --j;
+        }
+        array[j + 1] = insertionNum;
+    }
+}
+
+void DualPivotPartition(int *array, int *partitionIndices, int left, int right) {
+	if(array[left] > array[right])
+		swap(&array[left], &array[right]);
+	int low = left + 1, high = right - 1, pivot1 = array[left], pivot2 = array[right];
+	for(int i = low; i <= high; ++i) {
+		if(array[i] < pivot1)
+			swap(&array[low++], &array[i]);
+		else if(array[i] > pivot2)
+			swap(&array[high--], &array[i--]);
+	}
+	swap(&array[--low], &array[left]);
+	swap(&array[++high], &array[right]);
+	partitionIndices[0] = low;
+	partitionIndices[1] = high;
+}
+
+void DualPivotQuickSort(int *array, int left, int right) {
+    if(left < right) {
+        int partitionIndices[2];
+        DualPivotPartition(array, partitionIndices, left, right);
+		DualPivotQuickSort(array, left, partitionIndices[0] - 1);
+		DualPivotQuickSort(array, partitionIndices[0] + 1, partitionIndices[1] - 1);
+		DualPivotQuickSort(array, partitionIndices[1] + 1, right);
+    }
+}
+
+int main() {
+	int n;
+	while(scanf("%d", &n) == 1) {
+	    int prev, curr;
+	    scanf("%d", &prev);
+	    int diff[--n];
+	    for(int i = 0; i < n; ++i) {
+	        scanf("%d", &curr);
+	        diff[i] = abs(prev - curr);
+	        prev = curr;
+	    }
+	    n <= 47 ? InsertionSort(diff, n) : DualPivotQuickSort(diff, 0, n - 1);
+        bool Jolly = true;
+	    for(int i = 0; i < n && Jolly; ++i) {
+	        if(diff[i] != i + 1)
+	            Jolly = false;
+	    }
+	    puts(Jolly ? "Jolly": "Not jolly");
+	}
+	return 0;
+}
