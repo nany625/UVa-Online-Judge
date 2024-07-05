f5cef81b203143bfa40e7fe1f09a378c4756efc9
nany625 <jimmy19980625@gmail.com>
2024-07-05 20:21:13 +0800

    Create solution.java

---
+++ 10082 WERTYU/solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        String keyboard = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+        int len = keyboard.length();
+        HashMap<Character, Character> decode = new HashMap<>();
+        for(int i = 1; i < len; ++i)
+            decode.put(keyboard.charAt(i), keyboard.charAt(i - 1));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text;
+        StringBuilder output = new StringBuilder();
+        while((text = br.readLine()) != null) {
+            len = text.length();
+            for(int i = 0; i < len; ++i) {
+                if(decode.containsKey(text.charAt(i)))
+                    output.append(decode.get(text.charAt(i)));
+                else
+                    output.append(text.charAt(i));
+            }
+            output.append("\n");
+		}
+        System.out.print(output);
+	}
+}
