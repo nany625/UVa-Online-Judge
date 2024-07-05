1f10a9b63080923a97180243990b9b46b366afda
nany625 <jimmy19980625@gmail.com>
2024-07-05 17:56:40 +0800

    Update solution.java

--- 10062 Tell me the frequencies!/solution.java
+++ 10062 Tell me the frequencies!/solution.java
@@ -10,45 +10,29 @@ public class Main {
         while((text = br.readLine()) != null) {
             if(!firstCase)
                 output.append("\n");
-            ArrayList<Frequency> frequencies = new ArrayList<>();
+            HashMap<Character, Short> frequencies = new HashMap<>();
+            ArrayList<Character> frequenciesKeys = new ArrayList<>();
             int len = text.length();
             for(int i = 0; i < len; ++i) {
-                int pos = contain(frequencies, text.charAt(i));
-                if(pos == -1)
-                    frequencies.add(new Frequency(text.charAt(i), (short)1));
-                else
-                    ++frequencies.get(pos).count;
+                if(frequencies.containsKey(text.charAt(i)))
+                    frequencies.replace(text.charAt(i), (short)(frequencies.get(text.charAt(i)) + 1));
+                else {
+                    frequencies.put(text.charAt(i), (short)1);
+                    frequenciesKeys.add(text.charAt(i));
+                }
             }
-            Collections.sort(frequencies, new Comparator<Frequency>() {
+            Collections.sort(frequenciesKeys, new Comparator<Character>() {
                 @Override
-                public int compare(Frequency f1, Frequency f2) {
-                    if(f1.count != f2.count)
-                        return f1.count - f2.count;
-                    return f2.value - f1.value;
+                public int compare(Character c1, Character c2) {
+                    if(frequencies.get(c1) != frequencies.get(c2))
+                        return frequencies.get(c1) - frequencies.get(c2);
+                    return c2 - c1;
                 }
             });
-            for(Frequency f : frequencies)
-                output.append((int)f.value + " " + f.count + "\n");
+            for(Character c : frequenciesKeys)
+                output.append((int)c + " " + frequencies.get(c) + "\n");
             firstCase = false;
         }
         System.out.print(output);
 	}
-	
-	static int contain(ArrayList<Frequency> arrayList, char key) {
-        int size = arrayList.size();
-        for(int i = 0; i < size; ++i) {
-            if(arrayList.get(i).value == key)
-                return i;
-        }
-        return -1;
-    }
-}
-
-class Frequency {
-    char value;
-    short count;
-    Frequency(char value, short count) {
-        this.value = value;
-        this.count = count;
-    }
 }
