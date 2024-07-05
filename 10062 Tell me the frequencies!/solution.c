4d4602201b5fe09c8683540bd68f5e3bbd2ed9db
nany625 <jimmy19980625@gmail.com>
2024-07-05 16:03:04 +0800

    Create solution.c

---
+++ 10062 Tell me the frequencies!/solution.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct {
+    char value;
+    short count;
+} Frequency;
+
+int contain(Frequency *array, int size, char key) {
+    for(int i = 0; i < size; ++i) {
+        if(array[i].value == key)
+            return i;
+    }
+    return -1;
+}
+
+int compare(const void *a, const void *b) {
+    Frequency *f1 = (Frequency*)a;
+    Frequency *f2 = (Frequency*)b;
+    if(f1->count != f2->count)
+        return f1->count - f2->count;
+    return f2->value - f1->value;
+}
+
+int main() {
+	char text[1002];
+	bool firstCase = true;
+	while(fgets(text, sizeof(text), stdin)) {
+	    if(!firstCase)
+	        puts("");
+	    text[strcspn(text, "\n")] = '\0';
+	    Frequency *frequencies = NULL;
+	    int size = 0, len = strlen(text);
+	    for(int i = 0; i < len; ++i) {
+	        int pos = contain(frequencies, size, text[i]);
+	        if(pos == -1) {
+	            frequencies = (Frequency*)realloc(frequencies, (size + 1) * sizeof(Frequency));
+	            frequencies[size].value = text[i];
+	            frequencies[size++].count = 1;
+	        } else
+	            ++frequencies[pos].count;
+	    }
+	    qsort(frequencies, size, sizeof(Frequency), compare);
+	    for(int i = 0; i < size; ++i)
+	        printf("%d %d\n", frequencies[i].value, frequencies[i].count);
+	    free(frequencies);
+	    firstCase = false;
+	}
+	return 0;
+}
