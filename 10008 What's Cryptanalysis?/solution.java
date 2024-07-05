74967d13cfc01ffe8b43ce945bff69778eabaca0
nany625 <jimmy19980625@gmail.com>
2024-07-05 18:18:04 +0800

    Update solution.java

--- 10008 What's Cryptanalysis?/solution.java
+++ 10008 What's Cryptanalysis?/solution.java
@@ -3,9 +3,8 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    Frequency[] frequencies = new Frequency[26];
-    	for(int i = 0; i < 26; ++i)
-    	    frequencies[i] = new Frequency((char)('A' + i), (short)0);
+    	HashMap<Character, Short> frequencies = new HashMap<>();
+    	ArrayList<Character> frequenciesKeys = new ArrayList<>();
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringBuilder output = new StringBuilder();
@@ -13,31 +12,29 @@ public class Main {
     	    String text = br.readLine();
     	    int len = text.length();
     	    for(int i = 0; i < len; ++i) {
-    	        if(Character.isUpperCase(text.charAt(i)))
-    	            ++frequencies[text.charAt(i) - 'A'].count;
-    	        else if(Character.isLowerCase(text.charAt(i)))
-    	            ++frequencies[text.charAt(i) - 'a'].count;
+    	        char ch = text.charAt(i);
+    	        if(Character.isLowerCase(ch))
+    	            ch = Character.toUpperCase(ch);
+    	        if(Character.isUpperCase(ch)) {
+    	            if(frequencies.containsKey(ch))
+    	                frequencies.replace(ch, (short)(frequencies.get(ch) + 1));
+    	            else {
+    	                frequencies.put(ch, (short)1);
+    	                frequenciesKeys.add(ch);
+    	            }
+    	        }
     	    }
     	}
-    	Arrays.sort(frequencies, new Comparator<Frequency>() {
+    	Collections.sort(frequenciesKeys, new Comparator<Character>() {
     	    @Override
-    	    public int compare(Frequency f1, Frequency f2) {
-    	        if(f1.count != f2.count)
-    	            return f2.count - f1.count;
-    	        return f1.alpha - f2.alpha;
+    	    public int compare(Character c1, Character c2) {
+    	        if(frequencies.get(c1).equals(frequencies.get(c2)))
+    	            return Character.compare(c1, c2);
+    	        return Short.compare(frequencies.get(c2), frequencies.get(c1));
     	    }
     	});
-    	for(int i = 0; i < 26 && frequencies[i].count > 0; ++i)
-    	    output.append(frequencies[i].alpha + " " + frequencies[i].count + "\n");
+    	for(Character c : frequenciesKeys)
+    	    output.append(c + " " + frequencies.get(c) + "\n");
         System.out.print(output);
 	}
-}
-
-class Frequency {
-    char alpha;
-    short count;
-    Frequency(char alpha, short count) {
-        this.alpha = alpha;
-        this.count = count;
-    }
 }
