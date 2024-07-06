93dc917d45f7375248e49a6ad53090efaa1e7310
nany625 <jimmy19980625@gmail.com>
2024-07-06 10:52:33 +0800

    Create solution.java

---
+++ 10170 The Hotel with Infinite Rooms/solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int S = (int)st.nval;
+            st.nextToken();
+            output.append((int)Math.ceil((-1 + Math.sqrt(1 + 4 * (S * S - S + 2 * (long)st.nval))) / 2) + "\n");
+		}
+        System.out.print(output);
+	}
+}
