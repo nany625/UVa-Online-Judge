3395470d8db5cda018a387187f039c7b7d83c9dc
nany625 <jimmy19980625@gmail.com>
2024-07-06 22:55:22 +0800

    Create solution.c

---
+++ 10189 Minesweeper/solution.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int dRow[] = {0, 1, 0, -1, 1, 1, -1, -1}, dCol[] = {1, 0, -1, 0, 1, -1, -1, 1};
+
+int main() {
+	int x = 0, n, m;
+	while(scanf("%d %d", &n, &m) && (n != 0 || m != 0)) {
+	    if(x >= 1)
+	        puts("");
+	    printf("Field #%d:\n", ++x);
+	    char field[n][m + 1];
+	    for(int i = 0; i < n; ++i)
+	        scanf("%s", field[i]);
+	    for(int i = 0; i < n; ++i) {
+	        for(int j = 0; j < m; ++j) {
+	            if(field[i][j] == '.') {
+	                field[i][j] = '0';
+	                for(int dir = 0; dir < 8; ++dir) {
+	                    if(i + dRow[dir] < n && j + dCol[dir] < m && field[i + dRow[dir]][j + dCol[dir]] == '*')
+	                        ++field[i][j];
+	                }
+	            }
+	        }
+	    }
+	    for(int i = 0; i < n; ++i)
+	        printf("%s\n", field[i]);
+	}
+	return 0;
+}
