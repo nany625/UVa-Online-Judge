b5b6bd2ad40305dca99ea3e1eb0c92d87c6f23dc
nany625 <jimmy19980625@gmail.com>
2024-07-06 23:20:20 +0800

    Create solution.java

---
+++ 10189 Minesweeper/solution.java
@@ -0,0 +1,39 @@
+import java.io.*;
+
+public class Main {
+    static int[] dRow = {0, 1, 0, -1, 1, 1, -1, -1}, dCol = {1, 0, -1, 0, 1, -1, -1, 1};
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        int x = 0;
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int n = (int)st.nval;
+            st.nextToken();
+            int m = (int)st.nval;
+            if(n == 0 && m == 0)
+                break;
+            if(x >= 1)
+                output.append("\n");
+            output.append("Field #" + (++x) + ":\n");
+            char[][] field = new char[n][m];
+            for(int i = 0; i < n; ++i)
+                field[i] = br.readLine().toCharArray();
+            for(int i = 0; i < n; ++i) {
+    	        for(int j = 0; j < m; ++j) {
+    	            if(field[i][j] == '.') {
+    	                field[i][j] = '0';
+    	                for(int dir = 0; dir < 8; ++dir) {
+    	                    int newRow = i + dRow[dir], newCol = j + dCol[dir];
+    	                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && field[newRow][newCol] == '*')
+    	                        ++field[i][j];
+    	                }
+    	            }
+    	        }
+    	    }
+    	    for(int i = 0; i < n; ++i)
+	            output.append(new String(field[i]) + "\n");
+        }
+        System.out.print(output);
+	}
+}
