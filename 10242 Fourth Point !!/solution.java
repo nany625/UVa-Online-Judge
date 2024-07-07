7c2a1191ed34334a6424fa3f2eb7c793e610cf8f
nany625 <jimmy19980625@gmail.com>
2024-07-07 18:34:56 +0800

    Create solution.java

---
+++ 10242 Fourth Point !!/solution.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            double x1 = st.nval;
+            st.nextToken();
+            double y1 = st.nval;
+            st.nextToken();
+            double x2 = st.nval;
+            st.nextToken();
+            double y2 = st.nval;
+            st.nextToken();
+            double x3 = st.nval;
+            st.nextToken();
+            double y3 = st.nval;
+            st.nextToken();
+            double x4 = st.nval;
+            st.nextToken();
+            double y4 = st.nval;
+            if(x1 == x3 && y1 == y3)
+                output.append(String.format("%.3f %.3f\n", x2 + x4 - x1, y2 + y4 - y1));
+            else if(x1 == x4 && y1 == y4)
+                output.append(String.format("%.3f %.3f\n", x2 + x3 - x1, y2 + y3 - y1));
+            else if(x2 == x3 && y2 == y3)
+                output.append(String.format("%.3f %.3f\n", x1 + x4 - x2, y1 + y4 - y2));
+            else
+                output.append(String.format("%.3f %.3f\n", x1 + x3 - x2, y1 + y3 - y2));
+        }
+        System.out.print(output);
+	}
+}
