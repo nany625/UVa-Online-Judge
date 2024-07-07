86dc759e1997ca62ae16e6bee56d2b32cbf32518
nany625 <jimmy19980625@gmail.com>
2024-07-07 08:55:52 +0800

    Update solution.c

--- 10209 Is This Integration ?/solution.c
+++ 10209 Is This Integration ?/solution.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <math.h>
-#define PI (2.0 * acos(0.0))
 
 int main() {
-    double a;
+    double a, PI = 2.0 * acos(0.0);
     while(scanf("%lf", &a) == 1)
-        printf("%.3f %.3f %.3f\n", a * a * (PI / 3 + 1 - sqrt(3.0)), 4 * a * a * (PI / 12 - 1 + sqrt(3.0) / 2), 4 * a * a * (-PI / 6 + 1 - sqrt(3.0) / 4));
+        printf("%.3f %.3f %.3f\n", a * a * (PI / 3 + 1 - sqrt(3)), 4 * a * a * (PI / 12 - 1 + sqrt(3) / 2), 4 * a * a * (-PI / 6 + 1 - sqrt(3) / 4));
     return 0;
 }
