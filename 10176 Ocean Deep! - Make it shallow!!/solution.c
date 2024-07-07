c06cc0bc7f6cf8a263d8cb80130424b90a50cf92
nany625 <jimmy19980625@gmail.com>
2024-07-07 10:38:18 +0800

    Update solution.c

--- 10176 Ocean Deep! - Make it shallow!!/solution.c
+++ 10176 Ocean Deep! - Make it shallow!!/solution.c
@@ -9,7 +9,7 @@ int main() {
         for(int i = 0; i < len; ++i) {
             if(buffer[i] != '#')
                 remainder = (2 * remainder + buffer[i] -'0') % 131071;
-            else{
+            else {
                 puts(remainder == 0 ? "YES" : "NO");
                 remainder = 0;
             }
