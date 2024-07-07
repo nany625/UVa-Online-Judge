cf8a30da1fa516ec6b3f61446fef09aa38ffac58
nany625 <jimmy19980625@gmail.com>
2024-07-07 19:35:37 +0800

    Update solution.c

--- 10252 Common Permutation/solution.c
+++ 10252 Common Permutation/solution.c
@@ -4,19 +4,18 @@
 int main() {
     char a[1002], b[1002];
     while(fgets(a, sizeof(a), stdin) && fgets(b, sizeof(b), stdin)) {
-        short frequencya[26] = {}, frequencyb[26] = {};
+        short frequencya[26] = {}, output[26] = {};
         int len = strlen(a);
         for(int i = 0; i < len; ++i)
             ++frequencya[a[i] - 'a'];
         len = strlen(b);
-        for(int i = 0; i < len; ++i)
-            ++frequencyb[b[i] - 'a'];
+        for(int i = 0; i < len; ++i) {
+            if(output[b[i] - 'a'] < frequencya[b[i] - 'a'])
+                ++output[b[i] - 'a'];
+        }
         for(int i = 0; i < 26; ++i) {
-            if(frequencya[i] > 0 && frequencyb[i] > 0) {
-                short min = frequencya[i] < frequencyb[i] ? frequencya[i] : frequencyb[i];
-                while(min--)
-                    putchar('a' + i);
-            }
+            while(output[i]--)
+                putchar('a' + i);
         }
         puts("");
     }
