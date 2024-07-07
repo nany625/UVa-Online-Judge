ce6dcb304b95c2094687be3850c7cd57ae674301
nany625 <jimmy19980625@gmail.com>
2024-07-07 23:44:57 +0800

    Create solution.java

---
+++ 10268 498-bis/solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int x = (int)st.nval;
+            String[] a = br.readLine().split("\\s+");
+            long result = 0;
+            int len = a.length - 1;
+            for(int i = 0; i < len; ++i)
+                result = result * x + Long.parseLong(a[i]) * (len - i);
+            output.append(result + "\n");
+        }
+        System.out.print(output);
+	}
+}
