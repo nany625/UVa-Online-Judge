df557b98d259a85fdf03f082dec6f0f4beccab24
nany625 <jimmy19980625@gmail.com>
2024-07-07 13:14:47 +0800

    Update solution.c

--- 10082 WERTYU/solution.c
+++ 10082 WERTYU/solution.c
@@ -2,17 +2,17 @@
 #include <string.h>
 
 int main() {
-    char text[100], keyboard[]="`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+    char text[100], keyboard[] = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
     while(fgets(text, sizeof(text), stdin)) {
         int len = strlen(text);
     	for(int i = 0; i < len; ++i) {
     		if(text[i] == ' ' || text[i] == '\n')
     			putchar(text[i]);
     		else {
-    			int j=0;
+    			int j = 0;
     			while(text[i] != keyboard[j])
     				++j;
-    			putchar(keyboard[j-1]);
+    			putchar(keyboard[j - 1]);
     		}
     	}
     }
