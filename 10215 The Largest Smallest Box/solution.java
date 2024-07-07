b60ab222d92abf5ac23ab98b56fbf99bc37cad16
nany625 <jimmy19980625@gmail.com>
2024-07-07 13:01:39 +0800

    Update solution.java

--- 10215 The Largest Smallest Box/solution.java
+++ 10215 The Largest Smallest Box/solution.java
@@ -8,7 +8,7 @@ public class Main {
             double L = st.nval;
             st.nextToken();
             double W = st.nval;
-            output.append(String.format("%.3f 0.000 %.3f\n", (L + W - Math.sqrt(L * L + W * W - L * W)) / 6, (L > W ? W : L) / 2));
+            output.append(String.format("%.3f 0.000 %.3f\n", (L + W - Math.sqrt(L * L + W * W - L * W)) / 6, Math.min(L, W) / 2));
         }
         System.out.print(output);
 	}
