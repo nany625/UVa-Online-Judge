ccbd176e488d4c83b24eb86ff9ca0284831e1bd8
nany625 <jimmy19980625@gmail.com>
2024-07-07 10:03:55 +0800

    Delete 10215 The Largest Smallest Box ... directory

--- 10215 The Largest Smallest Box .../solution.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-    double L, W, eps = 1e-6;
-    while(scanf("%lf %lf", &L, &W) == 2)
-        printf("%.3f 0.000 %.3f\n", (L + W - sqrt(L * L + W * W - L * W)) / 6, (L > W ? W : L) / 2 + eps);
-    return 0;
-}
