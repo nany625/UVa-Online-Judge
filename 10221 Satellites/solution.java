57a038d80357243d7380e6daf1f17f970a324304
nany625 <jimmy19980625@gmail.com>
2024-07-07 11:03:33 +0800

    Create solution.java

---
+++ 10221 Satellites/solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+    	    double s = st.nval;
+    	    st.nextToken();
+    	    double a = st.nval;
+    	    st.nextToken();
+    	    if(st.sval.equals("min"))
+    	        a /= 60;
+    	    if(a > 180)
+    	        a = 360 - a;
+            output.append(String.format("%.6f %.6f\n", (6440 + s) * Math.PI * a / 180, Math.sqrt(2 * Math.pow(6440 + s, 2) * (1 - Math.cos(a * Math.PI / 180)))));
+    	}
+    	System.out.print(output);
+	}
+}
