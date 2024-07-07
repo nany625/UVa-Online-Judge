6200ee68142641e990d9d884d8bd5d9634ce0c7a
nany625 <jimmy19980625@gmail.com>
2024-07-07 10:38:08 +0800

    Create solution.c

---
+++ 10176 Ocean Deep! - Make it shallow!!/solution.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char buffer[101];
+    int remainder = 0;
+    while(scanf("%s", buffer) == 1) {
+        int len = strlen(buffer);
+        for(int i = 0; i < len; ++i) {
+            if(buffer[i] != '#')
+                remainder = (2 * remainder + buffer[i] -'0') % 131071;
+            else{
+                puts(remainder == 0 ? "YES" : "NO");
+                remainder = 0;
+            }
+        }
+    }
+    return 0;
+}
