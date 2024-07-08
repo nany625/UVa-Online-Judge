9ddc7c7e5e46641374ee4649576b9ee059ced03a
nany625 <jimmy19980625@gmail.com>
2024-07-08 20:24:32 +0800

    Update solution.c

--- 10279 Mine Sweeper/solution.c
+++ 10279 Mine Sweeper/solution.c
@@ -21,10 +21,8 @@ int main() {
         for(int i = 0; i < n; ++i) {
 	        for(int j = 0; j < n; ++j) {
 	            if(touch[i][j] == 'x') {
-	                if(mines[i][j] == '*') {
-	                    touch[i][j] = '*';
+	                if(mines[i][j] == '*')
 	                    boom = true;
-	                }
 	                else {
     	                touch[i][j] = '0';
     	                for(int dir = 0; dir < 8; ++dir) {
