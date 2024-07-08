c3ab3a5f58f4d8211c1318aebe692af29a786da5
nany625 <jimmy19980625@gmail.com>
2024-07-08 11:34:05 +0800

    Create solution.java

---
+++ 10279 Mine Sweeper/solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+
+public class Main {
+    static int[] dRow = {0, 1, 0, -1, 1, 1, -1, -1}, dCol = {1, 0, -1, 0, 1, -1, -1, 1};
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        st.nextToken();
+        int games = (int)st.nval;
+        boolean firstCase = true;
+        StringBuilder output = new StringBuilder();
+        while(games-- > 0) {
+            st.nextToken();
+            int n = (int)st.nval;
+            if(!firstCase)
+                output.append("\n");
+            char[][] mines = new char[n][n], touch = new char[n][n];
+            for(int i = 0; i < n; ++i)
+                mines[i] = br.readLine().toCharArray();
+            for(int i = 0; i < n; ++i)
+                touch[i] = br.readLine().toCharArray();
+            boolean boom = false;
+            for(int i = 0; i < n; ++i) {
+    	        for(int j = 0; j < n; ++j) {
+    	            if(touch[i][j] == 'x') {
+    	                if(mines[i][j] == '*') {
+    	                    touch[i][j] = '*';
+    	                    boom = true;
+    	                }
+    	                else {
+        	                touch[i][j] = '0';
+        	                for(int dir = 0; dir < 8; ++dir) {
+        	                    int newRow = i + dRow[dir], newCol = j + dCol[dir];
+        	                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && mines[newRow][newCol] == '*')
+        	                        ++touch[i][j];
+        	                }
+    	                }
+    	            }
+    	        }
+    	    }
+    	    if(boom) {
+    	        for(int i = 0; i < n; ++i) {
+        	        for(int j = 0; j < n; ++j) {
+        	            if(mines[i][j] == '*')
+        	                touch[i][j] = '*';
+        	        }
+    	        }
+    	    }
+    	    for(int i = 0; i < n; ++i)
+    	        output.append(new String(touch[i]) + "\n");
+    	    firstCase = false;
+        }
+        System.out.print(output);
+	}
+}
