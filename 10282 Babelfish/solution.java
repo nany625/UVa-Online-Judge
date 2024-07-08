384813ae8ee3d76714de93859c349a8437d0efcd
nany625 <jimmy19980625@gmail.com>
2024-07-08 16:46:58 +0800

    Create solution.java

---
+++ 10282 Babelfish/solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, String> dict = new HashMap<>();
+        String line;
+        StringBuilder output = new StringBuilder();
+        while(!(line = br.readLine()).isEmpty()) {
+            String[] tokens = line.split("\\s+");
+            dict.put(tokens[1], tokens[0]);
+        }
+        while((line = br.readLine()) != null)
+            output.append((dict.containsKey(line) ? dict.get(line) : "eh") + "\n");
+        System.out.print(output);
+	}
+}
