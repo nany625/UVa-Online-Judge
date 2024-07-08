0e8d59bf10df0add301b2ffafb969d3c1dcbc1e9
nany625 <jimmy19980625@gmail.com>
2024-07-08 21:59:37 +0800

    Update solution.c

--- 10093 An Easy Problem!/solution.c
+++ 10093 An Easy Problem!/solution.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 int main() {
-	char R[945];
-	while(scanf("%s", R) == 1) {
+	char *R = NULL;
+	while(scanf("%ms", &R) == 1) {
 		int max = 1, sumOfDigits = 0, len = strlen(R);
 		for(int i = 0; i < len; ++i) {
-			int digit;
+			int digit = 0;
 			if(isdigit(R[i]))
 				digit = R[i] - '0';
 			else if(isupper(R[i]))
@@ -24,6 +25,8 @@ int main() {
 		    printf("%d\n", N);
 		else
 			puts("such number is impossible!");
+		free(R);
+		R = NULL;
 	}
 	return 0;
 }
