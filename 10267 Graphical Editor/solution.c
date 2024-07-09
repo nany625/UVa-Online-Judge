fea626fcf36a36b26e057c7664554a5be11fee6b
nany625 <jimmy19980625@gmail.com>
2024-07-09 19:17:03 +0800

    Update solution.c

--- 10267 Graphical Editor/solution.c
+++ 10267 Graphical Editor/solution.c
@@ -34,38 +34,46 @@ int main() {
 	size_t bufsize = 0;
 	int M, N, X, X1, X2, Y, Y1, Y2;
 	while(scanf("%c", &command) && command != 'X') {
-		if(command == 'I') {
-			scanf("%d %d", &M, &N);
-			fill(0, N - 1, 0, M - 1, 'O');
-		} else if(command == 'C')
-			fill(0, N - 1, 0, M - 1, 'O');
-		else if(command == 'L') {
-			scanf("%d %d %c", &X, &Y, &C);
-			table[Y - 1][X - 1] = C;
-		} else if(command == 'V') {
-			scanf("%d %d %d %c", &X, &Y1, &Y2, &C);
-			if(Y1 > Y2)
-			    swap(&Y1, &Y2);
-			for(int i = Y1 - 1; i <= Y2 - 1; ++i)
-				table[i][X - 1] = C;
-		} else if(command == 'H') {
-			scanf("%d %d %d %c", &X1, &X2, &Y, &C);
-			if(X1 > X2)
-			    swap(&X1, &X2);
-			memset(table[Y - 1] + X1 - 1, C, X2 - X1 + 1);
-		} else if(command == 'K') {
-			scanf("%d %d %d %d %c", &X1, &Y1, &X2, &Y2, &C);
-			fill(Y1 - 1, Y2 - 1, X1 - 1, X2 - 1, C);
-		} else if(command == 'F') {
-			scanf("%d %d %c", &X, &Y, &C);
-			dfs(Y - 1, N, X - 1, M, table[Y - 1][X - 1], C);
-		} else if(command == 'S') {
-			scanf("%ms", &buffer);
-			printf("%s\n", buffer);
-			for(int i = 0; i < N; ++i)
-			    printf("%.*s\n", M, table[i]);
-		}
-    	getline(&buffer, &bufsize, stdin);
+	    switch(command) {
+    		case 'I':
+    			scanf("%d %d", &M, &N);
+    			fill(0, N - 1, 0, M - 1, 'O');
+    			break;
+    		case 'C':
+    			fill(0, N - 1, 0, M - 1, 'O');
+    			break;
+    		case 'L':
+    			scanf("%d %d %c", &X, &Y, &C);
+    			table[Y - 1][X - 1] = C;
+    			break;
+    		case 'V':
+    			scanf("%d %d %d %c", &X, &Y1, &Y2, &C);
+    			if(Y1 > Y2)
+    			    swap(&Y1, &Y2);
+    			for(int i = Y1 - 1; i <= Y2 - 1; ++i)
+    				table[i][X - 1] = C;
+    			break;
+    		case 'H':
+    			scanf("%d %d %d %c", &X1, &X2, &Y, &C);
+    			if(X1 > X2)
+    			    swap(&X1, &X2);
+    			memset(table[Y - 1] + X1 - 1, C, X2 - X1 + 1);
+    		    break;
+    		case 'K':
+    			scanf("%d %d %d %d %c", &X1, &Y1, &X2, &Y2, &C);
+    			fill(Y1 - 1, Y2 - 1, X1 - 1, X2 - 1, C);
+    		    break;
+    		case 'F':
+    			scanf("%d %d %c", &X, &Y, &C);
+    			dfs(Y - 1, N, X - 1, M, table[Y - 1][X - 1], C);
+    		    break;
+    		case 'S':
+    			scanf("%ms", &buffer);
+    			printf("%s\n", buffer);
+    			for(int i = 0; i < N; ++i)
+    			    printf("%.*s\n", M, table[i]);
+	    }
+	    getline(&buffer, &bufsize, stdin);
 		free(buffer);
 		buffer = NULL;
 	}
