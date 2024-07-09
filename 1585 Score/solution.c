29d48598166b2dcaf7392d371cb443e41fd596a3
nany625 <jimmy19980625@gmail.com>
2024-07-09 13:29:38 +0800

    Update solution.c

--- 1585 Score/solution.c
+++ 1585 Score/solution.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main() {
 	int T;
 	scanf("%d", &T);
 	while(T--) {
-		char s[80];
-		scanf("%s", s);
+		char *s = NULL;
+		scanf("%ms", &s);
 		int score = 0, temp = 0, i = 0, len = strlen(s);
         do {
             if(s[i] == 'O')
@@ -15,6 +16,7 @@ int main() {
                 temp = 0;
         } while(++i < len);
 		printf("%d\n", score);
+		free(s);
 	}
 	return 0;
 }
