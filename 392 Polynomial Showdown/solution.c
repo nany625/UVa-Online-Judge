4a8367b1975f9c12bb08a530524d996d0d98dade
nany625 <jimmy19980625@gmail.com>
2024-07-10 11:30:00 +0800

    Create solution.c

---
+++ 392 Polynomial Showdown/solution.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+    short coefficients[9];
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	while(getline(&buffer, &bufsize, stdin) != -1) {
+		int i = 8;
+		char* token = strtok(buffer, " ");
+		while(token) {
+			coefficients[i--] = atoi(token);
+			token = strtok(NULL, " ");
+		}
+		bool leadingZero = true;
+		for(i = 8; i >= 0; --i) {
+			if(coefficients[i] != 0) {
+				if(!leadingZero) {
+					if(coefficients[i] < 0)
+						printf(" - ");
+					else
+						printf(" + ");
+				} else {
+					if(coefficients[i] < 0)
+						putchar('-');
+				}
+				if(i == 0)
+					printf("%hd", abs(coefficients[i]));
+				else if(i == 1) {
+					if(abs(coefficients[i]) > 1)
+						printf("%d", abs(coefficients[i]));
+					putchar('x');
+				} else {
+					if(abs(coefficients[i]) > 1)
+						printf("%d", abs(coefficients[i]));
+					printf("x^%d", i);
+				}
+				leadingZero = false;
+			}
+		}
+		if(leadingZero)
+			putchar('0');
+		puts("");
+	}
+	return 0;
+}
