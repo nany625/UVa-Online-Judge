58f76f56756d5fa7f1cfd3133e8dd3a2e685d11d
nany625 <jimmy19980625@gmail.com>
2024-07-10 11:08:26 +0800

    Create solution.java

---
+++ 10530 Guessing Game/solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		int min = 1, max = 10;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
+		int guess;
+		StringBuilder output = new StringBuilder();
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (guess = (int)st.nval) != 0) {
+		    String resp = br.readLine();
+		    switch(resp) {
+		        case "too high":
+		            max = Math.min(max, guess - 1);
+		            break;
+		        case "too low":
+		            min = Math.max(min, guess + 1);
+		            break;
+		        case "right on":
+		            if(guess >= min && guess <= max)
+		                output.append("Stan may be honest\n");
+		            else
+		                output.append("Stan is dishonest\n");
+		            min = 1;
+		            max = 10;
+		    }
+		}
+		System.out.print(output);
+	}
+}
