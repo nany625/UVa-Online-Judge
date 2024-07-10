841bfa4daef40b20bb6451bdee3bbd352c0e2d56
nany625 <jimmy19980625@gmail.com>
2024-07-10 11:34:32 +0800

    Update solution.c

--- 392 Polynomial Showdown/solution.c
+++ 392 Polynomial Showdown/solution.c
@@ -4,18 +4,18 @@
 #include <stdbool.h>
 
 int main() {
-    int coefficients[9];
+    short coefficients[9];
 	char *buffer = NULL;
 	size_t bufsize = 0;
 	while(getline(&buffer, &bufsize, stdin) != -1) {
-		int i = 8;
+		int i = 0;
 		char* token = strtok(buffer, " ");
 		while(token) {
-			coefficients[i--] = atoi(token);
+			coefficients[i++] = atoi(token);
 			token = strtok(NULL, " ");
 		}
 		bool leadingZero = true;
-		for(i = 8; i >= 0; --i) {
+		for(i = 0; i < 9; ++i) {
 			if(coefficients[i] != 0) {
 				if(!leadingZero) {
 					if(coefficients[i] < 0)
@@ -26,16 +26,16 @@ int main() {
 					if(coefficients[i] < 0)
 						putchar('-');
 				}
-				if(i == 0)
+				if(i == 8)
 					printf("%d", abs(coefficients[i]));
-				else if(i == 1) {
+				else if(i == 7) {
 					if(abs(coefficients[i]) > 1)
 						printf("%d", abs(coefficients[i]));
 					putchar('x');
 				} else {
 					if(abs(coefficients[i]) > 1)
 						printf("%d", abs(coefficients[i]));
-					printf("x^%d", i);
+					printf("x^%d", 8 - i);
 				}
 				leadingZero = false;
 			}
