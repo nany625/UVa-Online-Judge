ff0b79b07f2c2650f69b4daa6c1bf00586128717
nany625 <jimmy19980625@gmail.com>
2024-07-10 13:40:20 +0800

    Create solution.java

---
+++ 834 Continued Fractions/solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		StringBuilder output = new StringBuilder();
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+		    int numerator = (int)st.nval;
+		    st.nextToken();
+		    int denominator = (int)st.nval, count = 0;
+		    do {
+		        if(count == 0)
+		            output.append("[" + numerator / denominator);
+		        else if(count == 1)
+		            output.append(";" + numerator / denominator);
+		        else
+		            output.append("," + numerator / denominator);
+		        int temp = numerator %= denominator;
+		        numerator = denominator;
+		        denominator = temp;
+		        ++count;
+		    } while(denominator > 0);
+		    output.append("]\n");
+		}
+		System.out.print(output);
+	}
+}
