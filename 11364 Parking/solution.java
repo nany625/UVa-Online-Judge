cdf4a58a217216d4f8ab2737218a7ec599d9f200
nany625 <jimmy19980625@gmail.com>
2024-07-10 10:49:17 +0800

    Create solution.java

---
+++ 11364 Parking/solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		st.nextToken();
+		int t = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		while(t-- > 0) {
+		    st.nextToken();
+		    int n = (int)st.nval;
+		    int position, min = 99, max = 0;
+		    while(n-- > 0) {
+		        st.nextToken();
+		        min = Math.min(min, (int)st.nval);
+		        max = Math.max(max, (int)st.nval);
+		    }
+		    output.append(2 * (max - min) + "\n");
+		}
+		System.out.print(output);
+	}
+}
