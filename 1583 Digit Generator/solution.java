cc5fcf09fd1e7e76c5991b42e023f3833010645a
nany625 <jimmy19980625@gmail.com>
2024-07-10 15:07:53 +0800

    Create solution.java

---
+++ 1583 Digit Generator/solution.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		st.nextToken();
+		int T = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		while(T-- > 0) {
+		    st.nextToken();
+		    int N = (int)st.nval;
+		    boolean found = false;
+		    for(int i = (int)(N - 9 * Math.log10(N)); i < N && !found; ++i) {
+		        int temp = i, sumOfDigits = i;
+		        while(temp > 0) {
+                    sumOfDigits += temp % 10;
+                    temp /= 10;
+                }
+                if(sumOfDigits == N) {
+                    output.append(i + "\n");
+                    found = true;
+                }
+		    }
+		    if(!found)
+		        output.append("0\n");
+		}
+		System.out.print(output);
+	}
+}
