a07a6bb38c1bb12bc08aa9323da852de6c785ece
nany625 <jimmy19980625@gmail.com>
2024-07-12 01:20:56 +0800

    Create solution.java

---
+++ 10370 Above Average/solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		st.nextToken();
+		int C = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		while(C-- > 0) {
+		    st.nextToken();
+		    int N = (int)st.nval, sum = 0;
+		    int[] grades = new int[N];
+		    for(int i = 0; i < N; ++i) {
+		        st.nextToken();
+                grades[i] = (int)st.nval;
+                sum += grades[i];
+            }
+            int count = 0;
+            for(int i = 0; i < N; ++i)
+                count += (grades[i] * N > sum ? 1 : 0);
+            output.append(String.format("%.3f%%\n", 100.0 * count / N));
+		}
+		System.out.print(output);
+	}
+}
