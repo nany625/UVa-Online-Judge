b8b00853304bcf01d80870d6b4ff0f67f7572bb3
nany625 <jimmy19980625@gmail.com>
2024-07-11 18:35:00 +0800

    Update solution.c

--- 299 Train Swapping/solution.c
+++ 299 Train Swapping/solution.c
@@ -1,3 +1,4 @@
+// 解法一
 #include <stdio.h>
 
 int main() {
@@ -14,6 +15,49 @@ int main() {
                     ++swap;
             }
         }
+        printf("Optimal train swapping takes %d swaps.\n", swap);
+    }
+    return 0;
+}
+
+// #解法二
+#include <stdio.h>
+
+void MergeSort(int *array, int *reg, int left, int right, int *swap) {
+    if(left < right) {
+        int mid = left + (right - left) / 2;
+        MergeSort(array, reg, left, mid, swap);
+        MergeSort(array, reg, mid + 1, right, swap);
+        int leftPointer = left, rightPointer = mid + 1;
+        for(int i = left; i <= right; ++i) {
+            if(leftPointer > mid) {
+                reg[i] = array[rightPointer++];
+                *swap += mid - leftPointer + 1;
+            }
+            else if(rightPointer > right)
+                reg[i] = array[leftPointer++];
+            else if(array[leftPointer] <= array[rightPointer])
+                reg[i] = array[leftPointer++];
+            else {
+                reg[i] = array[rightPointer++];
+                *swap += mid - leftPointer + 1;
+            }
+        }
+        for(int i = left; i <= right; ++i)
+            array[i] = reg[i];
+    }
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    while(N--) {
+        int L;
+        scanf("%d", &L);
+        int a[L], reg[L], swap = 0;
+        for(int i = 0; i < L; ++i)
+            scanf("%d", &a[i]);
+        MergeSort(a, reg, 0, L - 1, &swap);
         printf("Optimal train swapping takes %d swaps.\n", swap);
     }
     return 0;
