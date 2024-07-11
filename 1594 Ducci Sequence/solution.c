ee1e53100f72abcd2d97090839fcb7a668b33237
nany625 <jimmy19980625@gmail.com>
2024-07-11 15:52:04 +0800

    Update solution.c

--- 1594 Ducci Sequence/solution.c
+++ 1594 Ducci Sequence/solution.c
@@ -11,19 +11,21 @@ int main() {
         int sequence[n];
         for(int i = 0; i < n; ++i)
             scanf("%d", &sequence[i]);
-        int limit = 1000;
+        int limit = 36;
         while(limit--) {
+            bool ZERO = true;
+            for(int i = 0; i < n && ZERO; ++i) {
+                if(sequence[i] != 0)
+                    ZERO = false;
+            }
+            if(ZERO)
+                break;
             int temp = sequence[0];
             for(int i = 0; i < n - 1; ++i)
                 sequence[i] = abs(sequence[i + 1] - sequence[i]);
             sequence[n - 1] = abs(temp - sequence[n - 1]);
         }
-        bool ZERO = true;
-        for(int i = 0; i < n && ZERO; ++i) {
-            if(sequence[i] != 0)
-                ZERO = false;
-        }
-        printf("%s\n", ZERO ? "ZERO" : "LOOP");
+        printf("%s\n", limit > 0 ? "ZERO" : "LOOP");
     }
 	return 0;
 }
