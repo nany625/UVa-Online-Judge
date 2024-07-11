5986557f0b227ab2a2bf6534699e837f79bd2117
nany625 <jimmy19980625@gmail.com>
2024-07-11 17:35:03 +0800

    Update solution.c

--- 10310 Dog and Gopher/solution.c
+++ 10310 Dog and Gopher/solution.c
@@ -15,11 +15,10 @@ int main() {
     while(scanf("%d", &n) == 1) {
         Coordinate gopher, dog, holes[n];
         scanf("%f %f %f %f", &gopher.x, &gopher.y, &dog.x, &dog.y);
-        for(int i = 0; i < n; ++i)
-            scanf("%f %f", &holes[i].x, &holes[i].y);
         bool escape = false;
-        for(int i = 0; i < n && !escape; ++i) {
-            if(2 * dist(gopher, holes[i]) <= dist(dog, holes[i])) {
+        for(int i = 0; i < n; ++i) {
+            scanf("%f %f", &holes[i].x, &holes[i].y);
+            if(!escape && 2 * dist(gopher, holes[i]) <= dist(dog, holes[i])) {
                 printf("The gopher can escape through the hole at (%.3f,%.3f).\n", holes[i].x, holes[i].y);
                 escape = true;
             }
