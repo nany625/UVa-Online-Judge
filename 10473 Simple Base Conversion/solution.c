f5ee0745c3ec66dc28cb09d45cc9106d894b51d1
nany625 <jimmy19980625@gmail.com>
2024-07-12 22:32:44 +0800

    Update solution.c

--- 10473 Simple Base Conversion/solution.c
+++ 10473 Simple Base Conversion/solution.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 int main() {
 	char *num = NULL;
 	while(scanf("%ms", &num) && num[0] != '-') {
-	    if(strncmp(num, "0x", 2) == 0)
+	    if(num[1] == 'x')
 	        printf("%ld\n", strtol(num, NULL, 16));
 	    else
 	        printf("0x%X\n", atoi(num));
