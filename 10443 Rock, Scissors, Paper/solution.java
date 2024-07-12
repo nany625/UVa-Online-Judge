4aa83b6503be1b9b19cbc92221d7d08af197795f
nany625 <jimmy19980625@gmail.com>
2024-07-12 21:22:26 +0800

    Create solution.java

---
+++ 10443 Rock, Scissors, Paper/solution.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
+		st.nextToken();
+		int t = (int)st.nval;
+		boolean firstCase = true;
+		StringBuilder output = new StringBuilder();
+		while(t-- > 0) {
+		    if(!firstCase)
+			    output.append("\n");
+			st.nextToken();
+    		int r = (int)st.nval;
+    		st.nextToken();
+    		int c = (int)st.nval;
+    		st.nextToken();
+    		int n = (int)st.nval;
+    		char[][] grid0 = new char[r][c], grid1 = new char[r][c];
+    		for(int i = 0; i < r; ++i)
+    		    grid0[i] = br.readLine().toCharArray();
+    		for(int i = 1; i <= n; ++i) {
+    			for(int j = 0; j < r; ++j) {
+    				for(int k = 0; k < c; ++k) {
+    					if(i % 2 == 1)
+    						update(grid0, grid1, j, k, r, c);
+    					else
+    						update(grid1, grid0, j, k, r, c);
+    				}
+    			}
+    		}
+    		for(int i = 0; i < r; ++i)
+    		    output.append((n % 2 == 1 ? new String(grid1[i]) : new String(grid0[i])) + "\n");
+        	firstCase = false;
+		}
+		System.out.print(output);
+	}
+	
+	static void update(char[][] today, char[][] tomorrow, int i, int j, int r, int c) {
+    	char curr = tomorrow[i][j] = today[i][j], winner = (curr == 'R') ? 'S' : (curr == 'S') ? 'P' : 'R', loser = (curr == 'R') ? 'P' : (curr == 'S') ? 'R' : 'S';
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for(int[] k : directions) {
+        	int ni = i + k[0], nj = j + k[1];
+        	if(ni >= 0 && ni < r && nj >= 0 && nj < c) {
+        		if(today[ni][nj] == winner)
+        			tomorrow[ni][nj] = curr;
+        		else if(today[ni][nj] == loser)
+        			tomorrow[i][j] = loser;
+        	}
+        }
+    }
+}
