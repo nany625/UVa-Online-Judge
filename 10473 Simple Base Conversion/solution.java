6da7c849ac536830ecf7e4281e7ab09290571b65
nany625 <jimmy19980625@gmail.com>
2024-07-12 22:44:30 +0800

    Create solution.java

---
+++ 10473 Simple Base Conversion/solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String num;
+		StringBuilder output = new StringBuilder();
+		while((num = br.readLine()) != null && num.charAt(0) != '-') {
+		    if(num.length() > 2 && num.charAt(1) == 'x')
+		        output.append(Integer.parseInt(num.substring(2), 16) + "\n");
+		    else
+		        output.append(String.format("0x%X\n", Integer.parseInt(num)));
+		}
+		System.out.print(output);
+	}
+}
