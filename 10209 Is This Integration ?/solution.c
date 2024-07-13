e48b98ce0b5de484f57d2a437da880e49c85ac9b
nany625 <jimmy19980625@gmail.com>
2024-07-13 23:53:19 +0800

    Update solution.c

--- 10209 Is This Integration ?/solution.c
+++ 10209 Is This Integration ?/solution.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 int main() {
-    double a, PI = 2 * acos(0.0);
+    double PI = acos(-1), a;
     while(scanf("%lf", &a) == 1)
         printf("%.3f %.3f %.3f\n", a * a * (PI / 3 + 1 - sqrt(3)), 4 * a * a * (PI / 12 - 1 + sqrt(3) / 2), 4 * a * a * (-PI / 6 + 1 - sqrt(3) / 4));
     return 0;
