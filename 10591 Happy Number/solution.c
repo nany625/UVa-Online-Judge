bcf3f032cee7bce47839784c2a17f052d8b9d79d
nany625 <jimmy19980625@gmail.com>
2024-07-14 02:22:24 +0800

    Update solution.c

--- 10591 Happy Number/solution.c
+++ 10591 Happy Number/solution.c
@@ -2,14 +2,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int table[] = {
+short table[] = {
     1, 7, 10, 13, 19, 23, 28, 31, 32, 44, 
     49, 68, 70, 79, 82, 86, 91, 94, 97, 100, 
     103, 109, 129, 130, 133, 139, 167, 176, 188, 190, 
     192, 193, 203, 208, 219, 226, 230, 236, 239, 262, 
     263, 280, 291, 293, 301, 302, 310, 313, 319, 320, 
     326, 329, 331, 338, 356, 362, 365, 367, 368, 376, 
-    379, 383, 386, 391, 392, 397
+    379, 383, 386, 391, 392, 397, 404, 409, 440, 446, 
+    464, 469, 478, 487, 490, 496, 536, 556, 563, 565, 
+    566, 608, 617, 622, 623, 632, 635, 637, 638, 644, 
+    649, 653, 655, 656, 665, 671, 673, 680, 683, 694, 
+    700, 709, 716
 };
 
 int squareSum(int n) {
@@ -22,18 +26,18 @@ int squareSum(int n) {
 }
 
 int main() {
-    bool happy[397] = {};
-    for(int i = 0; i < 66; ++i)
-        happy[table[i] - 1] = true;
+    bool isHappy[716] = {};
+    for(int i = 0; i < 103; ++i)
+        isHappy[table[i] - 1] = true;
     int cases;
     scanf("%d", &cases);
     for(int i = 1; i <= cases; ++i) {
         int N;
         scanf("%d", &N);
         printf("Case #%d: %d is ", i, N);
-        while(N > 397)
+        while(N > 716)
             N = squareSum(N);
-        puts(happy[N - 1] ? "a Happy number." : "an Unhappy number.");
+        puts(isHappy[N - 1] ? "a Happy number." : "an Unhappy number.");
     }
     return 0;
 }
