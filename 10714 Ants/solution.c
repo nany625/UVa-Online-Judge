5e1973f24ff0b0778ce70ddf6b3a0a4b38cf5cb6
nany625 <jimmy19980625@gmail.com>
2024-07-14 22:27:04 +0800

    Update solution.c

--- 10714 Ants/solution.c
+++ 10714 Ants/solution.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int compare(const void *a, const void *b) {
-    return *(int*)a - *(int*)b;
+int findMin(int a, int b) {
+    return a < b ? a : b;
 }
 
-int binarySearch(int *array, int size, double key) {
-    int left = 0, right = size - 1;
-    while(left <= right) {
-        int mid = left + (right - left) / 2;
-        if(array[mid] == key)
-            return mid;
-        if(array[mid] < key)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return left;
+int findMax(int a, int b) {
+    return a > b ? a : b;
 }
 
 int main() {
@@ -25,13 +14,12 @@ int main() {
     while(cases--) {
         int l, n;
         scanf("%d %d", &l, &n);
-        int pos[n];
-        for(int i = 0; i < n; ++i)
-            scanf("%d", &pos[i]);
-        qsort(pos, n, sizeof(int), compare);
-        int temp = binarySearch(pos, n, l / 2.0);
-        int min = pos[temp - 1] > l - pos[temp] ? pos[temp - 1] : l - pos[temp];
-        int max = l - pos[0] > pos[n - 1] ? l - pos[0] : pos[n - 1];
+        int pos, min = 0, max = 0;
+        for(int i = 0; i < n; ++i) {
+            scanf("%d", &pos);
+            min = findMax(min, findMin(pos, l - pos));
+            max = findMax(max, findMax(pos, l - pos));
+        }
         printf("%d %d\n", min, max);
     }
     return 0;
