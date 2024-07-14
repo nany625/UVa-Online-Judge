1badbc4eaef5ee95736a165ef3fa6c253adbbd22
nany625 <jimmy19980625@gmail.com>
2024-07-14 16:48:55 +0800

    Create solution.java

---
+++ 10642 Can You Solve It?/solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+	public static void main (String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		st.nextToken();
+		int n = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		for(int i = 1; i <= n; ++i) {
+		    Coordinate src = new Coordinate(), dest = new Coordinate();
+		    st.nextToken();
+		    src.x = (long)st.nval;
+		    st.nextToken();
+		    src.y = (long)st.nval;
+		    st.nextToken();
+		    dest.x = (long)st.nval;
+		    st.nextToken();
+		    dest.y = (long)st.nval;
+		    output.append("Case " + i + ": " + ((src.x + src.y + dest.x + dest.y + 1) * (dest.x + dest.y - src.x - src.y) / 2 + dest.x - src.x) + "\n");
+		}
+		System.out.print(output);
+	}
+}
+
+class Coordinate {
+    long x, y;
+}
