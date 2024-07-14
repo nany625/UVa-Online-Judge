51b1c5402bcc36d0f2eb1edee2deb3f0307e9f1c
nany625 <jimmy19980625@gmail.com>
2024-07-14 22:27:42 +0800

    Create solution.java

---
+++ 10714 Ants/solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main (String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		st.nextToken();
+		int cases = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		while(cases-- > 0) {
+		    st.nextToken();
+    		int l = (int)st.nval;
+    		st.nextToken();
+    		int n = (int)st.nval, min = 0, max = 0;
+    		for(int i = 0; i < n; ++i) {
+    		    st.nextToken();
+    		    int pos = (int)st.nval;
+    		    min = Math.max(min, Math.min(pos, l - pos));
+                max = Math.max(max, Math.max(pos, l - pos));
+    		}
+            output.append(min + " " + max + "\n");
+		}
+		System.out.print(output);
+	}
+}
