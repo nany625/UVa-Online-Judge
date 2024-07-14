e42c826870f7128fb88b4b433ae95bf60a677086
nany625 <jimmy19980625@gmail.com>
2024-07-14 18:25:47 +0800

    Update solution.c

--- 10699 Count the factors/solution.c
+++ 10699 Count the factors/solution.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
-void pfCheck(int *n, int factor, int *count) {
-    if(*n % factor == 0) {
-        ++(*count);
-        do {
-            *n /= factor;
-        } while(*n % factor == 0);
+short primes[169] = {2};
+int count = 1;
+
+bool isPrime(short n) {
+    short limit = sqrt(n);
+    for(int i = 0; i < count && primes[i] <= limit; ++i) {
+        if(n % primes[i] == 0)
+            return false;
     }
+    return true;
 }
 
 int main() {
+    for(short i = 3; count < 169; i += 2) {
+        if(isPrime(i))
+            primes[count++] = i;
+    }
     int num;
     while(scanf("%d", &num) && num != 0) {
         printf("%d : ", num);
-        int limit = sqrt(num), factor = 3, pfCount = 0;
-        pfCheck(&num, 2, &pfCount);
-        while(factor <= limit) {
-            pfCheck(&num, factor, &pfCount);
-            factor += 2;
+        short limit = sqrt(num);
+        int pfCount = 0;
+        for(int i = 0; num > 1 && primes[i] <= limit; ++i) {
+            if(num % primes[i] == 0) {
+                ++pfCount;
+                do {
+                    num /= primes[i];
+                } while(num % primes[i] == 0);
+            }
         }
         if(num > 1)
             ++pfCount;
