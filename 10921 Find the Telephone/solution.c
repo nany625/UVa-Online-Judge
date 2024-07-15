b62032424f0a476adeb90221055428a562893b4d
nany625 <jimmy19980625@gmail.com>
2024-07-16 01:07:43 +0800

    Update solution.c

--- 10921 Find the Telephone/solution.c
+++ 10921 Find the Telephone/solution.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
 int main() {
-    char expression[31];
-    while(scanf("%s", expression) == 1) {
-        int len = strlen(expression);
-        for(int i = 0; i < len; ++i) {
-            if(isupper(expression[i])) {
-                if(expression[i] <= 'C')
-                    putchar('2');
-                else if(expression[i] <= 'F')
-                    putchar('3');
-                else if(expression[i] <= 'I')
-                    putchar('4');
-                else if(expression[i] <= 'L')
-                    putchar('5');
-                else if(expression[i] <= 'O')
-                    putchar('6');
-                else if(expression[i] <= 'S')
-                    putchar('7');
-                else if(expression[i] <= 'V')
-                    putchar('8');
-                else
-                    putchar('9');
-            } else
-                putchar(expression[i]);
-        }
-        puts("");
+    char ch;
+    while((ch = getchar()) != EOF) {
+        if(isupper(ch)) {
+            if(ch <= 'C')
+                putchar('2');
+            else if(ch <= 'F')
+                putchar('3');
+            else if(ch <= 'I')
+                putchar('4');
+            else if(ch <= 'L')
+                putchar('5');
+            else if(ch <= 'O')
+                putchar('6');
+            else if(ch <= 'S')
+                putchar('7');
+            else if(ch <= 'V')
+                putchar('8');
+            else
+                putchar('9');
+        } else
+            putchar(ch);
     }
     return 0;
 }
