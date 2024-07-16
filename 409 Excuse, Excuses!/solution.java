1bc86f368dfafb31e4ffaa6b96ef397c8fb411d4
nany625 <jimmy19980625@gmail.com>
2024-07-17 04:44:26 +0800

    Update solution.java

--- 409 Excuse, Excuses!/solution.java
+++ 409 Excuse, Excuses!/solution.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    int set = 0;
         StreamTokenizer st = new StreamTokenizer(br);
+        int set = 0;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
             int K = (int)st.nval;
