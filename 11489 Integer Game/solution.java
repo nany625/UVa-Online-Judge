5ab89285042605416f467a4d516744d0389ea659
nany625 <jimmy19980625@gmail.com>
2024-07-17 04:52:31 +0800

    Update solution.java

--- 11489 Integer Game/solution.java
+++ 11489 Integer Game/solution.java
@@ -3,7 +3,9 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    int T = Integer.parseInt(br.readLine());
+	    StreamTokenizer st = new StreamTokenizer(br);
+        st.nextToken();
+        int T = (int)st.nval;
 	    StringBuilder output = new StringBuilder();
         for(int i = 1; i <= T; ++i) {
 	        String N = br.readLine();
