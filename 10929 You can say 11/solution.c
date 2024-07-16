2dbb936de7ac8a5e71d235f389966a7dbd486a44
nany625 <jimmy19980625@gmail.com>
2024-07-16 16:09:55 +0800

    Create solution.c

---
+++ 10929 You can say 11/solution.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char N[1001];
+	while(scanf("%s", N) && strcmp(N, "0") != 0) {
+	    int sum = 0, len = strlen(N);
+	    for(int i = 0; i < len; ++i) {
+	        if(i % 2 == 0)
+	            sum += N[i] - '0';
+	        else
+	            sum -= N[i] - '0';
+	    }
+	    printf("%s is %sa multiple of 11.\n", N, sum % 11 == 0 ? "" : "not ");
+	}
+	return 0;
+}
