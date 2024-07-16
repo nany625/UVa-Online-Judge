d246b75f20056b320b5c777096300a41b9e30880
nany625 <jimmy19980625@gmail.com>
2024-07-17 05:29:54 +0800

    Create solution.java

---
+++ 11001 Necklace/solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        double V, Vo;
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (V = st.nval) != 0 && st.nextToken() == StreamTokenizer.TT_NUMBER && (Vo = st.nval) != 0) {
+            double discs = V / 2 / Vo;
+    	    if(discs - (int)discs == 0.5)
+    	        output.append("0\n");
+    	    else if(discs - (int)discs < 0.5)
+           		output.append((int)discs + "\n");
+           	else
+           		output.append((int)discs + 1 + "\n");
+        }
+        System.out.print(output);
+	}
+}
