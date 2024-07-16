d2e778dc60aac9564c8b446d08c1834611518755
nany625 <jimmy19980625@gmail.com>
2024-07-17 05:38:37 +0800

    Create solution.java

---
+++ 11044 Searching for Nessy/solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int t = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(t-- > 0) {
+            st.nextToken();
+            int n = (int)st.nval;
+            st.nextToken();
+            output.append((n / 3) * ((int)st.nval / 3) + "\n");
+        }
+        System.out.print(output);
+	}
+}
