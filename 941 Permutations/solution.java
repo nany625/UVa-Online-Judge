c3cdcf15e4611cc5c7d9dae63c20e976a7de8590
nany625 <jimmy19980625@gmail.com>
2024-07-17 01:56:14 +0800

    Update solution.java

--- 941 Permutations/solution.java
+++ 941 Permutations/solution.java
@@ -2,13 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static long[] factorials = {
+        1L, 2L, 6L, 24L, 120L, 720L, 5040L, 40320L, 362880L, 3628800L, 
+        39916800L, 479001600L, 6227020800L, 87178291200L, 1307674368000L, 20922789888000L, 355687428096000L, 6402373705728000L, 121645100408832000L
+    };
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    long[] factorials = {
-            1L, 2L, 6L, 24L, 120L, 720L, 5040L, 40320L, 362880L, 3628800L, 
-            39916800L, 479001600L, 6227020800L, 87178291200L, 1307674368000L, 20922789888000L, 355687428096000L, 6402373705728000L, 121645100408832000L
-        };
-        int samples = Integer.parseInt(br.readLine());
+        StreamTokenizer st = new StreamTokenizer(br);
+        st.nextToken();
+        int samples = (int)st.nval;
         StringBuilder output = new StringBuilder();
     	while(samples-- > 0) {
     	    char[] S = br.readLine().toCharArray();
