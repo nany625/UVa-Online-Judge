9f08f4e066cbdce61c997116759c8078255246cf
nany625 <jimmy19980625@gmail.com>
2024-07-17 02:28:21 +0800

    Update solution.java

--- 10193 All You Need Is Love/solution.java
+++ 10193 All You Need Is Love/solution.java
@@ -3,18 +3,12 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StreamTokenizer st = new StreamTokenizer(br);
+        st.nextToken();
+        int N = (int)st.nval;
         StringBuilder output = new StringBuilder();
-        for(int i = 1; i <= N; ++i) {
-            String S1 = br.readLine(), S2 = br.readLine();
-            int num1 = 0, num2 = 0, len = S1.length();
-            for(int j = 0; j < len; ++j)
-                num1 = 2 * num1 + S1.charAt(j) - '0';
-            len = S2.length();
-            for(int j = 0; j < len; ++j)
-                num2 = 2 * num2 + S2.charAt(j) - '0';
-            output.append("Pair #" + i + ": " + (GCD(num1, num2) > 1 ? "All you need is love" : "Love is not all you need") + "!\n");
-        }
+        for(int i = 1; i <= N; ++i)
+            output.append("Pair #" + i + ": " + (GCD(Integer.parseInt(br.readLine(), 2), Integer.parseInt(br.readLine(), 2)) > 1 ? "All you need is love" : "Love is not all you need") + "!\n");
         System.out.print(output);
 	}
 	
