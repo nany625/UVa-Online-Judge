dda428c5c1cd7aa60776d136d18acc85a3263b0e
nany625 <jimmy19980625@gmail.com>
2024-07-17 17:23:34 +0800

    Update solution.c

--- 11219 How old are you?/solution.c
+++ 11219 How old are you?/solution.c
@@ -8,7 +8,7 @@ int main() {
     int T;
     scanf("%d", &T);
     for(int i = 1; i <= T; ++i) {
-        Date birth, curr;
+        Date curr, birth;
         scanf("%d %*c %d %*c %d %d %*c %d %*c %d", &curr.day, &curr.month, &curr.year, &birth.day, &birth.month, &birth.year);
         if(birth.year * 10000 + birth.month * 100 + birth.day > curr.year * 10000 + curr.month * 100 + curr.day)
             printf("Case #%d: Invalid birth date\n", i);
