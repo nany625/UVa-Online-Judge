7fc4c588625d8e9e6a44b1867dc6863c467f8c65
nany625 <jimmy19980625@gmail.com>
2024-07-17 16:55:49 +0800

    Create solution.java

---
+++ 11192 Group Reverse/solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        int G;
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (G = (int)st.nval) != 0) {
+            String s = br.readLine();
+            int len = s.length();
+            for(int i = 0; i < G; ++i)
+                output.append(new StringBuilder(s.substring(i * len / G, (i + 1) * len / G)).reverse());
+    		output.append("\n");
+        }
+        System.out.print(output);
+	}
+}
