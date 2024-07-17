9a8673086a2657d91283a9437be66b41e79e7de4
nany625 <jimmy19980625@gmail.com>
2024-07-17 17:50:54 +0800

    Create solution.java

---
+++ 11219 How old are you?/solution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        st.nextToken();
+        int T = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        for(int i = 1; i <= T; ++i) {
+            br.readLine();
+            Date curr = new Date(), birth = new Date();
+            String[] tokens = br.readLine().split("/"); 
+            curr.day = Integer.parseInt(tokens[0]);
+            curr.month = Integer.parseInt(tokens[1]);
+            curr.year = Integer.parseInt(tokens[2]);
+            tokens = br.readLine().split("/"); 
+            birth.day = Integer.parseInt(tokens[0]);
+            birth.month = Integer.parseInt(tokens[1]);
+            birth.year = Integer.parseInt(tokens[2]);
+            if(birth.year * 10000 + birth.month * 100 + birth.day > curr.year * 10000 + curr.month * 100 + curr.day)
+                output.append("Case #" + i + ": Invalid birth date\n");
+            else {
+                int age = curr.year - birth.year - (birth.month * 100 + birth.day > curr.month * 100 + curr.day ? 1 : 0);
+                if(age > 130)
+                    output.append("Case #" + i + ": Check birth date\n");
+    			else
+    				output.append("Case #" + i + ": " + age + "\n");
+            }
+        }
+        System.out.print(output);
+	}
+}
+
+class Date {
+    int year, month, day;
+}
