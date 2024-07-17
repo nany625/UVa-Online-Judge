81eae719488d6109231fcd26cf83f3cc3af3da3f
nany625 <jimmy19980625@gmail.com>
2024-07-17 15:56:51 +0800

    Create solution.java

---
+++ 11063 B2-Sequence/solution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        int cases = 0;
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int N = (int)st.nval;
+            short[] b = new short[N];
+            for(int i = 0; i < N; ++i) {
+                st.nextToken();
+                b[i] = (short)st.nval;
+            }
+            boolean isB2 = true;
+            for(int i = 0; i < N - 1 && isB2; ++i) {
+                if(b[i] < 1 || b[i] >= b[i + 1])
+                    isB2 = false;
+            }
+            if(!isB2)
+                output.append("Case #" + (++cases) + ": It is not a B2-Sequence.\n\n");
+            else {
+                boolean[] diff = new boolean[b[N - 1] - b[0]];
+                for(int i = 0; i < N - 1 && isB2; ++i) {
+                    for(int j = i + 1; j < N && isB2; ++j) {
+                        if(!diff[b[j] - b[i] - 1])
+                            diff[b[j] - b[i] - 1] = true;
+                        else
+                            isB2 = false;
+                    }
+                }
+                output.append("Case #" + (++cases) + ": It is " + (isB2 ? "" : "not ") + "a B2-Sequence.\n\n");
+            }
+        }
+        System.out.print(output);
+	}
+}
