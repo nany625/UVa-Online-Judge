9f86624210617a4b14c136ddf024eac4864626f8
nany625 <jimmy19980625@gmail.com>
2024-07-18 04:12:03 +0800

    Create solution.java

---
+++ 11313 Gourmet Games/solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int t = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(t-- > 0) {
+            st.nextToken();
+            int n = (int)st.nval;
+            st.nextToken();
+            int m = (int)st.nval;
+            if((n - m) % (m - 1) == 0)
+                output.append((n - m) / (m - 1) + 1 + "\n");
+            else
+                output.append("cannot do this\n");
+        }
+        System.out.print(output);
+	}
+}
