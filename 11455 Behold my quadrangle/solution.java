f89d8a0391cb34687d48c14ced9867802823b06f
nany625 <jimmy19980625@gmail.com>
2024-07-18 18:15:19 +0800

    Create solution.java

---
+++ 11455 Behold my quadrangle/solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        int[] sides = new int[4];
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int cases = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(cases-- > 0) {
+            for(int i = 0; i < 4; ++i) {
+                st.nextToken();
+    			sides[i] = (int)st.nval;
+            }
+    		Arrays.sort(sides);
+    		if(sides[0] == sides[1] && sides[1] == sides[2] && sides[2] == sides[3])
+    			output.append("square\n");
+    		else if(sides[0] == sides[1] && sides[2] == sides[3])
+    			output.append("rectangle\n");
+    		else if(sides[0] + sides[1] + sides[2] > sides[3])
+    			output.append("quadrangle\n");
+    		else
+    			output.append("banana\n");
+        }
+        System.out.print(output);
+	}
+}
