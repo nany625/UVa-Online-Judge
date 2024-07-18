5a8ee84a0837e6fed40961061d862ee5ddcd204d
nany625 <jimmy19980625@gmail.com>
2024-07-19 04:09:22 +0800

    Create solution.java

---
+++ 11677 Alarm Clock/solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int H1 = (int)st.nval;
+            st.nextToken();
+            int M1 = (int)st.nval;
+            st.nextToken();
+            int H2 = (int)st.nval;
+            st.nextToken();
+            int M2 = (int)st.nval;
+            if(H1 == 0 && M1 == 0 && H2 == 0 && M2 == 0)
+                break;
+            int time1 = 60 * H1 + M1, time2 = 60 * H2 + M2;
+            output.append((time1 <= time2 ? time2 - time1 : time2 - time1 + 1440) + "\n");
+        }
+        System.out.print(output);
+	}
+}
