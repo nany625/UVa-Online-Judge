f672a911b8cecfaa0cbbf52d65dc331e2fd3a8a8
nany625 <jimmy19980625@gmail.com>
2024-07-18 15:51:20 +0800

    Update solution.c

--- 11349 Symmetric Matrix/solution.c
+++ 11349 Symmetric Matrix/solution.c
@@ -7,19 +7,16 @@ int main() {
     for(int i = 1; i <= T; ++i) {
         int n;
         scanf("%*s %*s %d", &n);
-        long M[n][n];
+        int size = n * n;
+        long M[n * n];
         bool isSymmetric = true;
-        for(int j = 0; j < n; ++j) {
-            for(int k = 0; k < n; ++k) {
-                scanf("%ld", &M[j][k]);
-                if(M[j][k] < 0)
-                    isSymmetric = false;
-            }
+        for(int j = 0; j < size; ++j) {
+            scanf("%ld", &M[j]);
+            if(M[j] < 0)
+                isSymmetric = false;
         }
-        for(int j = 0; j < (n + 1) / 2 && isSymmetric; ++j) {
-            for(int k = 0; k < n && isSymmetric; ++k)
-                isSymmetric = M[j][k] == M[n - 1 - j][n - 1 - k];
-        }
+        for(int j = 0; j < size / 2 && isSymmetric; ++j)
+            isSymmetric = M[j] == M[size - 1 - j];
         printf("Test #%d: %s.\n", i, isSymmetric ? "Symmetric" : "Non-symmetric");
     }
     return 0;
