bd9b4200232ce1832b53926e966eab87ce17686d
nany625 <jimmy19980625@gmail.com>
2024-07-19 06:23:57 +0800

    Update solution.c

--- 11728 Alternate Task/solution.c
+++ 11728 Alternate Task/solution.c
@@ -1,9 +1,37 @@
+// #解法一
+#include <stdio.h>
+#include <math.h>
+#define MAX_NUM 1000
+
+short factorSum(int n) {
+    short result = 1 + n, limit = sqrt(n);
+    for(short i = 2; i <= limit; ++i) {
+        if(n % i == 0)
+            result += i + n / i;
+    }
+    if(sqrt(n) == limit)
+        result -= limit;
+    return result;
+}
+
+int main() {
+    short table[MAX_NUM] = {1};
+    for(int i = 2; i <= MAX_NUM; ++i) {
+        short temp = factorSum(i);
+        if(temp <= MAX_NUM)
+            table[temp - 1] = i;
+    }
+	int cases = 0, S;
+	while(scanf("%d", &S) && S != 0)
+	    printf("Case %d: %hd\n", ++cases, table[S - 1] != 0 ? table[S - 1] : -1);
+	return 0;
+}
+
+// #解法二
 #include <stdio.h>
 #include <math.h>
 
 int factorSum(int n) {
-    if(n == 1)
-        return 1;
     int result = 1 + n, limit = sqrt(n);
     for(int i = 2; i <= limit; ++i) {
         if(n % i == 0)
@@ -17,7 +45,11 @@ int factorSum(int n) {
 int main() {
 	int cases = 0, S;
 	while(scanf("%d", &S) && S != 0) {
-	    int ans = S, limit = ceil(sqrt(1 + 8 * S) - 1) / 2;
+	    if(S == 1) {
+	        printf("Case %d: 1\n", ++cases);
+	        continue;
+	    }
+	    int ans = S - 1, limit = ceil(sqrt(1 + 8 * S) - 1) / 2;
 	    while(ans >= limit && factorSum(ans) != S)
 	        --ans;
 	    printf("Case %d: %d\n", ++cases, ans >= limit ? ans : -1);
