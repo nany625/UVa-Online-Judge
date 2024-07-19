908569cd9645a677649dc7f0b2f7cef5dbf8e25c
nany625 <jimmy19980625@gmail.com>
2024-07-20 07:25:42 +0800

    Update solution.c

--- 12820 Cool Word/solution.c
+++ 12820 Cool Word/solution.c
@@ -10,18 +10,25 @@ int compare(const void *a, const void *b) {
 int main() {
     int cases = 0, n;
 	while(scanf("%d", &n) == 1) {
-	    char word[12];
+	    char word[31];
 	    int count = 0;
         while(n--) {
             scanf("%s", word);
             int frequency[26] = {}, len = strlen(word);
             for(int i = 0; i < len; ++i)
                 ++frequency[word[i] - 'a'];
-            qsort(frequency, 26, sizeof(int), compare);
-            bool isCool = frequency[1] > 0;
-            for(int i = 0; i < 26 && frequency[i] > 0 && isCool; ++i)
-                isCool = frequency[i] > frequency[i + 1];
-            count += isCool;
+            bool ans[26] = {}, isCool = true;
+            int found = 0, diff = 0;
+            for(int i = 0; i < 26 && found < len && isCool; ++i) {
+                if(frequency[i] > 0) {
+                    if(!ans[frequency[i] - 1]) {
+                        diff += ans[frequency[i] - 1] = true;
+                        found += frequency[i];
+                    } else
+                        isCool = false;
+                }
+            }
+            count += (isCool && diff > 1);
         }
         printf("Case %d: %d\n", ++cases, count);
     }
