c18320060907edc0f3e002ae5cbc0d6d407953f8
nany625 <jimmy19980625@gmail.com>
2024-07-19 16:53:04 +0800

    Update solution.c

--- 11743 Credit Check/solution.c
+++ 11743 Credit Check/solution.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+int tokenSum(char *number, int first, int last) {
+	int result = 0;
+    for(int i = first; i < last; ++i) {
+        int digit = number[i] - '0';
+	    if(i % 5 % 4 == 0 || i % 5 % 4 == 2)
+	        result += (2 * digit) / 10 + (2 * digit) % 10;
+	    else
+	        result += digit;
+    }
+    return result;
+}
+	
 int main() {
 	int N;
 	scanf("%d", &N);
@@ -7,17 +19,7 @@ int main() {
 	char number[21];
 	while(N--) {
 	    fgets(number, sizeof(number), stdin);
-	    int sum = 0;
-    	for(int i = 0; i < 19; ++i) {
-    	    if((i - 4) % 5 == 0)
-    	        continue;
-            int digit = number[i] - '0';
-	        if(i % 5 % 4 == 0 || i % 5 % 4 == 2)
-	            sum += (2 * digit) / 10 + (2 * digit) % 10;
-	        else
-	            sum += digit;
-	    }
-	    puts(sum % 10 == 0 ? "Valid" : "Invalid");
+	    puts((tokenSum(number, 0, 4) + tokenSum(number, 5, 9) + tokenSum(number, 10, 14) + tokenSum(number, 15, 19)) % 10 == 0 ? "Valid" : "Invalid");
 	}
 	return 0;
 }
