030ecfa6fcce408c858c4bd3b59f91ba8569c372
nany625 <jimmy19980625@gmail.com>
2024-07-19 16:49:24 +0800

    Update solution.java

--- 11743 Credit Check/solution.java
+++ 11743 Credit Check/solution.java
@@ -9,18 +9,20 @@ public class Main {
         StringBuilder output = new StringBuilder();
         while(N-- > 0) {
             String number = br.readLine();
-            int sum = 0;
-            for(int i = 0; i < 19; ++i) {
-        	    if((i - 4) % 5 == 0)
-        	        continue;
-                int digit = number.charAt(i) - '0';
-    	        if(i % 5 % 4 == 0 || i % 5 % 4 == 2)
-    	            sum += (2 * digit) / 10 + (2 * digit) % 10;
-    	        else
-    	            sum += digit;
-    	    }
-    	    output.append(sum % 10 == 0 ? "Valid\n" : "Invalid\n");
+    	    output.append((tokenSum(number, 0, 4) + tokenSum(number, 5, 9) + tokenSum(number, 10, 14) + tokenSum(number, 15, 19)) % 10 == 0 ? "Valid\n" : "Invalid\n");
         }
         System.out.print(output);
+	}
+	
+	static int tokenSum(String number, int first, int last) {
+	    int result = 0;
+	    for(int i = first; i < last; ++i) {
+            int digit = number.charAt(i) - '0';
+    	    if(i % 5 % 4 == 0 || i % 5 % 4 == 2)
+    	        result += (2 * digit) / 10 + (2 * digit) % 10;
+    	    else
+    	        result += digit;
+        }
+        return result;
 	}
 }
