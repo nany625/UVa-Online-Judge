960ace74f1385db82919dc95015cc6ab6c35afd8
nany625 <jimmy19980625@gmail.com>
2024-07-20 04:37:16 +0800

    Update solution.c

--- 10409 Die Game/solution.c
+++ 10409 Die Game/solution.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
+    char command[6];
     int n;
     while(scanf("%d", &n) && n != 0) {
-        char command[6], top = '1', north = '2', west = '3', east = '4', south = '5', bottom = '6';
+        char top = '1', north = '2', west = '3', east = '4', south = '5', bottom = '6';
         while(n--) {
             scanf("%s", command);
             char temp = top;
-            if(strcmp(command, "north") == 0) {
-                top = south;
-                south = bottom;
-                bottom = north;
-                north = temp;
-            } else if(strcmp(command, "east") == 0) {
-                top = west;
-                west = bottom;
-                bottom = east;
-                east = temp;
-            } else if(strcmp(command, "south") == 0) {
-                top = north;
-                north = bottom;
-                bottom = south;
-                south = temp;
-            } else if(strcmp(command, "west") == 0) {
-                top = east;
-                east = bottom;
-                bottom = west;
-                west = temp;
+            switch(command[0]) {
+                case 'n':
+                    top = south;
+                    south = bottom;
+                    bottom = north;
+                    north = temp;
+                    break;
+                case 'e':
+                    top = west;
+                    west = bottom;
+                    bottom = east;
+                    east = temp;
+                    break;
+                case 's':
+                    top = north;
+                    north = bottom;
+                    bottom = south;
+                    south = temp;
+                    break;
+                case 'w':
+                    top = east;
+                    east = bottom;
+                    bottom = west;
+                    west = temp;
             }
         }
         printf("%c\n", top);
