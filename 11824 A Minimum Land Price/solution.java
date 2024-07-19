e985bb41d5924e11a0ce2bba5d4c1384dafd92a2
nany625 <jimmy19980625@gmail.com>
2024-07-19 18:44:59 +0800

    Create solution.java

---
+++ 11824 A Minimum Land Price/solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int T = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(T-- > 0) {
+            ArrayList<Integer> cost = new ArrayList<>();
+            int L;
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER && (L = (int)st.nval) != 0)
+                cost.add(L);
+            cost.sort(Comparator.reverseOrder());
+            int total = 0;
+        	for(int i = 0; i < cost.size() && total <= 5000000; ++i)
+        		total += 2 * Math.pow(cost.get(i), i + 1);
+            output.append((total <= 5000000 ? total : "Too expensive") + "\n");
+        }
+        System.out.print(output);
+	}
+}
