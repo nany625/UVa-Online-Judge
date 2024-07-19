f24dfd65f162549f69fcef6fb22fd6d6d3b917de
nany625 <jimmy19980625@gmail.com>
2024-07-20 03:26:23 +0800

    Update solution.c

--- 12019 Doom's Day Algorithm/solution.c
+++ 12019 Doom's Day Algorithm/solution.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
+    int Mondays[] = {3, 0, 0, 4, 2, 6, 4, 1, 5, 3, 0, 5};
+	char str[][10] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
     int cases;
 	scanf("%d", &cases);
-	int Mondays[] = {3, 0, 0, 4, 2, 6, 4, 1, 5, 3, 0, 5};
-	char str[][10] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 	while(cases--) {
 		int M, D;
 		scanf("%d %d", &M, &D);
