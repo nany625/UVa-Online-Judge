72240dedcf0d76b08449ef749c715c80979899f7
nany625 <jimmy19980625@gmail.com>
2024-07-21 03:16:55 +0800

    Create solution.java

---
+++ 846 Steps/solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		st.nextToken();
+		int n = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		while(n-- > 0) {
+		    st.nextToken();
+		    int x = (int)st.nval;
+		    st.nextToken();
+		    int y = (int)st.nval;
+		    if(x == y)
+		        output.append("0\n");
+		    else {
+		        int maxLen = (int)Math.sqrt(y - x);
+		        if(maxLen == Math.sqrt(y - x))
+    				output.append(2 * maxLen - 1 + "\n");
+    			else if(maxLen < Math.sqrt(y - x - maxLen))
+    				output.append(2 * maxLen + 1 + "\n");
+    			else
+    				output.append(2 * maxLen + "\n");
+		    }
+		}
+		System.out.print(output);
+	}
+}
