da2846be51c80597866ad05e042f9fc962c15195
nany625 <jimmy19980625@gmail.com>
2024-07-21 04:59:54 +0800

    Create solution.c

---
+++ 630 Anagrams (II)/solution.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct {
+    char word[20], sortedWord[20];
+} Anagram;
+
+int compare(const void *a, const void *b) {
+    return *(char*)a - *(char*)b;
+}
+
+int main() {
+	int T;
+	scanf("%d", &T);
+	bool firstCase = true;
+	while(T--) {
+	    if(!firstCase)
+	        puts("");
+	    int N;
+	    scanf("%d", &N);
+	    Anagram anagrams[N];
+	    for(int i = 0; i < N; ++i) {
+	        scanf("%s", anagrams[i].word);
+	        strcpy(anagrams[i].sortedWord, anagrams[i].word);
+	        int len = strlen(anagrams[i].sortedWord);
+	        qsort(anagrams[i].sortedWord, len, sizeof(char), compare);
+	    }
+	    char test[20], sortedTest[20];
+	    while(scanf("%s", test) && strcmp(test, "END") != 0) {
+	        printf("Anagrams for: %s\n", test);
+	        strcpy(sortedTest, test);
+	        int len = strlen(sortedTest);
+	        qsort(sortedTest, len, sizeof(char), compare);
+	        int count = 0;
+	        for(int i = 0; i < N; ++i) {
+	            if(strcmp(sortedTest, anagrams[i].sortedWord) == 0)
+	                printf("  %d) %s\n", ++count, anagrams[i].word);
+	        }
+	        if(count == 0)
+	            printf("No anagrams for: %s\n", test);
+	    }
+	    firstCase = false;
+	}
+	return 0;
+}
