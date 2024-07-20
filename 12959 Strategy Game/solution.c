c4f5c21281303e8b1a94065d1c73f3f5528e5d06
nany625 <jimmy19980625@gmail.com>
2024-07-20 18:35:53 +0800

    Update solution.c

--- 12959 Strategy Game/solution.c
+++ 12959 Strategy Game/solution.c
@@ -1,3 +1,4 @@
+// #解法一
 #include <stdio.h>
 
 int main() {
@@ -21,5 +22,40 @@ int main() {
 		}
 		printf("%d\n", winner);
 	}
+	return 0;
+}
+
+// #解法二
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    char *buffer = NULL;
+	size_t bufsize = 0;
+	int J, R;
+	while(scanf("%d %d", &J, &R) == 2 && J != 0 && R != 0) {
+		getchar();
+		int points[J];
+		for(int i = 0; i < J; ++i)
+			points[i] = 0;
+		getline(&buffer, &bufsize, stdin);
+		char *token = strtok(buffer, " ");
+		int max = 0, winner, point;
+		while(R--) {
+			for(int j = 0; j < J; ++j) {
+				points[j] += atoi(token);
+				if(R == 0) {
+					if(max <= points[j]) {
+						max = points[j];
+						winner = j + 1;
+					}
+				}
+				token = strtok(NULL, " ");
+			}
+		}
+		printf("%d\n", winner);
+	}
+	free(buffer);
 	return 0;
 }
