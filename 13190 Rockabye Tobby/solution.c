747bc5d2cac4f827a001b353c10c98a469dacfbb
nany625 <jimmy19980625@gmail.com>
2024-07-21 02:13:14 +0800

    Create solution.c

---
+++ 13190 Rockabye Tobby/solution.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char *name;
+    int frequency, next;
+} Medicine;
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while(T--) {
+        int n, k;
+        scanf("%d %d", &n, &k);
+        Medicine medicines[n];
+        for(int i = 0; i < n; ++i) {
+            medicines[i].name = NULL;
+            scanf("%ms %d", &medicines[i].name, &medicines[i].frequency);
+            medicines[i].next = medicines[i].frequency;
+        }
+        while(k) {
+            int min = medicines[0].next;
+            for(int i = 1; i < n; ++i)
+                min = min < medicines[i].next ? min : medicines[i].next;
+            for(int i = 0; i < n && k; ++i) {
+				if(min == medicines[i].next){
+					printf("%d %s\n", medicines[i].next, medicines[i].name);
+					medicines[i].next += medicines[i].frequency;
+					--k;
+				}
+			}
+        }
+        for(int i = 0; i < n; ++i)
+            free(medicines[i].name);
+    }
+	return 0;
+}
