588a209a1ffe3f6788486ac3aa32c616f0f40e7b
nany625 <jimmy19980625@gmail.com>
2024-07-20 08:05:22 +0800

    Create solution.java

---
+++ 12908 The book thief/solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		int s;
+		StringBuilder output = new StringBuilder();
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (s = (int)st.nval) != 0) {
+		    int pages = (-1 + (int)Math.sqrt(1 + 8 * s)) / 2 + 1;
+            output.append((pages + 1) * pages / 2 - s + " " + pages + "\n");
+		}
+		System.out.print(output);
+	}
+}
