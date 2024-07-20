03e41aae8731caec907aec1768176acd6731183d
nany625 <jimmy19980625@gmail.com>
2024-07-20 18:16:42 +0800

    Create solution.java

---
+++ 12959 Strategy Game/solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		StringBuilder output = new StringBuilder();
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+		    int J = (int)st.nval;
+		    st.nextToken();
+		    int R = (int)st.nval, max = 0, winner = 0;
+		    int[] points = new int[J];
+		    while(R-- > 0) {
+		        for(int i = 0; i < J; ++i) {
+		            st.nextToken();
+		            points[i] += (int)st.nval;
+		            if(R == 0) {
+		                if(max <= points[i]) {
+		                    max = points[i];
+		                    winner = i + 1;
+		                }
+		            }
+		        }
+		    }
+            output.append(winner + "\n");
+		}
+		System.out.print(output);
+	}
+}
