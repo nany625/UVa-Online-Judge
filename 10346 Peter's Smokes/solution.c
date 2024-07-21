f9825f4fbf028fb97bfa6a7a59515834eedf2112
nany625 <jimmy19980625@gmail.com>
2024-07-22 06:11:07 +0800

    Update solution.c

--- 10346 Peter's Smokes/solution.c
+++ 10346 Peter's Smokes/solution.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
     int n, k;
-    while(scanf("%d %d", &n, &k) == 2) {
-        int cigarettes = n;
-        while(n / k) {
-            cigarettes += n / k;
-            n = n / k + n % k;
-        }
-        printf("%d\n", cigarettes);
-    }
+    while(scanf("%d %d", &n, &k) == 2)
+        printf("%d\n", n + (int)floor((n - k) / (k - 1.0)) + 1);
     return 0;
 }
