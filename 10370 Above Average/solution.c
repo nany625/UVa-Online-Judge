292841b1de0912f5242bbd7f76ca2045842420d2
nany625 <jimmy19980625@gmail.com>
2024-07-22 06:18:25 +0800

    Update solution.c

--- 10370 Above Average/solution.c
+++ 10370 Above Average/solution.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
     int C;
     scanf("%d", &C);
+    char *buffer = NULL;
+    size_t bufsize = 0;
     while(C--) {
         int N;
         scanf("%d", &N);
+        getline(&buffer, &bufsize, stdin);
+        char *token = strtok(buffer, " ");
         int grades[N], sum = 0;
         for(int i = 0; i < N; ++i) {
-            scanf("%d", &grades[i]);
-            sum += grades[i];
+            sum += grades[i] = atoi(token);
+            token = strtok(NULL, " ");
         }
         int count = 0;
         for(int i = 0; i < N; ++i)
             count += grades[i] * N > sum;
         printf("%.3f%%\n", 100.0 * count / N);
     }
+    free(buffer);
     return 0;
 }
