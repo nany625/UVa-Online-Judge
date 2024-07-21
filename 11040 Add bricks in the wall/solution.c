4b8ca232d41e1f1f9d4feaaa4f48c28e768195b8
nany625 <jimmy19980625@gmail.com>
2024-07-22 00:02:34 +0800

    Create solution.c

---
+++ 11040 Add bricks in the wall/solution.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+	int brick[9][9], N;
+	scanf("%d", &N);
+	while(N--) {
+	    for(int i = 0; i < 9; i += 2) {
+	        for(int j = 0; j <= i; j += 2)
+	            scanf("%d", &brick[i][j]);
+	    }
+	    for(int i = 2; i < 9; i += 2) {
+	        for(int j = 1; j < i; j += 2)
+	            brick[i][j] = (brick[i - 2][j - 1] - brick[i][j - 1] - brick[i][j + 1]) / 2;
+	    }
+	    for(int i = 0; i < 9; ++i) {
+	        for(int j = 0; j <= i; ++j) {
+	            if(j > 0)
+	                putchar(' ');
+	            printf("%d", i % 2 == 0 ? brick[i][j] : brick[i + 1][j] + brick[i + 1][j + 1]);
+	        }
+	        puts("");
+	    }
+	}
+	return 0;
+}
