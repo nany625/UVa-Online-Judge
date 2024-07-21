1766534bbdb75a00fefabba89e3e09fd1af8be68
nany625 <jimmy19980625@gmail.com>
2024-07-22 04:25:19 +0800

    Update solution.java

--- 10106 Product/solution.java
+++ 10106 Product/solution.java
@@ -6,9 +6,8 @@ public class Main {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String X;
         StringBuilder output = new StringBuilder();
-        while((X = br.readLine()) != null) {
+        while((X = br.readLine()) != null)
             output.append(new BigInteger(X).multiply(new BigInteger(new String(br.readLine()))) + "\n");
-        }
         System.out.print(output);
 	}
 }
