8c57de6c9e4d165c19072cf1424857a5b332e44b
nany625 <jimmy19980625@gmail.com>
2024-07-22 06:42:08 +0800

    Update solution.c

--- 10530 Guessing Game/solution.c
+++ 10530 Guessing Game/solution.c
@@ -7,17 +7,20 @@ int main() {
 	while(scanf("%d", &guess) && guess != 0) {
 	    getchar();
 	    fgets(resp, sizeof(resp), stdin);
-	    if(strcmp(resp, "too high\n") == 0)
-	        max = max < (guess - 1) ? max : guess - 1;
-	    else if(strcmp(resp, "too low\n") == 0)
-	        min = min > (guess + 1) ? min : guess + 1;
-	    else if(strcmp(resp, "right on\n") == 0) {
-	        if(guess >= min && guess <= max)
-	            puts("Stan may be honest");
-	        else
-	            puts("Stan is dishonest");
-	        min = 1;
-	        max = 10;
+	    switch(resp[4]) {
+	        case 'h':
+	            max = max < (guess - 1) ? max : guess - 1;
+	            break;
+	        case 'l':
+	            min = min > (guess + 1) ? min : guess + 1;
+	            break;
+	        case 't':
+	            if(guess >= min && guess <= max)
+    	            puts("Stan may be honest");
+    	        else
+    	            puts("Stan is dishonest");
+    	        min = 1;
+    	        max = 10;
 	    }
 	}
 	return 0;
