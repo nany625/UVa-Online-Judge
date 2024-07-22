e9e7122e3df44a3421567ae07441788212ba6f5e
nany625 <jimmy19980625@gmail.com>
2024-07-22 08:11:28 +0800

    Update solution.c

--- 11220 Decoding the message./solution.c
+++ 11220 Decoding the message./solution.c
@@ -8,12 +8,12 @@ int main() {
     scanf("%d", &T);
     getchar();
     getchar();
+    char *buffer = NULL;
+    size_t bufsize = 0;
     for(int i = 1; i <= T; ++i) {
         if(i > 1)
             puts("");
         printf("Case #%d:\n", i);
-        char *buffer = NULL;
-        size_t bufsize = 0;
         while(getline(&buffer, &bufsize, stdin) != -1 && strcmp(buffer, "\n") != 0) {
             char *token = strtok(buffer, " ");
             int count = 0;
@@ -24,7 +24,7 @@ int main() {
             }
             puts("");
         }
-        free(buffer);
     }
+    free(buffer);
 	return 0;
 }
