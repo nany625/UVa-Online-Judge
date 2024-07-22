4ebad9aa53a4128a474c894a0f3cf6327c3721fb
nany625 <jimmy19980625@gmail.com>
2024-07-23 06:59:18 +0800

    Update solution.c

--- 686 Goldbach’s Conjecture (II)/solution.c
+++ 686 Goldbach’s Conjecture (II)/solution.c
@@ -3,8 +3,7 @@
 #include <stdbool.h>
 #define MAX_SIZE 3512
 
-bool map[32768] = {false, false, true};
-short primes[MAX_SIZE] = {2};
+short primes[MAX_SIZE] = {2}, pairs[16382] = {1};
 int count = 1;
 
 bool isPrime(short n) {
@@ -18,20 +17,18 @@ bool isPrime(short n) {
 
 int main() {
     for(short i = 3; count < MAX_SIZE; i += 2) {
-        if(isPrime(i)) {
-            map[i] = true;
+        if(isPrime(i))
             primes[count++] = i;
+    }
+    for(int i = 1; i < MAX_SIZE; ++i) {
+        for(int j = i; j < MAX_SIZE; ++j) {
+            int sum = primes[i] + primes[j];
+            if(sum < 32768)
+                ++pairs[sum / 2 - 2];
         }
     }
 	int n;
-	while(scanf("%d", &n) && n != 0) {
-		count = map[n - 2];
-		int limit = n / 2;
-		for(int i = 1; primes[i] <= limit; ++i) {
-			if(map[n - primes[i]])
-				++count;
-		}
-		printf("%d\n", count);
-	}
+	while(scanf("%d", &n) && n != 0)
+		printf("%d\n", pairs[n / 2 - 2]);
 	return 0;
 }
