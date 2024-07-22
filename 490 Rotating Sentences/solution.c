6870efcae2c9def75b43af2e879ddadb73e539c9
nany625 <jimmy19980625@gmail.com>
2024-07-23 03:57:15 +0800

    Update solution.c

--- 490 Rotating Sentences/solution.c
+++ 490 Rotating Sentences/solution.c
@@ -15,7 +15,7 @@ int main() {
 	for(int i = 0; i < maxLen; ++i) {
 	    for(int j = size - 1; j >= 0; --j)
 	        putchar(i < strlen(sentences[j]) ? sentences[j][i] : ' ');
-	    puts("");
+	    putchar('\n');
 	}
 	for(int i = 0; i < size; ++i)
 	    free(sentences[i]);
