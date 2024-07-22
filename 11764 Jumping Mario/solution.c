001385acf6c2090ed9ac14b99e4de8c8fa5727c5
nany625 <jimmy19980625@gmail.com>
2024-07-22 09:47:37 +0800

    Update solution.c

--- 11764 Jumping Mario/solution.c
+++ 11764 Jumping Mario/solution.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
     int T;
     scanf("%d", &T);
+    char *buffer = NULL;
+    size_t bufsize = 0;
     for(int i = 1; i <= T; ++i) {
         int N;
         scanf("%d", &N);
-        int prev;
-        scanf("%d", &prev);
-        --N;
-        int curr, highJumps = 0, lowJumps = 0;
-        while(N--) {
-            scanf("%d", &curr);
-            if(curr > prev)
+        getchar();
+        getline(&buffer, &bufsize, stdin);
+        int highJumps = 0, lowJumps = 0, len = strlen(buffer);
+        for(int j = 2; j < len; j += 2) {
+            if(buffer[j] > buffer[j - 2])
                 ++highJumps;
-            else if(curr < prev)
+            else if(buffer[j] < buffer[j - 2])
                 ++lowJumps;
-            prev = curr;;
         }
         printf("Case %d: %d %d\n", i, highJumps, lowJumps);
     }
+    free(buffer);
     return 0;
 }
