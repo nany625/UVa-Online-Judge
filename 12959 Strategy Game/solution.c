b3b8ec216eec277513791aa80b9ba45a4264e88a
nany625 <jimmy19980625@gmail.com>
2024-07-22 10:59:35 +0800

    Update solution.c

--- 12959 Strategy Game/solution.c
+++ 12959 Strategy Game/solution.c
@@ -1,39 +1,11 @@
-// #解法一
-#include <stdio.h>
-
-int main() {
-	int J, R;
-	while(scanf("%d %d", &J, &R) == 2) {
-		int points[J];
-		for(int i = 0; i < J; ++i)
-			points[i] = 0;
-		int max = 0, winner, point;
-		while(R--) {
-			for(int i = 0; i < J; ++i) {
-				scanf("%d", &point);
-				points[i] += point;
-				if(R == 0) {
-					if(max <= points[i]) {
-						max = points[i];
-						winner = i + 1;
-					}
-				}
-			}
-		}
-		printf("%d\n", winner);
-	}
-	return 0;
-}
-
-// #解法二
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 int main() {
+	int J, R;
     char *buffer = NULL;
 	size_t bufsize = 0;
-	int J, R;
 	while(scanf("%d %d", &J, &R) == 2) {
 		getchar();
 		int points[J];
