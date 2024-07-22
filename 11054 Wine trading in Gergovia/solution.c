c46ed20344da741900abebb067544bc729b7fdd7
nany625 <jimmy19980625@gmail.com>
2024-07-22 08:03:03 +0800

    Update solution.c

--- 11054 Wine trading in Gergovia/solution.c
+++ 11054 Wine trading in Gergovia/solution.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
 	int n;
+	char *buffer = NULL;
+	size_t bufsize = 0;
 	while(scanf("%d", &n) && n != 0) {
+	    getchar();
+	    getline(&buffer, &bufsize, stdin);
+	    char *token = strtok(buffer, " ");
 	    long work = 0;
 	    int curr = 0;
-	    while(n--) {
-	        short a;
-	        scanf("%hd", &a);
-	        curr += a;
+	    while(token) {
+	        curr += atoi(token);
 	        work += abs(curr);
+	        token = strtok(NULL, " ");
 	    }
 	    printf("%ld\n", work);
 	}
+	free(buffer);
 	return 0;
 }
