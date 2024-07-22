a5f526bd1e06dcb508731c361b5abdd7982cc822
nany625 <jimmy19980625@gmail.com>
2024-07-22 10:27:52 +0800

    Update solution.c

--- 1260 Sales/solution.c
+++ 1260 Sales/solution.c
@@ -45,50 +45,6 @@ int main() {
 // #解法二
 #include <stdio.h>
 
-int binarySearch(short *array, int size, short key) {
-    int left = 0, right = size - 1;
-    while(left <= right) {
-        int mid = left + (right - left) / 2;
-        if(array[mid] == key) {
-            while(mid < size && array[mid] == array[mid + 1])
-                ++mid;
-            return mid + 1;
-        }
-        if(array[mid] < key)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return left;
-}
-
-int main() {
-	int T;
-	scanf("%d", &T);
-	while(T--) {
-		int n;
-		scanf("%d", &n);
-		short A[n - 1];
-		int sum = 0;
-		for(int i = 0; i < n - 1; ++i) {
-		    short temp;
-		    scanf("%hd", &temp);
-		    int pos = binarySearch(A, i, temp);
-		    sum += pos;
-		    for(int j = i; j > pos; --j)
-		        A[j] = A[j - 1];
-		    A[pos] = temp;
-		}
-		int temp;
-		scanf("%d", &temp);
-		printf("%d\n", sum + binarySearch(A, n - 1, temp));
-	}
-	return 0;
-}
-
-// #解法三
-#include <stdio.h>
-
 int main() {
 	int T;
 	scanf("%d", &T);
