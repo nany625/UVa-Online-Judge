2c3d3f7426b48dc4f0df68dd3aa54d11fa3c5840
nany625 <jimmy19980625@gmail.com>
2024-07-23 07:10:01 +0800

    Update solution.java

--- 686 Goldbach’s Conjecture (II)/solution.java
+++ 686 Goldbach’s Conjecture (II)/solution.java
@@ -1,30 +1,27 @@
 import java.io.*;
 
 public class Main {
-    static boolean[] map = new boolean[32768];
-    static short[] primes = new short[3512];
+    static short[] primes = new short[3512], pairs = new short[16382];
     static int count = 1;
 	public static void main(String[] args) throws IOException {
-        map[2] = true;
     	primes[0] = 2;
     	for(short i = 3; count < primes.length; i += 2) {
-            if(isPrime(i)) {
-                map[i] = true;
+            if(isPrime(i))
                 primes[count++] = i;
+        }
+        pairs[0] = 1;
+        for(int i = 1; i < 3512; ++i) {
+            for(int j = i; j < 3512; ++j) {
+                int sum = primes[i] + primes[j];
+                if(sum < 32768)
+                    ++pairs[sum / 2 - 2];
             }
         }
         StreamTokenizer st = new StreamTokenizer(System.in);
         int n;
         StringBuilder output = new StringBuilder();
-        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0) {
-    	    count = map[n - 2] ? 1 : 0;
-    		int limit = n / 2;
-    		for(int i = 1; primes[i] <= limit; ++i) {
-    			if(map[n - primes[i]])
-    				++count;
-    		}
-    		output.append(count + "\n");
-        }
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0)
+    		output.append(pairs[n / 2 - 2] + "\n");
 	    System.out.print(output);
 	}
 	
