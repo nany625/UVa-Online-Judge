6894e94a8e3974bfdedc434ba84c3216c73db54d
nany625 <jimmy19980625@gmail.com>
2024-07-23 08:23:50 +0800

    Update solution.c

--- 10191 Longest Nap/solution.c
+++ 10191 Longest Nap/solution.c
@@ -13,14 +13,14 @@ int compare(const void *a, const void *b) {
 
 int main() {
 	int day = 0, s;
+    char *buffer = NULL;
+	size_t bufsize = 0;
 	while(scanf("%d", &s) == 1) {
 	    Appointment appointments[s + 2];
 	    appointments[0].time1 = 0;
 	    appointments[0].time2 = 600;
 	    appointments[s + 1].time1 = 1080;
 	    int hh, mm;
-	    char *buffer = NULL;
-	    size_t bufsize = 0;
 	    for(int i = 1; i < s + 1; ++i) {
 	        scanf("%d %*c %d", &hh, &mm);
 	        appointments[i].time1 = 60 * hh + mm;
@@ -28,7 +28,6 @@ int main() {
 	        appointments[i].time2 = 60 * hh + mm;
 	        getline(&buffer, &bufsize, stdin);
 	    }
-	    free(buffer);
 	    qsort(appointments, s + 2, sizeof(Appointment), compare);
 	    int longest = appointments[1].time1 - 600, start = 600;
 	    for(int i = 2; i < s + 2; ++i) {
@@ -43,5 +42,6 @@ int main() {
 	    else
 	        printf("%d minutes.\n", longest);
 	}
+    free(buffer);
 	return 0;
 }
