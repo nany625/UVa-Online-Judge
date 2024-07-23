23f78641b4f2d63f1b3076429ccdd9dcc2ed7f01
nany625 <jimmy19980625@gmail.com>
2024-07-23 14:15:39 +0800

    Update solution.c

--- 10533 Digit Primes/solution.c
+++ 10533 Digit Primes/solution.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
-#define MAX_SIZE 78498
+#define MAX_SIZE 1000000
 
-int primes[MAX_SIZE] = {2}, dpCount[999999] = {0, 1}, primesCount = 1;
-
-bool isPrime(int n) {
-    int limit = sqrt(n);
-    for(int i = 0; i < primesCount && primes[i] <= limit; ++i) {
-        if(n % primes[i] == 0)
-            return false;
-    }
-    return true;
-}
+bool isComposite[MAX_SIZE] = {true, true};
+short dpCount[MAX_SIZE] = {}, digitsSum[MAX_SIZE] = {0, 1, 2};
 
 int sumOfDigits(int n) {
     int result = 0;
@@ -24,21 +16,19 @@ int sumOfDigits(int n) {
 }
 
 int main() {
-	for(int i = 3; i < 1000000; ++i) {
-	    dpCount[i - 1] = dpCount[i - 2];
-	    if(i % 2 == 1 && primesCount < MAX_SIZE) {
-	        if(isPrime(i)) {
-	            primes[primesCount++] = i;
-	            dpCount[i - 1] += isPrime(sumOfDigits(i));
-	        }
-	    }
-	}
+    for(int i = 2; i < MAX_SIZE; ++i) {
+        dpCount[i] = dpCount[i - 1];
+        for(int j = 2 * i; j < MAX_SIZE; j += i)
+            isComposite[j] = true;
+        if(!isComposite[i])
+            dpCount[i] += !isComposite[sumOfDigits(i)];
+    }
 	int N;
 	scanf("%d", &N);
 	while(N--) {
 		int t1, t2;
 		scanf("%d %d", &t1, &t2);
-		printf("%d\n", dpCount[t2 - 1] - dpCount[t1 - 1] + (t1 > 1 && dpCount[t1 - 1] > dpCount[t1 - 2]));
+		printf("%d\n", dpCount[t2] - dpCount[t1 - 1]);
 	}
 	return 0;
 }
