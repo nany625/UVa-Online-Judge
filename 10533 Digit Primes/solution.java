344e92e15d178fea00410f009a8f8753af930b75
nany625 <jimmy19980625@gmail.com>
2024-07-23 14:23:15 +0800

    Update solution.java

--- 10533 Digit Primes/solution.java
+++ 10533 Digit Primes/solution.java
@@ -1,22 +1,17 @@
 import java.io.*;
 
 public class Main {
-    static int[] primes = new int[78498];
-    static short[] dpCount = new short[999999];
-    static int primesCount = 1;
+    static boolean[] isComposite = new boolean[1000000];
+    static short[] dpCount = new short[1000000];
 	public static void main(String[] args) throws IOException {
-	    primes[0] = 2;
-		dpCount[1] = 1;
-		for(int i = 3; i < 1000000; ++i) {
-		    dpCount[i - 1] = dpCount[i - 2];
-    	    if(i % 2 == 1 && primesCount < primes.length) {
-    	        if(isPrime(i)) {
-    	            primes[primesCount++] = i;
-    	            if(isPrime(sumOfDigits(i)))
-       	                ++dpCount[i - 1];
-    	        }
-    	    }
-		}
+	    isComposite[0] = isComposite[1] = true;
+	    for(int i = 2; i < 1000000; ++i) {
+	        dpCount[i] = dpCount[i - 1];
+	        for(int j = 2 * i; j < 1000000; j += i)
+	            isComposite[j] = true;
+	        if(!isComposite[i] && !isComposite[sumOfDigits(i)])
+	            ++dpCount[i];
+	    }
         StreamTokenizer st = new StreamTokenizer(System.in);
         st.nextToken();
         int N = (int)st.nval;
@@ -26,19 +21,10 @@ public class Main {
 		    int t1 = (int)st.nval;
 		    st.nextToken();
 		    int t2 = (int)st.nval;
-			output.append(dpCount[t2 - 1] - dpCount[t1 - 1] + (t1 > 1 && dpCount[t1 - 1] > dpCount[t1 - 2] ? 1 : 0) + "\n");
+			output.append(dpCount[t2] - dpCount[t1 - 1] + "\n");
     	}
         System.out.print(output);
 	}
-	
-	static boolean isPrime(int n) {
-	    int limit = (int)Math.sqrt(n);
-        for(int i = 0; i < primesCount && primes[i] <= limit; ++i) {
-            if(n % primes[i] == 0)
-                return false;
-        }
-        return true;
-    }
 	
 	static int sumOfDigits(int n) {
         int result = 0;
