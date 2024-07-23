2ea5bf8866e4b65a16f7e4448fe15dc5ee4c8d1f
nany625 <jimmy19980625@gmail.com>
2024-07-23 08:24:25 +0800

    Update solution.c

--- 10193 All You Need Is Love/solution.c
+++ 10193 All You Need Is Love/solution.c
@@ -8,8 +8,8 @@ int GCD(long i, long j) {
 int main() {
     int N;
     scanf("%d", &N);
+    char S1[17], S2[17];
     for(int i = 1; i <= N; ++i) {
-        char S1[17], S2[17];
         scanf("%s %s", S1, S2);
         printf("Pair #%d: %s!\n", i, GCD(strtol(S1, NULL, 2), strtol(S2, NULL, 2)) > 1 ? "All you need is love" : "Love is not all you need");
     }
