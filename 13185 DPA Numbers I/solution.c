1e126ca9418761ceeb012c366d1dd5db259bbf7e
nany625 <jimmy19980625@gmail.com>
2024-07-23 22:57:52 +0800

    Update solution.c

--- 13185 DPA Numbers I/solution.c
+++ 13185 DPA Numbers I/solution.c
@@ -2,40 +2,40 @@
 #include <stdio.h>
 #include <math.h>
 
-char table[998];
+char table[1001];
 
 void sumOfDivisors(int n) {
     if(table[n] == 'a')
         return;
-    int sum = 1, i, num = n + 2;
-    float limit = sqrt(num);
-    for(i = 2; i < limit && sum <= num; ++i) {
-        if(num % i == 0)
-            sum += i + num / i;
+    int sum = 1, i;
+    float limit = sqrt(n);
+    for(i = 2; i < limit && sum <= n; ++i) {
+        if(n % i == 0)
+            sum += i + n / i;
     }
     if(i == limit)
         sum += i;
-    if(sum < num)
+    if(sum < n)
         table[n] = 'd';
-    else if(sum == num)
+    else if(sum == n)
         table[n] = 'p';
     else {
-        for(i = num; i <= 999; i += num)
-            table[i - 2] = 'a';
+        for(i = n; i <= 1000; i += n)
+            table[i] = 'a';
     }
 }
 
 int main() {
-    for(int i = 0; i < 998; ++i)
+    for(int i = 2; i <= 1000; ++i)
         sumOfDivisors(i);
 	int t;
 	scanf("%d", &t);
 	while(t--) {
 		int n;
 		scanf("%d", &n);
-		if(table[n - 2] == 'd')
+		if(table[n] == 'd')
 			puts("deficient");
-		else if(table[n - 2] == 'p')
+		else if(table[n] == 'p')
 			puts("perfect");
 		else
 			puts("abundant");
