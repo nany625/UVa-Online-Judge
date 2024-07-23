e8b9fe1d8fee37d5960f96923cca94800d3d3625
nany625 <jimmy19980625@gmail.com>
2024-07-23 22:24:27 +0800

    Update solution.c

--- 13185 DPA Numbers I/solution.c
+++ 13185 DPA Numbers I/solution.c
@@ -1,3 +1,4 @@
+// #解法一
 #include <stdio.h>
 #include <math.h>
 
@@ -35,6 +36,31 @@ int main() {
 		if(table[n - 2] == 'd')
 			puts("deficient");
 		else if(table[n - 2] == 'p')
+			puts("perfect");
+		else
+			puts("abundant");
+	}
+	return 0;
+}
+
+// #解法二
+#include <stdio.h>
+
+int sum[1001] = {};
+
+int main() {
+    for(int i = 1; i <= 1000; ++i) {
+        for(int j = 2 * i; j <= 1000; j += i)
+            sum[j] += i;
+    }
+	int t;
+	scanf("%d", &t);
+	while(t--) {
+		int n;
+		scanf("%d", &n);
+		if(sum[n] < n)
+			puts("deficient");
+		else if(sum[n] == n)
 			puts("perfect");
 		else
 			puts("abundant");
