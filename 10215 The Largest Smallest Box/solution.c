8aff554cf04910a8a5a9382ad1d351eca8545a12
nany625 <jimmy19980625@gmail.com>
2024-07-23 08:38:35 +0800

    Update solution.c

--- 10215 The Largest Smallest Box/solution.c
+++ 10215 The Largest Smallest Box/solution.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <math.h>
+#define EPS 1e-4
 
 int main() {
-    double L, W, eps = 1e-6;
+    double L, W;
     while(scanf("%lf %lf", &L, &W) == 2)
-        printf("%.3f 0.000 %.3f\n", (L + W - sqrt(L * L + W * W - L * W)) / 6, (L > W ? W : L) / 2 + eps);
+        printf("%.3f 0.000 %.3f\n", (L + W - sqrt(L * L + W * W - L * W)) / 6, (L > W ? W : L) / 2 + EPS);
     return 0;
 }
