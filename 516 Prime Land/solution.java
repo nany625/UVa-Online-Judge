aa77b763c83ec23cff8ebd08ce33c31bf4c3a731
nany625 <jimmy19980625@gmail.com>
2024-07-24 16:02:38 +0800

    Update solution.java

--- 516 Prime Land/solution.java
+++ 516 Prime Land/solution.java
@@ -1,25 +1,30 @@
 import java.io.*;
 
 public class Main {
+    static boolean[] isComposite = new boolean[32694];
     static short[] primes = new short[3507];
-    static int count = 1;
+    static int count = 0;
 	public static void main(String[] args) throws IOException {
-	    primes[0] = 2;
-		for(short i = 3; count < primes.length; i += 2) {
-    	    if(isPrime(i))
-    	        primes[count++] = i;
+		for(short i = 2; i <= 32693; ++i) {
+    		if(!isComposite[i]) {
+    			primes[count++] = i;
+    			if(i <= 180) {
+    				for(int j = i * i; j <= 32693; j += i)
+    					isComposite[j] = true;
+    			}
+    		}
     	}
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	String line;
     	StringBuilder output = new StringBuilder();
-    	while(!(line = br.readLine()).equals("0")) {
+    	while((line = br.readLine()).charAt(0) != '0') {
     	    int x = 1;
     	    String[] tokens = line.split("\\s+");
     	    for(int i = 0; i < tokens.length; i += 2) 
     	        x *= Math.pow(Integer.parseInt(tokens[i]), Integer.parseInt(tokens[i + 1]));
-    	    int pos = binarySearch(primes, --x);
+    	    int pos = --x >= 32693 ? 3506 : binarySearch(x);
     	    boolean space = false;
-    	    while(pos >= 0 && x > 1) {
+    	    while(x > 1) {
     	        if(x % primes[pos] == 0) {
     	            if(space)
     	                output.append(" ");
@@ -38,22 +43,25 @@ public class Main {
 	    System.out.print(output);
 	}
 	
-	static boolean isPrime(short n) {
-	    short limit = (short)Math.sqrt(n);
-	    for(int i = 1; i < count && primes[i] <= limit; ++i) {
-	        if(n % primes[i] == 0)
-	            return false;
-	    }
-	    return true;
-	}
-	
-	static int binarySearch(short[] array, int key) {
-        int left = 0, right = array.length - 1;
+	static int binarySearch(int key) {
+        int left = 0, right = 3506;
         while(left <= right) {
             int mid = left + (right - left) / 2;
-            if(array[mid] == key)
+            if(primes[mid] == key)
                 return mid;
-            if(array[mid] < key)
+            if(primes[mid] < key)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        right = left - 1;
+        left = 0;
+        key /= 2;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(primes[mid] == key)
+                return mid;
+            if(primes[mid] < key)
                 left = mid + 1;
             else
                 right = mid - 1;
