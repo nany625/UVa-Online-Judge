9d96f5e8ae57954ea7c33d103a2b391594aab89b
nany625 <jimmy19980625@gmail.com>
2024-07-24 09:45:33 +0800

    Create solution.java

---
+++ 13194 DPA Numbers II/solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+
+public class Main {
+    static boolean[] isComposite = new boolean[1000001]; 
+    static int[] primes = new int[78498];
+    static int count = 0;
+	public static void main(String[] args) throws IOException {
+        isComposite[0] = isComposite[1] = true;
+        int limit = (int)Math.sqrt(1000000);
+        for(int i = 2; i <= 1000000; ++i) {
+            if(!isComposite[i]) {
+                primes[count++] = i;
+                if(i <= limit) {
+                    for(int j = i * i; j <= 1000000; j += i)
+                        isComposite[j] = true;
+                }
+            }
+        }
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int t = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+		while(t-- > 0) {
+    		st.nextToken();
+            long n = (long)st.nval;
+            if(n % 2 == 1) {
+                output.append("deficient\n");
+                continue;
+            }
+            long sum = sumOfDivisors(n) - n;
+    		if(sum < n)
+    			output.append("deficient\n");
+    		else if(sum == n)
+        		output.append("perfect\n");
+        	else
+        		output.append("abundant\n");
+    	}
+        System.out.print(output);
+	}
+	
+	static long sumOfDivisors(long n) {
+        long sum = 1, limit = (long)Math.sqrt(n);
+        for(int i = 0; i < 78498 && primes[i] <= limit; ++i) {
+            if(n % primes[i] == 0) {
+                long tempSum = 1, term = 1;
+                do {
+                    tempSum += term *= primes[i];
+                    n /= primes[i];
+                } while(n % primes[i] == 0);
+                sum *= tempSum;
+                limit = (long)Math.sqrt(n);
+            }
+        }
+        if(n > 1)
+            sum *= (1 + n);
+        return sum;
+    }
+}
