760e4ac23ab4a7d82f93a22aa0a8a7226817b1f2
nany625 <jimmy19980625@gmail.com>
2024-07-24 09:53:05 +0800

    Update solution.c

--- 1339 Ancient Cipher/solution.c
+++ 1339 Ancient Cipher/solution.c
@@ -2,8 +2,8 @@
 #include <string.h>
 #include <stdbool.h>
 
-void InsertionSort(int *array, int size) {
-    for(int i = 1; i < size; ++i) {
+void InsertionSort(int *array) {
+    for(int i = 1; i < 26; ++i) {
         int j = i - 1, insertionNum = array[i];
         while(j >= 0 && insertionNum < array[j]) {
             array[j + 1] = array[j];
@@ -21,8 +21,8 @@ int main() {
 	        ++frequency[0][encryption[i] - 'A'];
 			++frequency[1][origin[i] - 'A'];
 	    }
-	    InsertionSort(frequency[0], 26);
-	    InsertionSort(frequency[1], 26);
+	    InsertionSort(frequency[0]);
+	    InsertionSort(frequency[1]);
 	    int i = 25;
 	    while(i >= 0 && frequency[0][i] == frequency[1][i] && frequency[0][i] > 0)
 	        --i;
