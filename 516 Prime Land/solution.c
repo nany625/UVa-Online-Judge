f2a375d1e8ecc5c6c31cf4f871fa7029185071ad
nany625 <jimmy19980625@gmail.com>
2024-07-26 00:06:37 +0800

    Update solution.c

--- 516 Prime Land/solution.c
+++ 516 Prime Land/solution.c
@@ -3,38 +3,82 @@
 #include <math.h>
 #include <string.h>
 #include <stdbool.h>
+#define MAX_PRIME_SIZE 3507
+
+typedef struct {
+    int p, e;
+} Factor;
+
+short primes[MAX_PRIME_SIZE] = {2};
+int count = 1;
+
+bool isPrime(int n) {
+    int limit = sqrt(n);
+    for(int i = 1; i < count && primes[i] <= limit; ++i) {
+        if(n % primes[i] == 0)
+            return false;
+    }
+    return true;
+}
+
+int main() {
+	for(int i = 3; count < MAX_PRIME_SIZE; i += 2) {
+	    if(isPrime(i))
+	        primes[count++] = i;
+	}
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	while(getline(&buffer, &bufsize, stdin) != -1 && buffer[0] != '0') {
+	    char *token = strtok(buffer, " ");
+	    int x = 1;
+	    while(token) {
+	        int p = atoi(token);
+	        token = strtok(NULL, " ");
+	        x *= pow(p, atoi(token));
+	        token = strtok(NULL, " ");
+	    }
+	    --x;
+	    Factor *factors = NULL;
+	    int size = 0;
+	    for(int i = 0; x > 1; ++i) {
+	        if(x % primes[i] == 0) {
+	            factors = (Factor*)realloc(factors, (size + 1) * sizeof(Factor));
+	            factors[size].p = primes[i];
+	            factors[size].e = 0;
+	            do {
+	                ++factors[size].e;
+	                x /= primes[i];
+	            } while(x % primes[i] == 0);
+	            ++size;
+	        }
+	    }
+	    for(int i = size - 1; i >= 0; --i) {
+	        printf("%d %d", factors[i].p, factors[i].e);
+	        if(i > 0)
+	            putchar(' ');
+	    }
+	    putchar('\n');
+	    free(factors);
+	}
+	free(buffer);
+	return 0;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
 #define MAX_NUM 32693
 #define MAX_PRIME_SIZE 3507
+
+typedef struct {
+    int p, e;
+} Factor;
 
 bool isComposite[MAX_NUM + 1] = {};
 short primes[MAX_PRIME_SIZE] = {};
 int count = 0;
-
-int binarySearch(int key) {
-    int left = 0, right = MAX_PRIME_SIZE - 1;
-    while(left <= right) {
-        int mid = left + (right - left) / 2;
-        if(primes[mid] == key)
-            return mid;
-        if(primes[mid] < key)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    right = left - 1;
-    left = 0;
-    key /= 2;
-    while(left <= right) {
-        int mid = left + (right - left) / 2;
-        if(primes[mid] == key)
-            return mid;
-        if(primes[mid] < key)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return left - 1;
-}
 
 int main() {
 	for(int i = 2; count < MAX_PRIME_SIZE; ++i) {
@@ -57,23 +101,28 @@ int main() {
 	        x *= pow(p, atoi(token));
 	        token = strtok(NULL, " ");
 	    }
-	    int pos = --x >= MAX_NUM ? MAX_PRIME_SIZE - 1 : binarySearch(x);
-	    bool space = false;
-	    while(x > 1) {
-	        if(x % primes[pos] == 0) {
-	            if(space)
-	                putchar(' ');
-	            int e = 0;
+	    --x;
+	    Factor *factors = NULL;
+	    int size = 0;
+	    for(int i = 0; x > 1; ++i) {
+	        if(x % primes[i] == 0) {
+	            factors = (Factor*)realloc(factors, (size + 1) * sizeof(Factor));
+	            factors[size].p = primes[i];
+	            factors[size].e = 0;
 	            do {
-	                ++e;
-	                x /= primes[pos];
-	            } while(x % primes[pos] == 0);
-	            printf("%hd %d", primes[pos], e);
-	            space = true;
+	                ++factors[size].e;
+	                x /= primes[i];
+	            } while(x % primes[i] == 0);
+	            ++size;
 	        }
-	        --pos;
+	    }
+	    for(int i = size - 1; i >= 0; --i) {
+	        printf("%d %d", factors[i].p, factors[i].e);
+	        if(i > 0)
+	            putchar(' ');
 	    }
 	    putchar('\n');
+	    free(factors);
 	}
 	free(buffer);
 	return 0;
