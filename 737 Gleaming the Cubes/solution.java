e7867227325f0eee622d6e1a543442e2143ac805
nany625 <jimmy19980625@gmail.com>
2024-07-26 06:43:15 +0800

    Create solution.java

---
+++ 737 Gleaming the Cubes/solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    	StreamTokenizer st = new StreamTokenizer(System.in);
+    	int cubes;
+    	StringBuilder output = new StringBuilder();
+    	while(st.nextToken() == StreamTokenizer.TT_NUMBER && (cubes = (int)st.nval) != 0) {
+    	    st.nextToken();
+    	    int x = (int)st.nval;
+    	    st.nextToken();
+    	    int y = (int)st.nval;
+    	    st.nextToken();
+    	    int z = (int)st.nval;
+    	    st.nextToken();
+    	    int dist = (int)st.nval;
+    	    int minX = x, maxX = x + dist, minY = y, maxY = y + dist, minZ = z, maxZ = z + dist;
+    	    --cubes;
+    	    while(cubes-- > 0) {
+    	        st.nextToken();
+        	    x = (int)st.nval;
+        	    st.nextToken();
+        	    y = (int)st.nval;
+        	    st.nextToken();
+        	    z = (int)st.nval;
+        	    st.nextToken();
+        	    dist = (int)st.nval;
+        	    minX = Math.max(minX, x);
+        	    maxX = Math.min(maxX, x + dist);
+        	    minY = Math.max(minY, y);
+        	    maxY = Math.min(maxY, y + dist);
+        	    minZ = Math.max(minZ, z);
+        	    maxZ = Math.min(maxZ, z + dist);
+    	    }
+    	    if(maxX < minX || maxY < minY || maxZ < minZ)
+	            output.append("0\n");
+	        else
+	            output.append((maxX - minX) * (maxY - minY) * (maxZ - minZ) + "\n");
+    	}
+	    System.out.print(output);
+	}
+}
