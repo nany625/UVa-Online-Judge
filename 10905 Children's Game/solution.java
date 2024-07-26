c580123320dc1e5ae379fee7634f4fedf15f42ad
nany625 <jimmy19980625@gmail.com>
2024-07-26 10:46:20 +0800

    Create solution.java

---
+++ 10905 Children's Game/solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StreamTokenizer st = new StreamTokenizer(br);
+    	int N;
+    	StringBuilder output = new StringBuilder();
+    	while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0) {
+	        String[] numbers = br.readLine().split("\\s+");
+	        Arrays.sort(numbers, new Comparator<String>() {
+	           @Override
+	           public int compare(String num1, String num2) {
+	               String comb1 = "" + num1 + num2;
+	               String comb2 = "" + num2 + num1;
+	               return comb2.compareTo(comb1);
+	           }
+	        });
+	        for(String s : numbers)
+	            output.append(s);
+	        output.append("\n");
+    	}
+	    System.out.print(output);
+	}
+}
