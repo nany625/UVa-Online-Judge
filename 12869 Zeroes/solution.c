38db81c4703c9c17a0642c234af08745a0aa8c36
nany625 <jimmy19980625@gmail.com>
2024-07-27 16:18:31 +0800

    Update solution.c

--- 12869 Zeroes/solution.c
+++ 12869 Zeroes/solution.c
@@ -3,6 +3,6 @@
 int main() {
     long low, high;
     while(scanf("%ld %ld", &low, &high) && low != 0 && high != 0)
-        printf("%ld\n", (high - low) / 5 + 1 + (high % 5 < low % 5));
+        printf("%ld\n", high / 5 - low / 5 + 1);
     return 0;
 }
