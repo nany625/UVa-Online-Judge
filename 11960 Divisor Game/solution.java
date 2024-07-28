d8ea81d61aafae82a76a00e7f8ce5b2500e2c5b9
nany625 <jimmy19980625@gmail.com>
2024-07-29 00:48:45 +0800

    Create solution.java

---
+++ 11960 Divisor Game/solution.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    static short[] factorCount = new short[1000001];
+    static int[] table = new int[500000];
+	public static void main(String[] args) throws IOException {
+        for(int i = 3; i <= 1000000; ++i) {
+            for(int j = i; j <= 1000000; j += i)
+                ++factorCount[j];
+        }
+        short max = 2;
+        table[0] = 2;
+        for(int i = 4; i <= 1000000; i += 2) {
+            if(max <= factorCount[i] + 2) {
+                max = (short)(factorCount[i] + 2);
+                table[i / 2 - 1] = i;
+            } else
+                table[i / 2 - 1] = table[i / 2 - 2];
+        }
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int T = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(T-- > 0) {
+            st.nextToken();
+    		int N = (int)st.nval;
+    		if(N == 1 || N == 3)
+    	        output.append(N + "\n");
+    	    else
+    	        output.append(table[N / 2 - 1] + "\n");
+		}
+	    System.out.print(output);
+	}
+}
