30bf26e26205faa8bf5b872fe2a9b97edc87530f
nany625 <jimmy19980625@gmail.com>
2024-07-28 19:44:20 +0800

    Update solution.c

--- 11960 Divisor Game/solution.c
+++ 11960 Divisor Game/solution.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #define MAX_NUM 1000000
 
-int factorCount[MAX_NUM + 1] = {0, 1}, table[MAX_NUM / 2 + 1] = {2};
+short factorCount[MAX_NUM + 1] = {0, 1};
+int table[MAX_NUM / 2 + 1] = {2};
 
 int main() {
-    for(int i = 2; i <= MAX_NUM; ++i) {
+    for(int i = 3; i <= MAX_NUM; ++i) {
         for(int j = i; j <= MAX_NUM; j += i)
             ++factorCount[j];
     }
-    int max = 2;
+    short max = 2;
     for(int i = 4; i <= MAX_NUM; i += 2) {
-        if(max <= factorCount[i] + 1) {
-            max = factorCount[i] + 1;
+        if(max <= factorCount[i] + 2) {
+            max = factorCount[i] + 2;
             table[i / 2 - 1] = i;
         } else
             table[i / 2 - 1] = table[i / 2 - 2];
