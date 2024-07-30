e19504329d0703d1dc80b7db068e3615fd999a84
nany625 <jimmy19980625@gmail.com>
2024-07-30 18:23:27 +0800

    Update solution.java

--- 941 Permutations/solution.java
+++ 941 Permutations/solution.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main {
     static long[] factorials = {
-        1L, 2L, 6L, 24L, 120L, 720L, 5040L, 40320L, 362880L, 3628800L, 
-        39916800L, 479001600L, 6227020800L, 87178291200L, 1307674368000L, 20922789888000L, 355687428096000L, 6402373705728000L, 121645100408832000L
+        1L, 1L, 2L, 6L, 24L, 120L, 720L, 5040L, 40320L, 362880L, 
+        3628800L, 39916800L, 479001600L, 6227020800L, 87178291200L, 1307674368000L, 20922789888000L, 355687428096000L, 6402373705728000L, 121645100408832000L
     };
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,13 +17,13 @@ public class Main {
     	    long N = Long.parseLong(br.readLine());
     	    Arrays.sort(S);
     	    int len = S.length;
-    	    for(int i = len - 2; i >= 0; --i) {
+    	    for(int i = len - 1; i >= 0; --i) {
     	        int index = (int)(N / factorials[i]);
     	        output.append(S[index]);
     	        System.arraycopy(S, index + 1, S, index, len-- - index - 1);
 		        N %= factorials[i];
 		    }
-		    output.append(S[0] + "\n");
+		    output.append("\n");
 		}
         System.out.print(output);
 	}
