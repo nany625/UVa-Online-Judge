8b03daa596f496c7757d22b03b73f2b9edb8dbf0
nany625 <jimmy19980625@gmail.com>
2024-07-31 05:45:26 +0800

    Update solution.c

--- 10700 Camel trading/solution.c
+++ 10700 Camel trading/solution.c
@@ -12,34 +12,9 @@ int main() {
     while(N--) {
         getline(&buffer, &bufsize, stdin);
         buffer[strcspn(buffer, "\n")] = '\0';
-        long min = 0, prev = 0;
-        int next = 1, len = strlen(buffer);
-        bool add = true;
-        for(int i = 0; i < len; ++i) {
-            switch(buffer[i]) {
-                case '+':
-                    add = true;
-                    min += prev * next;
-                    prev = 0;
-                    next = 1;
-                    break;
-                case '*':
-                    add = false;
-                    prev *= next;
-                    next = 0;
-                    break;
-                default:
-                    if(add)
-                        prev = 10 * prev + buffer[i] - '0';
-                    else
-                        next = 10 * next + buffer[i] - '0';
-            }
-        }
-        min += prev * next;
-        long max = 1;
-        prev = 0;
-        next = 0;
-        add = false;
+        long max = 1, prev = 0;
+        int next = 0, len = strlen(buffer);
+        bool add = false;
         for(int i = 0; i < len; ++i) {
             switch(buffer[i]) {
                 case '+':
@@ -61,6 +36,31 @@ int main() {
             }
         }
         max *= (prev + next);
+        long min = 0;
+        prev = 0,
+        next = 1;
+        add = true;
+        for(int i = 0; i < len; ++i) {
+            switch(buffer[i]) {
+                case '+':
+                    add = true;
+                    min += prev * next;
+                    prev = 0;
+                    next = 1;
+                    break;
+                case '*':
+                    add = false;
+                    prev *= next;
+                    next = 0;
+                    break;
+                default:
+                    if(add)
+                        prev = 10 * prev + buffer[i] - '0';
+                    else
+                        next = 10 * next + buffer[i] - '0';
+            }
+        }
+        min += prev * next;
         printf("The maximum and minimum are %ld and %ld.\n", max, min);
     }
     free(buffer);
