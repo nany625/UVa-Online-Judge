25e5f89685d81fcb30004bc88403bf85c5ca49c1
nany625 <jimmy19980625@gmail.com>
2024-07-30 18:37:29 +0800

    Update solution.c

--- 12335 Lexicographic Order/solution.c
+++ 12335 Lexicographic Order/solution.c
@@ -3,8 +3,8 @@
 #include <stdbool.h>
 
 long factorials[] = {
-    1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 
-    39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000, 121645100408832000
+    1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 
+    3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000, 121645100408832000
 };
 
 int main() {
@@ -15,27 +15,28 @@ int main() {
         printf("Case %d: ", i);
         long k;
         scanf("%s %ld", s, &k);
-        int len = strlen(s);
         --k;
-        char original[len];
-        bool put[len];
+        int len = strlen(s);
+        char origin[len];
+        bool used[len];
         for(int j = 0; j < len; ++j)
-            put[j] = false;
-        for(int j = len - 2; j >= 0; --j) {
-            int index = k / factorials[j], count = -1;
+            used[j] = false;
+        for(int j = len - 1; j >= 0; --j) {
+            int index = k / factorials[j], count = 0;
             for(int l = 0; l < len; ++l) {
-                count += !put[l];
-                if(count == index) {
-                    original[l] = s[len - 2 - j];
-                    put[l] = true;
-                    break;
+                if(!used[l]) {
+                    if(count == index) {
+                        origin[l] = s[len - 1 - j];
+                        used[l] = true;
+                        break;
+                    } else
+                        ++count;
                 }
             }
             k %= factorials[j];
         }
-        for(int j = 0; j < len; ++j) {
-            putchar(put[j] ? original[j] : s[len - 1]);
-        }
+        for(int j = 0; j < len; ++j)
+            putchar(origin[j]);
         putchar('\n');
     }
 	return 0;
