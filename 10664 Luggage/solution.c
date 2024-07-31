814b03df601b7d41cd27e60e6091a5509837c05d
nany625 <jimmy19980625@gmail.com>
2024-08-01 05:05:00 +0800

    Create solution.c

---
+++ 10664 Luggage/solution.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isSubsetSum(int *array, int size, int sum) {
+    bool subsetSum[sum + 1];
+    subsetSum[0] = true;
+    for(int i = 1; i <= sum; ++i)
+        subsetSum[i] = false;
+    for(int i = 0; i < size; ++i) {
+        for(int j = sum; j >= array[i] && !subsetSum[sum]; --j)
+            subsetSum[j] = subsetSum[j] || subsetSum[j - array[i]];
+    }
+    return subsetSum[sum];
+}
+
+int main() {
+    int cases;
+    scanf("%d", &cases);
+    getchar();
+    char *buffer = NULL;
+    size_t bufsize = 0;
+    while(cases--) {
+        getline(&buffer, &bufsize, stdin);
+        char *token = strtok(buffer, " ");
+        int *weights = NULL, size = 0, sum = 0;
+        while(token) {
+            weights = (int*)realloc(weights, (size + 1) * sizeof(int));
+            sum += weights[size++] = atoi(token);
+            token = strtok(NULL, " ");
+        }
+        if(sum % 2 != 0)
+            puts("NO");
+        else
+            puts(isSubsetSum(weights, size, sum / 2) ? "YES" : "NO");
+        free(weights);
+    }
+    free(buffer);
+	return 0;
+}
