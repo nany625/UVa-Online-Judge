8138975f98c6ed1a3c232e9ef00a9b46cb8b6ac1
nany625 <jimmy19980625@gmail.com>
2024-07-31 12:34:07 +0800

    Create solution.c

---
+++ 10534 Wavio Sequence/solution.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int binarySearch(int *array, int size, int key) {
+    int left = 0, right = size - 1;
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+        if(array[mid] == key)
+            return mid;
+        if(array[mid] < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return left;
+}
+
+int main() {
+    int N;
+    while(scanf("%d", &N) == 1) {
+        int sequence[N];
+        for(int i = 0; i < N; ++i)
+            scanf("%d", &sequence[i]);
+        int *LIS = NULL, lisSize = 0, lisDP[N];
+        for(int i = 0; i < N; ++i) {
+            int pos = binarySearch(LIS, lisSize, sequence[i]);
+            if(pos == lisSize)
+                LIS = (int*)realloc(LIS, ++lisSize * sizeof(int));
+            LIS[pos] = sequence[i];
+            lisDP[i] = lisSize;
+        }
+        int *LDS = NULL, ldsSize = 0, ldsDP[N], max = 1;
+        for(int i = N - 1; i >= 0; --i) {
+            int pos = binarySearch(LDS, ldsSize, sequence[i]);
+            if(pos == ldsSize)
+                LDS = (int*)realloc(LDS, ++ldsSize * sizeof(int));
+            LDS[pos] = sequence[i];
+            ldsDP[i] = ldsSize;
+            int temp = lisDP[i] < ldsDP[i] ? lisDP[i] : ldsDP[i];
+            if(max <= temp)
+                max = temp;
+            else
+                break;
+        }
+        printf("%d\n", 2 * max - 1);
+        free(LIS);
+        free(LDS);
+    }
+	return 0;
+}
