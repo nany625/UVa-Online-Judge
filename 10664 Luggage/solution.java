57c783cd935fd25155d37386a2cb34416d097ba8
nany625 <jimmy19980625@gmail.com>
2024-08-01 05:16:11 +0800

    Create solution.java

---
+++ 10664 Luggage/solution.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        st.nextToken();
+        int cases = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+    	while(cases-- > 0) {
+    	    String[] tokens = br.readLine().split("\\s+");
+    	    int size = tokens.length, sum = 0;
+    	    int[] weights = new int[size];
+    	    for(int i = 0; i < size; ++i)
+    	        sum += weights[i] = Integer.parseInt(tokens[i]);
+    	    if(sum % 2 == 1)
+    	        output.append("NO\n");
+    	    else
+    	        output.append(isSubsetSum(weights, size, sum / 2) ? "YES\n" : "NO\n");
+    	}
+        System.out.print(output);
+	}
+	
+	static boolean isSubsetSum(int[] array, int size, int sum) {
+        boolean[] subsetSum = new boolean[sum + 1];
+        subsetSum[0] = true;
+        for(int i = 0; i < size; ++i) {
+            for(int j = sum; j >= array[i] && !subsetSum[sum]; --j)
+                subsetSum[j] = subsetSum[j] || subsetSum[j - array[i]];
+        }
+        return subsetSum[sum];
+    }
+}
