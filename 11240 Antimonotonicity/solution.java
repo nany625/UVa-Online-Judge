af6bb448aec7091b873e08a620b1cd134fb7a92a
nany625 <jimmy19980625@gmail.com>
2024-08-02 07:37:19 +0800

    Create solution.java

---
+++ 11240 Antimonotonicity/solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int T = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(T-- > 0) {
+	        st.nextToken();
+	        int n = (int)st.nval, prev = 0, size = 0;
+	        while(n-- > 0) {
+    	        st.nextToken();
+                int curr = (int)st.nval;
+                if((size % 2 == 0) == curr > prev)
+                    ++size;
+                prev = curr;
+	        }
+            output.append(size + "\n");
+        }
+        System.out.print(output);
+	}
+}
