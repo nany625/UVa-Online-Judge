39c5122d049298520ab470cb8f322b41a8805efe
nany625 <jimmy19980625@gmail.com>
2024-08-01 17:53:14 +0800

    Update solution.c

--- 10298 Power String/solution.c
+++ 10298 Power String/solution.c
@@ -4,19 +4,19 @@
 int main() {
     char s[1000001];
 	while(scanf("%s", s) && s[0] != '.') {
-		int len = strlen(s), LPS[len], i = 1, length = 0;
-		LPS[0] = 0;
+		int len = strlen(s), lps[len], i = 1, length = 0;
+		lps[0] = 0;
 		while(i < len) {
 		    if(s[i] == s[length])
-		        LPS[i++] = ++length;
+		        lps[i++] = ++length;
 		    else {
 		        if(length != 0)
-		            length = LPS[length - 1];
+		            length = lps[length - 1];
 		        else
-		            LPS[i++] = 0;
+		            lps[i++] = 0;
 		    }
 		}
-		printf("%d\n", len % (len - LPS[len - 1]) == 0 ? len / (len - LPS[len - 1]) : 1);
+		printf("%d\n", len % (len - lps[len - 1]) == 0 ? len / (len - lps[len - 1]) : 1);
 	}
 	return 0;
 }
