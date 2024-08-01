d9c2e099852fd3397c23f37812a6e53718bd81ea
nany625 <jimmy19980625@gmail.com>
2024-08-01 18:27:34 +0800

    Update solution.java

--- 10664 Luggage/solution.java
+++ 10664 Luggage/solution.java
@@ -25,7 +25,7 @@ public class Main {
 	static boolean isSubsetSum(int[] array, int size, int sum) {
         boolean[] subsetSum = new boolean[sum + 1];
         subsetSum[0] = true;
-        for(int i = 0; i < size; ++i) {
+        for(int i = 0; i < size && !subsetSum[sum]; ++i) {
             for(int j = sum; j >= array[i] && !subsetSum[sum]; --j)
                 subsetSum[j] = subsetSum[j] || subsetSum[j - array[i]];
         }
