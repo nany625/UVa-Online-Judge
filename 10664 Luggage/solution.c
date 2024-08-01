9e1cd348c1c449e418e5a34211d0ba00977cefc2
nany625 <jimmy19980625@gmail.com>
2024-08-01 18:26:40 +0800

    Update solution.c

--- 10664 Luggage/solution.c
+++ 10664 Luggage/solution.c
@@ -8,7 +8,7 @@ bool isSubsetSum(int *array, int size, int sum) {
     subsetSum[0] = true;
     for(int i = 1; i <= sum; ++i)
         subsetSum[i] = false;
-    for(int i = 0; i < size; ++i) {
+    for(int i = 0; i < size && !subsetSum[sum]; ++i) {
         for(int j = sum; j >= array[i] && !subsetSum[sum]; --j)
             subsetSum[j] = subsetSum[j] || subsetSum[j - array[i]];
     }
