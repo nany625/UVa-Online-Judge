e296513d380a7b53b1a92b03845c3cf4d90c03a6
nany625 <jimmy19980625@gmail.com>
2024-08-03 20:28:39 +0800

    Update solution.c

--- 11157 Dynamic Frog/solution.c
+++ 11157 Dynamic Frog/solution.c
@@ -9,9 +9,9 @@ typedef struct {
 
 int main() {
     int T;
+    scanf("%d", &T);
     char *buffer = NULL;
     size_t bufsize = 0;
-    scanf("%d", &T);
     for(int i = 1; i <= T; ++i) {
         int N, D;
         scanf("%d %d", &N, &D);
@@ -20,20 +20,20 @@ int main() {
         char *token = strtok(buffer, " ");
         Stone stones[N + 2];
         stones[0].M = 0;
-        for(int i = 1; i <= N; ++i) {
-            stones[i].S = token[0];
-            stones[i].M = atoi(token + 2);
+        for(int j = 1; j <= N; ++j) {
+            stones[j].S = token[0];
+            stones[j].M = atoi(token + 2);
             token = strtok(NULL, " ");
         }
         stones[N + 1].M = D;
         int max = 0;
-        for(int i = 1; i <= N; ++i) {
-            switch(stones[i].S) {
+        for(int j = 1; j <= N; ++j) {
+            switch(stones[j].S) {
                 case 'B':
-                    max = max > stones[i].M - stones[i - 1].M ? max : stones[i].M - stones[i - 1].M;
+                    max = max > stones[j].M - stones[j - 1].M ? max : stones[j].M - stones[j - 1].M;
                     break;
                 case 'S':
-                    max = max > stones[i + 1].M - stones[i - 1].M ? max : stones[i + 1].M - stones[i - 1].M;
+                    max = max > stones[j + 1].M - stones[j - 1].M ? max : stones[j + 1].M - stones[j - 1].M;
             }
         }
         max = max > D - stones[N].M ? max : D - stones[N].M;
