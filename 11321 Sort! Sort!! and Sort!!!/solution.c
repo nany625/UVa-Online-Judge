9c4fc3bffad71920fcdec4bf41f54864cc3e5cec
nany625 <jimmy19980625@gmail.com>
2024-08-04 16:10:34 +0800

    Create solution.c

---
+++ 11321 Sort! Sort!! and Sort!!!/solution.c
@@ -0,0 +1,38 @@
+// 尚未通過UVa
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int number, mod;
+} Data;
+
+int compare(const void *a, const void *b) {
+    Data *d1 = (Data*)a;
+    Data *d2 = (Data*)b;
+    if(d1->mod != d2->mod)
+        return d1->mod - d2->mod;
+    int abs1 = abs(d1->number % 2);
+    int abs2 = abs(d2->number % 2);
+    if(abs1 + abs2 == 1)
+        return abs1 == 1 ? -1 : 1;
+    if(abs1 == 1)
+        return d2->number - d1->number;
+    return d1->number - d2->number;
+}
+
+int main() {
+	int N, M;
+	while(scanf("%d %d", &N, &M) && N != 0 && M != 0) {
+	    printf("%d %d\n", N, M);
+	    Data datas[N];
+	    for(int i = 0; i < N; ++i) {
+	        scanf("%d", &datas[i].number);
+	        datas[i].mod = datas[i].number % M;
+	    }
+	    qsort(datas, N, sizeof(Data), compare);
+	    for(int i = 0; i < N; ++i)
+	        printf("%d\n", datas[i].number);
+	}
+	puts("0 0");
+	return 0;
+}
