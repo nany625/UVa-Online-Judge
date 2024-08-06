dc8be2b9af474d5db9b62d5eb3cb0017d1f52ba7
nany625 <jimmy19980625@gmail.com>
2024-08-06 10:01:30 +0800

    Update solution.c

--- 562 Dividing coins/solution.c
+++ 562 Dividing coins/solution.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
 
 int main() {
     int n;
     scanf("%d", &n);
+    char *buffer = NULL;
+    size_t bufsize = 0;
     while(n--) {
         int m;
         scanf("%d", &m);
+        getchar();
+        getline(&buffer, &bufsize, stdin);
+        char *token = strtok(buffer, " ");
         int coins[m], sum = 0;
         for(int i = 0; i < m; ++i) {
-            scanf("%d", &coins[i]);
-            sum += coins[i];
+            sum += coins[i] = atoi(token);
+            token = strtok(NULL, " ");
         }
         int temp = sum / 2, max = 0;
         bool subsetSum[temp + 1];
@@ -27,5 +34,6 @@ int main() {
         }
         printf("%d\n", sum - 2 * max);
     }
+    free(buffer);
 	return 0;
 }
