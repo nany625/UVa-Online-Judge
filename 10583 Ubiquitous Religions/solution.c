7a114f4c5558f50bc24e0500df17d58061232394
nany625 <jimmy19980625@gmail.com>
2024-08-08 14:30:32 +0800

    Create solution.c

---
+++ 10583 Ubiquitous Religions/solution.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int findRoot(int *root, int child) {
+    if(root[child] == child)
+        return child;
+    return root[child] = findRoot(root, root[child]);
+}
+
+int main() {
+    int cases = 0, n, m;
+    while(scanf("%d %d", &n, &m) && n != 0 && m != 0) {
+        int root[n + 1];
+        for(int i = 1; i <= n; ++i)
+            root[i] = i;
+        int count = n;
+        while(m--) {
+            int i, j;
+            scanf("%d %d", &i, &j);
+            int rootI = findRoot(root, i), rootJ = findRoot(root, j);
+            count -= rootI != rootJ;
+            root[rootI] = root[rootJ];
+        }
+        printf("Case %d: %d\n", ++cases, count);
+    }
+	return 0;
+}
