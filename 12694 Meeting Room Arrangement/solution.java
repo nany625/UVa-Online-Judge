486678616756742cf968201344413128acd09ea1
nany625 <jimmy19980625@gmail.com>
2024-08-12 14:34:02 +0800

    Create solution.java

---
+++ 12694 Meeting Room Arrangement/solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		st.nextToken();
+		int n = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		while(n-- > 0) {
+		    ArrayList<Event> events = new ArrayList<>();
+		    while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+		        short s = (short)st.nval;
+		        st.nextToken();
+		        short f = (short)st.nval;
+		        if(s == 0 && f == 0)
+		            break;
+		        events.add(new Event(s, f));
+		    }
+		    events.sort(new Comparator<Event>() {
+		        @Override
+		        public int compare(Event e1, Event e2) {
+		            if(e1.f == e2.f)
+		                return Short.compare(e2.s, e1.s);
+		            return Short.compare(e1.f, e2.f);
+		        }
+		    });
+		    int end = 0, count = 0;
+		    for(Event e : events) {
+		        if(e.s >= end) {
+		            end = e.f;
+		            ++count;
+		        }
+		    }
+		    output.append(count + "\n");
+		}
+        System.out.print(output);
+	}
+}
+
+class Event {
+    short s, f;
+    Event(short s, short f) {
+        this.s = s;
+        this.f = f;
+    }
+}
