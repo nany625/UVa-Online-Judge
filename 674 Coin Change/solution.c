115398593f69022445081fdef9b60ab9668b89f7
nany625 <jimmy19980625@gmail.com>
2024-08-13 22:56:04 +0800

    Create solution.c

---
+++ 674 Coin Change/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define MAX_AMOUNT 7489
+
+int dp[MAX_AMOUNT + 1], coins[] = {1, 5, 10, 25, 50};
+
+int main() {
+    dp[0] = 1;
+    for(int i = 0; i < 5; ++i) {
+        for(int j = coins[i]; j <= MAX_AMOUNT; ++j)
+            dp[j] += dp[j - coins[i]];
+    }
+    int money;
+    while(scanf("%d", &money) == 1)
+        printf("%d\n", dp[money]);
+    return 0;
+}
