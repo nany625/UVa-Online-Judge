7e66f34dd4b943f92334fc35e2cd37ba5d8c7c55
nany625 <jimmy19980625@gmail.com>
2024-08-13 11:52:13 +0800

    Create solution.java

---
+++ 439 Knight Moves/solution.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dRow = {2, 2, 1, 1, -1, -1, -2, -2}, dCol = {1, -1, 2, -2, 2, -2, 1, -1};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		StringBuilder output = new StringBuilder();
+		while((line = br.readLine()) != null) {
+		    if(line.charAt(0) == line.charAt(3) && line.charAt(1) == line.charAt(4)) {
+		        output.append("To get from " + line.charAt(0) + line.charAt(1) + " to " + line.charAt(3) + line.charAt(4) + " takes 0 knight moves.\n");
+		        continue;
+		    }
+		    ArrayList<Square> squares = new ArrayList<>();
+		    squares.add(new Square(line.charAt(1) - '1', line.charAt(0) - 'a'));
+		    int endRow = line.charAt(4) - '1', endCol = line.charAt(3) - 'a', currSize = 1, move = 1;
+		    boolean[][] visited = new boolean[8][8];
+		    visited[squares.get(0).row][squares.get(0).col] = true;
+		    boolean found = false;
+		    while(!found) {
+		        ArrayList<Square> nextSquares = new ArrayList<>();
+		        int nextSize = 0;
+		        for(int i = 0; i < currSize && !found; ++i) {
+		            for(int j = 0; j < 8; ++j) {
+		                int newRow = squares.get(i).row + dRow[j];
+		                int newCol = squares.get(i).col + dCol[j];
+		                if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+                            if(newRow == endRow && newCol == endCol) {
+                                output.append("To get from " + line.charAt(0) + line.charAt(1) + " to " + line.charAt(3) + line.charAt(4) + " takes " + move + " knight moves.\n");
+                                found = true;
+                            } else if(!visited[newRow][newCol]) {
+                                nextSquares.add(new Square(newRow, newCol));
+                                ++nextSize;
+                                visited[newRow][newCol] = true;
+                            }
+                        }
+		            }
+		        }
+		        squares = new ArrayList<>(nextSquares);
+		        currSize = nextSize;
+		        ++move;
+		    }
+		}
+        System.out.print(output);
+	}
+}
+
+class Square {
+    int row, col;
+    Square(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
