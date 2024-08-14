8cef9e08b004e77f6d9305f086c6beb9b1b8ab3e
nany625 <jimmy19980625@gmail.com>
2024-08-14 23:12:04 +0800

    Update solution.c

--- 10066 The Twin Towers/solution.c
+++ 10066 The Twin Towers/solution.c
@@ -8,18 +8,21 @@ int main() {
 	        scanf("%d", &tiles1[i]);
 	    for(int i = 0; i < N2; ++i)
 	        scanf("%d", &tiles2[i]);
-	    int L[N1 + 1][N2 + 1];
-	    for(int i = 0; i <= N1; ++i) {
-	        for(int j = 0; j <= N2; ++j) {
-	            if(i == 0 || j == 0)
-	                L[i][j] = 0;
-	            else if(tiles1[i - 1] == tiles2[j - 1])
-	                L[i][j] = L[i - 1][j - 1] + 1;
+	    int L[N2 + 1];
+	    for(int i = 0; i <= N2; ++i)
+	        L[i] = 0;
+	    for(int i = 0; i < N1; ++i) {
+	        int prev = 0;
+	        for(int j = 1; j <= N2; ++j) {
+	            int temp = L[j];
+	            if(tiles1[i] == tiles2[j - 1])
+	                L[j] = prev + 1;
 	            else
-	                L[i][j] = L[i - 1][j] > L[i][j - 1] ? L[i - 1][j] : L[i][j - 1];
+	                L[j] = L[j] > L[j - 1] ? L[j] : L[j - 1];
+	            prev = temp;
 	        }
 	    }
-	    printf("Twin Towers #%d\nNumber of Tiles : %d\n\n", ++twinTowers, L[N1][N2]);
+	    printf("Twin Towers #%d\nNumber of Tiles : %d\n\n", ++twinTowers, L[N2]);
 	}
 	return 0;
 }
