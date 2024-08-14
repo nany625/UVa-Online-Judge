f07b65bc54aa0d4d2be7522b101997f5ef39fd8a
nany625 <jimmy19980625@gmail.com>
2024-08-14 23:05:30 +0800

    Update solution.c

--- 10405 Longest Common Subsequence/solution.c
+++ 10405 Longest Common Subsequence/solution.c
@@ -7,18 +7,21 @@ int main() {
 	    str1[strcspn(str1, "\n")] = '\0';
 	    str2[strcspn(str2, "\n")] = '\0';
 	    int len1 = strlen(str1), len2 = strlen(str2);
-	    int L[len1 + 1][len2 + 1];
-	    for(int i = 0; i <= len1; ++i) {
-	        for(int j = 0; j <= len2; ++j) {
-	            if(i == 0 || j == 0)
-	                L[i][j] = 0;
-	            else if(str1[i - 1] == str2[j - 1])
-	                L[i][j] = L[i - 1][j - 1] + 1;
+	    int L[len2 + 1];
+	    for(int i = 0; i <= len2; ++i)
+	        L[i] = 0;
+	    for(int i = 0; i < len1; ++i) {
+	        int prev = 0;
+	        for(int j = 1; j <= len2; ++j) {
+	            int temp = L[j];
+	            if(str1[i] == str2[j - 1])
+	                L[j] = prev + 1;
 	            else
-	                L[i][j] = L[i - 1][j] > L[i][j - 1] ? L[i - 1][j] : L[i][j - 1];
+	                L[j] = L[j] > L[j - 1] ? L[j] : L[j - 1];
+	            prev = temp;
 	        }
 	    }
-	    printf("%d\n", L[len1][len2]);
+	    printf("%d\n", L[len2]);
 	}
 	return 0;
 }
