1a5d288dd3a8021ec595c8047c00ee720b1d7691
nany625 <jimmy19980625@gmail.com>
2024-08-14 23:07:52 +0800

    Update solution.java

--- 10405 Longest Common Subsequence/solution.java
+++ 10405 Longest Common Subsequence/solution.java
@@ -7,16 +7,19 @@ public class Main {
         StringBuilder output = new StringBuilder();
         while((s1 = br.readLine()) != null && (s2 = br.readLine()) != null) {
     	    int len1 = s1.length(), len2 = s2.length();
-    	    int[][] L = new int[len1 + 1][len2 + 1];
-    	    for(int i = 1; i <= len1; ++i) {
+    	    int[] L = new int[len2 + 1];
+    	    for(int i = 0; i < len1; ++i) {
+    	        int prev = 0;
     	        for(int j = 1; j <= len2; ++j) {
-    	            if(s1.charAt(i - 1) == s2.charAt(j - 1))
-    	                L[i][j] = L[i - 1][j - 1] + 1;
+    	            int temp = L[j];
+    	            if(s1.charAt(i) == s2.charAt(j - 1))
+    	                L[j] = prev + 1;
     	            else
-    	                L[i][j] = Math.max(L[i - 1][j], L[i][j - 1]);
+    	                L[j] = Math.max(L[j], L[j - 1]);
+    	            prev = temp;
     	        }
     	    }
-    	    output.append(L[len1][len2] + "\n");
+    	    output.append(L[len2] + "\n");
     	}
         System.out.print(output);
 	}
