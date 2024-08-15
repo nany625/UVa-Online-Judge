ce132d75217cf9fce0b2bc928e768c0e669faf5d
nany625 <jimmy19980625@gmail.com>
2024-08-15 22:13:50 +0800

    Create solution.java

---
+++ 10130 SuperSale/solution.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int T = (int)st.nval;
+        boolean firstCase = true;
+        StringBuilder output = new StringBuilder();
+        while(T-- > 0) {
+            st.nextToken();
+            int N = (int)st.nval;
+            Object[] objects = new Object[N];
+            for(int i = 0; i < N; ++i) {
+                objects[i] = new Object();
+                st.nextToken();
+                objects[i].P = (short)st.nval;
+                st.nextToken();
+                objects[i].W = (short)st.nval;
+            }
+            st.nextToken();
+            int G = (int)st.nval;
+            short[] MW = new short[G];
+            short capacity = 0;
+            for(int i = 0; i < G; ++i) {
+                st.nextToken();
+                capacity = (short)Math.max(capacity, MW[i] = (short)st.nval);
+            }
+            int[] dp = new int[capacity + 1];
+            for(Object o : objects) {
+                for(int j = capacity; j >= o.W; --j)
+                    dp[j] = Math.max(dp[j], dp[j - o.W] + o.P);
+            }
+            int maxValue = 0;
+            for(Short s : MW)
+                maxValue += dp[s];
+            output.append(maxValue + "\n");
+    	}
+        System.out.print(output);
+	}
+}
+
+class Object {
+    short P, W;
+}
