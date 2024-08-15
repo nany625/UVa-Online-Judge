b2040a686e44b12b2e516a190254773b17963821
nany625 <jimmy19980625@gmail.com>
2024-08-16 02:20:52 +0800

    Update solution.c

--- 1210 Sum of Consecutive Prime Numbers/solution.c
+++ 1210 Sum of Consecutive Prime Numbers/solution.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+#define MAX_NUM 10000
 #define MAX_PRIME_SIZE 1229
 
-short primes[MAX_PRIME_SIZE] = {2}, primesSumCount[10001];
+bool isComposite[MAX_NUM + 1];
+short primes[MAX_PRIME_SIZE] = {2}, primesSumCount[MAX_NUM + 1];
 int count = 1;
-
-bool isPrime(short n) {
-    short limit = sqrt(n);
-    for(int i = 1; i < count && primes[i] <= limit; ++i) {
-        if(n % primes[i] == 0)
-            return false;
-    }
-    return true;
-}
 
 int main() {
     for(short i = 3; count < MAX_PRIME_SIZE; i += 2) {
-        if(isPrime(i))
+        if(!isComposite[i]) {
             primes[count++] = i;
+            if(i <= 100) {
+                for(int j = i * i; j <= MAX_NUM; j += i)
+                    isComposite[j] = true;
+            }
+        }
     }
     for(int i = 0; i < MAX_PRIME_SIZE; ++i) {
         int sum = 0;
-        for(int j = i; j < MAX_PRIME_SIZE && (sum += primes[j]) <= 10000; ++j)
+        for(int j = i; j < MAX_PRIME_SIZE && (sum += primes[j]) <= MAX_NUM; ++j)
             ++primesSumCount[sum];
     }
     int num;
