bbdc6a9bf21e97bd1118369b1d282e4beb4ea631
nany625 <jimmy19980625@gmail.com>
2024-08-16 23:57:46 +0800

    Update solution.c

--- 147 Dollars/solution.c
+++ 147 Dollars/solution.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-long dp[30001];
-int coins[] = {5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000};
+long dp[6001];
+int coins[] = {1, 2, 4, 10, 20, 40, 100, 200, 400, 1000, 2000};
 
 int main() {
     dp[0] = 1;
     for(int i = 0; i < 11; ++i) {
-        for(int j = coins[i]; j <= 30000; ++j)
+        for(int j = coins[i]; j <= 6000; ++j)
             dp[j] += dp[j - coins[i]];
     }
     int a, b;
     while(scanf("%d.%d", &a, &b) && a + b > 0)
-        printf("%3d.%02d%17ld\n", a, b, dp[100 * a + b]);
+        printf("%3d.%02d%17ld\n", a, b, dp[20 * a + b / 5]);
 	return 0;
 }
