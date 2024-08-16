eb4042354d017798090bb10e09483a0b2266df69
nany625 <jimmy19980625@gmail.com>
2024-08-17 03:34:42 +0800

    Create solution.java

---
+++ 13181 Sleeping in hostels/solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String bed;
+        StringBuilder output = new StringBuilder();
+        while((bed = br.readLine()) != null) {
+            int len = bed.length(), prev = 0;
+            while(prev < len && bed.charAt(prev) != 'X')
+                ++prev;
+            int curr = prev + 1, max = prev - 1;
+            while(curr < len) {
+                if(bed.charAt(curr) == 'X') {
+                    max = Math.max(max, (curr - prev - 2) / 2);
+                    prev = curr;
+                }
+                ++curr;
+            }
+            max = Math.max(max, (len - prev - 2));
+            output.append(max + "\n");
+        }
+        System.out.print(output);
+	}
+}
