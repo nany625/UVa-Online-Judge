3506a6cc463d04193cb0de2a82a15a40705d56c4
nany625 <jimmy19980625@gmail.com>
2024-08-16 15:54:32 +0800

    Create solution.java

---
+++ 514 Rails/solution.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    StreamTokenizer st = new StreamTokenizer(System.in);
+        int N;
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0) {
+            int prev;
+            while(st.nextToken() == StreamTokenizer.TT_NUMBER && (prev = (int)st.nval) != 0) {
+                int max = prev;
+                boolean YES = true;
+                for(int i = 1; i < N; ++i) {
+                    st.nextToken();
+                    int curr = (int)st.nval;
+                    if(YES) {
+                        if(curr > prev && curr < max)
+                            YES = false;
+                        else {
+                            prev = curr;
+                            max = Math.max(max, curr);
+                        }
+                    }
+                }
+                output.append(YES ? "Yes\n" : "No\n");
+            }
+            output.append("\n");
+    	}
+        System.out.print(output);
+	}
+}
