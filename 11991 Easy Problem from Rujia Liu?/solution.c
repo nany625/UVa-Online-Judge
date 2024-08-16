a5013dad8218ea47401214106b69ddd50b9aa315
nany625 <jimmy19980625@gmail.com>
2024-08-17 02:14:36 +0800

    Create solution.c

---
+++ 11991 Easy Problem from Rujia Liu?/solution.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_NUM 1000000
+
+int main() {
+    int **index = (int**)malloc((MAX_NUM + 1) * sizeof(int*)), size[MAX_NUM + 1];
+    for(int i = 1; i <= MAX_NUM; ++i)
+        index[i] = NULL;
+    int n, m;
+    char *buffer = NULL;
+    size_t bufsize = 0;
+    while(scanf("%d %d", &n, &m) == 2) {
+        getchar();
+        memset(size, 0, sizeof(size));
+        getline(&buffer, &bufsize, stdin);
+        char *token = strtok(buffer, " ");
+        int pos = 0;
+        while(token) {
+            int num = atoi(token);
+            index[num] = (int*)realloc(index[num], (size[num] + 1) * sizeof(int));
+            index[num][size[num]++] = ++pos;
+            token = strtok(NULL, " ");
+        }
+        while(m--) {
+            int k, v;
+            scanf("%d %d", &k, &v);
+            printf("%d\n", size[v] >= k ? index[v][k - 1] : 0);
+        }
+    }
+    free(buffer);
+    for(int i = 1; i <= MAX_NUM; ++i)
+        free(index[i]);
+    free(index);
+    return 0;
+}
