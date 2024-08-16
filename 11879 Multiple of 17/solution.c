09504ff29dcc5519b4d180781eb7740a4d543911
nany625 <jimmy19980625@gmail.com>
2024-08-17 02:58:53 +0800

    Update solution.c

--- 11879 Multiple of 17/solution.c
+++ 11879 Multiple of 17/solution.c
@@ -10,26 +10,8 @@ int main() {
             int num = atoi(n);
             printf("%d\n", (num / 10 - num % 10 * 5) % 17 == 0);
         } else {
-            int subtract = 5 * (n[len - 1] - '0');
-            if(n[len - 2] - '0' >= subtract % 10)
-                n[len - 2] -= subtract % 10;
-            else {
-                n[len - 2] += 10 - subtract % 10;
-                --n[len - 3];
-            }
-            if(n[len - 3] - '0' >= subtract / 10)
-                n[len - 3] -= subtract / 10;
-            else {
-                n[len - 3] += 10 - subtract / 10;
-                --n[len - 4];
-            }
-            int i = len - 4;
-            while(n[i] - '0' < 0) {
-                n[i] += 10;
-                --n[i--];
-            }
             int dividend = 0;
-            for(i = 0; i < len - 1; ++i)
+            for(int i = 0; i < len; ++i)
                 dividend = (10 * dividend + n[i] - '0') % 17;
             printf("%d\n", dividend == 0);
         }
