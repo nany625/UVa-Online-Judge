66c53439ebaba2a761e0eb557b31c93f0b9de12e
nany625 <jimmy19980625@gmail.com>
2024-08-17 03:07:07 +0800

    Create solution.java

---
+++ 11879 Multiple of 17/solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n;
+        StringBuilder output = new StringBuilder();
+        while((n = br.readLine()) != null && n.charAt(0) != '0') {
+            int len = n.length(), dividend = 0;
+            for(int i = 0; i < len; ++i)
+                dividend = (10 * dividend + n.charAt(i) - '0') % 17;
+            output.append((dividend == 0 ? 1 : 0) + "\n");
+        }
+        System.out.print(output);
+	}
+}
