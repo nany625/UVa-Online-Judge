23c5f3bc28751f64801850cc9905a05fcc0f1e29
nany625 <jimmy19980625@gmail.com>
2024-08-17 02:22:21 +0800

    Update solution.c

--- 11991 Easy Problem from Rujia Liu?/solution.c
+++ 11991 Easy Problem from Rujia Liu?/solution.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #define MAX_NUM 1000000
 
+int **pos, size[MAX_NUM + 1];
+
 int main() {
-    int **index = (int**)malloc((MAX_NUM + 1) * sizeof(int*)), size[MAX_NUM + 1];
+    pos = (int**)malloc((MAX_NUM + 1) * sizeof(int*));
     for(int i = 1; i <= MAX_NUM; ++i)
-        index[i] = NULL;
+        pos[i] = NULL;
     int m;
     char *buffer = NULL;
     size_t bufsize = 0;
@@ -15,22 +17,22 @@ int main() {
         memset(size, 0, sizeof(size));
         getline(&buffer, &bufsize, stdin);
         char *token = strtok(buffer, " ");
-        int pos = 0;
+        int index = 0;
         while(token) {
             int num = atoi(token);
-            index[num] = (int*)realloc(index[num], (size[num] + 1) * sizeof(int));
-            index[num][size[num]++] = ++pos;
+            pos[num] = (int*)realloc(pos[num], (size[num] + 1) * sizeof(int));
+            pos[num][size[num]++] = ++index;
             token = strtok(NULL, " ");
         }
         while(m--) {
             int k, v;
             scanf("%d %d", &k, &v);
-            printf("%d\n", size[v] >= k ? index[v][k - 1] : 0);
+            printf("%d\n", size[v] >= k ? pos[v][k - 1] : 0);
         }
     }
     free(buffer);
     for(int i = 1; i <= MAX_NUM; ++i)
-        free(index[i]);
-    free(index);
+        free(pos[i]);
+    free(pos);
     return 0;
 }
