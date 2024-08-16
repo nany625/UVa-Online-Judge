4a5b65e40c865e932446179733dc80aebf69234a
nany625 <jimmy19980625@gmail.com>
2024-08-17 01:46:31 +0800

    Create solution.c

---
+++ 11877 The Coco-Cola Store/solution.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    while(scanf("%d", &n) && n != 0) {
+        int exchange = (n - 3) / 2 + 1;
+        printf("%d\n", exchange + ((n - 2 * exchange) % 3 == 2));
+    }
+    return 0;
+}
