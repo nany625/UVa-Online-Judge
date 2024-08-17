125921771382a1f623fc05c85e1d23063b833420
nany625 <jimmy19980625@gmail.com>
2024-08-18 05:08:36 +0800

    Create solution.java

---
+++ 11369 Shopaholic/solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int t = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(t-- > 0) {
+            st.nextToken();
+            int n = (int)st.nval;
+            int[] p = new int[n];
+            for(int i = 0; i < n; ++i) {
+                st.nextToken();
+                p[i] = (int)st.nval;
+            }
+            Arrays.sort(p);
+            int save = 0;
+            for(int i = n % 3; i < n; i += 3)
+                save += p[i];
+            output.append(save + "\n");
+        }
+        System.out.print(output);
+	}
+}
