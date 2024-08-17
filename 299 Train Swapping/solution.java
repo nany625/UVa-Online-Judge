7e9c1b85f6d523e301fcf701585c94fead54c682
nany625 <jimmy19980625@gmail.com>
2024-08-17 14:42:57 +0800

    Update solution.java

--- 299 Train Swapping/solution.java
+++ 299 Train Swapping/solution.java
@@ -1,31 +1,3 @@
-// #解法一
-import java.io.*;
-
-public class Main {
-	public static void main(String[] args) throws IOException {
-        StreamTokenizer st = new StreamTokenizer(System.in);
-        st.nextToken();
-        int N = (int)st.nval;
-        StringBuilder output = new StringBuilder();
-        while(N-- > 0) {
-            st.nextToken();
-            int L = (int)st.nval, swap = 0;
-            int[] a = new int[L];
-	        for(int i = 0; i < L; ++i) {
-                st.nextToken();
-	            a[i] = (int)st.nval;
-                for(int j = 0; j < i; ++j) {
-                    if(a[j] > a[i])
-                        ++swap;
-                }
-	        }
-	        output.append("Optimal train swapping takes " + swap + " swaps.\n");
-		}
-        System.out.print(output);
-	}
-}
-
-// #解法二
 import java.io.*;
 
 public class Main {
