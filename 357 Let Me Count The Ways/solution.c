03765d55e0327a43bce66e52e0a90de5a9ea93f6
nany625 <jimmy19980625@gmail.com>
2024-08-18 03:38:16 +0800

    Create solution.c

---
+++ 357 Let Me Count The Ways/solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define MAX_AMOUNT 30000
+
+long dp[MAX_AMOUNT + 1];
+int coins[] = {1, 5, 10, 25, 50};
+
+int main() {
+    dp[0] = 1;
+    for(int i = 0; i < 5; ++i) {
+        for(int j = coins[i]; j <= MAX_AMOUNT; ++j)
+            dp[j] += dp[j - coins[i]];
+    }
+    int n;
+    while(scanf("%d", &n) == 1) {
+        if(dp[n] == 1)
+            printf("There is only 1 way to produce %d cents change.\n", n);
+        else
+            printf("There are %ld ways to produce %d cents change.\n", dp[n], n);
+    }
+    return 0;
+}
