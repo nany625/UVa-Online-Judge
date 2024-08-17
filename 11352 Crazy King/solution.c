c47754e97b61afa7a6227cd953cfa965a0ecc5cf
nany625 <jimmy19980625@gmail.com>
2024-08-18 06:42:38 +0800

    Create solution.c

---
+++ 11352 Crazy King/solution.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int horsedRow[] = {2, 2, 1, 1, -1, -1, -2, -2}, horsedCol[] = {1, -1, 2, -2, 2, -2, 1, -1};
+int kingdRow[] = {1, 1, 1, -1, -1, -1, 0, 0}, kingdCol[] = {1, 0, -1, 1, 0, -1, 1, -1};
+
+typedef struct {
+    int row, col;
+} Coordinate;
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    while(T--) {
+        int M, N;
+        scanf("%d %d", &M, &N);
+        char **forest = (char**)malloc(M * sizeof(char*));
+        for(int i = 0; i < M; ++i) {
+            forest[i] = (char*)malloc((N + 1) * sizeof(char));
+            scanf("%s", forest[i]);
+        }
+        for(int i = 0; i < M; ++i) {
+            for(int j = 0; j < N; ++j) {
+                if(forest[i][j] == 'Z') {
+                    for(int k = 0; k < 8; ++k) {
+                        int newRow = i + horsedRow[k], newCol = j + horsedCol[k];
+                        if(newRow >= 0 && newRow < M && newCol >= 0 && newCol < N && forest[newRow][newCol] == '.')
+                            forest[newRow][newCol] = 'z';
+                    }
+                }
+            }
+        }
+        int kingRow = 0, kingCol = 0;
+        while(forest[kingRow][kingCol] != 'A') {
+            ++kingCol;
+            if(kingCol == N) {
+                ++kingRow;
+                kingCol = 0;
+            }
+        }
+        Coordinate **trip = (Coordinate**)malloc(sizeof(Coordinate*));
+        trip[0] = (Coordinate*)malloc(sizeof(Coordinate));
+        trip[0][0].row = kingRow;
+        trip[0][0].col = kingCol;
+        bool visited[M][N], safe = false;
+        memset(visited, 0, sizeof(visited));
+        visited[kingRow][kingCol] = true;
+        int currSize = 1, length = 1;
+        while(currSize > 0 && !safe) {
+            trip = (Coordinate**)realloc(trip, (length + 1) * sizeof(Coordinate*));
+            trip[length] = NULL;
+            int nextSize = 0;
+            for(int i = 0; i < currSize && !safe; ++i) {
+                for(int j = 0; j < 8 && !safe; ++j) {
+                    int newRow = trip[length - 1][i].row + kingdRow[j];
+                    int newCol = trip[length - 1][i].col + kingdCol[j];
+                    if(newRow >= 0 && newRow < M && newCol >= 0 && newCol < N) {
+                        if(forest[newRow][newCol] == 'B') {
+                            printf("Minimal possible length of a trip is %d\n", length);
+                            safe = true;
+                        } else if(forest[newRow][newCol] == '.' && !visited[newRow][newCol]) {
+                            trip[length] = (Coordinate*)realloc(trip[length], (nextSize + 1) * sizeof(Coordinate));
+                            trip[length][nextSize].row = newRow;
+                            trip[length][nextSize++].col = newCol;
+                            visited[newRow][newCol] = true;
+                        }
+                    }
+                }
+            }
+            currSize = nextSize;
+            ++length;
+        }
+        if(!safe)
+            puts("King Peter, you can't go now!");
+        for(int i = 0; i < length; ++i)
+            free(trip[i]);
+        free(trip);
+    }
+    return 0;
+}
