a8123c97252d67c00eae0733bbb265621ae5cf03
nany625 <jimmy19980625@gmail.com>
2024-08-18 03:46:37 +0800

    Create solution.java

---
+++ 357 Let Me Count The Ways/solution.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    static long[] dp = new long[30001];
+    static int[] coins = {1, 5, 10, 25, 50};
+	public static void main(String[] args) throws IOException {
+		dp[0] = 1;
+		for(int coin : coins) {
+            for(int i = coin; i <= 30000; ++i)
+                dp[i] += dp[i - coin];
+        }
+		StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int n = (int)st.nval;
+            if(dp[n] == 1)
+                output.append("There is only 1 way to produce " + n +  " cents change.\n");
+            else
+	            output.append("There are " + dp[n] + " ways to produce " + n +  " cents change.\n");
+        }
+        System.out.print(output);
+	}
+}
