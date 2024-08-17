cf5518d7151f7571407994cccbd50c3da14a2019
nany625 <jimmy19980625@gmail.com>
2024-08-17 21:26:57 +0800

    Create solution.java

---
+++ 10079 Pizza Cutting/solution.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+    	StreamTokenizer st = new StreamTokenizer(System.in);
+    	long N;
+    	StringBuilder output = new StringBuilder();
+    	while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (long)st.nval) >= 0)
+	        output.append((1 + N) * N / 2 + 1 + "\n");
+	    System.out.print(output);
+	}
+}
