905640076ad0de3cfb16a2e0b1e04699df45ab38
nany625 <jimmy19980625@gmail.com>
2024-08-17 14:42:39 +0800

    Update solution.c

--- 299 Train Swapping/solution.c
+++ 299 Train Swapping/solution.c
@@ -1,26 +1,3 @@
-// 解法一
-#include <stdio.h>
-
-int main() {
-    int N;
-    scanf("%d", &N);
-    while(N--) {
-        int L;
-        scanf("%d", &L);
-        int a[L], swap = 0;
-        for(int i = 0; i < L; ++i) {
-            scanf("%d", &a[i]);
-            for(int j = 0; j < i; ++j) {
-                if(a[j] > a[i])
-                    ++swap;
-            }
-        }
-        printf("Optimal train swapping takes %d swaps.\n", swap);
-    }
-    return 0;
-}
-
-// #解法二
 #include <stdio.h>
 
 void MergeSort(int *array, int *reg, int left, int right, int *swap) {
