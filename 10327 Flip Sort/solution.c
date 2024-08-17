cd31ee6b4fedda53c79d1b8b6002d1fd7513cf8a
nany625 <jimmy19980625@gmail.com>
2024-08-17 14:31:50 +0800

    Create solution.c

---
+++ 10327 Flip Sort/solution.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void MergeSort(int *array, int *reg, int left, int right, int *swap) {
+    if(left < right) {
+        int mid = left + (right - left) / 2;
+        MergeSort(array, reg, left, mid, swap);
+        MergeSort(array, reg, mid + 1, right, swap);
+        int leftPointer = left, rightPointer = mid + 1;
+        for(int i = left; i <= right; ++i) {
+            if(leftPointer > mid) {
+                reg[i] = array[rightPointer++];
+                *swap += mid - leftPointer + 1;
+            } else if(rightPointer > right)
+                reg[i] = array[leftPointer++];
+            else if(array[leftPointer] <= array[rightPointer])
+                reg[i] = array[leftPointer++];
+            else {
+                reg[i] = array[rightPointer++];
+                *swap += mid - leftPointer + 1;
+            }
+        }
+        for(int i = left; i <= right; ++i)
+            array[i] = reg[i];
+    }
+}
+
+int main() {
+    int N;
+    while(scanf("%d", &N) == 1) {
+        int array[N], reg[N];
+        for(int i = 0; i < N; ++i)
+            scanf("%d", &array[i]);
+        int swap = 0;
+        MergeSort(array, reg, 0, N - 1, &swap);
+        printf("Minimum exchange operations : %d\n", swap);
+    }
+    return 0;
+}
