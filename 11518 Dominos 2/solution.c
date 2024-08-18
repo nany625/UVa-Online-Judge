714b3c8bf5df19cb249657daa58bb3078ac9e1ad
nany625 <jimmy19980625@gmail.com>
2024-08-18 17:11:22 +0800

    Update solution.c

--- 11518 Dominos 2/solution.c
+++ 11518 Dominos 2/solution.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdbool.h>
+#define MAX_NUM 10000
 
 typedef struct {
-    short numVertices, **knockList, *knockCount;
-    bool *visited;
+    short numVertices, **knockList, knockCount[MAX_NUM + 1];
+    bool visited[MAX_NUM + 1];
 } Graph;
-
-Graph graph;
 
 void dfs(Graph *graph, int startVertex, int *count) {
     graph->visited[startVertex] = true;
@@ -19,22 +19,23 @@ void dfs(Graph *graph, int startVertex, int *count) {
 }
 
 int main() {
+    Graph graph;
+    graph.knockList = (short**)malloc((MAX_NUM + 1) * sizeof(short*));
+    for(short i = 1; i <= MAX_NUM; ++i)
+        graph.knockList[i] = NULL;
     int cases;
     scanf("%d", &cases);
     while(cases--) {
         short m, l;
         scanf("%hd %hd %hd", &graph.numVertices, &m, &l);
-        graph.knockList = (short**)malloc((graph.numVertices + 1) * sizeof(short*));
-        for(short i = 1; i <= graph.numVertices; ++i)
-            graph.knockList[i] = NULL;
-        graph.knockCount = (short*)calloc(graph.numVertices + 1, sizeof(short));
+        memset(graph.knockCount, 0, (graph.numVertices + 1) * sizeof(short));
         while(m--) {
             short x, y;
             scanf("%hd %hd", &x, &y);
             graph.knockList[x] = (short*)realloc(graph.knockList[x], (graph.knockCount[x] + 1) * sizeof(short));
             graph.knockList[x][graph.knockCount[x]++] = y;
         }
-        graph.visited = (bool*)calloc(graph.numVertices + 1, sizeof(bool));
+        memset(graph.visited, 0, (graph.numVertices + 1) * sizeof(bool));
         int count = 0;
         while(l--) {
             short z;
@@ -43,13 +44,9 @@ int main() {
                 dfs(&graph, z, &count);
         }
         printf("%d\n", count);
-        for(short i = 1; i <= graph.numVertices; ++i) {
-            if(graph.knockCount[i] > 0)
-                free(graph.knockList[i]);
-        }
-        free(graph.knockList);
-        free(graph.knockCount);
-        free(graph.visited);
     }
+    for(short i = 0; i <= graph.numVertices; ++i)
+        free(graph.knockList[i]);
+    free(graph.knockList);
 	return 0;
 }
