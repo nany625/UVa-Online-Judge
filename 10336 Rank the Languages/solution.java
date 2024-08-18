ac608977264c5f2adeae5de661ffdaf1f6b88c72
nany625 <jimmy19980625@gmail.com>
2024-08-18 17:40:09 +0800

    Update solution.java

--- 10336 Rank the Languages/solution.java
+++ 10336 Rank the Languages/solution.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] dRow = {1, 0, 0, -1}, dCol = {0, 1, -1, 0};
 	public static void main(String[] args) throws IOException {
 	    StreamTokenizer st = new StreamTokenizer(System.in);
         st.nextToken();
@@ -46,14 +47,11 @@ public class Main {
 	static void dfs(char[][] map, int row, int rowLimit, int col, int colLimit, char language) {
         if(map[row][col] == language) {
             map[row][col] = ' ';
-            if(row - 1 >= 0)
-                dfs(map, row - 1, rowLimit, col, colLimit, language);
-            if(col - 1 >= 0)
-                dfs(map, row, rowLimit, col - 1, colLimit, language);
-            if(row + 1 < rowLimit)
-                dfs(map, row + 1, rowLimit, col, colLimit, language);
-            if(col + 1 < colLimit)
-                dfs(map, row, rowLimit, col + 1, colLimit, language);
+            for(int i = 0; i < 4; ++i) {
+                int newRow = row + dRow[i], newCol = col + dCol[i];
+                if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit)
+                    dfs(map, newRow, rowLimit, newCol, colLimit, language);
+            }
         }
     }
 }
