88d079fa13da130dfc27e3b8414d7dcbe85d0a82
nany625 <jimmy19980625@gmail.com>
2024-08-20 03:50:46 +0800

    Update solution.java

--- 256 Quirksome Squares/solution.java
+++ 256 Quirksome Squares/solution.java
@@ -1,4 +1,24 @@
-// #解法一(偷吃步)
+// #解法一(正規演算法)
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int n = (int)st.nval;
+            for(int i = 0; i < Math.pow(10, n / 2); ++i) {
+            	int split1 = i * i / (int)Math.pow(10, n / 2);
+            	int split2 = i * i % (int)Math.pow(10, n / 2);
+            	if(i * i == (split1 + split2) * (split1 + split2))
+            		output.append(String.format("%0" + n + "d\n", i * i));
+           	}
+		}
+        System.out.print(output);
+	}
+}
+
+// #解法二(捷徑)
 import java.io.*;
 
 public class Main {
@@ -14,26 +34,6 @@ public class Main {
     			output.append("000000\n000001\n088209\n494209\n998001\n");
 			else if((int)st.nval == 8)
     			output.append("00000000\n00000001\n04941729\n07441984\n24502500\n25502500\n52881984\n60481729\n99980001\n");
-		}
-        System.out.print(output);
-	}
-}
-
-// #解法二(正規演算法)
-import java.io.*;
-
-public class Main {
-	public static void main(String[] args) throws IOException {
-        StreamTokenizer st = new StreamTokenizer(System.in);
-        StringBuilder output = new StringBuilder();
-        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-            int n = (int)st.nval;
-            for(int i = 0; i < Math.pow(10, n / 2); ++i) {
-            	int split1 = i * i / (int)Math.pow(10, n / 2);
-            	int split2 = i * i % (int)Math.pow(10, n / 2);
-            	if(i * i == (split1 + split2) * (split1 + split2))
-            		output.append(String.format("%0" + n + "d\n", i * i));
-           	}
 		}
         System.out.print(output);
 	}
