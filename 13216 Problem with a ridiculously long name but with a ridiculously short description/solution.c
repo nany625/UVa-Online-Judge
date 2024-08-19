b4fecc63394b5d11d125a1b74f9e3f934e329858
nany625 <jimmy19980625@gmail.com>
2024-08-19 19:30:08 +0800

    Create solution.c

---
+++ 13216 Problem with a ridiculously long name but with a ridiculously short description/solution.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ans[] = {76, 16, 56, 96, 36};
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    char n[1002];
+    while(T--) {
+        scanf("%s", n);
+        if(n[0] == '0')
+            puts("1");
+        else if(atoi(n) == 1)
+            puts("66");
+        else {
+            int power = n[strlen(n) - 1] - '0';
+            printf("%d\n", ans[power % 5]);
+        }
+    }
+	return 0;
+}
