a6f5f876c695daf8afaf7a48f78e19bf6ad85785
nany625 <jimmy19980625@gmail.com>
2024-08-19 10:49:05 +0800

    Create solution.c

---
+++ 11727 Cost Cutting/solution.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(short *a, short *b) {
+    short temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    for(int i = 1; i <= T; ++i) {
+        short salary[3];
+        for(int j = 0; j < 3; ++j)
+            scanf("%hd", &salary[j]);
+        if(salary[0] > salary[1])
+            swap(&salary[0], &salary[1]);
+        if(salary[1] > salary[2])
+            swap(&salary[1], &salary[2]);
+        printf("Case %d: %d\n", i, salary[0] > salary[1] ? salary[0] : salary[1]);
+    }
+	return 0;
+}
