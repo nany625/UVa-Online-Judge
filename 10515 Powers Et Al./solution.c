4128bcd17d5bcaf2b8d8e17251a443e096832aa3
nany625 <jimmy19980625@gmail.com>
2024-08-19 08:10:10 +0800

    Update solution.c

--- 10515 Powers Et Al./solution.c
+++ 10515 Powers Et Al./solution.c
@@ -7,20 +7,20 @@ int cycle[] = {4, 4, 2, 0, 0, 4, 4, 2};
 int main() {
     char m[102], n[102];
     while(scanf("%s %s", m, n) && (m[0] != '0' || n[0] != '0')) {
-        char base = m[strlen(m) - 1];
-        if(base == '0')
+        int base = m[strlen(m) - 1] - '0';
+        if(base == 0)
             puts("0");
-        else if(base == '1' || n[0] == '0')
+        else if(base == 1 || n[0] == '0')
             puts("1");
-        else if(base == '5' || base == '6')
-            printf("%c\n", base);
+        else if(base == 5 || base == 6)
+            printf("%d\n", base);
         else {
-            int len = strlen(n), N = 0;
+            int len = strlen(n), N;
             if(len == 1)
                 N = n[0] - '0';
             else
                 N = 10 * (n[len - 2] - '0') + n[len - 1] - '0';
-            printf("%c\n", lastDigit[base - '0' - 2][N % cycle[base - '0' - 2]]);
+            printf("%c\n", lastDigit[base - 2][N % cycle[base - 2]]);
         }
     }
 	return 0;
