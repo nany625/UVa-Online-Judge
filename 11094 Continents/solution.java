51f6cffb7996a690d1f55ebbd528aaa59b4ac33f
nany625 <jimmy19980625@gmail.com>
2024-08-21 04:52:00 +0800

    Update solution.java

--- 11094 Continents/solution.java
+++ 11094 Continents/solution.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public class Main {
     static int[] dRow = {1, 0, 0, -1}, dCol = {0, 1, -1, 0};
+    static char[][] map = new char[20][20];
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    StreamTokenizer st = new StreamTokenizer(br);
@@ -10,7 +11,6 @@ public class Main {
             int M = (int)st.nval;
             st.nextToken();
             int N = (int)st.nval;
-            char[][] map = new char[M][N];
             for(int i = 0; i < M; ++i)
                 map[i] = br.readLine().toCharArray();
             st.nextToken();
@@ -18,13 +18,13 @@ public class Main {
             st.nextToken();
             int Y = (int)st.nval;
             char region = map[X][Y];
-            dfs(map, X, M, Y, N, region);
+            dfs(X, M, Y, N, region);
             int max = 0;
             for(int i = 0; i < M; ++i) {
                 for(int j = 0; j < N; ++j) {
                     if(map[i][j] == region) {
                         int[] area = new int[1];
-                        capture(map, i, M, j, N, region, area);
+                        capture(i, M, j, N, region, area);
                         max = Math.max(max, area[0]);
                     }
                 }
@@ -34,33 +34,33 @@ public class Main {
         System.out.print(output);
 	}
 	
-	static void dfs(char[][] map, int row, int rowLimit, int col, int colLimit, char region) {
+	static void dfs(int row, int rowLimit, int col, int colLimit, char region) {
         if(map[row][col] == region) {
             map[row][col] = ' ';
             for(int i = 0; i < 4; ++i) {
             int newRow = row + dRow[i], newCol = col + dCol[i];
                 if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit)
-                    dfs(map, newRow, rowLimit, newCol, colLimit, region);
+                    dfs(newRow, rowLimit, newCol, colLimit, region);
                 else if(newCol == -1)
-                    dfs(map, newRow, rowLimit, colLimit - 1, colLimit, region);
+                    dfs(newRow, rowLimit, colLimit - 1, colLimit, region);
                 else if(newCol == colLimit)
-                    dfs(map, newRow, rowLimit, 0, colLimit, region);
+                    dfs(newRow, rowLimit, 0, colLimit, region);
             }
         }
     }
     
-    static void capture(char[][] map, int row, int rowLimit, int col, int colLimit, char region, int[] area) {
+    static void capture(int row, int rowLimit, int col, int colLimit, char region, int[] area) {
         if(map[row][col] == region) {
             map[row][col] = ' ';
             ++area[0];
             for(int i = 0; i < 4; ++i) {
                 int newRow = row + dRow[i], newCol = col + dCol[i];
                 if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit)
-                    capture(map, newRow, rowLimit, newCol, colLimit, region, area);
+                    capture(newRow, rowLimit, newCol, colLimit, region, area);
                 else if(newCol == -1)
-                    capture(map, newRow, rowLimit, colLimit - 1, colLimit, region, area);
+                    capture(newRow, rowLimit, colLimit - 1, colLimit, region, area);
                 else if(newCol == colLimit)
-                    capture(map, newRow, rowLimit, 0, colLimit, region, area);
+                    capture(newRow, rowLimit, 0, colLimit, region, area);
             }
         }
     }
