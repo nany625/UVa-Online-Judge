9eccce851ddb4ef9e23ad31efac82ce73237644a
nany625 <jimmy19980625@gmail.com>
2024-08-21 06:43:20 +0800

    Create solution.java

---
+++ 10285 Longest Run on a Snowboard/solution.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] dRow = {1, 0, 0, -1}, dCol = {0, 1, -1, 0};
+    static short[][] height = new short[100][100], memo = new short[100][100];
+    public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int N = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        while(N-- > 0) {
+            st.nextToken();
+            output.append(st.sval + ": ");
+            st.nextToken();
+            int R = (int)st.nval;
+            st.nextToken();
+            int C = (int)st.nval;
+            for(int i = 0; i < R; ++i) {
+                for(int j = 0; j < C; ++j) {
+                    st.nextToken();
+                    height[i][j] = (short)st.nval;
+                }
+            }
+            for(int i = 0; i < R; ++i)
+                Arrays.fill(memo[i], (short)0);
+            short max = 1;
+            for(int i = 0; i < R; ++i) {
+                for(int j = 0; j < C; ++j) {
+                    short length = dfs(i, R, j, C);
+                    max = (short)Math.max(max, length);
+                }
+            }
+            output.append(max + "\n");
+        }
+        System.out.print(output);
+    }
+    
+    static short dfs(int row, int rowLimit, int col, int colLimit) {
+        if(memo[row][col] != 0)
+            return memo[row][col];
+        short maxLength = 1;
+        for(int i = 0; i < 4; ++i) {
+            int newRow = row + dRow[i], newCol = col + dCol[i];
+            if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit && height[newRow][newCol] < height[row][col]) {
+                short temp = (short)(1 + dfs(newRow, rowLimit, newCol, colLimit));
+                maxLength = (short)Math.max(maxLength, temp);
+            }
+        }
+        return memo[row][col] = maxLength;
+    }
+}
