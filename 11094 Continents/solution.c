c28bc64ba75ad4be90aae28827cd4913655dd0ff
nany625 <jimmy19980625@gmail.com>
2024-08-21 04:47:45 +0800

    Update solution.c

--- 11094 Continents/solution.c
+++ 11094 Continents/solution.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int dRow[] = {1, 0, 0, -1}, dCol[] = {0, 1, -1, 0};
+char map[20][21];
 
-void dfs(char **map, int row, int rowLimit, int col, int colLimit, char region) {
+void dfs(int row, int rowLimit, int col, int colLimit, char region) {
     if(map[row][col] == region) {
         map[row][col] = ' ';
         for(int i = 0; i < 4; ++i) {
             int newRow = row + dRow[i], newCol = col + dCol[i];
             if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit)
-                dfs(map, newRow, rowLimit, newCol, colLimit, region);
+                dfs(newRow, rowLimit, newCol, colLimit, region);
             else if(newCol == -1)
-                dfs(map, newRow, rowLimit, colLimit - 1, colLimit, region);
+                dfs(newRow, rowLimit, colLimit - 1, colLimit, region);
             else if(newCol == colLimit)
-                dfs(map, newRow, rowLimit, 0, colLimit, region);
+                dfs(newRow, rowLimit, 0, colLimit, region);
         }
     }
 }
 
-void capture(char **map, int row, int rowLimit, int col, int colLimit, char region, int *area) {
+void capture(int row, int rowLimit, int col, int colLimit, char region, int *area) {
     if(map[row][col] == region) {
         map[row][col] = ' ';
         ++(*area);
         for(int i = 0; i < 4; ++i) {
             int newRow = row + dRow[i], newCol = col + dCol[i];
             if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit)
-                capture(map, newRow, rowLimit, newCol, colLimit, region, area);
+                capture(newRow, rowLimit, newCol, colLimit, region, area);
             else if(newCol == -1)
-                capture(map, newRow, rowLimit, colLimit - 1, colLimit, region, area);
+                capture(newRow, rowLimit, colLimit - 1, colLimit, region, area);
             else if(newCol == colLimit)
-                capture(map, newRow, rowLimit, 0, colLimit, region, area);
+                capture(newRow, rowLimit, 0, colLimit, region, area);
         }
     }
 }
@@ -37,29 +37,23 @@ void capture(char **map, int row, int rowLimit, int col, int colLimit, char regi
 int main() {
     int M, N;
     while(scanf("%d %d", &M, &N) == 2) {
-        char **map = (char**)malloc(M * sizeof(char*));
-        for(int i = 0; i < M; ++i) {
-            map[i] = (char*)malloc((N + 1) * sizeof(char));
+        for(int i = 0; i < M; ++i)
             scanf("%s", map[i]);
-        }
         int X, Y;
         scanf("%d %d", &X, &Y);
         char region = map[X][Y];
-        dfs(map, X, M, Y, N, region);
+        dfs(X, M, Y, N, region);
         int max = 0;
         for(int i = 0; i < M; ++i) {
             for(int j = 0; j < N; ++j) {
                 if(map[i][j] == region) {
                     int area = 0;
-                    capture(map, i, M, j, N, region, &area);
+                    capture(i, M, j, N, region, &area);
                     max = max > area ? max : area;
                 }
             }
         }
         printf("%d\n", max);
-        for(int i = 0; i < M; ++i)
-            free(map[i]);
-        free(map);
     }
 	return 0;
 }
