d33a75b9c9c2bbe572224c486b11ecbf546b7b24
nany625 <jimmy19980625@gmail.com>
2024-08-21 06:25:51 +0800

    Update solution.c

--- 10285 Longest Run on a Snowboard/solution.c
+++ 10285 Longest Run on a Snowboard/solution.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int dRow[] = {1, 0, 0, -1}, dCol[] = {0, 1, -1, 0};
-short height[100][100];
+short height[100][100], memo[100][100];
 
-void dfs(int row, int rowLimit, int col, int colLimit, int *max, int curr) {
+int dfs(int row, int rowLimit, int col, int colLimit) {
+    if(memo[row][col] != 0)
+        return memo[row][col];
+    int maxLength = 1;
     for(int i = 0; i < 4; ++i) {
         int newRow = row + dRow[i], newCol = col + dCol[i];
-        if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit && height[newRow][newCol] < height[row][col])
-            dfs(newRow, rowLimit, newCol, colLimit, max, curr + 1);
+        if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit && height[newRow][newCol] < height[row][col]) {
+            int temp = 1 + dfs(newRow, rowLimit, newCol, colLimit);
+            maxLength = maxLength > temp ? maxLength : temp;
+        }
     }
-    *max = *max > curr ? *max : curr;
+    return memo[row][col] = maxLength;
 }
 
 int main() {
     int N;
     scanf("%d", &N);
-    while(N--) {
+    while (N--) {
         char *name = NULL;
         int R, C;
         scanf("%ms %d %d", &name, &R, &C);
@@ -23,14 +30,16 @@ int main() {
             for(int j = 0; j < C; ++j)
                 scanf("%hd", &height[i][j]);
         }
+        memset(memo, 0, sizeof(memo));
         int max = 1;
         for(int i = 0; i < R; ++i) {
             for(int j = 0; j < C; ++j) {
-                int curr = 1;
-                dfs(i, R, j, C, &max, curr);
+                int length = dfs(i, R, j, C);
+                max = max > length ? max : length;
             }
         }
         printf("%s: %d\n", name, max);
+        free(name);
     }
-	return 0;
+    return 0;
 }
