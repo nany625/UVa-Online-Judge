a81dc3f350a6c3817fa72c06e67a6f8b8ee953e7
nany625 <jimmy19980625@gmail.com>
2024-08-21 07:00:45 +0800

    Create solution.java

---
+++ 11727 Cost Cutting/solution.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        st.nextToken();
+        int T = (int)st.nval;
+        StringBuilder output = new StringBuilder();
+        for(int i = 1; i <= T; ++i) {
+            short[] salary = new short[3];
+            for(int j = 0; j < 3; ++j) {
+                st.nextToken();
+                salary[j] = (short)st.nval;
+            }
+            Arrays.sort(salary);
+            output.append("Case " + i + ": " + salary[1] + "\n");
+        }
+        System.out.print(output);
+    }
+}
