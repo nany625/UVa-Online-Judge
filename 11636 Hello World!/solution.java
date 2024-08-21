3c3e77495ddda110b8b5e52f53b3e4942fffabff
nany625 <jimmy19980625@gmail.com>
2024-08-22 03:53:40 +0800

    Update solution.java

--- 11636 Hello World!/solution.java
+++ 11636 Hello World!/solution.java
@@ -6,7 +6,7 @@ public class Main {
         int X = 0, N;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) >= 0)
-            output.append("Case " + (++X) + ": " + (int)Math.ceil(Math.log(N) / Math.log(2)) + "\n");
+            output.append("Case ").append(++X).append(": ").append((int)Math.ceil(Math.log(N) / Math.log(2))).append('\n');
         System.out.print(output);
 	}
 }
