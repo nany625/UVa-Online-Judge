d72b5f79263d381c7afacbea51898ff2b2bed6d7
nany625 <jimmy19980625@gmail.com>
2024-08-21 08:27:37 +0800

    Update solution.java

--- 10019 Funny Encryption Method/solution.java
+++ 10019 Funny Encryption Method/solution.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    StreamTokenizer st = new StreamTokenizer(System.in);
-	    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	    st.nextToken();
 	    int T = (int)st.nval;
 	    StringBuilder output = new StringBuilder();
@@ -27,8 +26,6 @@ public class Main {
     	    }
     	    output.append(count).append('\n');
 	    }
-        bw.write(output.toString());
-        bw.flush();
-        bw.close();
+        System.out.print(output);
 	}
 }
