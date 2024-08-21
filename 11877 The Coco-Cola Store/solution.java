078e36d40a000c4ef5acb800e088a1b270ba5052
nany625 <jimmy19980625@gmail.com>
2024-08-22 04:30:16 +0800

    Update solution.java

--- 11877 The Coco-Cola Store/solution.java
+++ 11877 The Coco-Cola Store/solution.java
@@ -7,7 +7,7 @@ public class Main {
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0) {
             int exchange = (n - 3) / 2 + 1;
-            output.append(exchange + (n - 2 * exchange) % 3 / 2 + "\n");
+            output.append(exchange + (n - 2 * exchange) % 3 / 2).append('\n');
         }   
         System.out.print(output);
 	}
