beaf350ad2a69fda25fe46ea44ea338d9697839e
nany625 <jimmy19980625@gmail.com>
2024-08-21 11:38:06 +0800

    Update solution.java

--- 10515 Powers Et Al./solution.java
+++ 10515 Powers Et Al./solution.java
@@ -17,14 +17,14 @@ public class Main {
             else if(base == 1 || tokens[1].charAt(0) == '0')
                 output.append("1\n");
             else if(base == 5 || base == 6)
-                output.append(base + "\n");
+                output.append(base).append('\n');
             else {
                 int len = tokens[1].length(), N;
                 if(len == 1)
                     N = tokens[1].charAt(0) - '0';
                 else
                     N = 10 * (tokens[1].charAt(len - 2) - '0') + tokens[1].charAt(len - 1) - '0';
-                output.append(lastDigit[base - 2].charAt(N % cycle[base - 2]) + "\n");
+                output.append(lastDigit[base - 2].charAt(N % cycle[base - 2])).append('\n');
             }
         }
         System.out.print(output);
