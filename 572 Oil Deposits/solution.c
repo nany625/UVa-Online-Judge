e8c051e6535c66113a6345d5533db14c470506fa
nany625 <jimmy19980625@gmail.com>
2024-08-22 07:14:49 +0800

    Update solution.c

--- 572 Oil Deposits/solution.c
+++ 572 Oil Deposits/solution.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int dRow[] = {0, 1, 0, -1, 1, 1, -1, -1}, dCol[] = {1, 0, -1, 0, 1, -1, 1, -1};
+char grid[99][100];
 
-void dfs(char **grid, int row, int rowLimit, int col, int colLimit) {
+void dfs(int row, int rowLimit, int col, int colLimit) {
     if(grid[row][col] == '@') {
         grid[row][col] = '*';
         for(int i = 0; i < 8; ++i) {
             int newRow = row + dRow[i], newCol = col + dCol[i];
             if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit)
-                dfs(grid, newRow, rowLimit, newCol, colLimit);
+                dfs(newRow, rowLimit, newCol, colLimit);
         }
     }
 }
@@ -17,24 +17,18 @@ void dfs(char **grid, int row, int rowLimit, int col, int colLimit) {
 int main() {
     int m, n;
     while(scanf("%d %d", &m, &n) && m != 0 && n != 0) {
-        char **grid = (char**)malloc(m * sizeof(char*));
-        for(int i = 0; i < m; ++i) {
-            grid[i] = (char*)malloc((n + 1) * sizeof(char));
+        for(int i = 0; i < m; ++i)
             scanf("%s", grid[i]);
-        }
         int count = 0;
         for(int i = 0; i < m; ++i) {
             for(int j = 0; j < n; ++j) {
                 if(grid[i][j] == '@') {
                     ++count;
-                    dfs(grid, i, m, j, n);
+                    dfs(i, m, j, n);
                 }
             }
         }
         printf("%d\n", count);
-        for(int i = 0; i < m; ++i)
-            free(grid[i]);
-        free(grid);
     }
 	return 0;
 }
