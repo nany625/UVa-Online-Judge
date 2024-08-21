b3929f3e8364bc68bcff81379d09660753e35e95
nany625 <jimmy19980625@gmail.com>
2024-08-21 08:15:46 +0800

    Update solution.java

--- 10055 Hashmat the Brave Warrior/solution.java
+++ 10055 Hashmat the Brave Warrior/solution.java
@@ -3,13 +3,16 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    StreamTokenizer st = new StreamTokenizer(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
             long Hashmat = (long)st.nval;
             st.nextToken();
             long opponent = (long)st.nval;
-            output.append(Math.abs(Hashmat - opponent) + "\n");
+            output.append(Math.abs(Hashmat - opponent)).append('\n');
         }
-        System.out.print(output);
+        bw.write(output.toString());
+        bw.flush();
+        bw.close();
 	}
 }
