33f1f4985ea29f199decca045e89983da6bfcdb9
nany625 <jimmy19980625@gmail.com>
2024-08-21 10:03:39 +0800

    Update solution.java

--- 10170 The Hotel with Infinite Rooms/solution.java
+++ 10170 The Hotel with Infinite Rooms/solution.java
@@ -7,7 +7,7 @@ public class Main {
         while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
             int S = (int)st.nval;
             st.nextToken();
-            output.append((int)Math.ceil((-1 + Math.sqrt(1 + 4 * (S * S - S + 2 * (long)st.nval))) / 2) + "\n");
+            output.append((int)Math.ceil((-1 + Math.sqrt(1 + 4 * (S * S - S + 2 * (long)st.nval))) / 2)).append('\n');
 		}
         System.out.print(output);
 	}
