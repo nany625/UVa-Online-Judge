8b7933b6f106ed2ce4fd8f9416f9223bfa7496d0
nany625 <jimmy19980625@gmail.com>
2024-08-21 20:33:27 +0800

    Update solution.java

--- 11094 Continents/solution.java
+++ 11094 Continents/solution.java
@@ -29,7 +29,7 @@ public class Main {
                     }
                 }
             }
-            output.append(max + "\n");
+            output.append(max).append('\n');
     	}
         System.out.print(output);
 	}
