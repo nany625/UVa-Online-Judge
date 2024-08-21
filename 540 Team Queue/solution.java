cbd726cb21096f86128a4b9e27df2037e0fbeb95
nany625 <jimmy19980625@gmail.com>
2024-08-22 07:06:34 +0800

    Update solution.java

--- 540 Team Queue/solution.java
+++ 540 Team Queue/solution.java
@@ -8,7 +8,7 @@ public class Main {
         int cases = 0, t;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (t = (int)st.nval) != 0) {
-    	    output.append("Scenario #" + (++cases) + "\n");
+    	    output.append("Scenario #").append(++cases).append('\n');
     		for(int i = 0; i < t; ++i) {
     			st.nextToken();
     			int elementCount = (int)st.nval;
@@ -26,7 +26,7 @@ public class Main {
     			    queue.add((int)st.nval);
     	        } else if(command.charAt(0) == 'D') {
     			    int dequeue = queue.get(next);
-    			    output.append(dequeue + "\n");
+    			    output.append(dequeue).append('\n');
     			    queue.remove(next);
     			    int currTeam = teamNum[dequeue];
     			    while(next < queue.size() && teamNum[queue.get(next)] != currTeam)
@@ -35,7 +35,7 @@ public class Main {
     			        next = 0;
     			}
     	    }
-    	    output.append("\n");
+    	    output.append('\n');
         }
 	    System.out.print(output);
 	}
