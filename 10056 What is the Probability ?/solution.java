bc00e26ab206961cc1c12ef80ca0733007e41dd0
nany625 <jimmy19980625@gmail.com>
2024-08-21 08:34:59 +0800

    Update solution.java

--- 10056 What is the Probability ?/solution.java
+++ 10056 What is the Probability ?/solution.java
@@ -3,7 +3,6 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 	    StreamTokenizer st = new StreamTokenizer(System.in);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         st.nextToken();
         int S = (int)st.nval;
         StringBuilder output = new StringBuilder();
@@ -16,8 +15,6 @@ public class Main {
             int I = (int)st.nval;
             output.append(String.format("%.4f\n", p > 0 ? p * Math.pow(1 - p, I - 1) / (1 - Math.pow(1 - p, N)) : p));
         }
-        bw.write(output.toString());
-        bw.flush();
-        bw.close();
+        System.out.print(output);
 	}
 }
