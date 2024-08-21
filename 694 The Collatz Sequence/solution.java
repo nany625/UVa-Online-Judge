b52370b3a93254296ca0f8a9c628a839e73373ef
nany625 <jimmy19980625@gmail.com>
2024-08-22 07:35:07 +0800

    Update solution.java

--- 694 The Collatz Sequence/solution.java
+++ 694 The Collatz Sequence/solution.java
@@ -7,13 +7,13 @@ public class Main {
         int cases = 0, L;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (A = (long)st.nval) >= 0 && st.nextToken() == StreamTokenizer.TT_NUMBER && (L = (int)st.nval) >= 0) {
-            output.append("Case " + (++cases) + ": A = " + A + ", limit = " + L + ", number of terms = ");
+            output.append("Case ").append(++cases).append(": A = ").append(A).append(", limit = ").append(L).append(", number of terms = ");
             int terms = 0;
             while(A != 1 && A <= L) {
                 ++terms;
                 A = A % 2 == 0 ? A / 2 : 3 * A + 1;
             }
-            output.append(terms + (A == 1 ? 1 : 0) + "\n");
+            output.append(terms + (A == 1 ? 1 : 0)).append('\n');
         }
         System.out.print(output);
     }
