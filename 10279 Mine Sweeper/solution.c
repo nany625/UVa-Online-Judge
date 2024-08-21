b012c464afe31e797a87f3f8c641a95da772e715
nany625 <jimmy19980625@gmail.com>
2024-08-21 10:50:04 +0800

    Update solution.c

--- 10279 Mine Sweeper/solution.c
+++ 10279 Mine Sweeper/solution.c
@@ -1,50 +1,53 @@
-#include <stdio.h>
-#include <stdbool.h>
+import java.io.*;
 
-int dRow[] = {0, 1, 0, -1, 1, 1, -1, -1}, dCol[] = {1, 0, -1, 0, 1, -1, -1, 1};
-
-int main() {
-    int games;
-    scanf("%d", &games);
-    bool firstCase = true;
-    while(games--) {
-        if(!firstCase)
-            putchar('\n');
-        int n;
-        scanf("%d", &n);
-        char mines[n][n + 1], touch[n][n + 1];
-        for(int i = 0; i < n; ++i)
-            scanf("%s", mines[i]);
-        for(int i = 0; i < n; ++i)
-            scanf("%s", touch[i]);
-        bool boom = false;
-        for(int i = 0; i < n; ++i) {
-	        for(int j = 0; j < n; ++j) {
-	            if(touch[i][j] == 'x') {
-	                if(mines[i][j] == '*')
-	                    boom = true;
-	                else {
-    	                touch[i][j] = '0';
-    	                for(int dir = 0; dir < 8; ++dir) {
-    	                    int newRow = i + dRow[dir], newCol = j + dCol[dir];
-    	                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && mines[newRow][newCol] == '*')
-    	                        ++touch[i][j];
-    	                }
-	                }
-	            }
-	        }
-	    }
-	    if(boom) {
-	        for(int i = 0; i < n; ++i) {
+public class Main {
+    static int[] dRow = {0, 1, 0, -1, 1, 1, -1, -1}, dCol = {1, 0, -1, 0, 1, -1, -1, 1};
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer st = new StreamTokenizer(br);
+        st.nextToken();
+        int games = (int)st.nval;
+        boolean firstCase = true;
+        StringBuilder output = new StringBuilder();
+        while(games-- > 0) {
+            st.nextToken();
+            int n = (int)st.nval;
+            if(!firstCase)
+                output.append('\n');
+            char[][] mines = new char[n][n], touch = new char[n][n];
+            for(int i = 0; i < n; ++i)
+                mines[i] = br.readLine().toCharArray();
+            for(int i = 0; i < n; ++i)
+                touch[i] = br.readLine().toCharArray();
+            boolean boom = false;
+            for(int i = 0; i < n; ++i) {
     	        for(int j = 0; j < n; ++j) {
-    	            if(mines[i][j] == '*')
-    	                touch[i][j] = '*';
+    	            if(touch[i][j] == 'x') {
+    	                if(mines[i][j] == '*')
+    	                    boom = true;
+    	                else {
+        	                touch[i][j] = '0';
+        	                for(int dir = 0; dir < 8; ++dir) {
+        	                    int newRow = i + dRow[dir], newCol = j + dCol[dir];
+        	                    if(newRow >= 0 && newRow < n && newCol >= 0 && newCol < n && mines[newRow][newCol] == '*')
+        	                        ++touch[i][j];
+        	                }
+    	                }
+    	            }
     	        }
-	        }
-	    }
-	    for(int i = 0; i < n; ++i)
-	        puts(touch[i]);
-	    firstCase = false;
-    }
-    return 0;
+    	    }
+    	    if(boom) {
+    	        for(int i = 0; i < n; ++i) {
+        	        for(int j = 0; j < n; ++j) {
+        	            if(mines[i][j] == '*')
+        	                touch[i][j] = '*';
+        	        }
+    	        }
+    	    }
+    	    for(int i = 0; i < n; ++i)
+    	        output.append(new String(touch[i])).append('\n');
+    	    firstCase = false;
+        }
+        System.out.print(output);
+	}
 }
