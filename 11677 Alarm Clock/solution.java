d38ea836b5cd0e1c7896ceeee8b4eced67d0aeef
nany625 <jimmy19980625@gmail.com>
2024-08-22 03:57:27 +0800

    Update solution.java

--- 11677 Alarm Clock/solution.java
+++ 11677 Alarm Clock/solution.java
@@ -15,7 +15,7 @@ public class Main {
             if(H1 == 0 && M1 == 0 && H2 == 0 && M2 == 0)
                 break;
             int time1 = 60 * H1 + M1, time2 = 60 * H2 + M2;
-            output.append((time1 <= time2 ? time2 - time1 : time2 - time1 + 1440) + "\n");
+            output.append(time1 <= time2 ? time2 - time1 : time2 - time1 + 1440).append('\n');
         }
         System.out.print(output);
 	}
