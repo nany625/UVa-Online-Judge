17bc9b72c263f2f5e0c0905b06c4b0208c026b33
nany625 <jimmy19980625@gmail.com>
2024-08-22 03:26:53 +0800

    Update solution.java

--- 11461 Square Numbers/solution.java
+++ 11461 Square Numbers/solution.java
@@ -6,7 +6,7 @@ public class Main {
         int a, b;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (a = (int)st.nval) != 0 && st.nextToken() == StreamTokenizer.TT_NUMBER && (b = (int)st.nval) != 0)
-    		output.append((int)Math.sqrt(b) - (int)Math.ceil(Math.sqrt(a)) + 1 + "\n");
+    		output.append((int)Math.sqrt(b) - (int)Math.ceil(Math.sqrt(a)) + 1).append('\n');
         System.out.print(output);
 	}
 }
