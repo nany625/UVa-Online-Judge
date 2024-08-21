7ab385373c6fbb1738e96a4a1f896e9c860298d8
nany625 <jimmy19980625@gmail.com>
2024-08-22 06:06:23 +0800

    Update solution.java

--- 264 Count on Cantor/solution.java
+++ 264 Count on Cantor/solution.java
@@ -9,10 +9,7 @@ public class Main {
             int i = (int)Math.ceil((-1 + Math.sqrt(1 + 8 * number)) / 2);
             int a = i + 1 - number + (i - 1) * i / 2;
             int b = number - (i - 1) * i / 2;
-            if(i % 2 == 1)
-                output.append(String.format("TERM %d IS %d/%d\n", number, a, b));
-    		else
-			    output.append(String.format("TERM %d IS %d/%d\n", number, b, a));
+            output.append("TERM ").append(number).append(" IS ").append(i % 2 == 1 ? a : b).append('/').append(i % 2 == 1 ? b : a).append('\n');
 		}
         System.out.print(output);
 	}
