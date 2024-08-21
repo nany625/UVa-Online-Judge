56f8ee8e7cefcd046dbb25a8b2b07c94b4dc226c
nany625 <jimmy19980625@gmail.com>
2024-08-21 19:20:48 +0800

    Update solution.java

--- 10783 Odd Sum/solution.java
+++ 10783 Odd Sum/solution.java
@@ -13,7 +13,7 @@ public class Main {
             int b = (int)st.nval;
             a += (a + 1) % 2;
     	 	b -= (b + 1) % 2;
-            output.append("Case " + i + ": " + (-a * a + b * b + 2 * a + 2 * b) / 4 + "\n");
+            output.append("Case ").append(i).append(": ").append((-a * a + b * b + 2 * a + 2 * b) / 4).append('\n');
         }
 		System.out.print(output);
 	}
