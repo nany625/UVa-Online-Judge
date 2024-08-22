dae498f1cb07800505a58ae52544fcaae234619e
nany625 <jimmy19980625@gmail.com>
2024-08-22 09:37:36 +0800

    Create solution.java

---
+++ 11614 Etruscan Warriors Never Play Chess/solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		st.nextToken();
+		int T = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		while(T-- > 0) {
+		    st.nextToken();
+		    long n = (long)st.nval;
+		    output.append((long)(1 + Math.sqrt(1 + 8 * n)) / 2 - 1).append('\n');
+		}
+        System.out.print(output);
+	}
+}
