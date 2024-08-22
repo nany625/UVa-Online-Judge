7b9ec48bb1338777d54b2bc484e31a0adbc6985f
nany625 <jimmy19980625@gmail.com>
2024-08-23 03:44:26 +0800

    Update solution.c

--- 481 What Goes Up/solution.c
+++ 481 What Goes Up/solution.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int binarySearch(int *array, int size, int key) {
+typedef struct {
+    int lastIndex, lis;
+} DP;
+
+int binarySearch(DP *array, int size, int key) {
     int left = 0, right = size - 1;
     while(left <= right) {
         int mid = left + (right - left) / 2;
-        if(array[mid] < key)
+        if(array[mid].lis < key)
             left = mid + 1;
         else
             right = mid - 1;
@@ -19,22 +23,25 @@ int main() {
 	    sequence = (int*)realloc(sequence, (size + 1) * sizeof(int));
 	    sequence[size++] = num;
 	}
-	int dp[size], *lis = NULL, lisSize = 0;
+	DP *dp = NULL;
+	int predecessor[size], lisSize = 0;
 	for(int i = 0; i < size; ++i) {
-	    int pos = binarySearch(lis, lisSize, sequence[i]);
+	    int pos = binarySearch(dp, lisSize, sequence[i]);
 	    if(pos == lisSize)
-	        lis = (int*)realloc(lis, ++lisSize * sizeof(int));
-	    lis[dp[i] = pos] = sequence[i];
+	        dp = (DP*)realloc(dp, ++lisSize * sizeof(DP));
+	    dp[pos].lastIndex = i;
+	    dp[pos].lis = sequence[i];
+	    predecessor[i] = pos > 0 ? dp[pos - 1].lastIndex : -1;
 	}
-	int ans[lisSize], i = lisSize - 1;
-	for(int j = size - 1; i >= 0; --j) {
-	    if(dp[j] == i)
-	        ans[i--] = sequence[j];
+	int ans[lisSize], i = dp[lisSize - 1].lastIndex;
+	for(int j = lisSize - 1; j >= 0; --j) {
+	    ans[j] = sequence[i];
+	    i = predecessor[i];
 	}
 	printf("%d\n-\n", lisSize);
 	for(i = 0; i < lisSize; ++i)
 	    printf("%d\n", ans[i]);
 	free(sequence);
-	free(lis);
+	free(dp);
 	return 0;
 }
