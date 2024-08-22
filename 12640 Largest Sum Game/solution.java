ccb97095a7cdf11fe5fa9df9c14790b04cd391f2
nany625 <jimmy19980625@gmail.com>
2024-08-22 10:17:15 +0800

    Create solution.java

---
+++ 12640 Largest Sum Game/solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		StringBuilder output = new StringBuilder();
+		while((line = br.readLine()) != null) {
+		    String[] tokens = line.split("\\s+");
+		    int max = Integer.parseInt(tokens[0]), curr = max, size = tokens.length;
+		    for(int i = 1; i < size; ++i) {
+		        int temp = Integer.parseInt(tokens[i]);
+		        curr = Math.max(temp, curr + temp);
+		        max = Math.max(max, curr);
+		    }
+		    output.append(max >= 0 ? max : 0).append('\n');
+		}
+        System.out.print(output);
+	}
+}
