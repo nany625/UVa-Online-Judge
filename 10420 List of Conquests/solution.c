19793f0f610914cf9baa39ad9603a736989cec89
nany625 <jimmy19980625@gmail.com>
2024-08-27 16:34:55 +0800

    Update solution.c

--- 10420 List of Conquests/solution.c
+++ 10420 List of Conquests/solution.c
@@ -9,7 +9,7 @@ typedef struct {
 int binarySearch(Conquest *array, int size, char *key) {
     int left = 0, right = size - 1;
     while(left <= right) {
-        int mid = left + (right - left) / 2;
+        int mid = left + ((right - left) >> 1);
         int cmp = strcmp(array[mid].country, key);
         if(cmp == 0)
             return mid;
