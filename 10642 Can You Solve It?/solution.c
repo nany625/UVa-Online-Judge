6c43a510fa91780c7333ab94afe9646b03559a37
nany625 <jimmy19980625@gmail.com>
2024-08-27 17:01:54 +0800

    Update solution.c

--- 10642 Can You Solve It?/solution.c
+++ 10642 Can You Solve It?/solution.c
@@ -10,7 +10,7 @@ int main() {
     for(int i = 1; i <= n; ++i) {
         Coordinate src, dest;
         scanf("%ld %ld %ld %ld", &src.x, &src.y, &dest.x, &dest.y);
-        printf("Case %d: %ld\n", i, (src.x + src.y + dest.x + dest.y + 1) * (dest.x + dest.y - src.x - src.y) / 2 + dest.x - src.x);
+        printf("Case %d: %ld\n", i, ((src.x + src.y + dest.x + dest.y + 1) * (dest.x + dest.y - src.x - src.y) >> 1) + dest.x - src.x);
     }
     return 0;
 }
