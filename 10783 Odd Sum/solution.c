9ed37295f2a4331272afc27749665e6c7958d331
nany625 <jimmy19980625@gmail.com>
2024-08-27 17:10:09 +0800

    Update solution.c

--- 10783 Odd Sum/solution.c
+++ 10783 Odd Sum/solution.c
@@ -6,9 +6,9 @@ int main() {
     for(int i = 1; i <= T; ++i) {
         int a, b;
         scanf("%d %d", &a, &b);
-        a += (a + 1) % 2;
-	 	b -= (b + 1) % 2;
-        printf("Case %d: %d\n", i, (-a * a + b * b + 2 * a + 2 * b) / 4);
+        a += (a + 1) & 1;
+	 	b -= (b + 1) & 1;
+        printf("Case %d: %d\n", i, (-a * a + b * b + (a << 1) + (b << 1)) >> 2);
     }
     return 0;
 }
