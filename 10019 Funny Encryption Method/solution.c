42314a60ae9a9b59fd468c0ebe95257ff0907e5e
nany625 <jimmy19980625@gmail.com>
2024-08-27 14:21:58 +0800

    Update solution.c

--- 10019 Funny Encryption Method/solution.c
+++ 10019 Funny Encryption Method/solution.c
@@ -8,16 +8,16 @@ int main() {
 	    scanf("%d", &N);
 	    int temp = N, count = 0;
 	    while(temp > 0) {
-	        count += temp % 2;
-	        temp /= 2;
+	        count += temp & 1;
+	        temp >>= 1;
 	    }
 	    printf("%d ", count);
 	    count = 0;
 	    while(N > 0) {
 	        temp = N % 10;
 	        while(temp > 0) {
-    	        count += temp % 2;
-    	        temp /= 2;
+    	        count += temp & 1;
+    	        temp >>= 1;
     	    }
     	    N /= 10;
 	    }
