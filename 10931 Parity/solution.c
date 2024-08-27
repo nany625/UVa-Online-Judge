25ea7fd3976ee34b33fac4cd6b0dc0a6295c2e6e
nany625 <jimmy19980625@gmail.com>
2024-08-27 17:43:49 +0800

    Update solution.c

--- 10931 Parity/solution.c
+++ 10931 Parity/solution.c
@@ -8,8 +8,8 @@ int main() {
 	    char binaryRep[len + 1];
 	    binaryRep[len] = '\0';
 	    for(int i = len - 1; i >= 0; --i) {
-	        bitsSum += (binaryRep[i] = I % 2 + '0') - '0';
-	        I /= 2;
+	        bitsSum += (binaryRep[i] = I & 1 + '0') - '0';
+	        I >>= 1;
 	    }
 	    printf("The parity of %s is %d (mod 2).\n", binaryRep, bitsSum);
 	}
