1532d8c90be4b85ebc4fd00172dad70fc341772d
nany625 <jimmy19980625@gmail.com>
2024-08-27 11:12:29 +0800

    Update solution.java

--- 11137 Ingenuous Cubrency/solution.java
+++ 11137 Ingenuous Cubrency/solution.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
 public class Main {
+    static long[] dp = new long[10000];
     static int[] coins = {
         1, 8, 27, 64, 125, 216, 343, 512, 729, 1000, 
         1331, 1728, 2197, 2744, 3375, 4096, 4913, 5832, 6859, 8000, 
         9261
     };
-    static long[] dp = new long[10000];
 	public static void main(String[] args) throws IOException {
 		dp[0] = 1;
 		for(Integer coin : coins) {
