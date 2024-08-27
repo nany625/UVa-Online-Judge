6e6ad36c4e03282f2ed7391f75341a19e986e0b7
nany625 <jimmy19980625@gmail.com>
2024-08-27 17:40:27 +0800

    Update solution.c

--- 10929 You can say 11/solution.c
+++ 10929 You can say 11/solution.c
@@ -6,10 +6,10 @@ int main() {
 	while(scanf("%s", N) && N[0] != '0') {
 	    int sum = 0, len = strlen(N);
 	    for(int i = 0; i < len; ++i) {
-	        if(i % 2 == 0)
-	            sum += N[i] - '0';
-	        else
+	        if(i & 1)
 	            sum -= N[i] - '0';
+	        else
+	            sum += N[i] - '0';
 	    }
 	    printf("%s is %sa multiple of 11.\n", N, sum % 11 == 0 ? "" : "not ");
 	}
