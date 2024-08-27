ace1198dd3bedc1d3d91f0ac761c96cda4843f7b
nany625 <jimmy19980625@gmail.com>
2024-08-27 11:11:05 +0800

    Create solution.java

---
+++ 11137 Ingenuous Cubrency/solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    static int[] coins = {
+        1, 8, 27, 64, 125, 216, 343, 512, 729, 1000, 
+        1331, 1728, 2197, 2744, 3375, 4096, 4913, 5832, 6859, 8000, 
+        9261
+    };
+    static long[] dp = new long[10000];
+	public static void main(String[] args) throws IOException {
+		dp[0] = 1;
+		for(Integer coin : coins) {
+		    for(int j = coin; j <= 9999; ++j)
+		        dp[j] += dp[j - coin];
+		}
+		StreamTokenizer st = new StreamTokenizer(System.in);
+		StringBuilder output = new StringBuilder();
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER)
+		    output.append(dp[(int)st.nval]).append('\n');
+		System.out.print(output);
+	}
+}
