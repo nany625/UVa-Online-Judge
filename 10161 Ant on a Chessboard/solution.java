c755f1242a66136b7f0756844f100091c011638d
nany625 <jimmy19980625@gmail.com>
2024-08-27 15:25:44 +0800

    Update solution.java

--- 10161 Ant on a Chessboard/solution.java
+++ 10161 Ant on a Chessboard/solution.java
@@ -7,7 +7,7 @@ public class Main {
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0) {
             int circle = (int)Math.ceil(Math.sqrt(N)), corner = 1 + circle * (circle - 1);
-            if(circle % 2 == 1) {
+            if((circle & 1) == 1) {
                 if(N < corner) 
                     output.append(circle).append(' ').append(circle - (corner - N)).append('\n');
                 else
