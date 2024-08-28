9aeca3aa1a6e8384f4d4b6b0a2681271f1e68411
nany625 <jimmy19980625@gmail.com>
2024-08-28 08:27:33 +0800

    Update solution.c

--- 11296 Counting Solutions to an Integral Equation/solution.c
+++ 11296 Counting Solutions to an Integral Equation/solution.c
@@ -3,6 +3,6 @@
 int main() {
     long n;
     while(scanf("%ld", &n) == 1)
-        printf("%ld\n", (n / 2 + 1) * (n / 2 + 2) / 2);
+        printf("%ld\n", ((n >> 1) + 1) * ((n >> 1) + 2) >> 1);
 	return 0;
 }
