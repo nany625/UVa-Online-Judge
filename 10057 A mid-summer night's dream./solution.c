7b2e62164a34d290e7e807dd96edc78f374accf9
nany625 <jimmy19980625@gmail.com>
2024-08-28 16:07:09 +0800

    Update solution.c

--- 10057 A mid-summer night's dream./solution.c
+++ 10057 A mid-summer night's dream./solution.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void MergeSort(int *array, int *reg, int left, int right) {
+void MergeSort(unsigned short *array, unsigned short *reg, int left, int right) {
     if(left < right) {
         int mid = left + ((right - left) >> 1);
         MergeSort(array, reg, left, mid);
@@ -24,16 +24,16 @@ void MergeSort(int *array, int *reg, int left, int right) {
 int main() {
     int n;
     while(scanf("%d", &n) == 1) {
-        int X[n], reg[n];
+        unsigned short X[n], reg[n];
         for(int i = 0; i < n; ++i)
-            scanf("%d", &X[i]);
+            scanf("%hu", &X[i]);
         MergeSort(X, reg, 0, n - 1);
         int low = (n - 1) >> 1, high = n >> 1;
         while(low > 0 && X[low] == X[low - 1])
             --low;
         while(high < n - 1 && X[high] == X[high + 1])
             ++high;
-        printf("%d %d %d\n", X[(n - 1) >> 1], high - low + 1, X[n >> 1] - X[(n - 1) >> 1] + 1);
+        printf("%hu %d %d\n", X[(n - 1) >> 1], high - low + 1, X[n >> 1] - X[(n - 1) >> 1] + 1);
     }
     return 0;
 }
