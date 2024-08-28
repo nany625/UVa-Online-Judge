bc41bbe34b706d9665931bb3948eade3b35914dd
nany625 <jimmy19980625@gmail.com>
2024-08-28 10:31:43 +0800

    Update solution.c

--- 231 Testing the CATCHER/solution.c
+++ 231 Testing the CATCHER/solution.c
@@ -5,7 +5,7 @@
 int binarySearch(short *array, int size, short key) {
     int left = 0, right = size - 1;
     while(left <= right) {
-        int mid = left + (right - left) / 2;
+        int mid = left + ((right - left) >> 1);
         if(array[mid] < key)
             left = mid + 1;
         else
