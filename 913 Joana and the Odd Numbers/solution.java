a41de9a46ed7811594b33a48b7048dc10bd82e5d
nany625 <jimmy19980625@gmail.com>
2024-08-28 14:48:05 +0800

    Update solution.java

--- 913 Joana and the Odd Numbers/solution.java
+++ 913 Joana and the Odd Numbers/solution.java
@@ -6,7 +6,7 @@ public class Main {
         StringBuilder output = new StringBuilder();
     	while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
     	    long N = (long)st.nval;
-    	    output.append((3 * N * N + 6 * N - 15) / 2).append('\n');
+    	    output.append((3 * N * N + 6 * N - 15) >> 1).append('\n');
     	}
         System.out.print(output);
 	}
