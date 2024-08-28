fe2747a4f3501bc62a4625398654bf663a2ebe7a
nany625 <jimmy19980625@gmail.com>
2024-08-28 13:35:00 +0800

    Update solution.java

--- 401 Palindromes/solution.java
+++ 401 Palindromes/solution.java
@@ -3,18 +3,18 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	    String Characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789";
         String Reverses = "A---3--HIL-JM-O---2TUVWXY51SE-Z--8-";
         HashMap<Character, Character> map = new HashMap<>();
         for(int i = 0; i < 35; ++i)
             map.put(Characters.charAt(i), Reverses.charAt(i));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s;
         StringBuilder output = new StringBuilder();
         while((s = br.readLine()) != null) {
 	        int len = s.length();
             boolean isMirror = true;
-	        for(int i = 0; i < (len + 1) / 2 && isMirror; ++i)
+	        for(int i = 0; i < (len + 1) >> 1 && isMirror; ++i)
                 isMirror = map.get(s.charAt(i)) == s.charAt(len - 1 - i);
             String reversed = new StringBuilder(s).reverse().toString();
 	        if(s.equals(reversed) && isMirror)
