585a95dd4c775f5199b5854e91e0d5486b602533
nany625 <jimmy19980625@gmail.com>
2024-08-28 16:09:42 +0800

    Update solution.c

--- 10082 WERTYU/solution.c
+++ 10082 WERTYU/solution.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    char ch, keyboard[] = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
-    char ASCII[128];
+    char ASCII[128], keyboard[] = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
     for(int i = 1; i < 47; ++i) 
         ASCII[(int)keyboard[i]] = keyboard[i - 1];
+    char ch;
     while((ch = getchar()) != EOF) {
     	if(ch == ' ' || ch == '\n')
     		putchar(ch);
