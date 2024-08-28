fab3c97ad9900fbf911311495f923a26fbe04341
nany625 <jimmy19980625@gmail.com>
2024-08-28 09:17:18 +0800

    Update solution.c

--- 11475 Extend to Palindrome/solution.c
+++ 11475 Extend to Palindrome/solution.c
@@ -5,15 +5,15 @@ int main() {
 	char str[100001];
 	while(scanf("%s", str) == 1) {
 	    int len = strlen(str);
-	    char concat[2 * len + 2];
+	    char concat[(len << 1) + 2];
 	    memset(concat + len + 1, '\0', len);
 	    for(int i = 0; i < len; ++i)
 	        concat[i] = str[len - 1 - i];
 	    concat[len] = '#';
 	    strcat(concat, str);
-	    int lps[2 * len + 1], i = 1, length = 0;
+	    int lps[(len << 1) + 1], i = 1, length = 0;
 	    lps[0] = 0;
-	    while(i < 2 * len + 1) {
+	    while(i < (len << 1) + 1) {
 	        if(concat[i] == concat[length])
 	            lps[i++] = ++length;
 	        else {
@@ -24,7 +24,7 @@ int main() {
 	        }
 	    }
 	    fputs(str, stdout);
-	    for(int j = len - lps[2 * len] - 1; j >= 0; --j)
+	    for(int j = len - lps[(len << 1)] - 1; j >= 0; --j)
 	        putchar(str[j]);
 	    putchar('\n');
 	}
