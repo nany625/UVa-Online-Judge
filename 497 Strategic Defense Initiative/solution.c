8617e921c5a80d3f93f5a743d7d529a9789c1e29
nany625 <jimmy19980625@gmail.com>
2024-08-28 14:03:53 +0800

    Update solution.c

--- 497 Strategic Defense Initiative/solution.c
+++ 497 Strategic Defense Initiative/solution.c
@@ -8,7 +8,7 @@ typedef struct {
 int binarySearch(int *array, int size, int key) {
     int left = 0, right = size - 1;
     while(left <= right) {
-        int mid = left + (right - left) / 2;
+        int mid = left + ((right - left) >> 1);
         if(array[mid] > key)
             left = mid + 1;
         else
