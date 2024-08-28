e03c44df8e9c73d80dff0248ac2d0944f3922b23
nany625 <jimmy19980625@gmail.com>
2024-08-28 08:43:37 +0800

    Update solution.c

--- 11401 Triangle Counting/solution.c
+++ 11401 Triangle Counting/solution.c
@@ -5,8 +5,8 @@ long ans[MAX_NUM + 1];
 
 int main() {
     for(int i = 4; i <= MAX_NUM; ++i) {
-        long temp = i / 2 - 1;
-        ans[i] = ans[i - 1] + (1 + temp) * temp - temp * ((i + 1) % 2);
+        long temp = (i >> 1) - 1;
+        ans[i] = ans[i - 1] + (1 + temp) * temp - temp * ((i + 1) & 1);
     }
     int n;
     while(scanf("%d", &n) && n >= 3)
