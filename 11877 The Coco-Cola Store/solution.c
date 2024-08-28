0ea10d4d8593874744c14d43287985099075fe0d
nany625 <jimmy19980625@gmail.com>
2024-08-28 09:46:21 +0800

    Update solution.c

--- 11877 The Coco-Cola Store/solution.c
+++ 11877 The Coco-Cola Store/solution.c
@@ -3,8 +3,8 @@
 int main() {
     int n;
     while(scanf("%d", &n) && n != 0) {
-        int exchange = (n - 3) / 2 + 1;
-        printf("%d\n", exchange + (n - 2 * exchange) % 3 / 2);
+        int exchange = ((n - 3) >> 1) + 1;
+        printf("%d\n", exchange + (((n - (exchange << 1)) % 3) >> 1));
     }
     return 0;
 }
