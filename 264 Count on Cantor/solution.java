3473daf5f55b6683fdc1060dac075a3b84dd106f
nany625 <jimmy19980625@gmail.com>
2024-08-28 10:39:48 +0800

    Update solution.java

--- 264 Count on Cantor/solution.java
+++ 264 Count on Cantor/solution.java
@@ -6,10 +6,10 @@ public class Main {
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
             int number = (int)st.nval;
-            int i = (int)Math.ceil((-1 + Math.sqrt(1 + 8 * number)) / 2);
-            int a = i + 1 - number + (i - 1) * i / 2;
-            int b = number - (i - 1) * i / 2;
-            output.append("TERM ").append(number).append(" IS ").append(i % 2 == 1 ? a : b).append('/').append(i % 2 == 1 ? b : a).append('\n');
+            int i = (int)Math.ceil((-1 + Math.sqrt(1 + (number << 3))) / 2);
+            int a = i + 1 - number + ((i - 1) * i >> 1);
+            int b = number - ((i - 1) * i >> 1);
+            output.append("TERM ").append(number).append(" IS ").append((i & 1) == 1 ? a : b).append('/').append((i & 1) == 1 ? b : a).append('\n');
 		}
         System.out.print(output);
 	}
