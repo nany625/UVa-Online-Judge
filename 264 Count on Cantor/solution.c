ab99dc513d1760adaa008d570c78feb06b72fb65
nany625 <jimmy19980625@gmail.com>
2024-08-28 10:38:26 +0800

    Update solution.c

--- 264 Count on Cantor/solution.c
+++ 264 Count on Cantor/solution.c
@@ -4,9 +4,9 @@
 int main() {
     int number;
     while(scanf("%d", &number) == 1) {
-        int i = ceil((-1 + sqrt(1 + 8 * number)) / 2);
-        int a = i + 1 - number + (i - 1) * i / 2;
-        int b = number - (i - 1) * i / 2;
+        int i = ceil((-1 + sqrt(1 + (number << 3))) / 2);
+        int a = i + 1 - number + ((i - 1) * i >> 1);
+        int b = number - ((i - 1) * i >> 1);
         if(i % 2 == 1)
             printf("TERM %d IS %d/%d\n", number, a, b);
         else
