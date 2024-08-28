476d07ea271b531829ef534fee72553f8ca62b34
nany625 <jimmy19980625@gmail.com>
2024-08-28 10:51:27 +0800

    Update solution.c

--- 340 Master-Mind Hints/solution.c
+++ 340 Master-Mind Hints/solution.c
@@ -7,30 +7,30 @@ int main() {
 	while(scanf("%d", &N) && N != 0) {
 		printf("Game %d:\n", ++games);
 		getchar();
-		char *secretCode = (char*)malloc((2 * N + 2) * sizeof(char)), *guess = (char*)malloc((2 * N + 2) * sizeof(char));
-		fgets(secretCode, 2 * N + 2, stdin);
+		char *secretCode = (char*)malloc(((N << 1) + 2) * sizeof(char)), *guess = (char*)malloc(((N << 1) + 2) * sizeof(char));
+		fgets(secretCode, (N << 1) + 2, stdin);
 		bool lastGuess;
 		do {
 		    lastGuess = true;
-		    fgets(guess, 2 * N + 2, stdin);
-			for(int i = 0; i < 2 * N && lastGuess; i += 2)
+		    fgets(guess, (N << 1) + 2, stdin);
+			for(int i = 0; i < N << 1 && lastGuess; i += 2)
 				lastGuess = guess[i] == '0';
 			if(!lastGuess) {
 			    bool check[N];
 				int a = 0, b = 0;
-				for(int i = 0; i < 2 * N; i += 2) {
+				for(int i = 0; i < N << 1; i += 2) {
 					if(secretCode[i] == guess[i]) {
 						++a;
-						check[i / 2] = true;
+						check[i >> 1] = true;
 						guess[i] = '0';
 					} else
-					    check[i / 2] = false;
+					    check[i >> 1] = false;
 				}
-				for(int i = 0; i < 2 * N; i += 2) {
-					for(int j = 0; j < 2 * N; j += 2) {
-						if(secretCode[i] == guess[j] && !check[i / 2] && guess[j] != '0') {
+				for(int i = 0; i < N << 1; i += 2) {
+					for(int j = 0; j < N << 1; j += 2) {
+						if(secretCode[i] == guess[j] && !check[i >> 1] && guess[j] != '0') {
 							++b;
-							check[i / 2] = true;
+							check[i >> 1] = true;
 							guess[j] = '0';
 							break;
 						}
