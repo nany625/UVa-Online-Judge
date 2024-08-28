e7f20b18c0e78c390132146bd45eee79f166fec5
nany625 <jimmy19980625@gmail.com>
2024-08-29 05:20:00 +0800

    Update solution.java

--- 495 Fibonacci Freeze/solution.java
+++ 495 Fibonacci Freeze/solution.java
@@ -52,3 +52,44 @@ public class Main {
 		System.out.print(output);
 	}
 }
+
+// #解法三
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static BigInteger[][] temp = new BigInteger[2][2];
+	public static void main(String[] args) throws IOException {
+        StreamTokenizer st = new StreamTokenizer(System.in);
+        StringBuilder output = new StringBuilder();
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+            int n = (int)st.nval;
+            if(n == 0) {
+                output.append("The Fibonacci number for 0 is 0\n");
+                continue;
+            }
+            output.append("The Fibonacci number for ").append(n).append(" is ").append(powerMatrix(new BigInteger[][]{{BigInteger.ONE, BigInteger.ONE}, {BigInteger.ONE, BigInteger.ZERO}}, n - 1)).append('\n');
+        }
+        System.out.print(output);
+	}
+	
+	static void multiplyMatrix(BigInteger[][] a, BigInteger[][] b) {
+	    temp[0][0] = a[0][0].multiply(b[0][0]).add(a[0][1].multiply(b[1][0]));
+	    temp[0][1] = a[0][0].multiply(b[0][1]).add(a[0][1].multiply(b[1][1]));
+	    temp[1][0] = a[1][0].multiply(b[0][0]).add(a[1][1].multiply(b[1][0]));
+	    temp[1][1] = a[1][0].multiply(b[0][1]).add(a[1][1].multiply(b[1][1]));
+	    for(int i = 0; i < 2; ++i)
+	        System.arraycopy(temp[i], 0, a[i], 0, 2);
+	}
+	
+	static BigInteger powerMatrix(BigInteger[][] matrix, int n) {
+	    BigInteger[][] result = {{BigInteger.ONE, BigInteger.ZERO}, {BigInteger.ZERO, BigInteger.ONE}};
+	    while(n > 0) {
+	        if((n & 1) == 1)
+	            multiplyMatrix(result, matrix);
+	        multiplyMatrix(matrix, matrix);
+	        n >>= 1;
+	    }
+	    return result[0][0];
+	}
+}
