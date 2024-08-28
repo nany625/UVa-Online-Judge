b94ab056bcfb25791fe9e6353a001ccb76adb764
nany625 <jimmy19980625@gmail.com>
2024-08-28 10:06:34 +0800

    Update solution.c

--- 12149 Feynman/solution.c
+++ 12149 Feynman/solution.c
@@ -3,6 +3,6 @@
 int main() {
     int N;
     while(scanf("%d", &N) && N != 0)
-        printf("%d\n", N * (N + 1) * (2 * N + 1) / 6);
+        printf("%d\n", N * (N + 1) * ((N << 1) + 1) / 6);
 	return 0;
 }
