9e5430b94a3188c9fc71c98fb7f3ac2c6bda160b
nany625 <jimmy19980625@gmail.com>
2024-08-28 10:38:40 +0800

    Update solution.c

--- 264 Count on Cantor/solution.c
+++ 264 Count on Cantor/solution.c
@@ -7,7 +7,7 @@ int main() {
         int i = ceil((-1 + sqrt(1 + (number << 3))) / 2);
         int a = i + 1 - number + ((i - 1) * i >> 1);
         int b = number - ((i - 1) * i >> 1);
-        if(i % 2 == 1)
+        if(i & 1)
             printf("TERM %d IS %d/%d\n", number, a, b);
         else
             printf("TERM %d IS %d/%d\n", number, b, a);
