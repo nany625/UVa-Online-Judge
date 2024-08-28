3352e949fb2a6a2674ea0cc0e801c83f557cc566
nany625 <jimmy19980625@gmail.com>
2024-08-28 13:51:44 +0800

    Update solution.c

--- 455 Periodic Strings/solution.c
+++ 455 Periodic Strings/solution.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
 int main() {
 	int N;
 	scanf("%d", &N);
 	char s[81];
-	bool firstCase = true;
 	while(N--) {
-		if(!firstCase)
-			putchar('\n');
 		scanf("%s", s);
 		int len = strlen(s), i = 1, length = 0;
         short LPS[len];
@@ -25,7 +21,8 @@ int main() {
 		    }
 		}
 		printf("%d\n", len % (len - LPS[len - 1]) == 0 ? len - LPS[len - 1] : len);
-		firstCase = false;
+		if(N)
+            putchar('\n');
 	}
 	return 0;
 }
