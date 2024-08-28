9a3355729ad3c48a3757fe4ea5fb9c320a9cc2ef
nany625 <jimmy19980625@gmail.com>
2024-08-28 08:38:13 +0800

    Update solution.java

--- 11360 Have Fun with Matrices/solution.java
+++ 11360 Have Fun with Matrices/solution.java
@@ -67,11 +67,8 @@ public class Main {
         			}
 				}
     		}
-    		for(int j = 0; j < N; ++j) {
-    		    for(int k = 0; k < N; ++k)
-    			    output.append(matrix[j][k]);
-    			output.append('\n');
-    		}
+    		for(int j = 0; j < N; ++j)
+    			output.append(new String(matrix[j])).append('\n');
     		output.append('\n');
     	}
 	    System.out.print(output);
