af3c589b9f3a058c61995afc3802801eaf7b9449
nany625 <jimmy19980625@gmail.com>
2024-08-28 08:17:31 +0800

    Update solution.java

--- 11040 Add bricks in the wall/solution.java
+++ 11040 Add bricks in the wall/solution.java
@@ -16,13 +16,13 @@ public class Main {
     	    }
     	    for(int i = 2; i < 9; i += 2) {
     	        for(int j = 1; j < i; j += 2)
-    	            brick[i][j] = (brick[i - 2][j - 1] - brick[i][j - 1] - brick[i][j + 1]) / 2;
+    	            brick[i][j] = (brick[i - 2][j - 1] - brick[i][j - 1] - brick[i][j + 1]) >> 1;
     	    }
     	    for(int i = 0; i < 9; ++i) {
     	        for(int j = 0; j <= i; ++j) {
     	            if(j > 0)
     	                output.append(' ');
-    	            output.append(i % 2 == 0 ? brick[i][j] : brick[i + 1][j] + brick[i + 1][j + 1]);
+    	            output.append(i & 1 == 1 ? brick[i + 1][j] + brick[i + 1][j + 1] : brick[i][j]);
     	        }
     	        output.append('\n');
     	    }
