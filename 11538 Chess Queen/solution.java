d3e6e3c68d9522b727e12aa911327bd17b632db5
nany625 <jimmy19980625@gmail.com>
2024-09-18 18:56:35 +0800

    Update solution.java

--- 11538 Chess Queen/solution.java
+++ 11538 Chess Queen/solution.java
@@ -2,7 +2,8 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		StreamTokenizer st = new StreamTokenizer(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
         long M, N;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (M = (long)st.nval) != 0 && st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (long)st.nval) != 0) {
