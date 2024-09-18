1f9c0a488a5ae7c1eada92d06a980bb98c330fbe
nany625 <jimmy19980625@gmail.com>
2024-09-18 18:30:15 +0800

    Update solution.java

--- 10931 Parity/solution.java
+++ 10931 Parity/solution.java
@@ -2,7 +2,8 @@ import java.io.*;
 
 public class Main {
 	public static void main (String[] args) throws IOException {
-		StreamTokenizer st = new StreamTokenizer(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
 		int I;
 		StringBuilder output = new StringBuilder();
 		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (I = (int)st.nval) != 0) {
