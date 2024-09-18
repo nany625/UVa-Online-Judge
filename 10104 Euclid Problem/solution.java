b3edc7f1b8a517158b124ad7e28037035136a86e
nany625 <jimmy19980625@gmail.com>
2024-09-18 18:09:54 +0800

    Update solution.java

--- 10104 Euclid Problem/solution.java
+++ 10104 Euclid Problem/solution.java
@@ -3,7 +3,8 @@ import java.io.*;
 public class Main {
     static int[] X = new int[1], Y = new int[1];
 	public static void main(String[] args) throws IOException {
-    	StreamTokenizer st = new StreamTokenizer(System.in);
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
     	StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
             int A = (int)st.nval;
