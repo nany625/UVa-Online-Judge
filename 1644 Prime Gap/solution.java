3609c0cecee679d2a677f260387dce9390e2ef16
nany625 <jimmy19980625@gmail.com>
2024-09-18 19:11:37 +0800

    Update solution.java

--- 1644 Prime Gap/solution.java
+++ 1644 Prime Gap/solution.java
@@ -3,7 +3,8 @@ import java.io.*;
 public class Main {
     static String[] firstGrid = new String[6], secondGrid = new String[6];
 	public static void main(String[] args) throws IOException {
-        StreamTokenizer st = new StreamTokenizer(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
         int k;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (k = (int)st.nval) != 0) {
