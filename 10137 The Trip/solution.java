2bb8fa03c15b162d7a19e78eb066982ab9cedd30
nany625 <jimmy19980625@gmail.com>
2024-09-18 18:11:05 +0800

    Update solution.java

--- 10137 The Trip/solution.java
+++ 10137 The Trip/solution.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    StreamTokenizer st = new StreamTokenizer(System.in);
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
         int n;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0) {
