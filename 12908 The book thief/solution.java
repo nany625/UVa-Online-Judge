b934f0aae3a7a01115c60ec8ec8ad3ddef665049
nany625 <jimmy19980625@gmail.com>
2024-09-18 19:07:13 +0800

    Update solution.java

--- 12908 The book thief/solution.java
+++ 12908 The book thief/solution.java
@@ -2,7 +2,8 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		StreamTokenizer st = new StreamTokenizer(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
 		int s;
 		StringBuilder output = new StringBuilder();
 		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (s = (int)st.nval) != 0) {
