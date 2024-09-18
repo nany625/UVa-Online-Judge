0875054c3bdf3422dd3b85886be0a48648bd5ed2
nany625 <jimmy19980625@gmail.com>
2024-09-18 18:28:53 +0800

    Update solution.java

--- 10813 Traditional BINGO/solution.java
+++ 10813 Traditional BINGO/solution.java
@@ -3,7 +3,8 @@ import java.io.*;
 public class Main {
     static Card[][] bingo = new Card[5][5];
 	public static void main (String[] args) throws IOException {
-		StreamTokenizer st = new StreamTokenizer(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
 		st.nextToken();
 		int n = (int)st.nval;
 		StringBuilder output = new StringBuilder();
