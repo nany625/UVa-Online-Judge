eeda9748a0a368af14eae9500f1f12ac8b24769b
nany625 <jimmy19980625@gmail.com>
2024-09-18 18:57:52 +0800

    Update solution.java

--- 11634 Generate random numbers/solution.java
+++ 11634 Generate random numbers/solution.java
@@ -2,7 +2,8 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        StreamTokenizer st = new StreamTokenizer(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
         short a;
         StringBuilder output = new StringBuilder();
 		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (a = (short)st.nval) != 0) {
