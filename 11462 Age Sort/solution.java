b896d89a38038034c64304bb5d4d732efd85b1bf
nany625 <jimmy19980625@gmail.com>
2024-09-18 18:54:16 +0800

    Update solution.java

--- 11462 Age Sort/solution.java
+++ 11462 Age Sort/solution.java
@@ -3,7 +3,8 @@ import java.io.*;
 public class Main {
     static int[] age = new int[100];
 	public static void main(String[] args) throws IOException {
-    	StreamTokenizer st = new StreamTokenizer(System.in);
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
     	int n;
     	StringBuilder output = new StringBuilder();
     	while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0) {
