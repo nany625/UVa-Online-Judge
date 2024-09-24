08aec17de563e2b4a8550924bc5d0938eeb50888
nany625 <jimmy19980625@gmail.com>
2024-09-25 02:43:01 +0800

    Update solution.java

--- 11827 Maximum GCD/solution.java
+++ 11827 Maximum GCD/solution.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) throws IOException {
@@ -13,9 +14,10 @@ public class Main {
             int[] numbers = new int[len];
             for(int i = 0; i < len; ++i)
                 numbers[i] = Integer.parseInt(tokens[i]);
+            Arrays.sort(numbers);
             int max = 1;
-            for(int i = 0; i < len - 1; ++i) {
-                for(int j = i + 1; j < len; ++j)
+            for(int i = len - 2; i >= 0 && numbers[i] > max; --i) {
+                for(int j = len - 1; j > i; --j)
                     max = Math.max(max, GCD(numbers[i], numbers[j]));
             }
             output.append(max).append('\n');
