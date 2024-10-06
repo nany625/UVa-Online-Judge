c4ad5b2508ecdb41d0918aa743d32aafaf0cb168
nany625 <jimmy19980625@gmail.com>
2024-10-07 03:21:58 +0800

    Update solution.java

--- 424 Integer Inquiry/solution.java
+++ 424 Integer Inquiry/solution.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.math.*;
 
 public class Main {
-	public static void main (String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line;
 		BigInteger ans = BigInteger.ZERO;
