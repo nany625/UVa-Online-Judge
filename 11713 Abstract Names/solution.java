34776b16ddaa1c7ba33dc8a096906a7fac9630bb
nany625 <jimmy19980625@gmail.com>
2024-10-13 16:18:33 +0800

    Create solution.java

---
+++ 11713 Abstract Names/solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
+		st.nextToken();
+		int n = (int)st.nval;
+		StringBuilder output = new StringBuilder();
+		while(n-- > 0) {
+		    st.nextToken();
+		    String realName = st.sval;
+		    st.nextToken();
+		    String gameName = st.sval;
+		    int len = realName.length();
+		    if(len == gameName.length()) {
+		        boolean same = true;
+		        for(int i = 0; i < len && same; ++i) {
+		            if(isVowel(realName.charAt(i)) != isVowel(gameName.charAt(i)) || (!isVowel(realName.charAt(i)) && realName.charAt(i) != gameName.charAt(i))) {
+    	                output.append("No\n");
+    	                same = false;
+    	            }
+		        }
+		        if(same)
+		            output.append("Yes\n");
+		    } else
+		        output.append("No\n");
+		}
+		System.out.print(output);
+	}
+	
+	static boolean isVowel(char ch) {
+	    return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+	}
+}
