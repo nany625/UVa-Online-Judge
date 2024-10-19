06a479935325dd47b4ce4e57f2db01006bb15538
nany625 <jimmy19980625@gmail.com>
2024-10-19 23:21:27 +0800

    Update solution.c

--- 10539 Almost Prime Numbers/solution.c
+++ 10539 Almost Prime Numbers/solution.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#define MAX_NUM 999999
 
-bool isComposite[1000000];
+bool isComposite[MAX_NUM + 1];
 long *almostPrimes = NULL;
 int size = 0;
 
 int compare(const void *a, const void *b) {
     return *(long*)a > *(long*)b;
+}
+
+void appendAlmostPrimes(int i) {
+    long p = (long)i * i;
+    while(p < 1000000000000) {
+	    almostPrimes = (long*)realloc(almostPrimes, (size + 1) * sizeof(long));
+	    almostPrimes[size++] = p;
+	    p *= i;
+    }
 }
 
 int binarySearch(long key) {
@@ -25,16 +35,12 @@ int binarySearch(long key) {
 }
 	
 int main() {
-	for(int i = 2; i < 1000000; ++i) {
+    appendAlmostPrimes(2);
+	for(int i = 3; i <= MAX_NUM; i += 2) {
 	    if(!isComposite[i]) {
-	        long p = (long)i * i;
-	        while(p < 1000000000000) {
-	            almostPrimes = (long*)realloc(almostPrimes, (size + 1) * sizeof(long));
-	            almostPrimes[size++] = p;
-	            p *= i;
-	        }
+	        appendAlmostPrimes(i);
 	        if(i < 1000) {
-	            for(int j = i * i; j < 1000000; j += i)
+	            for(int j = i * i; j <= MAX_NUM; j += i)
 	                isComposite[j] = true;
 	        }
 	    }
