090f102e6482763285a2e4452e5e50c8eda4d474
nany625 <jimmy19980625@gmail.com>
2024-10-22 23:24:50 +0800

    Update solution.c

--- 11040 Add bricks in the wall/solution.c
+++ 11040 Add bricks in the wall/solution.c
@@ -13,12 +13,9 @@ int main() {
 	            brick[i][j] = (brick[i - 2][j - 1] - brick[i][j - 1] - brick[i][j + 1]) >> 1;
 	    }
 	    for(int i = 0; i < 9; ++i) {
-	        for(int j = 0; j <= i; ++j) {
-	            if(j > 0)
-	                putchar(' ');
-	            printf("%d", i & 1 ? brick[i + 1][j] + brick[i + 1][j + 1] : brick[i][j]);
-	        }
-	        putchar('\n');
+	        for(int j = 0; j < i; ++j)
+	            printf("%d ", i & 1 ? brick[i + 1][j] + brick[i + 1][j + 1] : brick[i][j]);
+	        printf("%d\n", i & 1 ? brick[i + 1][i] + brick[i + 1][i + 1] : brick[i][i]);
 	    }
 	}
 	return 0;
