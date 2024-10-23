783737c08b264e85f1db6f8edb386e35b2fe1f87
nany625 <jimmy19980625@gmail.com>
2024-10-23 10:35:22 +0800

    Update solution.c

--- 10170 The Hotel with Infinite Rooms/solution.c
+++ 10170 The Hotel with Infinite Rooms/solution.c
@@ -5,6 +5,6 @@ int main() {
     int S;
     long D;
     while(scanf("%d %ld", &S, &D) == 2)
-        printf("%d\n", (int)ceil((-1 + sqrt(1 + 4 * (S * S - S + 2 * D))) / 2));
+        printf("%d\n", (int)ceil((-1 + sqrt(1 + ((S * S - S + (D << 1)) << 2))) / 2));
     return 0;
 }
