55b4edf5c9fc7356a1fc7d6d0c8180b41b624965
nany625 <jimmy19980625@gmail.com>
2024-10-23 10:07:28 +0800

    Update solution.java

--- 10049 Self-describing Sequence/solution.java
+++ 10049 Self-describing Sequence/solution.java
@@ -23,7 +23,7 @@ public class Main {
 	static int binarySearch(int key) {
 	    int left = 0, right = size - 2;
         while(left <= right) {
-            int mid = left + ((right - left) >> 1);
+            int mid = left + (right - left >> 1);
             if(f.get(mid) == key)
                 return mid + 1;
             if(f.get(mid) < key)
