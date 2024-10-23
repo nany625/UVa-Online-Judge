6908e82f6ec16bc0aaabbce25a7d66b1159a23c4
nany625 <jimmy19980625@gmail.com>
2024-10-23 12:22:38 +0800

    Update solution.c

--- 10474 Where is the Marble?/solution.c
+++ 10474 Where is the Marble?/solution.c
@@ -8,7 +8,7 @@ int compare(const void *a, const void *b) {
 int binarySearch(short *array, int size, short key) {
     int left = 0, right = size - 1;
     while(left <= right) {
-        int mid = left + ((right - left) >> 1);
+        int mid = left + (right - left >> 1);
         if(array[mid] == key) {
             while(mid > left && array[mid] == array[mid-1])
                 --mid;
