59c0a63feae50a4f03012fdb525af0d217deb9cb
nany625 <jimmy19980625@gmail.com>
2024-10-23 12:56:10 +0800

    Update solution.java

--- 10940 Throwing cards away II/solution.java
+++ 10940 Throwing cards away II/solution.java
@@ -7,7 +7,7 @@ public class Main {
 		int n;
 		StringBuilder output = new StringBuilder();
 		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0)
-		    output.append(n == 1 ? 1 : (n - (1 << (int)(Math.log(n - 1) / Math.log(2)))) << 1).append('\n');
+		    output.append(n == 1 ? 1 : n - (1 << (int)(Math.log(n - 1) / Math.log(2))) << 1).append('\n');
 		System.out.print(output);
 	}
 }
