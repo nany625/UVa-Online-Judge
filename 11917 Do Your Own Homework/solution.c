28816767ed616996c08e50e17b3707c867c16573
nany625 <jimmy19980625@gmail.com>
2024-10-24 03:44:02 +0800

    Update solution.c

--- 11917 Do Your Own Homework/solution.c
+++ 11917 Do Your Own Homework/solution.c
@@ -13,7 +13,7 @@ int compare(const void *a, const void *b) {
 const char *binarySearch(Subject *array, int size, Subject key) {
     int left = 0, right = size - 1;
     while(left <= right) {
-        int mid = left + ((right - left) >> 1);
+        int mid = left + (right - left >> 1);
         int cmp = strcmp(array[mid].name, key.name);
         if(cmp == 0) {
             if(key.days >= array[mid].days)
