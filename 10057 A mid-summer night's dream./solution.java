c6a70c6c371afd08748b45b4408f2c0ea57c5bc5
nany625 <jimmy19980625@gmail.com>
2024-10-23 10:20:54 +0800

    Update solution.java

--- 10057 A mid-summer night's dream./solution.java
+++ 10057 A mid-summer night's dream./solution.java
@@ -14,12 +14,12 @@ public class Main {
                 X[i] = (int)st.nval;
             }
             Arrays.sort(X);
-            int low = (n - 1) >> 1, high = n >> 1;
+            int low = n - 1 >> 1, high = n >> 1;
             while(low > 0 && X[low] == X[low - 1])
                 --low;
             while(high < n - 1 && X[high] == X[high + 1])
                 ++high;
-            output.append(X[(n - 1) >> 1]).append(' ').append(high - low + 1).append(' ').append(X[n >> 1] - X[(n - 1) >> 1] + 1).append('\n');
+            output.append(X[n - 1 >> 1]).append(' ').append(high - low + 1).append(' ').append(X[n >> 1] - X[n - 1 >> 1] + 1).append('\n');
     	}
     	System.out.print(output);
 	}
