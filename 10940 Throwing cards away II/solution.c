1caaa8164f3b9c6fb753f7b8781cc9ea5361689c
nany625 <jimmy19980625@gmail.com>
2024-10-23 12:55:05 +0800

    Update solution.c

--- 10940 Throwing cards away II/solution.c
+++ 10940 Throwing cards away II/solution.c
@@ -4,6 +4,6 @@
 int main() {
 	int n;
 	while(scanf("%d", &n) && n != 0)
-	    printf("%d\n", n == 1 ? 1 : (n - (1 << (int)log2(n - 1))) << 1);
+	    printf("%d\n", n == 1 ? 1 : n - (1 << (int)log2(n - 1)) << 1);
 	return 0;
 }
