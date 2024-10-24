8a03688f566965f33a9baed1db7eef1d3fbefad5
nany625 <jimmy19980625@gmail.com>
2024-10-24 20:56:30 +0800

    Update solution.java

--- 497 Strategic Defense Initiative/solution.java
+++ 497 Strategic Defense Initiative/solution.java
@@ -41,7 +41,7 @@ public class Main {
 	static int binarySearch(ArrayList<Integer> arrayList, int size, int key) {
         int left = 0, right = size - 1;
         while(left <= right) {
-            int mid = left + ((right - left) >> 1);
+            int mid = left + (right - left >> 1);
             if(arrayList.get(mid) > key)
                 left = mid + 1;
             else
