1d8971c8840d3f03e12cfafc008220f7a5b5af60
nany625 <jimmy19980625@gmail.com>
2024-10-27 00:54:40 +0800

    Update solution.c

--- 1210 Sum of Consecutive Prime Numbers/solution.c
+++ 1210 Sum of Consecutive Prime Numbers/solution.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #define MAX_NUM 10000
-#define MAX_PRIME_SIZE 1229
 
 bool isComposite[MAX_NUM + 1];
-short primes[MAX_PRIME_SIZE] = {2}, primesSumCount[MAX_NUM + 1];
-int count = 1;
+short *primes = NULL, primesSumCount[MAX_NUM + 1];
+int size = 1;
 
 int main() {
-    for(short i = 3; count < MAX_PRIME_SIZE; i += 2) {
+    primes = (short*)malloc(sizeof(short));
+    primes[0] = 2;
+    for(short i = 3; i <= MAX_NUM; i += 2) {
         if(!isComposite[i]) {
-            primes[count++] = i;
+            primes = (short*)realloc(primes, (size + 1) * sizeof(short));
+            primes[size++] = i;
             if(i <= 100) {
                 for(int j = i * i; j <= MAX_NUM; j += i)
                     isComposite[j] = true;
             }
         }
     }
-    for(int i = 0; i < MAX_PRIME_SIZE; ++i) {
+    for(int i = 0; i < size; ++i) {
         int sum = 0;
-        for(int j = i; j < MAX_PRIME_SIZE && (sum += primes[j]) <= MAX_NUM; ++j)
+        for(int j = i; j < size && (sum += primes[j]) <= MAX_NUM; ++j)
             ++primesSumCount[sum];
     }
     int num;
     while(scanf("%d", &num) && num != 0)
         printf("%d\n", primesSumCount[num]);
+    free(primes);
 	return 0;
 }
