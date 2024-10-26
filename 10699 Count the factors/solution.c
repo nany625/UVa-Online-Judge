9c266f1d5978ea037e84baa5f5166c2926f0416e
nany625 <jimmy19980625@gmail.com>
2024-10-27 00:40:47 +0800

    Update solution.c

--- 10699 Count the factors/solution.c
+++ 10699 Count the factors/solution.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
-#define MAX_NUM 997
-#define MAX_PRIME_SIZE 168
+#define MAX_NUM 1000
 
 bool isComposite[MAX_NUM + 1];
-short primes[MAX_PRIME_SIZE] = {2};
-int count = 1;
+short *primes = NULL;
+int size = 1;
 
 int main() {
-    for(short i = 3; count < MAX_PRIME_SIZE; i += 2) {
+    primes = (short*)malloc(sizeof(short));
+    primes[0] = 2;
+    for(short i = 3; i <= MAX_NUM; i += 2) {
         if(!isComposite[i]) {
-            primes[count++] = i;
+            primes = (short*)realloc(primes, (size + 1) * sizeof(short));
+            primes[size++] = i;
             if(i <= 31) {
-                for(short j = i * i; j <= MAX_NUM; j += i)
+                for(int j = i * i; j <= MAX_NUM; j += i)
                     isComposite[j] = true;
             }
         }
@@ -23,7 +26,7 @@ int main() {
         printf("%d : ", num);
         short limit = sqrt(num);
         int pfCount = 0;
-        for(int i = 0; i < MAX_PRIME_SIZE && primes[i] <= limit; ++i) {
+        for(int i = 0; i < size && primes[i] <= limit; ++i) {
             if(num % primes[i] == 0) {
                 ++pfCount;
                 do {
@@ -34,5 +37,6 @@ int main() {
         }
         printf("%d\n", pfCount + (num > 1));
     }
+    free(primes);
     return 0;
 }
