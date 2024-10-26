7e7b33469f8a0f973e650fb243ae4c8a9a995ff1
nany625 <jimmy19980625@gmail.com>
2024-10-27 00:26:23 +0800

    Update solution.c

--- 10200 Prime Time/solution.c
+++ 10200 Prime Time/solution.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 #include <stdbool.h>
-#define MAX_PRIMES_SIZE 1230
+#define MAX_NUM 10007
 #define EPS 1e-6
 
-short primes[MAX_PRIMES_SIZE] = {2}, ppCount[10001] = {
+bool isComposite[MAX_NUM + 1];
+short *primes = NULL, ppCount[MAX_NUM + 1] = {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
     21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
     40, 40
 };
-int count = 1;
-
-bool isPrime(int n) {
-    short limit = sqrt(n);
-	for(int i = 1; i < count && primes[i] <= limit; ++i) {
-		if(n % primes[i] == 0)
-			return false;
-	}
-	return true;
-}
+int size = 1;
 
 bool producePrime(int n) {
     int num = n * n + n + 41;
@@ -33,11 +25,19 @@ bool producePrime(int n) {
 
 
 int main() {
-	for(short i = 3; count < MAX_PRIMES_SIZE; i += 2) {
-	    if(isPrime(i))
-	        primes[count++] = i;
+    primes = (short*)malloc(sizeof(short));
+    primes[0] = 2;
+	for(short i = 3; i <= MAX_NUM; i += 2) {
+	    if(!isComposite[i]) {
+	        primes = (short*)realloc(primes, (size + 1) * sizeof(short));
+            primes[size++] = i;
+	        if(i <= 100) {
+	            for(int j = i * i; j <= MAX_NUM; j += i)
+	                isComposite[j] = true;
+	        }
+	    }
 	}
-	for(int i = 42; i <= 10000; ++i) {
+	for(int i = 42; i <= MAX_NUM; ++i) {
 	    ppCount[i] = ppCount[i - 1];
 	    if(i % 41 == 0 || (i + 1) % 41 == 0)
 	        continue;
@@ -46,5 +46,6 @@ int main() {
     int a, b;
     while(scanf("%d %d", &a, &b) == 2)
         printf("%.2f\n", 100.0 * (ppCount[b] - ppCount[a] + (a == 0 || ppCount[a] > ppCount[a - 1])) / (b - a + 1) + EPS);
+    free(primes);
     return 0;
 }
