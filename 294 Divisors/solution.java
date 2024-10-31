fccdde11e337b69fd847eb756647a695dba4263c
nany625 <jimmy19980625@gmail.com>
2024-11-01 00:01:00 +0800

    Update solution.java

--- 294 Divisors/solution.java
+++ 294 Divisors/solution.java
@@ -1,8 +1,21 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static short[] primes = {2, 3, 5, 7, 11, 13};
+    static short MAX_NUM = 31607;
+    static boolean[] isComposite = new boolean[MAX_NUM + 1]; 
+    static ArrayList<Short> primes = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
+        primes.add((short)2);
+    	for(short i = 3; i <= MAX_NUM; i += 2) {
+    		if(!isComposite[i]) {
+    		    primes.add(i);
+    		    if(i <= 177) {
+    	            for(int j = i * i; j <= MAX_NUM; j += i)
+    	                isComposite[j] = true;
+    	        }
+    		}
+    	}
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
         st.nextToken();
@@ -24,21 +37,13 @@ public class Main {
     			}
     			L += 2;
     		}
-    		while(L % 12 != 0 && L <= U) {
-    		    int temp = factorCount(L);
-    			if(count < temp) {
-    				count = temp;
-    				ans = L;
-    			}
-    			L += 6;
-    		}
     		while(L <= U) {
     		    int temp = factorCount(L);
     			if(count < temp) {
     				count = temp;
     				ans = L;
     			}
-    			L += 12;
+    			L += 6;
     		}
     		output.append(ans).append(" has a maximum of ").append(count).append(" divisors.\n");
 		}
@@ -47,11 +52,11 @@ public class Main {
     
     static int factorCount(int n) {
         int result = 1, i = 0;
-    	while(i < 6 && n > 1) {
+    	while(i < primes.size() && n > 1) {
     	    int currPow = 0;
-    	    while(n % primes[i] == 0) {
+    	    while(n % primes.get(i) == 0) {
     	        ++currPow;
-    	        n /= primes[i];
+    	        n /= primes.get(i);
     	    }
     	    result *= currPow + 1;
     	    ++i;
