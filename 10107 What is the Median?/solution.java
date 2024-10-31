67e76943785d3442d3c19469d0163ec84a1ddefc
nany625 <jimmy19980625@gmail.com>
2024-11-01 04:57:35 +0800

    Update solution.java

--- 10107 What is the Median?/solution.java
+++ 10107 What is the Median?/solution.java
@@ -4,23 +4,22 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
         ArrayList<Integer> numbers = new ArrayList<>();
-        int size = 0;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
         StringBuilder output = new StringBuilder();
     	while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
     	    int X = (int)st.nval;
     	    numbers.add(X);
-    	    int pos = size++;
+    	    int pos = numbers.size() - 1;
     	    while(pos > 0 && X < numbers.get(pos - 1)) {
     	        numbers.set(pos, numbers.get(pos - 1));
     	        --pos;
     	    }
     	    numbers.set(pos, X);
-    	    if((size & 1) == 1)
-    	        output.append(numbers.get(size >> 1)).append('\n');
+    	    if((numbers.size() & 1) == 1)
+    	        output.append(numbers.get(numbers.size() >> 1)).append('\n');
     	    else
-    	        output.append(numbers.get((size >> 1) - 1) + ((numbers.get(size >> 1) - numbers.get((size >> 1) - 1)) >> 1)).append('\n');
+    	        output.append(numbers.get((numbers.size() >> 1) - 1) + ((numbers.get(numbers.size() >> 1) - numbers.get((numbers.size() >> 1) - 1)) >> 1)).append('\n');
     	}
         System.out.print(output);
 	}
