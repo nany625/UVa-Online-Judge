7e284aa9650d54a683468300203edfe7c743fb75
nany625 <jimmy19980625@gmail.com>
2024-11-01 02:40:15 +0800

    Update solution.java

--- 543 Goldbach's Conjecture/solution.java
+++ 543 Goldbach's Conjecture/solution.java
@@ -1,15 +1,16 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static boolean[] isComposite = new boolean[1000000];
-    static int[] primes = new int[78497];
-    static int count = 0;
+    static int MAX_NUM = 999999;
+    static boolean[] isComposite = new boolean[MAX_NUM + 1];
+    static ArrayList<Integer> primes = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
-    	for(int i = 3; i <= 999999; i += 2) {
+    	for(int i = 3; i <= MAX_NUM; i += 2) {
             if(!isComposite[i]) {
-                primes[count++] = i;
+                primes.add(i);
                 if(i <= 999) {
-                    for(int j = i * i; j <= 999999; j += i)
+                    for(int j = i * i; j <= MAX_NUM; j += i)
                         isComposite[j] = true;
                 }
             }
@@ -20,9 +21,9 @@ public class Main {
     	StringBuilder output = new StringBuilder();
     	while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0) {
 	        int i = 0;
-	        while(isComposite[n - primes[i]])
+	        while(isComposite[n - primes.get(i)])
 	            ++i;
-	        output.append(n).append(" = ").append(primes[i]).append(" + ").append(n - primes[i]).append('\n');
+	        output.append(n).append(" = ").append(primes.get(i)).append(" + ").append(n - primes.get(i)).append('\n');
     	}
 	    System.out.print(output);
 	}
