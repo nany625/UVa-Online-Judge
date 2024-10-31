9dcd6ff8db8d6718fd06c089499d3623fd547fe6
nany625 <jimmy19980625@gmail.com>
2024-11-01 04:27:37 +0800

    Update solution.java

--- 10010 Where's Waldorf?/solution.java
+++ 10010 Where's Waldorf?/solution.java
@@ -23,12 +23,11 @@ public class Main {
             while(k-- > 0) {
                 st.nextToken();
                 String word = st.sval;
-			    int wordLen = word.length();
                 boolean found = false;
     			for(int row = 0; row < m && !found; ++row) {
     				for(int col = 0; col < n && !found; ++col) {
     					for(int dir = 0; dir < 8 && !found; ++dir) {
-                            if(searchInDirection(grid, m, n, word, wordLen, row, col, dir)) {
+                            if(searchInDirection(grid, m, n, word, row, col, dir)) {
                                 output.append(row + 1).append(' ').append(col + 1).append('\n');
                                 found = true;
                             }
@@ -42,8 +41,8 @@ public class Main {
         System.out.print(output);
 	}
 	
-	static boolean searchInDirection(String[] grid, int m, int n, String word, int wordLen, int row, int col, int dir) {
-        for(int i = 0; i < wordLen; ++i) {
+	static boolean searchInDirection(String[] grid, int m, int n, String word, int row, int col, int dir) {
+        for(int i = 0; i < wordLen.length(); ++i) {
             int newRow = row + i * dRow[dir], newCol = col + i * dCol[dir];
             if(newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || 
                 Character.toUpperCase(word.charAt(i)) != Character.toUpperCase(grid[newRow].charAt(newCol))) {
