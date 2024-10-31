de0d6a5f4fdb82db8041dacba7aae1f481711e36
nany625 <jimmy19980625@gmail.com>
2024-11-01 02:50:13 +0800

    Update solution.c

--- 686 Goldbach’s Conjecture (II)/solution.c
+++ 686 Goldbach’s Conjecture (II)/solution.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 #include <stdbool.h>
-#define MAX_SIZE 3512
+#define MAX_NUM 32768
 
-short primes[MAX_SIZE] = {2}, pairs[16382] = {1};
-int count = 1;
-
-bool isPrime(short n) {
-    short limit = sqrt(n);
-	for(int i = 1; i < count && primes[i] <= limit; ++i) {
-		if(n % primes[i] == 0)
-			return false;
-	}
-	return true;
-}
+bool isComposite[MAX_NUM + 1];
+short *primes, pairs[(MAX_NUM >> 1) - 2] = {1};
+int size = 1;
 
 int main() {
-    for(short i = 3; count < MAX_SIZE; i += 2) {
-        if(isPrime(i))
-            primes[count++] = i;
+    primes = (short*)malloc(sizeof(short));
+    primes[0] = 2;
+    for(int i = 3; i <= MAX_NUM; i += 2) {
+        if(!isComposite[i]) {
+	        primes = (short*)realloc(primes, (size + 1) * sizeof(short));
+            primes[size++] = i;
+            if(i <= 181) {
+                for(int j = i * i; j <= MAX_NUM; j += i)
+                    isComposite[j] = true;
+            }
+	    }
     }
-    for(int i = 1; i < MAX_SIZE; ++i) {
-        for(int j = i; j < MAX_SIZE; ++j) {
+    for(int i = 1; i < size; ++i) {
+        for(int j = i; j < size; ++j) {
             int sum = primes[i] + primes[j];
-            if(sum < 32768)
+            if(sum < MAX_NUM)
                 ++pairs[(sum >> 1) - 2];
         }
     }
 	int n;
 	while(scanf("%d", &n) && n != 0)
 		printf("%d\n", pairs[(n >> 1) - 2]);
+    free(primes);
 	return 0;
 }
