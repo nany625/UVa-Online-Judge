3caf989566bd46218c3d3399f8f4dda6562e56ce
nany625 <jimmy19980625@gmail.com>
2024-11-01 12:38:15 +0800

    Update solution.java

--- 10222 Decode the Mad man/solution.java
+++ 10222 Decode the Mad man/solution.java
@@ -5,17 +5,15 @@ public class Main {
 	public static void main(String[] args) throws IOException {
         String keyboard = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./";
         HashMap<Character, Character> decode = new HashMap<>();
-        int len = keyboard.length();
-        for(int i = 2; i < len; ++i)
+        for(int i = 2; i < keyboard.length(); ++i)
             decode.put(keyboard.charAt(i), keyboard.charAt(i - 2));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String message;
         StringBuilder output = new StringBuilder();
         while((message = br.readLine()) != null) {
-    	    len = message.length();
-    	    for(int i = 0; i < len; ++i) {
+    	    for(int i = 0; i < message.length(); ++i) {
     	        if(message.charAt(i) == ' ')
-    	            output.append(" ");
+    	            output.append(' ');
     	        else {
     	            if(Character.isUpperCase(message.charAt(i)))
     	                Character.toLowerCase(message.charAt(i));
