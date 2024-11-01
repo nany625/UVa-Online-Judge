48da844ce4bc323cf32c54747770b3607de5ea48
nany625 <jimmy19980625@gmail.com>
2024-11-01 14:25:28 +0800

    Update solution.java

--- 10534 Wavio Sequence/solution.java
+++ 10534 Wavio Sequence/solution.java
@@ -7,29 +7,27 @@ public class Main {
 		StreamTokenizer st = new StreamTokenizer(br);
         StringBuilder output = new StringBuilder();
     	while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-    	    int N = (int)st.nval, lisSize = 0;
+    	    int N = (int)st.nval;
             int[] sequence = new int[N], lisDP = new int[N];
             ArrayList<Integer> LIS = new ArrayList<>();
             for(int i = 0; i < N; ++i) {
                 st.nextToken();
                 sequence[i] = (int)st.nval;
-                int pos = binarySearch(LIS, lisSize, sequence[i]);
-                if(pos == lisSize) {
+                int pos = binarySearch(LIS sequence[i]);
+                if(pos == LIS.size())
                     LIS.add(sequence[i]);
-                    ++lisSize;
-                } else
+                else
                     LIS.set(pos, sequence[i]);
                 lisDP[i] = pos + 1;
             }
-            int ldsSize = 0, max = 1;
+            int max = 1;
             int[] ldsDP = new int[N];
             ArrayList<Integer> LDS = new ArrayList<>();
             for(int i = N - 1; i >= 0 && max < lisSize; --i) {
-                int pos = binarySearch(LDS, ldsSize, sequence[i]);
-                if(pos == ldsSize) {
+                int pos = binarySearch(LDS, sequence[i]);
+                if(pos == LDS.size())
                     LDS.add(sequence[i]);
-                    ++ldsSize;
-                } else
+                else
                     LDS.set(pos, sequence[i]);
                 ldsDP[i] = pos + 1;
                 max = Math.max(max, Math.min(lisDP[i], ldsDP[i]));
@@ -39,8 +37,8 @@ public class Main {
         System.out.print(output);
 	}
 	
-	static int binarySearch(ArrayList<Integer> arrayList, int size, int key) {
-        int left = 0, right = size - 1;
+	static int binarySearch(ArrayList<Integer> arrayList, int key) {
+        int left = 0, right = arrayList.size() - 1;
         while(left <= right) {
             int mid = left + (right - left >> 1);
             if(arrayList.get(mid) == key)
