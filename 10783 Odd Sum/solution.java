6eb2896dd39e4f76a98c6698f11792be2ab92e60
nany625 <jimmy19980625@gmail.com>
2024-11-07 04:49:58 +0800

    Update solution.java

--- 10783 Odd Sum/solution.java
+++ 10783 Odd Sum/solution.java
@@ -12,8 +12,8 @@ public class Main {
             int a = (int)st.nval;
             st.nextToken();
             int b = (int)st.nval;
-            a += (a + 1) % 2;
-    	 	b -= (b + 1) % 2;
+            a += (a + 1) & 1;
+    	 	b -= (b + 1) & 1;
             output.append("Case ").append(i).append(": ").append(-a * a + b * b + (a << 1) + (b << 1) >> 2).append('\n');
         }
 		System.out.print(output);
