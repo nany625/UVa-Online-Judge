935a517663e27ba1395cc76730013e79bc5e551c
nany625 <jimmy19980625@gmail.com>
2024-11-29 11:23:38 +0800

    Update solution.java

--- 11233 Deli Deli/solution.java
+++ 11233 Deli Deli/solution.java
@@ -23,12 +23,11 @@ public class Main {
             if(irregular.containsKey(word))
                 output.append(irregular.get(word)).append('\n');
             else {
-                int len = word.length();
                 if(word.endsWith("y")) {
-        			if(word.charAt(len - 2) == 'a' || word.charAt(len - 2) == 'e' || word.charAt(len - 2) == 'i' || word.charAt(len - 2) == 'o' || word.charAt(len - 2) == 'u')
+        			if(word.charAt(word.length() - 2) == 'a' || word.charAt(word.length() - 2) == 'e' || word.charAt(word.length() - 2) == 'i' || word.charAt(word.length() - 2) == 'o' || word.charAt(word.length() - 2) == 'u')
         				output.append(word).append("s\n");
         			else
-        				output.append(word.substring(0, len - 1)).append("ies\n");
+        				output.append(word.substring(0, word.length() - 1)).append("ies\n");
         		}
         		else if(word.endsWith("o") || word.endsWith("s") || word.endsWith("ch") || word.endsWith("sh") || word.endsWith("x"))
         			output.append(word).append("es\n");
