f322d9a3dd18ff6dc6913ca32dd5635c9cd5a1b6
nany625 <jimmy19980625@gmail.com>
2024-11-30 12:07:31 +0800

    Create solution.c

---
+++ 12626 I ❤ Pizza/solution.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    char ingredients[601];
+    int requirement[] = {3, 1, 1, 1, 2, 1};
+    while(N--) {
+        scanf("%s", ingredients);
+        int count[6] = {}, len = strlen(ingredients);
+        for(int i = 0; i < len; ++i) {
+            switch(ingredients[i]) {
+                case 'A':
+                    ++count[0];
+                    break;
+                case 'G':
+                    ++count[1];
+                    break;
+                case 'I':
+                    ++count[2];
+                    break;
+                case 'M':
+                    ++count[3];
+                    break;
+                case 'R':
+                    ++count[4];
+                    break;
+                case 'T':
+                    ++count[5];
+            }
+        }
+        int min = count[0] / 3;
+        for(int i = 1; i < 6; ++i)
+            min = min < count[i] / requirement[i] ? min : count[i] / requirement[i];
+        printf("%d\n", min);
+    }
+    return 0;
+}
