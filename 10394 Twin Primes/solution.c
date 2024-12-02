4053555309461b74d5d69cf843c671442ac0384f
nany625 <jimmy19980625@gmail.com>
2024-12-02 11:07:22 +0800

    Create solution.c

---
+++ 10394 Twin Primes/solution.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define MAX_NUM 20000000
+
+bool isComposite[MAX_NUM + 1];
+int twinPrimes[100000], count = 1;
+
+int main() {
+    twinPrimes[0] = 3;
+    for(int i = 5; i <= MAX_NUM; i += 4) {
+        if(!isComposite[i]) {
+            if(i <= 4472) {
+                for(int j = i * i; j <= MAX_NUM; j += i << 1)
+                    isComposite[j] = true;
+            }
+        }
+        i += 2;
+        if(!isComposite[i]) {
+            if(i <= 4472) {
+                for(int j = i * i; j <= MAX_NUM; j += i << 1)
+                    isComposite[j] = true;
+            }
+        }
+        if(!isComposite[i - 2] && !isComposite[i])
+            twinPrimes[count++] = i - 2;
+    }
+    int S;
+    while(scanf("%d", &S) == 1)
+        printf("(%d, %d)\n", twinPrimes[S - 1], twinPrimes[S - 1] + 2);
+	return 0;
+}
