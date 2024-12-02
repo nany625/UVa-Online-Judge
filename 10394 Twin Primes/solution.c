3ba385b7a50a89ef05e789b2a6456a3e06756a65
nany625 <jimmy19980625@gmail.com>
2024-12-02 11:33:09 +0800

    Update solution.c

--- 10394 Twin Primes/solution.c
+++ 10394 Twin Primes/solution.c
@@ -8,18 +8,14 @@ int twinPrimes[100000], count = 1;
 int main() {
     twinPrimes[0] = 3;
     for(int i = 5; i <= MAX_NUM; i += 4) {
-        if(!isComposite[i]) {
-            if(i <= 4472) {
-                for(int j = i * i; j <= MAX_NUM; j += i << 1)
-                    isComposite[j] = true;
-            }
+        if(!isComposite[i] && i <= 4472) {
+            for(int j = i * i; j <= MAX_NUM; j += i << 1)
+                isComposite[j] = true;
         }
         i += 2;
-        if(!isComposite[i]) {
-            if(i <= 4472) {
-                for(int j = i * i; j <= MAX_NUM; j += i << 1)
-                    isComposite[j] = true;
-            }
+        if(!isComposite[i] && i <= 4472) {
+            for(int j = i * i; j <= MAX_NUM; j += i << 1)
+                isComposite[j] = true;
         }
         if(!isComposite[i - 2] && !isComposite[i])
             twinPrimes[count++] = i - 2;
