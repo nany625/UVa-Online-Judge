22bd6ccdf1e290a8fc59881733f88968dae8c1aa
nany625 <jimmy19980625@gmail.com>
2024-12-02 11:18:36 +0800

    Update solution.c

--- 10699 Count the factors/solution.c
+++ 10699 Count the factors/solution.c
@@ -16,7 +16,7 @@ int main() {
             primes = (short*)realloc(primes, (size + 1) * sizeof(short));
             primes[size++] = i;
             if(i <= 31) {
-                for(int j = i * i; j <= MAX_NUM; j += i)
+                for(int j = i * i; j <= MAX_NUM; j += i << 1)
                     isComposite[j] = true;
             }
         }
