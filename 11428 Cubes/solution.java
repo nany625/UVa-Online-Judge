36f6c3db8b22f58a7a3829b06a6831fb0928525f
nany625 <jimmy19980625@gmail.com>
2024-12-03 16:40:15 +0800

    Update solution.java

--- 11428 Cubes/solution.java
+++ 11428 Cubes/solution.java
@@ -1,12 +1,13 @@
 import java.io.*;
 
 public class Main {
+    static int MAX_NUM = 10000;
 	public static void main(String[] args) throws IOException {
-        Solution[] solutions = new Solution[10000];
-        boolean[] table = new boolean[10000];
+        Solution[] solutions = new Solution[MAX_NUM];
+        boolean[] table = new boolean[MAX_NUM];
         for(short y = 1; y <= 57; ++y) {
             int N;
-            for(short x = (short)(y + 1); (N = x * x * x - y * y * y) <= 10000; ++x) {
+            for(short x = (short)(y + 1); (N = x * x * x - y * y * y) <= MAX_NUM; ++x) {
                 if(!table[N - 1]) {
                     table[N - 1] = true;
                     solutions[N - 1] = new Solution(x, y);
