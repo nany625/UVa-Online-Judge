8d110f81a3430cf3263666717fa7aab92262454a
nany625 <jimmy19980625@gmail.com>
2024-12-03 20:44:15 +0800

    Update solution.java

--- 11764 Jumping Mario/solution.java
+++ 11764 Jumping Mario/solution.java
@@ -10,8 +10,8 @@ public class Main {
         for(int i = 1; i <= T; ++i) {
             st.nextToken();
             String wall = br.readLine();
-            int highJumps = 0, lowJumps = 0, len = wall.length();
-            for(int j = 2; j < len; j += 2) {
+            int highJumps = 0, lowJumps = 0;
+            for(int j = 2; j < wall.length(); j += 2) {
                 if(wall.charAt(j) > wall.charAt(j - 2))
                     ++highJumps;
                 else if(wall.charAt(j) < wall.charAt(j - 2))
