156dff73b1163703178858fa6db9dd6b17846314
nany625 <jimmy19980625@gmail.com>
2024-12-05 06:18:21 +0800

    Update solution.c

--- 11408 Count DePrimes/solution.c
+++ 11408 Count DePrimes/solution.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #define MAX_NUM 5000000
 
 bool isComposite[MAX_NUM + 1] = {true, true};
-int sum[MAX_NUM + 1], count[MAX_NUM + 1];
+int *primes, sum[MAX_NUM + 1], count[MAX_NUM + 1], size;
 
 int main() {
     for(int i = 2; i <= MAX_NUM; ++i) {
         if(!isComposite[i]) {
-            for(int j = i; j <= MAX_NUM; j += i)
-                sum[j] += i;
-            if(i <= 2236) {
-                for(int j = i * i; j <= MAX_NUM; j += i)
-                    isComposite[j] = true;
-            }
+            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
+            primes[size++] = i;
+            sum[i] = i;
+        }
+        for(int j = 0, temp; j < size && (temp = primes[j] * i) <= MAX_NUM; ++j) {
+            isComposite[temp] = true;
+            if(i % primes[j] == 0) {
+                sum[temp] = sum[i];
+                break;
+            } else
+                sum[temp] = sum[i] + primes[j];
         }
         count[i] = count[i - 1] + !isComposite[sum[i]];
     }
@@ -23,5 +29,6 @@ int main() {
         scanf("%d", &b);
         printf("%d\n", count[b] - count[a - 1]);
     }
+    free(primes);
     return 0;
 }
