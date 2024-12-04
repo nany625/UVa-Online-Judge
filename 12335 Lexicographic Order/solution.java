1f9676b9ff322a57a7d4e843073fe212ad0f1b1e
nany625 <jimmy19980625@gmail.com>
2024-12-05 07:45:47 +0800

    Update solution.java

--- 12335 Lexicographic Order/solution.java
+++ 12335 Lexicographic Order/solution.java
@@ -16,15 +16,14 @@ public class Main {
     	    st.nextToken();
     	    char[] s = st.sval.toCharArray();
     	    long k = Long.parseLong(br.readLine()) - 1;
-    	    int len = s.length;
-    	    char[] origin = new char[len];
-    	    boolean[] used = new boolean[len];
-    	    for(int j = len - 1; j >= 0; --j) {
+    	    char[] origin = new char[s.length];
+    	    boolean[] used = new boolean[s.length];
+    	    for(int j = s.length - 1; j >= 0; --j) {
                 int index = (int)(k / factorials[j]), count = 0;
-                for(int l = 0; l < len; ++l) {
+                for(int l = 0; l < s.length; ++l) {
                     if(!used[l]) {
                         if(count == index) {
-                            origin[l] = s[len - 1 - j];
+                            origin[l] = s[s.length - 1 - j];
                             used[l] = true;
                             break;
                         } else
@@ -33,7 +32,7 @@ public class Main {
                 }
                 k %= factorials[j];
             }
-            for(int j = 0; j < len; ++j)
+            for(int j = 0; j < s.length; ++j)
                 output.append(origin[j]);
 		    output.append('\n');
 		}
