fb4902dff48d64cbd11c3c4819c20859d72f93d6
nany625 <jimmy19980625@gmail.com>
2024-12-04 11:34:13 +0800

    Update solution.java

--- 11827 Maximum GCD/solution.java
+++ 11827 Maximum GCD/solution.java
@@ -10,14 +10,13 @@ public class Main {
         StringBuilder output = new StringBuilder();
         while(N-- > 0) {
             String[] tokens = br.readLine().split("\\s+");
-            int len = tokens.length;
-            int[] numbers = new int[len];
-            for(int i = 0; i < len; ++i)
+            int[] numbers = new int[tokens.length];
+            for(int i = 0; i < tokens.length; ++i)
                 numbers[i] = Integer.parseInt(tokens[i]);
             Arrays.sort(numbers);
             int max = 1;
-            for(int i = len - 2; i >= 0 && numbers[i] > max; --i) {
-                for(int j = len - 1; j > i; --j)
+            for(int i = tokens.length - 2; i >= 0 && numbers[i] > max; --i) {
+                for(int j = tokens.length - 1; j > i; --j)
                     max = Math.max(max, GCD(numbers[i], numbers[j]));
             }
             output.append(max).append('\n');
