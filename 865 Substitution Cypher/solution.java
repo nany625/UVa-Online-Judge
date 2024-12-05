81edc39ef284b37d0cc75bb542d5c368b607bd74
nany625 <jimmy19980625@gmail.com>
2024-12-05 18:54:54 +0800

    Update solution.java

--- 865 Substitution Cypher/solution.java
+++ 865 Substitution Cypher/solution.java
@@ -11,15 +11,13 @@ public class Main {
         StringBuilder output = new StringBuilder();
     	while(cases-- > 0) {
     	    String plaintext = br.readLine(), substitution = br.readLine();
-    	    output.append(substitution + "\n" + plaintext + "\n");
-    	    int len = plaintext.length();
+    	    output.append(substitution).append('\n').append(plaintext).append('\n');
     	    HashMap<Character, Character> cypher = new HashMap<>();
-    	    for(int i = 0; i < len; ++i)
+    	    for(int i = 0; i < plaintext.length(); ++i)
     	        cypher.put(plaintext.charAt(i), substitution.charAt(i));
     	    String text;
     	    while((text = br.readLine()) != null && !text.isEmpty()) {
-    	        len = text.length();
-    	        for(int i = 0; i < len; ++i)
+    	        for(int i = 0; i < text.length(); ++i)
     	            output.append(cypher.containsKey(text.charAt(i)) ? (char)cypher.get(text.charAt(i)) : text.charAt(i));
     	        output.append('\n');
     	    }
