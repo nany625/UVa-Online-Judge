a30b2a9a3f5b6dd3bffb377ee6d2ecde509c1b59
nany625 <jimmy19980625@gmail.com>
2024-12-05 10:38:32 +0800

    Update solution.java

--- 12640 Largest Sum Game/solution.java
+++ 12640 Largest Sum Game/solution.java
@@ -7,8 +7,8 @@ public class Main {
 		StringBuilder output = new StringBuilder();
 		while((line = br.readLine()) != null) {
 		    String[] tokens = line.split("\\s+");
-		    int max = Integer.parseInt(tokens[0]), curr = max, size = tokens.length;
-		    for(int i = 1; i < size; ++i) {
+		    int max = Integer.parseInt(tokens[0]), curr = max;
+		    for(int i = 1; i < tokens.length; ++i) {
 		        int temp = Integer.parseInt(tokens[i]);
 		        curr = Math.max(temp, curr + temp);
 		        max = Math.max(max, curr);
