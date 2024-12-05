436508272f3a63000a610794e01057b712970d0c
nany625 <jimmy19980625@gmail.com>
2024-12-05 12:32:26 +0800

    Update solution.java

--- 13181 Sleeping in hostels/solution.java
+++ 13181 Sleeping in hostels/solution.java
@@ -6,18 +6,18 @@ public class Main {
         String bed;
         StringBuilder output = new StringBuilder();
         while((bed = br.readLine()) != null) {
-            int len = bed.length(), prev = 0;
-            while(prev < len && bed.charAt(prev) != 'X')
+            int prev = 0;
+            while(prev < bed.length() && bed.charAt(prev) != 'X')
                 ++prev;
             int curr = prev + 1, max = prev - 1;
-            while(curr < len) {
+            while(curr < bed.length()) {
                 if(bed.charAt(curr) == 'X') {
                     max = Math.max(max, curr - prev - 2 >> 1);
                     prev = curr;
                 }
                 ++curr;
             }
-            max = Math.max(max, len - prev - 2);
+            max = Math.max(max, bed.length() - prev - 2);
             output.append(max).append('\n');
         }
         System.out.print(output);
