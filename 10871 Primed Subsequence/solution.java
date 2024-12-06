6aa7ec0ec2d79dc2059bb17047b6c7ffa4e81db4
nany625 <jimmy19980625@gmail.com>
2024-12-06 17:09:42 +0800

    Update solution.java

--- 10871 Primed Subsequence/solution.java
+++ 10871 Primed Subsequence/solution.java
@@ -6,18 +6,7 @@ public class Main {
     static boolean[] isComposite = new boolean[MAX_NUM + 1];
     static ArrayList<Short> primes = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        primes.add((short)2);
-        for(short i = 4; i <= MAX_NUM; i += 2)
-            isComposite[i] = true;
-        for(short i = 3; i <= MAX_NUM; i += 2) {
-            if(!isComposite[i]) {
-                primes.add(i);
-                if(i <= 100) {
-                    for(int j = i * i; j <= MAX_NUM; j += i << 1)
-                        isComposite[j] = true;
-                }
-            }
-        }
+        eulerSieve();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StreamTokenizer st = new StreamTokenizer(br);
         st.nextToken();
@@ -50,6 +39,19 @@ public class Main {
                 output.append("This sequence is anti-primed.\n");
         }
         System.out.print(output);
+    }
+    
+    static void eulerSieve() {
+	    isComposite[0] = isComposite[1] = true;
+        for(short n = 2; n <= MAX_NUM; ++n) {
+            if(!isComposite[n])
+                primes.add(n);
+            for(int i = 0, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
+                isComposite[temp] = true;
+                if(n % primes.get(i) == 0)
+                    break;
+            }
+        }
     }
     
     static boolean isPrime(int n) {
