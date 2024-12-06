ded50502d29081ce33b248ec9423898538ec990b
nany625 <jimmy19980625@gmail.com>
2024-12-06 19:02:52 +0800

    Update solution.c

--- 11105 Semi-prime H-numbers/solution.c
+++ 11105 Semi-prime H-numbers/solution.c
@@ -4,20 +4,27 @@
 #define MAX_NUM 1000001
 
 bool isHComposite[MAX_NUM + 1], isHSemiPrime[MAX_NUM + 1];
-int ans[MAX_NUM + 1], *hPrimes, hPrimeSize;
+int ans[MAX_NUM + 1], *hPrimes, size;
+
+void eulerSieve() {
+    for(int n = 5; n <= MAX_NUM; n += 4) {
+        if(!isHComposite[n]) {
+            hPrimes = (int*)realloc(hPrimes, (size + 1) * sizeof(int));
+            hPrimes[size++] = n;
+        }
+        for(int i = 0, temp; i < size && (temp = hPrimes[i] * n) <= MAX_NUM; ++i) {
+            isHComposite[temp] = true;
+            if(n % hPrimes[i] == 0)
+                break;
+        }
+    }
+}
 
 int main() {
-    for(int i = 5; i <= MAX_NUM; i += 4) {
-        if(!isHComposite[i]) {
-            hPrimes = (int*)realloc(hPrimes, (hPrimeSize + 1) * sizeof(int));
-            hPrimes[hPrimeSize++] = i;
-            for(int j = 0, temp; j < hPrimeSize && (temp = hPrimes[j] * i) <= MAX_NUM; ++j)
-                isHSemiPrime[temp] = true;
-            if(i <= 1000) {
-                for(int j = i * i; j <= MAX_NUM; j += i << 2)
-                    isHComposite[j] = true;
-            }
-        }
+    eulerSieve();
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0, temp; j < size && (temp = hPrimes[i] * hPrimes[j]) <= MAX_NUM; ++j)
+            isHSemiPrime[temp] = true;
     }
     for(int i = 1; i <= MAX_NUM; ++i)
         ans[i] = ans[i - 1] + isHSemiPrime[i];
