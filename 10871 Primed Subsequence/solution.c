874501177e086bd0883def26afb284ecbe22bf9e
nany625 <jimmy19980625@gmail.com>
2024-12-06 16:58:59 +0800

    Update solution.c

--- 10871 Primed Subsequence/solution.c
+++ 10871 Primed Subsequence/solution.c
@@ -6,7 +6,21 @@
 
 bool isComposite[MAX_NUM + 1] = {true, true};
 short *primes;
-int size = 1;
+int size;
+
+void eulerSieve() {
+    for(short n = 2; n <= MAX_NUM; ++n) {
+        if(!isComposite[n]) {
+            primes = (short*)realloc(primes, (size + 1) * sizeof(short));
+            primes[size++] = n;
+        }
+        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            isComposite[temp] = true;
+            if(n % primes[i] == 0)
+                break;
+        }
+    }
+}
 
 bool isPrime(int n) {
     int limit = sqrt(n);
@@ -18,20 +32,7 @@ bool isPrime(int n) {
 }
 
 int main() {
-    primes = (short*)malloc(sizeof(short));
-    primes[0] = 2;
-    for(short i = 4; i <= MAX_NUM; i += 2)
-        isComposite[i] = true;
-    for(short i = 3; i <= MAX_NUM; i += 2) {
-        if(!isComposite[i]) {
-            primes = (short*)realloc(primes, (size + 1) * sizeof(short));
-            primes[size++] = i;
-            if(i <= 100) {
-                for(int j = i * i; j <= MAX_NUM; j += i << 1)
-                    isComposite[j] = true;
-            }
-        }
-    }
+    eulerSieve();
     int t;
     scanf("%d", &t);
     while(t--) {
