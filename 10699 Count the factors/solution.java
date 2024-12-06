88a67eb1ee759bf10ebd1047d46ab6670e7075ca
nany625 <jimmy19980625@gmail.com>
2024-12-06 14:41:45 +0800

    Update solution.java

--- 10699 Count the factors/solution.java
+++ 10699 Count the factors/solution.java
@@ -6,17 +6,7 @@ public class Main {
     static boolean[] isComposite = new boolean[MAX_NUM + 1];
     static ArrayList<Short> primes = new ArrayList<>();
 	public static void main (String[] args) throws IOException {
-		primes.add((short)2);
-    	for(short i = 3; i <= MAX_NUM; i += 2) {
-    		if(!isComposite[i]) {
-    		    primes.add(i);
-    		    if(i <= 31) {
-    	            for(int j = i * i; j <= MAX_NUM; j += i << 1)
-    	                isComposite[j] = true;
-    	        }
-    		}
-    	}
-    	int size = primes.size();
+		eratosthenesSieve();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
 		int num;
@@ -24,18 +14,31 @@ public class Main {
 		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (num = (int)st.nval) != 0) {
 		    output.append(num).append(" : ");
 		    short limit = (short)Math.sqrt(num);
-		    int pfCount = 0;
-		    for(int i = 0; i < size && primes.get(i) <= limit; ++i) {
+		    int count = 0;
+		    for(int i = 0; i < primes.size() && primes.get(i) <= limit; ++i) {
 		        if(num % primes.get(i) == 0) {
-                    ++pfCount;
+                    ++count;
                     do {
                         num /= primes.get(i);
                     } while(num % primes.get(i) == 0);
                     limit = (short)Math.sqrt(num);
 		        }
 		    }
-		    output.append(pfCount + (num > 1 ? 1 : 0)).append('\n');
+		    output.append(count + (num > 1 ? 1 : 0)).append('\n');
 		}
 		System.out.print(output);
 	}
+	
+	static void eratosthenesSieve() {
+	    primes.add((short)2);
+        for(short n = 3; n <= MAX_NUM; n += 2) {
+            if(!isComposite[n]) {
+                primes.add(n);
+                if(n <= 31) {
+                    for(int i = n * n; i <= MAX_NUM; i += n << 1)
+                        isComposite[i] = true;
+                }
+            }
+        }
+    }
 }
