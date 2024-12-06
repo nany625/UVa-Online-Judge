2a74dd43d4aeec7a975744149e5b630df7100c34
nany625 <jimmy19980625@gmail.com>
2024-12-06 23:10:53 +0800

    Update solution.c

--- 11466 Largest Prime Divisor/solution.c
+++ 11466 Largest Prime Divisor/solution.c
@@ -5,7 +5,21 @@
 #define MAX_NUM 10000000
 
 bool isComposite[MAX_NUM + 1];
-int *primes, size = 1;
+int *primes, size;
+
+void eulerSieve() {
+    for(int n = 2; n <= MAX_NUM; ++n) {
+        if(!isComposite[n]) {
+            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
+            primes[size++] = n;
+        }
+        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            isComposite[temp] = true;
+            if(n % primes[i] == 0)
+                break;
+        }
+    }
+}
 
 long solve(long n) {
     int limit = sqrt(n), count = 0;
@@ -28,18 +42,7 @@ long solve(long n) {
 }
 
 int main() {
-    primes = (int*)malloc(sizeof(int));
-    primes[0] = 2;
-    for(int i = 3; i <= MAX_NUM; i += 2) {
-        if(!isComposite[i]) {
-            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
-            primes[size++] = i;
-            if(i <= 3162) {
-                for(int j = i * i; j <= MAX_NUM; j += i << 1)
-                    isComposite[j] = true;
-            }
-        }
-    }
+    eulerSieve();
     long N;
     while(scanf("%ld", &N) && N != 0)
         printf("%ld\n", solve(labs(N)));
