698a8f8209a26d4236ba5bb9063488ecbe374c92
nany625 <jimmy19980625@gmail.com>
2024-12-06 23:13:42 +0800

    Update solution.java

--- 11466 Largest Prime Divisor/solution.java
+++ 11466 Largest Prime Divisor/solution.java
@@ -6,16 +6,7 @@ public class Main {
     static boolean[] isComposite = new boolean[MAX_NUM + 1];
     static ArrayList<Integer> primes = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        primes.add(2);
-        for(int i = 3; i <= MAX_NUM; i += 2) {
-            if(!isComposite[i]) {
-                primes.add(i);
-                if(i <= 3162) {
-                    for(int j = i * i; j <= MAX_NUM; j += i << 1)
-                        isComposite[j] = true;
-                }
-            }
-        }
+        eulerSieve();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StreamTokenizer st = new StreamTokenizer(br);
         long N;
@@ -23,6 +14,18 @@ public class Main {
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (long)st.nval) != 0)
             output.append(solve(Math.abs(N))).append('\n');
         System.out.print(output);
+    }
+    
+    static void eulerSieve() {
+        for(int n = 2; n <= MAX_NUM; ++n) {
+            if(!isComposite[n])
+                primes.add(n);
+            for(int i = 0, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
+                isComposite[temp] = true;
+                if(n % primes.get(i) == 0)
+                    break;
+            }
+        }
     }
     
     static long solve(long n) {
