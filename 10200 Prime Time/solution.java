e5337e854e45bfbb13bc6d6b2ad7617c08b54df2
nany625 <jimmy19980625@gmail.com>
2024-12-06 12:28:57 +0800

    Update solution.java

--- 10200 Prime Time/solution.java
+++ 10200 Prime Time/solution.java
@@ -31,7 +31,6 @@ public class Main {
 	}
 	
 	static void eulerSieve() {
-	    isComposite[0] = isComposite[1] = true;
         for(short n = 2; n <= MAX_NUM; ++n) {
             if(!isComposite[n])
                 primes.add(n);
