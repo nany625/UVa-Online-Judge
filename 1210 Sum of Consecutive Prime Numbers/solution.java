2e4a4fb51ed3006367aab69a82a97b4487b8e791
nany625 <jimmy19980625@gmail.com>
2024-12-07 10:19:41 +0800

    Update solution.java

--- 1210 Sum of Consecutive Prime Numbers/solution.java
+++ 1210 Sum of Consecutive Prime Numbers/solution.java
@@ -5,30 +5,31 @@ public class Main {
     static short MAX_NUM = 10000;
     static boolean[] isComposite = new boolean[MAX_NUM + 1];
     static ArrayList<Short> primes = new ArrayList<>();
-	static short[] primesSumCount = new short[MAX_NUM + 1];
+	static short[] count = new short[MAX_NUM + 1];
 	public static void main(String[] args) throws IOException {
-    	primes.add((short)2);
-    	for(short i = 3; i <= MAX_NUM; i += 2) {
-    		if(!isComposite[i]) {
-    		    primes.add(i);
-    		    if(i <= 100) {
-    	            for(int j = i * i; j <= MAX_NUM; j += i << 1)
-    	                isComposite[j] = true;
-    	        }
-    		}
-    	}
-    	int size = primes.size();
-	    for(int i = 0; i < size; ++i) {
-            int sum = 0;
-            for(int j = i; j < size && (sum += primes.get(j)) <= MAX_NUM; ++j)
-                ++primesSumCount[sum];
+    	eulerSieve();
+	    for(int i = 0; i < primes.size(); ++i) {
+            for(int j = i, sum = 0; j < primes.size() && (sum += primes.get(j)) <= MAX_NUM; ++j)
+                ++count[sum];
         }
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
     	int num;
     	StringBuilder output = new StringBuilder();
     	while(st.nextToken() == StreamTokenizer.TT_NUMBER && (num = (int)st.nval) != 0)
-	        output.append(primesSumCount[num]).append('\n');
+	        output.append(count[num]).append('\n');
 	    System.out.print(output);
 	}
+	
+	static void eulerSieve() {
+        for(short n = 2; n <= MAX_NUM; ++n) {
+            if(!isComposite[n])
+                primes.add(n);
+            for(int i = 0, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
+                isComposite[temp] = true;
+                if(n % primes.get(i) == 0)
+                    break;
+            }
+        }
+    }
 }
