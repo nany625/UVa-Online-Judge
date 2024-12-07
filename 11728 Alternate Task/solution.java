4561882c717342914a83ffd03ed2f0f30547bd3f
nany625 <jimmy19980625@gmail.com>
2024-12-07 18:04:03 +0800

    Update solution.java

--- 11728 Alternate Task/solution.java
+++ 11728 Alternate Task/solution.java
@@ -1,66 +1,38 @@
-// #解法一
 import java.io.*;
 
 public class Main {
+    static short MAX_NUM = 1000;
+    static short[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31}, table = new short [MAX_NUM + 1];
 	public static void main(String[] args) throws IOException {
-        int[] table = new int[1000];
-        table[0] = 1;
-        for(int i = 2; i <= 1000; ++i) {
-            int temp = factorSum(i);
-            if(temp <= 1000)
-                table[temp - 1] = i;
+        for(short i = 1; i <= MAX_NUM; ++i) {
+            short temp = factorSum(i);
+            if(temp <= MAX_NUM)
+                table[temp] = i;
         }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
         int cases = 0, S;
         StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER && (S = (int)st.nval) != 0)
-	        output.append("Case ").append(++cases).append(": ").append(table[S - 1] != 0 ? table[S - 1] : -1).append('\n');
+	        output.append("Case ").append(++cases).append(": ").append(table[S] == 0 ? -1 : table[S]).append('\n');
         System.out.print(output);
 	}
 	
-	static int factorSum(int n) {
-        int result = 1 + n, limit = (int)Math.sqrt(n);
-        for(int i = 2; i <= limit; ++i) {
-            if(n % i == 0)
-                result += i + n / i;
-        }
-        if(Math.sqrt(n) == limit)
-            result -= limit;
-        return result;
-    }
-}
-
-// #解法二
-import java.io.*;
-
-public class Main {
-	public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StreamTokenizer st = new StreamTokenizer(br);
-        int cases = 0, S;
-        StringBuilder output = new StringBuilder();
-        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (S = (int)st.nval) != 0) {
-            if(S == 1) {
-                output.append("Case ").append(++cases).append(": 1\n");
-                continue;
+	static short factorSum(short n) {
+        short sum = 1, limit = (short)Math.sqrt(n), temp = n;
+        for(int i = 0; i < 11 && primes[i] <= limit; ++i) {
+            if(n % primes[i] == 0) {
+                short tempSum = 1, term = 1;
+                do {
+                    tempSum += term *= primes[i];
+                    n /= primes[i];
+                } while(n % primes[i] == 0);
+                sum *= tempSum;
+                limit = (short)Math.sqrt(n);
             }
-            int ans = S - 1, limit = (int)Math.ceil(Math.sqrt(1 + (S << 3)) - 1) >> 1;
-            while(ans >= limit && factorSum(ans) != S)
-	            --ans;
-	        output.append("Case ").append(++cases).append(": ").append(ans >= limit ? ans : -1).append('\n');
         }
-        System.out.print(output);
-	}
-	
-	static int factorSum(int n) {
-        int result = 1 + n, limit = (int)Math.sqrt(n);
-        for(int i = 2; i <= limit; ++i) {
-            if(n % i == 0)
-                result += i + n / i;
-        }
-        if(Math.sqrt(n) == limit)
-            result -= limit;
-        return result;
+        if(n > 1)
+            sum *= (1 + n);
+        return sum;
     }
 }
