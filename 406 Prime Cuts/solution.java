8707d0c7f9e468f9c89e8e360745501a620b7fc0
nany625 <jimmy19980625@gmail.com>
2024-12-07 13:47:43 +0800

    Update solution.java

--- 406 Prime Cuts/solution.java
+++ 406 Prime Cuts/solution.java
@@ -5,44 +5,46 @@ public class Main {
     static short MAX_NUM = 1000;
     static boolean[] isComposite = new boolean[MAX_NUM + 1];
     static ArrayList<Short> primes = new ArrayList<>();
-    static short[] pos = new short[MAX_NUM];
+    static short[] pos = new short[MAX_NUM + 1];
+    static StringBuilder output = new StringBuilder();
 	public static void main(String[] args) throws IOException {
-        primes.add(pos[0] = 1);
-        primes.add(pos[1] = 2);
-	    for(short i = 3; i <= MAX_NUM; ++i) {
-	        pos[i - 1] = pos[i - 2];
-	        if((i & 1) == 1 && !isComposite[i]) {
-	            primes.add(i);
-	            ++pos[i - 1];
-	            if(i <= 31) {
-    	            for(int j = i * i; j <= MAX_NUM; j += i << 1)
-    	                isComposite[j] = true;
-    	        }
-	        }
-	    }
+        eulerSieve();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
-        StringBuilder output = new StringBuilder();
         while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
             int N = (int)st.nval;
             st.nextToken();
             int C = (int)st.nval;
             output.append(N).append(' ').append(C).append(':');
-            if((pos[N - 1] & 1) == 0 && C << 1 < pos[N - 1])
-        		output.append(printPrimes((pos[N - 1] >> 1) - C, (pos[N - 1] >> 1) + C - 1));
-        	else if((pos[N - 1] & 1) == 1 && (C << 1) - 1 < pos[N - 1])
-        		output.append(printPrimes((pos[N - 1] + 1 >> 1) - C, (pos[N - 1] - 3 >> 1) + C));
+            if((pos[N] & 1) == 0 && C << 1 < pos[N])
+        		printPrimes((pos[N] >> 1) - C, (pos[N] >> 1) + C - 1);
+        	else if((pos[N] & 1) == 1 && (C << 1) - 1 < pos[N])
+        		printPrimes((pos[N] + 1 >> 1) - C, (pos[N] - 3 >> 1) + C);
         	else
-        		output.append(printPrimes(0, pos[N - 1] - 1));
+        		printPrimes(0, pos[N] - 1);
     	    output.append("\n\n");
         }
         System.out.print(output);
 	}
+	
+	static void eulerSieve() {
+        primes.add(pos[1] = 1);
+        for(short n = 2; n <= MAX_NUM; ++n) {
+            pos[n] = pos[n - 1];
+            if(!isComposite[n]) {
+                primes.add(n);
+                ++pos[n];
+            }
+            for(int i = 1, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
+                isComposite[temp] = true;
+                if(n % primes.get(i) == 0)
+                    break;
+            }
+        }
+    }
     
-    static StringBuilder printPrimes(int first, int last) {
-        StringBuilder ans = new StringBuilder();
+    static void printPrimes(int first, int last) {
     	for(int i = first; i <= last; ++i)
-    		ans.append(' ').append(primes.get(i));
-    	return ans;
+    		output.append(' ').append(primes.get(i));
     }
 }
