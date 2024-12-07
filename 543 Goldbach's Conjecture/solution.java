e04da2a25b30bc36cfca76354dab4da43581a210
nany625 <jimmy19980625@gmail.com>
2024-12-07 14:49:39 +0800

    Update solution.java

--- 543 Goldbach's Conjecture/solution.java
+++ 543 Goldbach's Conjecture/solution.java
@@ -2,29 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int MAX_NUM = 999999;
-    static boolean[] isComposite = new boolean[MAX_NUM + 1];
+    static int MAX_NUM = 999995;
+    static boolean[] isComposite = new boolean[(MAX_NUM >> 1) + 1];
     static ArrayList<Integer> primes = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
-    	for(int i = 3; i <= MAX_NUM; i += 2) {
-            if(!isComposite[i]) {
-                primes.add(i);
-                if(i <= 999) {
-                    for(int j = i * i; j <= MAX_NUM; j += i << 1)
-                        isComposite[j] = true;
-                }
-            }
-        }
+    	eulerSieve();
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
     	int n;
     	StringBuilder output = new StringBuilder();
     	while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0) {
 	        int i = 0;
-	        while(isComposite[n - primes.get(i)])
+	        while(isComposite[n - primes.get(i) >> 1])
 	            ++i;
 	        output.append(n).append(" = ").append(primes.get(i)).append(" + ").append(n - primes.get(i)).append('\n');
     	}
 	    System.out.print(output);
 	}
+	
+	static void eulerSieve() {
+        for(int n = 3; n <= MAX_NUM; n += 2) {
+            if(!isComposite[n >> 1])
+                primes.add(n);
+            for(int i = 0, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
+                isComposite[temp >> 1] = true;
+                if(n % primes.get(i) == 0)
+                    break;
+            }
+        }
+    }
 }
