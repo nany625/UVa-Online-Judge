5c8bfd611803e4b6c04e5a1badf7f3dd9858233b
nany625 <jimmy19980625@gmail.com>
2024-12-07 13:40:41 +0800

    Update solution.c

--- 406 Prime Cuts/solution.c
+++ 406 Prime Cuts/solution.c
@@ -4,8 +4,25 @@
 #define MAX_NUM 1000
 
 bool isComposite[MAX_NUM + 1];
-short *primes, pos[MAX_NUM] = {1, 2};
-int size = 2;
+short *primes, pos[MAX_NUM + 1] = {0, 1};
+int size = 1;
+
+void eulerSieve() {
+    primes = (short*)malloc(sizeof(short));
+    primes[0] = 1;
+    for(short n = 2; n <= MAX_NUM; ++n) {
+        pos[n] = pos[n - 1];
+        if(!isComposite[n]) {
+            primes = (short*)realloc(primes, (size + 1) * sizeof(short));
+            ++pos[primes[size++] = n];
+        }
+        for(int i = 1, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            isComposite[temp] = true;
+            if(n % primes[i] == 0)
+                break;
+        }
+    }
+}
 
 void printPrimes(int first, int last) {
 	for(int i = first; i <= last; ++i)
@@ -13,29 +30,16 @@ void printPrimes(int first, int last) {
 }
 
 int main() {
-    primes = (short*)malloc(2 * sizeof(short));
-    primes[0] = 1;
-    primes[1] = 2;
-	for(short i = 3; i <= MAX_NUM; ++i) {
-	    pos[i - 1] = pos[i - 2];
-	    if(i & 1 && !isComposite[i]) {
-	        primes = (short*)realloc(primes, (size + 1) * sizeof(short));
-	        ++pos[(primes[size++] = i) - 1];
-	        if(i <= 31) {
-	            for(int j = i * i; j <= MAX_NUM; j += i << 1)
-	                isComposite[j] = true;
-	        }
-	    }
-	}
+	eulerSieve();
 	int N, C;
     while(scanf("%d %d", &N, &C) == 2) {
     	printf("%d %d:", N, C);
-    	if(!(pos[N - 1] & 1) && C << 1 < pos[N - 1])
-    		printPrimes((pos[N - 1] >> 1) - C, (pos[N - 1] >> 1) + C - 1);
-    	else if(pos[N - 1] & 1 && (C << 1) - 1 < pos[N - 1])
-    		printPrimes((pos[N - 1] + 1 >> 1) - C, (pos[N - 1] - 3 >> 1) + C);
+    	if(!(pos[N] & 1) && C << 1 < pos[N])
+    		printPrimes((pos[N] >> 1) - C, (pos[N] >> 1) + C - 1);
+    	else if(pos[N] & 1 && (C << 1) - 1 < pos[N])
+    		printPrimes((pos[N] + 1 >> 1) - C, (pos[N] - 3 >> 1) + C);
     	else
-    		printPrimes(0, pos[N - 1] - 1);
+    		printPrimes(0, pos[N] - 1);
     	puts("\n");
     }
     free(primes);
