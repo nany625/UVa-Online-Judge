4d632129feb339c8c3e6329be5d35615ded1d26d
nany625 <jimmy19980625@gmail.com>
2024-12-07 14:46:28 +0800

    Update solution.c

--- 543 Goldbach's Conjecture/solution.c
+++ 543 Goldbach's Conjecture/solution.c
@@ -3,17 +3,17 @@
 #include <stdbool.h>
 #define MAX_NUM 999995
 
-bool isComposite[MAX_NUM + 1];
+bool isComposite[(MAX_NUM >> 1) + 1];
 int *primes, size;
 
 void eulerSieve() {
     for(int n = 3; n <= MAX_NUM; n += 2) {
-        if(!isComposite[n]) {
+        if(!isComposite[n >> 1]) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
         for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp] = true;
+            isComposite[temp >> 1] = true;
             if(n % primes[i] == 0)
                 break;
         }
@@ -25,7 +25,7 @@ int main() {
     int n;
     while(scanf("%d", &n) && n != 0) {
         int i = 0;
-        while(isComposite[n - primes[i]])
+        while(isComposite[n - primes[i] >> 1])
             ++i;
         printf("%d = %d + %d\n", n, primes[i], n - primes[i]);
     }
