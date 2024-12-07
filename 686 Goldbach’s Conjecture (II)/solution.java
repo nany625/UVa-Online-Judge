40c63ca15b8a7574f4b319c66addd618cfe70ee8
nany625 <jimmy19980625@gmail.com>
2024-12-07 15:09:35 +0800

    Update solution.java

--- 686 Goldbach’s Conjecture (II)/solution.java
+++ 686 Goldbach’s Conjecture (II)/solution.java
@@ -2,27 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int MAX_NUM = 32765;
-    static boolean[] isComposite = new boolean[MAX_NUM + 1];
+    static int MAX_NUM = 32763;
+    static boolean[] isComposite = new boolean[(MAX_NUM >> 1) + 1];
     static ArrayList<Short> primes = new ArrayList<>();
-    static short[] pairs = new short[16382];
+    static short[] pairs = new short[(MAX_NUM >> 1) + 1];
 	public static void main(String[] args) throws IOException {
-    	for(short i = 3; i <= MAX_NUM; i += 2) {
-            if(!isComposite[i]) {
-    	        primes.add(i);
-                if(i <= 181) {
-                    for(int j = i * i; j <= MAX_NUM; j += i << 1)
-                        isComposite[j] = true;
-                }
-    	    }
-        }
+    	eulerSieve();
         pairs[0] = 1;
         for(int i = 0; i < primes.size(); ++i) {
-            for(int j = i; j < primes.size(); ++j) {
-                int sum = primes.get(i) + primes.get(j);
-                if(sum < 32768)
-                    ++pairs[(sum >> 1) - 2];
-            }
+            for(int j = i, sum; j < primes.size() && (sum = primes.get(i) + primes.get(j)) <= 32766; ++j)
+                ++pairs[(sum >> 1) - 2];
         }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
@@ -32,4 +21,16 @@ public class Main {
     		output.append(pairs[(n >> 1) - 2]).append('\n');
 	    System.out.print(output);
 	}
+	
+	static void eulerSieve() {
+        for(short n = 3; n <= MAX_NUM; n += 2) {
+            if(!isComposite[n >> 1])
+                primes.add(n);
+    	    for(int i = 0, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
+    	        isComposite[temp >> 1] = true;
+    	        if(n % primes.get(i) == 0)
+    	            break;
+    	    }
+        }
+    }
 }
