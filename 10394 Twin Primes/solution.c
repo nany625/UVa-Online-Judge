55b7f35a849ce925ca53327c948a3624bdacb5cd
nany625 <jimmy19980625@gmail.com>
2024-12-08 18:47:05 +0800

    Update solution.c

--- 10394 Twin Primes/solution.c
+++ 10394 Twin Primes/solution.c
@@ -3,31 +3,31 @@
 #include <stdbool.h>
 #define MAX_NUM 20000000
 
-bool isComposite[MAX_NUM + 1];
+bool isComposite[(MAX_NUM >> 1) + 1];
 int *primes, size, twinPrimes[100000], count = 1;
 
 void eulerSieve() {
     for(int n = 5; count < 100000; n += 4) {
-        if(!isComposite[n]) {
+        if(!isComposite[n >> 1]) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
         for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp] = true;
+            isComposite[temp >> 1] = true;
             if(n % primes[i] == 0)
                 break;
         }
         n += 2;
-        if(!isComposite[n]) {
+        if(!isComposite[n >> 1]) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
         for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp] = true;
+            isComposite[temp >> 1] = true;
             if(n % primes[i] == 0)
                 break;
         }
-        if(!isComposite[n - 2] && !isComposite[n])
+        if(!isComposite[(n >> 1) - 1] && !isComposite[n >> 1])
             twinPrimes[count++] = n - 2;
     }
 }
