1e405c5d4029e9589373ad9999b405d84f693642
nany625 <jimmy19980625@gmail.com>
2024-12-08 21:09:26 +0800

    Update solution.c

--- 516 Prime Land/solution.c
+++ 516 Prime Land/solution.c
@@ -10,18 +10,20 @@ typedef struct {
     int e;
 } Factor;
 
-bool isComposite[MAX_NUM + 1];
+bool isComposite[(MAX_NUM >> 1) + 1];
 short *primes;
-int size;
+int size = 1;
 
 void eulerSieve() {
-    for(short n = 2; n <= MAX_NUM; ++n) {
-        if(!isComposite[n]) {
+    primes = (short*)malloc(sizeof(short));
+    primes[0] = 2;
+    for(short n = 3; n <= MAX_NUM; n += 2) {
+        if(!isComposite[n >> 1]) {
             primes = (short*)realloc(primes, (size + 1) * sizeof(short));
             primes[size++] = n;
         }
-        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp] = true;
+        for(int i = 1, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            isComposite[temp >> 1] = true;
             if(n % primes[i] == 0)
                 break;
         }
