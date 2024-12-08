ba8134f38a92a4bf04a1cf72546986819e6b8153
nany625 <jimmy19980625@gmail.com>
2024-12-08 18:15:04 +0800

    Update solution.c

--- 10200 Prime Time/solution.c
+++ 10200 Prime Time/solution.c
@@ -4,7 +4,7 @@
 #define MAX_NUM 10007
 #define EPS 1e-6
 
-bool isComposite[MAX_NUM + 1];
+bool isComposite[(MAX_NUM >> 1) + 1];
 short *primes, ppCount[MAX_NUM + 1] = {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
@@ -12,16 +12,18 @@ short *primes, ppCount[MAX_NUM + 1] = {
     31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
     40, 40
 };
-int size;
+int size = 1;
 
 void eulerSieve() {
-    for(short n = 2; n <= MAX_NUM; ++n) {
-        if(!isComposite[n]) {
+    primes = (short*)malloc(sizeof(short));
+    primes[0] = 2;
+    for(short n = 3; n <= MAX_NUM; n += 2) {
+        if(!isComposite[n >> 1]) {
             primes = (short*)realloc(primes, (size + 1) * sizeof(short));
             primes[size++] = n;
         }
-        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp] = true;
+        for(int i = 1, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            isComposite[temp >> 1] = true;
             if(n % primes[i] == 0)
                 break;
         }
