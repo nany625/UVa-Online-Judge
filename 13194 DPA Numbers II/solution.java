fa840e3b76e6fd66faa106a5fef5d10667ae6e00
nany625 <jimmy19980625@gmail.com>
2024-12-08 20:53:09 +0800

    Update solution.java

--- 13194 DPA Numbers II/solution.java
+++ 13194 DPA Numbers II/solution.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     static int MAX_NUM = 1000000;
-    static boolean[] isComposite = new boolean[MAX_NUM + 1]; 
+    static boolean[] isComposite = new boolean[(MAX_NUM >> 1) + 1]; 
     static ArrayList<Integer> primes = new ArrayList<>();
 	public static void main(String[] args) throws IOException {
         eulerSieve();
@@ -31,11 +31,12 @@ public class Main {
 	}
 	
 	static void eulerSieve() {
-        for(int n = 2; n <= MAX_NUM; ++n) {
-            if(!isComposite[n])
+	    primes.add(2);
+        for(int n = 3; n <= MAX_NUM; n += 2) {
+            if(!isComposite[n >> 1])
                 primes.add(n);
-            for(int i = 0, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
-                isComposite[temp] = true;
+            for(int i = 1, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
+                isComposite[temp >> 1] = true;
                 if(n % primes.get(i) == 0)
                     break;
             }
