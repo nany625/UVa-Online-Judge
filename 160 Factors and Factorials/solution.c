f6f3caab7f0f5cb6650989610964dcfd4b567070
nany625 <jimmy19980625@gmail.com>
2024-12-22 00:55:56 +0800

    Update solution.c

--- 160 Factors and Factorials/solution.c
+++ 160 Factors and Factorials/solution.c
@@ -5,28 +5,36 @@ char primes[] = {
     31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
     73, 79, 83, 89, 97
 };
+short table[101][25];
+int size[101];
+
+void initTable(int n) {
+    int temp = n;
+    for(int i = 0; i < size[n - 1]; ++i)
+        table[n][i] = table[n - 1][i];
+    size[n] = size[n - 1];
+    for(int i = 0; i < size[temp - 1] && n > 1; ++i) {
+        while(n % primes[i] == 0) {
+            ++table[temp][i];
+            n /= primes[i];
+        }
+    }
+    if(n > 1)
+        table[temp][size[temp]++] = 1;
+}
 
 int main() {
-	int N;
+    for(int n = 2; n <= 100; ++n)
+        initTable(n);
+    int N;
 	while(scanf("%d", &N) && N != 0) {
 	    printf("%3d! =", N);
-		char count[25] = {};
-		for(int i = 2; i <= N; ++i) {
-		    int j = 0, temp = i;
-		    while(temp > 1) {
-		        if(temp % primes[j] == 0) {
-		            ++count[j];
-		            temp /= primes[j];
-		        } else
-		            ++j;
-		    }
-		}
-		for(int i = 0; i < 25 && count[i] > 0; ++i) {
+		for(int i = 0; i < size[N]; ++i) {
     		if(i == 15)
     		    fputs("\n      ", stdout);
-    		printf("%3d", count[i]);
+    		printf("%3hd", table[N][i]);
 		}
 		putchar('\n');
 	}
-	return 0;
+    return 0;
 }
