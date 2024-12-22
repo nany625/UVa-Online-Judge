3c2c92643883248942fea2598e98dc7bed137a34
nany625 <jimmy19980625@gmail.com>
2024-12-23 01:57:37 +0800

    Create solution.c

---
+++ 11714 Blind Sorting/solution.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int n;
+    while(scanf("%d", &n) == 1) {
+        double temp = log2(n);
+        printf("%d\n", (int)log2(n) + n - 2 + (temp != (int)temp));
+    }
+	return 0;
+}
