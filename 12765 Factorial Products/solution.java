40a73f5862581e2e4a8852f6dd8dab852ee4c3a0
nany625 <jimmy19980625@gmail.com>
2024-12-24 15:52:18 +0800

    Create solution.java

---
+++ 12765 Factorial Products/solution.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+public class Main {
+    static int[][] table = {
+        {0, 0, 0, 0}, 
+        {0, 0, 0, 0}, 
+        {1, 0, 0, 0}, 
+        {1, 1, 0, 0}, 
+        {3, 1, 0, 0}, 
+        {3, 1, 1, 0}, 
+        {4, 2, 1, 0}, 
+        {4, 2, 1, 1}, 
+        {7, 2, 1, 1}, 
+        {7, 4, 1, 1}
+    };
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
+		StringBuilder output = new StringBuilder();
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
+		    int N = (int)st.nval;
+		    st.nextToken();
+		    int M = (int)st.nval;
+		    if(N == 0 || M == 0)
+		        break;
+		    int[] generator1 = new int[4], generator2 = new int[4];
+		    while(N-- > 0) {
+		        st.nextToken();
+                int digit = (int)st.nval;
+                for(int i = 0; i < 4; ++i)
+                    generator1[i] += table[digit][i];
+            }
+            while(M-- > 0) {
+		        st.nextToken();
+                int digit = (int)st.nval;
+                for(int i = 0; i < 4; ++i)
+                    generator2[i] += table[digit][i];
+            }
+            output.append(equal(generator1, generator2) ? "YES\n" : "NO\n");
+		}
+		System.out.print(output);
+	}
+	
+	static boolean equal(int[] array1, int[] array2) {
+        for(int i = 0; i < 4; ++i) {
+            if(array1[i] != array2[i])
+                return false;
+        }
+        return true;
+    }
+}
