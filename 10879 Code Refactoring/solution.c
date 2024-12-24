0e069553685b62c3179aa35247012b7100613d82
nany625 <jimmy19980625@gmail.com>
2024-12-24 14:39:34 +0800

    Update solution.c

--- 10879 Code Refactoring/solution.c
+++ 10879 Code Refactoring/solution.c
@@ -6,8 +6,7 @@ int main() {
     for(int i = 1; i <= N; ++i) {
         int K;
         scanf("%d", &K);
-        int count = 0;
-        for(int j = 2; count < 2; ++j) {
+        for(int j = 2, count = 0; count < 2; ++j) {
             if(K % j == 0)
                 temp[count++] = j;
         }
