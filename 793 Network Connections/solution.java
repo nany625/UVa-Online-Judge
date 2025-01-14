15c1dda176ae3e7513020befe24e4d583a385cb9
nany625 <jimmy19980625@gmail.com>
2025-01-15 06:18:53 +0800

    Update solution.java

--- 793 Network Connections/solution.java
+++ 793 Network Connections/solution.java
@@ -1,9 +1,7 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-	    Graph graph = new Graph();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
 		st.nextToken();
@@ -11,29 +9,23 @@ public class Main {
 		StringBuilder output = new StringBuilder();
 		while(cases-- > 0) {
 			st.nextToken();
-			graph.numVertices = (short)st.nval;
-			graph.conn = (ArrayList<Short>[])new ArrayList[graph.numVertices + 1];
-			for(int i = 1; i <= graph.numVertices; ++i)
-			    graph.conn[i] = new ArrayList<>();
+			int computers = (int)st.nval;
+			int[] root = new int[computers + 1];
+			for(int i = 1; i <= computers; ++i)
+			    root[i] = i;
 			int query = 0, succ = 0;
 			String s;
 			while((s = br.readLine()) != null && !s.isEmpty()) {
 			    String[] tokens = s.split("\\s+");
-			    short computeri = Short.parseShort(tokens[1]);
-			    short computerj = Short.parseShort(tokens[2]);
+			    int computeri = Integer.parseInt(tokens[1]);
+			    int computerj = Integer.parseInt(tokens[2]);
+			    int rootI = findRoot(root, computeri);
+			    int rootJ = findRoot(root, computerj);
 			    if(tokens[0].charAt(0) == 'c') {
-			        graph.conn[computeri].add(computerj);
-			        graph.conn[computerj].add(computeri);
+			        root[rootI] = root[rootJ];
 			    } else if(tokens[0].charAt(0) == 'q') {
 			        ++query;
-			        if(computeri == computerj)
-			            ++succ;
-			        else {
-			            graph.visited = new boolean[graph.numVertices + 1];
-			            boolean[] isConn = new boolean[1];
-			            dfs(graph, computeri, computerj, isConn);
-			            succ += isConn[0] ? 1 : 0;
-			        }
+			        succ += root[rootI] == root[rootJ] ? 1 : 0;
 			    }
 			}
 			output.append(succ).append(',').append(query - succ).append('\n');
@@ -43,21 +35,7 @@ public class Main {
 		System.out.print(output);
 	}
 	
-	static void dfs(Graph graph, short computeri, short computerj, boolean[] isConn) {
-        graph.visited[computeri] = true;
-        for(int i = 0; i < graph.conn[computeri].size(); ++i) {
-            if(graph.conn[computeri].get(i) == computerj) {
-                isConn[0] = true;
-                return;
-            }
-            if(!graph.visited[graph.conn[computeri].get(i)])
-                dfs(graph, graph.conn[computeri].get(i), computerj, isConn);
-        }
-    }
-}
-
-class Graph {
-    short numVertices;
-    ArrayList<Short>[] conn;
-    boolean[] visited; 
+	static int findRoot(int[] root, int child) {
+	    return root[child] == child ? child : (root[child] = findRoot(root, root[child]));
+	}
 }
