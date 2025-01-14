5b61a0b04e8055e62f73d91f9effb02188f64547
nany625 <jimmy19980625@gmail.com>
2025-01-15 06:37:32 +0800

    Create solution.c

---
+++ 793 Network Connections/solution.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int findRoot(int* root, int child) {
+	return root[child] == child ? child : root[child] = findRoot(root, root[child]);
+}
+
+int main() {
+    int cases;
+    scanf("%d", &cases);
+    char *buffer = NULL;
+    size_t bufsize = 0;
+    while(cases--) {
+        int computers;
+        scanf("%d ", &computers);
+        int root[computers + 1];
+        for(int i = 1; i <= computers; ++i)
+			root[i] = i;
+	    int query = 0, succ = 0;
+	    while(getline(&buffer, &bufsize, stdin) != -1 && buffer[0] != '\n') {
+	        char *token = strtok(buffer, " ");
+	        char command = token[0];
+	        token = strtok(NULL, " ");
+            int computeri = atoi(token);
+            token = strtok(NULL, " ");
+            int computerj = atoi(token);
+            int rootI = findRoot(root, computeri);
+			int rootJ = findRoot(root, computerj);
+			if(command == 'c')
+			    root[rootI] = root[rootJ];
+			else if(command == 'q') {
+			    ++query;
+			    succ += root[rootI] == root[rootJ];
+			}
+	    }
+        printf("%d,%d\n", succ, query - succ);
+        if(cases)
+            putchar('\n');
+    }
+    free(buffer);
+	return 0;
+}
