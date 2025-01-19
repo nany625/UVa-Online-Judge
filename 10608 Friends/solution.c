61159f2f9fdf21c8017f35bfa136c6b553e886c0
nany625 <jimmy19980625@gmail.com>
2025-01-20 01:55:28 +0800

    Create solution.c

---
+++ 10608 Friends/solution.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define MAX_NUM 30000
+
+int root[MAX_NUM + 1], depth[MAX_NUM + 1];
+
+void init(int N) {
+    for(int n = 1; n <= N; ++n) {
+        root[n] = n;
+        depth[n] = 1;
+    }
+}
+
+int findRoot(int child) {
+    return root[child] == child ? child : findRoot(root[child]);
+}
+
+int main() {
+    int cases;
+    scanf("%d", &cases);
+    while(cases--) {
+        int N, M;
+        scanf("%d %d", &N, &M);
+        init(N);
+        int max = 1;
+        while(M--) {
+            int A, B;
+            scanf("%d %d", &A, &B);
+            int rootA = findRoot(A), rootB = findRoot(B);
+            if(rootA != rootB) {
+                depth[root[rootA]] += depth[root[rootB]];
+                max = max > depth[root[rootA]] ? max : depth[root[rootA]];
+                root[rootB] = root[rootA];
+            }
+        }
+        printf("%d\n", max);
+    }
+	return 0;
+}
