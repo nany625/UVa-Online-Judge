142aeae337cdffa9611455bc93d590c5fb693aae
nany625 <jimmy19980625@gmail.com>
2025-01-20 05:09:25 +0800

    Create solution.c

---
+++ 11057 Exact Sum/solution.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX_NUM 1000000
+
+int count[MAX_NUM + 1];
+
+int main() {
+    int N;
+    while(scanf("%d", &N) == 1) {
+        for(int i = 1; i <= MAX_NUM; ++i)
+            count[i] = 0;
+        int price;
+        for(int i = 0; i < N; ++i) {
+            scanf("%d", &price);
+            ++count[price];
+        }
+        int M;
+        scanf("%d", &M);
+        int temp = M >> 1;
+        if(!(M & 1) && count[temp] >= 2)
+            printf("Peter should buy books whose prices are %d and %d.\n\n", temp, temp);
+        else {
+            while(!(count[temp] && count[M - temp]))
+                --temp;
+            printf("Peter should buy books whose prices are %d and %d.\n\n", temp, M - temp);
+        }
+    }
+	return 0;
+}
