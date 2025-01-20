d14f1b92332905d02ead99612652a79440579c21
nany625 <jimmy19980625@gmail.com>
2025-01-20 20:30:56 +0800

    Create solution.c

---
+++ 11849 CD/solution.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int N, M;
+    while(scanf("%d %d", &N, &M) && N != 0 && M != 0) {
+        int Jack[N], Jill[M];
+        for(int i = 0; i < N; ++i)
+            scanf("%d", &Jack[i]);
+        for(int i = 0; i < M; ++i)
+            scanf("%d", &Jill[i]);
+        int i = 0, j = 0, count = 0;
+        while(i < N && j < M) {
+            if(Jack[i] == Jill[j]) {
+                ++count;
+                ++i;
+                ++j;
+            } else if(Jack[i] > Jill[j])
+                ++j;
+            else
+                ++i;
+        }
+        printf("%d\n", count);
+    }
+	return 0;
+}
