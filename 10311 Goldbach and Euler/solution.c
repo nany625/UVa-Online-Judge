1f369ebb6cc896c22e977468b39862e54a46f1e6
nany625 <jimmy19980625@gmail.com>
2025-01-24 03:56:42 +0800

    Create solution.c

---
+++ 10311 Goldbach and Euler/solution.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAX_NUM 100000000
+
+bool isComposite[(MAX_NUM >> 1) + 1];
+int *primes, size;
+
+void eulerSieve() {
+    for(int n = 3; n <= MAX_NUM; n += 2) {
+        if(!isComposite[n >> 1]) {
+            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
+            primes[size++] = n;
+        }
+        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            isComposite[temp >> 1] = true;
+            if(n % primes[i] == 0)
+                break;
+        }
+    }
+}
+
+int main() {
+    eulerSieve();
+    int n;
+    while(scanf("%d", &n) == 1) {
+        if(n <= 4)
+            printf("%d is not the sum of two primes!\n", n);
+        else {
+            if(n & 1) {
+                if(isComposite[n - 2 >> 1])
+                    printf("%d is not the sum of two primes!\n", n);
+                else
+                    printf("%d is the sum of 2 and %d.\n", n, n - 2);
+            } else {
+                int temp = n - 1 >> 1;
+                temp -= !(temp & 1);
+                while(temp >= 3 && (isComposite[temp >> 1] || isComposite[n - temp >> 1]))
+                    temp -= 2;
+                if(temp >= 3)
+                    printf("%d is the sum of %d and %d.\n", n, temp, n - temp);
+                else
+                    printf("%d is not the sum of two primes!\n", n);
+            }
+        }
+    }
+    free(primes);
+    return 0;
+}
