766926e8e14698cb0854f55a2051a41d6ddb387a
nany625 <jimmy19980625@gmail.com>
2025-02-03 15:13:23 +0800

    Update solution.c

--- 10338 Mischievous Children/solution.c
+++ 10338 Mischievous Children/solution.c
@@ -2,7 +2,6 @@
 #include <string.h>
 
 long fac[21] = {1, 1};
-int count[26];
 
 int main() {
     for(int i = 2; i < 21; ++i)
@@ -12,14 +11,13 @@ int main() {
     char s[21];
     for(int i = 1; i <= dataSets; ++i) {
         scanf("%s", s);
-        int len = strlen(s);
+        int count[26] = {}, len = strlen(s);
         for(int j = 0; j < len; ++j)
             ++count[s[j] - 'A'];
         long ans = fac[len];
         for(int j = 0; j < 26; ++j)
             ans /= fac[count[j]];
         printf("Data set %d: %ld\n", i, ans);
-        memset(count, 0, sizeof(count));
     }
 	return 0;
 }
