f622b8dae53f8482f3f38227ce8b1f3188b7ea95
nany625 <jimmy19980625@gmail.com>
2025-02-03 13:14:37 +0800

    Create solution.java

---
+++ 10945 Mother bear/solution.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer st = new StreamTokenizer(br);
+		String s;
+		StringBuilder output = new StringBuilder();
+		while((s = br.readLine()) != null && !s.equals("DONE")) {
+		    int i = 0, j = s.length() - 1;
+		    while(i < j) {
+		        while(i < j && !Character.isLetter(s.charAt(i)))
+    		        ++i;
+    		    while(i < j && !Character.isLetter(s.charAt(j)))
+    		        --j;
+    		    if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+    		        break;
+    		    ++i;
+    		    --j;
+		    }
+		    output.append(i >= j ? "You won't be eaten!\n" : "Uh oh..\n");
+		}
+		System.out.print(output);
+	}
+}
