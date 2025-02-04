32cf7971a066303fc1473ae6ad37c3da921381b6
nany625 <jimmy19980625@gmail.com>
2025-02-04 08:21:59 +0800

    Update solution.c

--- 11057 Exact Sum/solution.c
+++ 11057 Exact Sum/solution.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 #define MAX_NUM 1000000
-
-int count[MAX_NUM + 1];
 
 int main() {
     int N;
     while(scanf("%d", &N) == 1) {
-        memset(count, 0, sizeof(count));
-        int price;
+        int count[MAX_NUM + 1] = {}, price;
         for(int i = 0; i < N; ++i) {
             scanf("%d", &price);
             ++count[price];
