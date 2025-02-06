94c1c4d448331fde8a8efbea3b8ef6ef13b5a2f6
nany625 <jimmy19980625@gmail.com>
2025-02-06 13:12:19 +0800

    Update solution.c

--- 10583 Ubiquitous Religions/solution.c
+++ 10583 Ubiquitous Religions/solution.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+#define MAX_NUM 50000
 
-int findRoot(int *root, int child) {
-    return root[child] == child ? child : (root[child] = findRoot(root, root[child]));
+int root[MAX_NUM + 1];
+
+int findRoot(int child) {
+    return root[child] == child ? child : (root[child] = findRoot(root[child]));
 }
 
 int main() {
     int cases = 0, n, m;
     while(scanf("%d %d", &n, &m) && n != 0 && m != 0) {
-        int root[n + 1];
         for(int i = 1; i <= n; ++i)
             root[i] = i;
         while(m--) {
             int i, j;
             scanf("%d %d", &i, &j);
-            int rootI = findRoot(root, i), rootJ = findRoot(root, j);
+            int rootI = findRoot(i), rootJ = findRoot(j);
             n -= rootI != rootJ;
-            root[rootI] = root[rootJ];
+            root[rootJ] = root[rootI];
         }
         printf("Case %d: %d\n", ++cases, n);
     }
