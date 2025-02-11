14bfb8927a37b680c0e9870a7c1942769b4c0644
nany625 <jimmy19980625@gmail.com>
2025-02-11 17:04:51 +0800

    Update solution.c

--- 10948 The primary problem/solution.c
+++ 10948 The primary problem/solution.c
@@ -4,17 +4,15 @@
 #define MAX_NUM 1000000
 
 bool isComposite[(MAX_NUM >> 1) + 1];
-int *primes, size = 1;
+int *primes, size;
 
 void eulerSieve() {
-    primes = (int*)malloc(sizeof(int));
-    primes[0] = 2;
     for(int n = 3; n <= MAX_NUM; n += 2) {
         if(!isComposite[n >> 1]) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
-        for(int i = 1, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
             isComposite[temp >> 1] = true;
             if(n % primes[i] == 0)
                 break;
