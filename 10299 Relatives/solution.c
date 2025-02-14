ce424068e39446a285137db4bcad9a1cc5157889
nany625 <jimmy19980625@gmail.com>
2025-02-14 16:27:10 +0800

    Create solution.c

---
+++ 10299 Relatives/solution.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#define MAX_NUM 31621
+
+bool isComposite[(MAX_NUM >> 1) + 1];
+short primes[3401];
+int size = 1;
+
+void eulerSieve() {
+    primes[0] = 2;
+    for(short n = 3; n <= MAX_NUM; n += 2) {
+        if(!isComposite[n >> 1])
+            primes[size++] = n;
+        for(int i = 1, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            isComposite[temp >> 1] = true;
+            if(n % primes[i] == 0)
+                break;
+        }
+    }
+}
+
+int eulerTotient(int n) {
+    int ans = n, limit = sqrt(n);
+    for(int i = 0; i < size && primes[i] <= limit; ++i) {
+        if(n % primes[i] == 0) {
+            ans -= ans / primes[i];
+            do {
+                n /= primes[i];
+            } while(n % primes[i] == 0);
+            limit = sqrt(n);
+        }
+    }
+    if(n > 1)
+        ans -= ans / n;
+    return ans;
+}
+
+int main() {
+    eulerSieve();
+    int n;
+    while(scanf("%d", &n) && n != 0)
+        printf("%d\n", n == 1 ? 0 : eulerTotient(n));
+    return 0;
+}
