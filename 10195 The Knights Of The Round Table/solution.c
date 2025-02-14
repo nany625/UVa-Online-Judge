c8f547f682a76158ea33732503141581d7c26a3c
nany625 <jimmy19980625@gmail.com>
2025-02-14 15:42:55 +0800

    Create solution.c

---
+++ 10195 The Knights Of The Round Table/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c;
+    while(scanf("%lf %lf %lf", &a, &b, &c) == 3) {
+        if(a + b + c == 0)
+            puts("The radius of the round table is: 0.000");
+        else {
+            double s = (a + b + c) / 2;
+            double area = sqrt(s * (s - a) * (s - b) * (s - c));
+            printf("The radius of the round table is: %.3lf\n", area * 2 / (a + b + c));
+        }
+    }
+    return 0;
+}
