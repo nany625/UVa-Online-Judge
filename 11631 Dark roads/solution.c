5d1557fbb183b3c49bf4e492cae18adb7e492830
nany625 <jimmy19980625@gmail.com>
2025-02-18 23:58:09 +0800

    Update solution.c

--- 11631 Dark roads/solution.c
+++ 11631 Dark roads/solution.c
@@ -4,18 +4,18 @@
 #define MAXE 200000
 
 typedef struct {
-	int parent, child, weight;
+	int u, v, w;
 } Edge;
 
 Edge edges[MAXE];
 int root[MAXV];
 
-int findRoot(int child) {
-	return root[child] == child ? child : (root[child] = findRoot(root[child]));
+int find(int x) {
+	return root[x] == x ? x : (root[x] = find(root[x]));
 }
 
 int compare(const void *a, const void *b) {
-	return ((Edge*)a)->weight > ((Edge*)b)->weight;
+	return ((Edge*)a)->w > ((Edge*)b)->w;
 }
 
 int main() {
@@ -23,17 +23,17 @@ int main() {
 	while(scanf("%d %d", &m, &n) && m != 0) {
 	    int cost = 0;
 	    for(int i = 0; i < n; ++i)
-	        scanf("%d %d %d", &edges[i].parent, &edges[i].child, &edges[i].weight);
+	        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
 	    qsort(edges, n, sizeof(Edge), compare);
 	    for(int i = 0; i < m; ++i)
 	        root[i] = i;
 	    int save = 0;
 	    for(int i = 0; i < n; ++i) {
-	        int root1 = findRoot(edges[i].parent), root2 = findRoot(edges[i].child);
+	        int root1 = find(edges[i].u), root2 = find(edges[i].v);
 	        if(root1 != root2)
 	            root[root2] = root1;
 	        else
-	            save += edges[i].weight;
+	            save += edges[i].w;
 	    }
 	    printf("%d\n", save);
 	}
