1691658b914e9b396bdb5cee12f6ba7ca6ba8a01
nany625 <jimmy19980625@gmail.com>
2025-02-18 23:14:00 +0800

    Update solution.c

--- 908 Re-connecting Computer Sites/solution.c
+++ 908 Re-connecting Computer Sites/solution.c
@@ -4,17 +4,17 @@
 #define MAXV 1000000
 
 typedef struct {
-	int parent, child, weight;
+	int u, v, w;
 } Edge;
 
 int root[MAXV + 1];
 
-int findRoot(int child) {
-	return root[child] == child ? child : (root[child] = findRoot(root[child]));
+int find(int x) {
+	return root[x] == x ? x : (root[x] = find(root[x]));
 }
 
 int compare(const void *a, const void *b) {
-	return ((Edge*)a)->weight > ((Edge*)b)->weight;
+	return ((Edge*)a)->w > ((Edge*)b)->w;
 }
 
 int main() {
@@ -35,19 +35,19 @@ int main() {
 		scanf("%d", &K);
 		Edge *edges = (Edge*)malloc(K * sizeof(Edge));
 		for(int i = 0; i < K; ++i)
-		    scanf("%d %d %d", &edges[i].parent, &edges[i].child, &edges[i].weight);
+		    scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
 		int M;
 		scanf("%d", &M);
 		edges = (Edge*)realloc(edges, (K + M) * sizeof(Edge));
 		for(int i = K; i < K + M; ++i)
-		    scanf("%d %d %d", &edges[i].parent, &edges[i].child, &edges[i].weight);
+		    scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
 		qsort(edges, K + M, sizeof(Edge), compare);
 		int count = 0;
 		cost = 0;
 		for(int i = 0; count < N - 1; ++i) {
-		    int root1 = findRoot(edges[i].parent), root2 = findRoot(edges[i].child);
+		    int root1 = find(edges[i].u), root2 = find(edges[i].v);
 		    if(root1 != root2) {
-		        cost += edges[i].weight;
+		        cost += edges[i].w;
 		        root[root2] = root1;
 		        ++count;
 		    }
