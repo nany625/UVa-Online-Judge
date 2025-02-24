071e786e92e08f3eeea6f3418ab3b4bfb4db052d
nany625 <jimmy19980625@gmail.com>
2025-02-24 15:43:33 +0800

    Update solution.c

--- 11417 GCD/solution.c
+++ 11417 GCD/solution.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#define MAX_NUM 500
 
-int table[499] = {1};
+int phi[MAX_NUM + 1], ans[MAX_NUM + 1];
 
-int GCD(int i, int j) {
-    return j == 0 ? i : GCD(j, i % j);
+void eratosthenesSieve() {
+    for(int n = 2; n <= MAX_NUM; ++n) {
+        if(phi[n] == n) {
+            for(int i = n; i <= MAX_NUM; i += n)
+                phi[i] -= phi[i] / n;
+        }
+    }
 }
 
 int main() {
-    for(int i = 3; i < 501; ++i) {
-        table[i - 2] = table[i - 3];
-        for(int j = 1; j < i; ++j)
-            table[i - 2] += GCD(i, j);
+    for(int n = 2; n <= MAX_NUM; ++n)
+        phi[n] = n;
+    eratosthenesSieve();
+    for(int n = 2; n <= MAX_NUM; ++n) {
+        for(int i = n << 1; i <= MAX_NUM; i += n)
+            ans[i] += n * phi[i / n];
+        ans[n] += ans[n - 1] + phi[n];
     }
     int N;
     while(scanf("%d", &N) && N != 0)
-        printf("%d\n", table[N - 2]);
-    return 0;
+        printf("%d\n", ans[N]);
 }
