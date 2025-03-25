a9d1dbaddbc8a9057939b792524ca7f558406e0f
nany625 <jimmy19980625@gmail.com>
2025-03-26 01:13:39 +0800

    Update solution.c

--- 11461 Square Numbers/solution.c
+++ 11461 Square Numbers/solution.c
@@ -3,7 +3,7 @@
 
 int main() {
     int a, b;
-    while(scanf("%d %d", &a, &b) && a != 0 && b != 0)
+    while(scanf("%d %d", &a, &b) && a != 0)
         printf("%d\n", (int)sqrt(b) - (int)ceil(sqrt(a)) + 1);
     return 0;
 }
