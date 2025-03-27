c7ef83000907a688513f7203eba70ff5473f7f1a
nany625 <jimmy19980625@gmail.com>
2025-03-28 03:43:17 +0800

    Update solution.java

--- 10815 Andy's First Dictionary/solution.java
+++ 10815 Andy's First Dictionary/solution.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		TreeSet<String> dict = new TreeSet<>();
+		Set<String> dict = new TreeSet<>();
 		StringBuilder temp = new StringBuilder();
 		int c;
 		while((c = br.read()) != -1) {
