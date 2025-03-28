e800de217b93bb46bcd3a4e439a32b1e63e23c1b
nany625 <jimmy19980625@gmail.com>
2025-03-28 14:02:00 +0800

    Update solution.java

--- 100 The 3n + 1 problem/solution.java
+++ 100 The 3n + 1 problem/solution.java
@@ -1,11 +1,12 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static ArrayList<Short> lenMap = new ArrayList<>();
+    static int MAXN = 999999;
+    static short[] collatz = new short[MAXN + 1];
 	public static void main(String[] args) throws IOException {
-        lenMap.add((short)0);
-		lenMap.add((short)1);
+        collatz[1] = 1;
+        for(int n = 2; n <= MAXN; ++n)
+            init(n);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
     	StringBuilder output = new StringBuilder();
@@ -19,34 +20,24 @@ public class Main {
     	        min = max;
     	        max = temp;
     	    }
-    	    for(int i = lenMap.size(); i <= min; ++i)
-    	        Collatz(i);
-    	    short maxLen = lenMap.get(min);
-    	    for(int i = min + 1; i <= max; ++i) {
-		        if(i >= lenMap.size())
-                    Collatz(i);
-                maxLen = maxLen > lenMap.get(i) ? maxLen : lenMap.get(i);
-            }
+    	    short maxLen = collatz[min];
+    	    for(int i = min + 1; i <= max; ++i)
+                maxLen = (short)Math.max(maxLen, collatz[i]);
             output.append(maxLen).append('\n');
 		}
 		System.out.print(output);
 	}
 	
-	static void Collatz(int n) {
-	    if(n % 2 == 0) {
-	        lenMap.add((short)(lenMap.get(n / 2) + 1));
-	        return;
-	    }
-	    int size = lenMap.size();
-	    lenMap.add((short)1);
-	    long temp = n;
-	    while(temp != 1) {
-	        if(temp < n) {
-	            lenMap.set(size, (short)(lenMap.get(size) + lenMap.get((int)temp) - 1));
-	            return;
-	        }
-	        temp = temp % 2 == 1 ? 3 * temp + 1 : temp / 2;
-            lenMap.set(size, (short)(lenMap.get(size) + 1));
-	    }
-	}
+	static void init(int n) {
+        collatz[n] = (short)1;
+        long temp = n;
+        while(temp != 1) {
+            if(temp < n) {
+                collatz[n] += collatz[(int)temp] - 1;
+                return;
+            }
+            temp = (temp & 1) == 1 ? 3 * temp + 1 : temp >> 1;
+            ++collatz[n];
+        }
+    }
 }
