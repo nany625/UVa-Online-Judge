ff9ae0a7162fb1f1d3d187907cbfa42122631a37
nany625 <jimmy19980625@gmail.com>
2025-03-28 14:13:15 +0800

    Update solution.c

--- 10000 Longest Paths/solution.c
+++ 10000 Longest Paths/solution.c
@@ -3,50 +3,48 @@
 #include <string.h>
 #define MAX_NUM 100
 
-typedef struct {
-    short **links, linksCount[MAX_NUM + 1];
-    bool visited[MAX_NUM + 1];
-} Graph;
+short **links, linksCount[MAX_NUM + 1];
+bool visited[MAX_NUM + 1];
 
 int main() {
-    Graph graph;
-    graph.links = (short**)malloc((MAX_NUM + 1) * sizeof(short*));
+    links = (short**)malloc((MAX_NUM + 1) * sizeof(short*));
     for(int i = 1; i <= MAX_NUM; ++i)
-        graph.links[i] = NULL;
+        links[i] = NULL;
     int cases = 0, n;
     while(scanf("%d", &n) && n != 0) {
         int s;
         scanf("%d", &s);
-        memset(graph.linksCount, 0, sizeof(graph.linksCount));
+        memset(linksCount, 0, sizeof(linksCount));
         int p, q;
         while(scanf("%d %d", &p, &q) && p != 0) {
-            graph.links[p] = (short*)realloc(graph.links[p], (graph.linksCount[p] + 1) * sizeof(short));
-            graph.links[p][graph.linksCount[p]++] = q;
+            links[p] = (short*)realloc(links[p], (linksCount[p] + 1) * sizeof(short));
+            links[p][linksCount[p]++] = q;
         }
-        int path[n][n - 1];
-        path[0][0] = s;
-        int length = 1, currSize = 1;
+        int curr[n - 1], next[n - 1], length = 0, currSize = 1;
+        curr[0] = s;
         while(true) {
-            memset(graph.visited, 0, sizeof(graph.visited));
+            memset(visited, 0, sizeof(visited));
             int nextSize = 0;
             for(int i = 0; i < currSize; ++i) {
-                for(int j = 0; j < graph.linksCount[path[length - 1][i]]; ++j) {
-                    if(!graph.visited[graph.links[path[length - 1][i]][j]])
-                        graph.visited[path[length][nextSize++] = graph.links[path[length - 1][i]][j]] = true;
+                for(int j = 0; j < linksCount[curr[i]]; ++j) {
+                    if(!visited[links[curr[i]][j]])
+                        visited[next[nextSize++] = links[curr[i]][j]] = true;
                 }
             }
             if(nextSize == 0)
                 break;
+            for(int i = 0; i < nextSize; ++i)
+                curr[i] = next[i];
             ++length;
             currSize = nextSize;
         }
-        int finish = path[length - 1][0];
+        int finish = curr[0];
         for(int i = 1; i < currSize; ++i)
-            finish = finish < path[length - 1][i] ? finish : path[length - 1][i];
-        printf("Case %d: The longest path from %d has length %d, finishing at %d.\n\n", ++cases, s, length - 1, finish);
+            finish = finish < curr[i] ? finish : curr[i];
+        printf("Case %d: The longest path from %d has length %d, finishing at %d.\n\n", ++cases, s, length, finish);
     }
     for(int i = 1; i <= MAX_NUM; ++i)
-        free(graph.links[i]);
-    free(graph.links);
+        free(links[i]);
+    free(links);
 	return 0;
 }
