ca7819d880e6fd83797c5662c349614f2b10bac4
nany625 <jimmy19980625@gmail.com>
2025-03-28 14:20:42 +0800

    Update solution.c

--- 10004 Bicoloring/solution.c
+++ 10004 Bicoloring/solution.c
@@ -2,19 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define MAX_NUM 198
+#define MAXN 199
 
-typedef struct {
-    short numVertices, **edges, edgesCount[MAX_NUM + 1];
-    char color[MAX_NUM + 1];
-} Graph;
+short numVertices, **edges, edgesCount[MAXN];
+char color[MAXN];
 
-void dfs(Graph *graph, int startVertex, char color, bool *bicolorable) {
-    graph->color[startVertex] = color;
-    for(int i = 0; i < graph->edgesCount[startVertex]; ++i) {
-        if(graph->color[graph->edges[startVertex][i]] == '\0')
-            dfs(graph, graph->edges[startVertex][i], color == '0' ? '1' : '0', bicolorable);
-        else if(graph->color[graph->edges[startVertex][i]] == color) {
+void dfs(int startVertex, char c, bool *bicolorable) {
+    color[startVertex] = c;
+    for(int i = 0; i < edgesCount[startVertex]; ++i) {
+        if(color[edges[startVertex][i]] == '\0')
+            dfs(edges[startVertex][i], c == '0' ? '1' : '0', bicolorable);
+        else if(color[edges[startVertex][i]] == c) {
             *bicolorable = false;
             return;
         }
@@ -22,30 +20,30 @@ void dfs(Graph *graph, int startVertex, char color, bool *bicolorable) {
 }
 
 int main() {
-    Graph graph;
-    graph.edges = (short**)malloc((MAX_NUM + 1) * sizeof(short*));
-    for(int i = 0; i <= MAX_NUM; ++i)
-        graph.edges[i] = NULL;
-    while(scanf("%hd", &graph.numVertices) && graph.numVertices != 0) {
+    edges = (short**)malloc((MAXN + 1) * sizeof(short*));
+    for(int i = 0; i < MAXN; ++i)
+        edges[i] = NULL;
+    short n;
+    while(scanf("%hd", &n) && n != 0) {
         short l;
         scanf("%hd", &l);
-        memset(graph.edgesCount, 0, graph.numVertices * sizeof(short));
+        memset(edgesCount, 0, sizeof(edgesCount));
         short startVertex;
         while(l--) {
             short a, b;
             scanf("%hd %hd", &a, &b);
-            graph.edges[a] = (short*)realloc(graph.edges[a], (graph.edgesCount[a] + 1) * sizeof(short));
-            graph.edges[a][graph.edgesCount[a]++] = b;
-            graph.edges[b] = (short*)realloc(graph.edges[b], (graph.edgesCount[b] + 1) * sizeof(short));
-            startVertex = graph.edges[b][graph.edgesCount[b]++] = a;
+            edges[a] = (short*)realloc(edges[a], (edgesCount[a] + 1) * sizeof(short));
+            edges[a][edgesCount[a]++] = b;
+            edges[b] = (short*)realloc(edges[b], (edgesCount[b] + 1) * sizeof(short));
+            startVertex = edges[b][edgesCount[b]++] = a;
         }
-        memset(graph.color, '\0', graph.numVertices * sizeof(char));
+        memset(color, '\0', sizeof(color));
         bool bicolorable = true;
-        dfs(&graph, startVertex, '0', &bicolorable);
+        dfs(startVertex, '0', &bicolorable);
         puts(bicolorable ? "BICOLORABLE." : "NOT BICOLORABLE.");
     }
-    for(int i = 0; i <= MAX_NUM; ++i)
-        free(graph.edges[i]);
-    free(graph.edges);
+    for(int i = 0; i < MAXN; ++i)
+        free(edges[i]);
+    free(edges);
 	return 0;
 }
