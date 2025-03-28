0c252bc5fb7c44989b4d4fb2925e95cc4b82e3ba
nany625 <jimmy19980625@gmail.com>
2025-03-28 14:57:38 +0800

    Update solution.c

--- 10056 What is the Probability ?/solution.c
+++ 10056 What is the Probability ?/solution.c
@@ -8,7 +8,7 @@ int main() {
 		int N, I;
 		double p;
 		scanf("%d %lf %d", &N, &p, &I);
-		printf("%.4lf\n", p > 0 ? p * pow(1 - p, I - 1) / (1 - pow(1 - p, N)) : p);
+		printf("%.4f\n", p > 0 ? p * pow(1 - p, I - 1) / (1 - pow(1 - p, N)) : p);
 	}
 	return 0;
 }
