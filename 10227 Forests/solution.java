4c9ff81b46c3bfbfcd460630c27c78682ba9834c
nany625 <jimmy19980625@gmail.com>
2025-03-28 16:10:08 +0800

    Update solution.java

--- 10227 Forests/solution.java
+++ 10227 Forests/solution.java
@@ -19,13 +19,12 @@ public class Main {
                 String[] tokens = s.split("\\s+");
                 hear[Integer.parseInt(tokens[0]) - 1][Integer.parseInt(tokens[1]) - 1] = 1;
             }
-            HashMap<String, Boolean> opinions = new HashMap<>();
+            Set<String> opinions = new HashSet<>();
             for(int[] array : hear) {
-                String opinion = "";
+                StringBuilder opinion = new StringBuilder();
                 for(Integer i : array)
-                    opinion += i;
-                if(!opinions.containsKey(opinion))
-                    opinions.put(opinion, true);
+                    opinion.append(i);
+                opinions.add(opinion.toString());
             }
             output.append(opinions.size()).append('\n');
             if(cases > 0)
