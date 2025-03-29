fa145a3699ccb1c4dc1d259b60947d7915f8a584
nany625 <jimmy19980625@gmail.com>
2025-03-29 12:49:11 +0800

    Update solution.java

--- 11001 Necklace/solution.java
+++ 11001 Necklace/solution.java
@@ -4,10 +4,11 @@ public class Main {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
-        double V, Vo;
+        double V;
         StringBuilder output = new StringBuilder();
-        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (V = st.nval) != 0 && st.nextToken() == StreamTokenizer.TT_NUMBER && (Vo = st.nval) != 0) {
-            double discs = V / 2 / Vo;
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (V = st.nval) != 0) {
+            st.nextToken();
+            double discs = V / 2 / st.nval;
     	    if(discs - (int)discs == 0.5)
     	        output.append("0\n");
     	    else if(discs - (int)discs < 0.5)
