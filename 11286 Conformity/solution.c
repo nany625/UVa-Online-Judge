5049b6560e54f72d89c00eb872982a362ab7f9d0
nany625 <jimmy19980625@gmail.com>
2025-03-29 13:55:35 +0800

    Update solution.c

--- 11286 Conformity/solution.c
+++ 11286 Conformity/solution.c
@@ -7,15 +7,17 @@ typedef struct {
     short popularity;
 } Combination;
 
-void InsertionSort(short *array) {
+short courses[5];
+
+void InsertionSort() {
     for(int i = 1; i < 5; ++i) {
         int j = i - 1;
-        short insertionNum = array[i];
-        while(j >= 0 && insertionNum < array[j]) {
-            array[j + 1] = array[j];
+        short insertionNum = courses[i];
+        while(j >= 0 && insertionNum < courses[j]) {
+            courses[j + 1] = courses[j];
             --j;
         }
-        array[j + 1] = insertionNum;
+        courses[j + 1] = insertionNum;
     }
 }
 
@@ -34,36 +36,33 @@ int binarySearch(Combination *array, short size, long key) {
 }
 
 int main() {
-    short courses[5];
 	int n;
 	while(scanf("%d", &n) && n != 0) {
 	    Combination *combinations = NULL;
-		short size = 0, max = 0, students = 0;
+		short size = 0;
 		while(n--) {
 		    for(int i = 0; i < 5; ++i)
 		        scanf("%hd", &courses[i]);
-		    InsertionSort(courses);
+		    InsertionSort();
 		    long temp = 0;
 		    for(int i = 0; i < 5; ++i)
 		        temp += 1000 * temp + courses[i];
 		    int pos = binarySearch(combinations, size, temp);
-		    if(pos < size && combinations[pos].froshes == temp) {
+		    if(pos < size && combinations[pos].froshes == temp)
 		        ++combinations[pos].popularity;
-		        if(max < combinations[pos].popularity)
-				    students = max = combinations[pos].popularity;
-				else if(max == combinations[pos].popularity)
-				    students += max;
-		    } else {
+		    else {
 		        combinations = (Combination*)realloc(combinations, (size + 1) * sizeof(Combination));
 		        for(int i = size++; i > pos; --i)
                     combinations[i] = combinations[i - 1];
-                combinations[pos].froshes = temp;
-                combinations[pos].popularity = 1;
-		        if(max < 1)
-				    students = max = 1;
-				else if(max == 1)
-					++students;
+                combinations[pos] = (Combination){temp, 1};
 		    }
+		}
+		short max = 0, students = 0;
+		for(int i = 0; i < size; ++i) {
+		    if(max < combinations[i].popularity)
+		        students = max = combinations[i].popularity;
+		    else if(max == combinations[i].popularity)
+		        students += max;
 		}
 		printf("%hd\n", students);
 		free(combinations);
