c3242d1092f7511c0a1e96350a83c6f935025479
nany625 <jimmy19980625@gmail.com>
2025-03-29 15:25:57 +0800

    Update solution.java

--- 11401 Triangle Counting/solution.java
+++ 11401 Triangle Counting/solution.java
@@ -1,12 +1,12 @@
 import java.io.*;
 
 public class Main {
-    static int MAX_NUM = 1000000;
-	static long[] ans = new long[MAX_NUM + 1];
+    static int MAXN = 1000000;
+	static long[] ans = new long[MAXN + 1];
 	public static void main(String[] args) throws IOException {
-	    for(int i = 4; i <= MAX_NUM; ++i) {
-            long temp = (i >> 1) - 1;
-            ans[i] = ans[i - 1] + (1 + temp) * temp - temp * ((i + 1) & 1);
+	    for(int n = 4; n <= MAXN; ++n) {
+            long temp = (n >> 1) - 1;
+            ans[n] = ans[n - 1] + (1 + temp) * temp - temp * ((n + 1) & 1);
         }
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
