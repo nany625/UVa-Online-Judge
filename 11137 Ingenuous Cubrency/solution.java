b1403f374c7fd40243d9d0fad2114f9f71f5f031
nany625 <jimmy19980625@gmail.com>
2025-03-29 13:11:33 +0800

    Update solution.java

--- 11137 Ingenuous Cubrency/solution.java
+++ 11137 Ingenuous Cubrency/solution.java
@@ -1,7 +1,8 @@
 import java.io.*;
 
 public class Main {
-    static long[] dp = new long[10000];
+    static int MAX_AMOUNT = 9999;
+    static long[] dp = new long[MAX_AMOUNT + 1];
     static int[] coins = {
         1, 8, 27, 64, 125, 216, 343, 512, 729, 1000, 
         1331, 1728, 2197, 2744, 3375, 4096, 4913, 5832, 6859, 8000, 
@@ -10,7 +11,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		dp[0] = 1;
 		for(Integer coin : coins) {
-		    for(int j = coin; j <= 9999; ++j)
+		    for(int j = coin; j <= MAX_AMOUNT; ++j)
 		        dp[j] += dp[j - coin];
 		}
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
