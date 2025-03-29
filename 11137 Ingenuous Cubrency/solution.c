8a6ce98bc24822e6a904df008d920861829fd3af
nany625 <jimmy19980625@gmail.com>
2025-03-29 13:10:41 +0800

    Update solution.c

--- 11137 Ingenuous Cubrency/solution.c
+++ 11137 Ingenuous Cubrency/solution.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define MAX_AMOUNT 9999
 
-long dp[MAX_AMOUNT + 1];
+long dp[MAX_AMOUNT + 1] = {1};
 int coins[] = {
     1, 8, 27, 64, 125, 216, 343, 512, 729, 1000, 
     1331, 1728, 2197, 2744, 3375, 4096, 4913, 5832, 6859, 8000, 
@@ -9,7 +9,6 @@ int coins[] = {
 };
 
 int main() {
-    dp[0] = 1;
     for(int i = 0; i < 21; ++i) {
         for(int j = coins[i]; j <= MAX_AMOUNT; ++j)
             dp[j] += dp[j - coins[i]];
