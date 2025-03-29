e72b4afb8498e4ece4c995119fb5185934c902a0
nany625 <jimmy19980625@gmail.com>
2025-03-29 14:17:00 +0800

    Update solution.c

--- 11332 Summing Digits/solution.c
+++ 11332 Summing Digits/solution.c
@@ -2,9 +2,7 @@
 
 int main() {
     int n;
-    while(scanf("%d", &n) && n != 0) {
-        n %= 9;
-        printf("%d\n", n == 0 ? 9 : n);
-    }
+    while(scanf("%d", &n) && n != 0)
+        printf("%d\n", (n %= 9) == 0 ? 9 : n);
     return 0;
 }
