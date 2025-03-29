c4d91befc7941f18ad69502a545246dfd1e597db
nany625 <jimmy19980625@gmail.com>
2025-03-29 14:58:50 +0800

    Update solution.c

--- 11352 Crazy King/solution.c
+++ 11352 Crazy King/solution.c
@@ -39,41 +39,39 @@ int main() {
             }
         }
         forest[kingRow][kingCol] = 'z';
-        Coordinate **trip = (Coordinate**)malloc(sizeof(Coordinate*));
-        trip[0] = (Coordinate*)malloc(sizeof(Coordinate));
-        trip[0][0].row = kingRow;
-        trip[0][0].col = kingCol;
+        Coordinate *curr = (Coordinate*)malloc(sizeof(Coordinate));
+        curr[0] = (Coordinate){kingRow, kingCol};
         bool safe = false;
         int currSize = 1, length = 1;
         while(currSize > 0 && !safe) {
-            trip = (Coordinate**)realloc(trip, (length + 1) * sizeof(Coordinate*));
-            trip[length] = NULL;
+            Coordinate *next = NULL;
             int nextSize = 0;
             for(int i = 0; i < currSize && !safe; ++i) {
                 for(int j = 0; j < 8 && !safe; ++j) {
-                    int newRow = trip[length - 1][i].row + kingdRow[j];
-                    int newCol = trip[length - 1][i].col + kingdCol[j];
+                    int newRow = curr[i].row + kingdRow[j];
+                    int newCol = curr[i].col + kingdCol[j];
                     if(newRow >= 0 && newRow < M && newCol >= 0 && newCol < N) {
                         if(forest[newRow][newCol] == 'B') {
                             printf("Minimal possible length of a trip is %d\n", length);
                             safe = true;
                         } else if(forest[newRow][newCol] == '.') {
-                            trip[length] = (Coordinate*)realloc(trip[length], (nextSize + 1) * sizeof(Coordinate));
-                            trip[length][nextSize].row = newRow;
-                            trip[length][nextSize++].col = newCol;
+                            next = (Coordinate*)realloc(next, (nextSize + 1) * sizeof(Coordinate));
+                            next[nextSize++] = (Coordinate){newRow, newCol};
                             forest[newRow][newCol] = 'z';
                         }
                     }
                 }
             }
+            curr = (Coordinate*)realloc(curr, (nextSize) * sizeof(Coordinate));
+            for(int i = 0; i < nextSize; ++i)
+                curr[i] = next[i];
             currSize = nextSize;
             ++length;
+            free(next);
         }
         if(!safe)
             puts("King Peter, you can't go now!");
-        for(int i = 0; i < length; ++i)
-            free(trip[i]);
-        free(trip);
+        free(curr);
     }
     return 0;
 }
