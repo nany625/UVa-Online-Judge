71f49f42408383c6faecac35dff7f67f6fe2f5f0
nany625 <jimmy19980625@gmail.com>
2025-03-29 16:14:03 +0800

    Update solution.c

--- 11518 Dominos 2/solution.c
+++ 11518 Dominos 2/solution.c
@@ -2,51 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#define MAX_NUM 10000
+#define MAXN 10000
 
-typedef struct {
-    short numVertices, **knockList, knockCount[MAX_NUM + 1];
-    bool visited[MAX_NUM + 1];
-} Graph;
+short**knock, knockCount[MAXN + 1];
+bool visited[MAXN + 1];
 
-void dfs(Graph *graph, int startVertex, int *count) {
-    graph->visited[startVertex] = true;
+void dfs(int start, int *count) {
+    visited[start] = true;
     ++(*count);
-    for(short i = 0; i < graph->knockCount[startVertex]; ++i) {
-        if(!graph->visited[graph->knockList[startVertex][i]])
-            dfs(graph, graph->knockList[startVertex][i], count);
+    for(short i = 0; i < knockCount[start]; ++i) {
+        if(!visited[knock[start][i]])
+            dfs(knock[start][i], count);
     }
 }
 
 int main() {
-    Graph graph;
-    graph.knockList = (short**)malloc((MAX_NUM + 1) * sizeof(short*));
-    for(short i = 1; i <= MAX_NUM; ++i)
-        graph.knockList[i] = NULL;
+    knock = (short**)malloc((MAXN + 1) * sizeof(short*));
+    for(short i = 1; i <= MAXN; ++i)
+        knock[i] = NULL;
     int cases;
     scanf("%d", &cases);
     while(cases--) {
-        short m, l;
-        scanf("%hd %hd %hd", &graph.numVertices, &m, &l);
-        memset(graph.knockCount, 0, (graph.numVertices + 1) * sizeof(short));
+        short n, m, l;
+        scanf("%hd %hd %hd", &n, &m, &l);
+        memset(knockCount, 0, sizeof(knockCount));
         while(m--) {
             short x, y;
             scanf("%hd %hd", &x, &y);
-            graph.knockList[x] = (short*)realloc(graph.knockList[x], (graph.knockCount[x] + 1) * sizeof(short));
-            graph.knockList[x][graph.knockCount[x]++] = y;
+            knock[x] = (short*)realloc(knock[x], (knockCount[x] + 1) * sizeof(short));
+            knock[x][knockCount[x]++] = y;
         }
-        memset(graph.visited, 0, (graph.numVertices + 1) * sizeof(bool));
+        memset(visited, 0, sizeof(visited));
         int count = 0;
         while(l--) {
             short z;
             scanf("%hd", &z);
-            if(!graph.visited[z])
-                dfs(&graph, z, &count);
+            if(!visited[z])
+                dfs(z, &count);
         }
         printf("%d\n", count);
     }
-    for(short i = 0; i <= graph.numVertices; ++i)
-        free(graph.knockList[i]);
-    free(graph.knockList);
+    for(short i = 1; i <= MAXN; ++i)
+        free(knock[i]);
+    free(knock);
 	return 0;
 }
