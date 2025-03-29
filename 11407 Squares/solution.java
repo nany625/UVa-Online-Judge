1f892f7a177938ce3f70a23b65c5390f376b7b05
nany625 <jimmy19980625@gmail.com>
2025-03-29 15:30:44 +0800

    Update solution.java

--- 11407 Squares/solution.java
+++ 11407 Squares/solution.java
@@ -8,9 +8,9 @@ public class Main {
             coins[n - 1] = n * n;
         for(int i = 1; i <= MAX_AMOUNT; ++i) {
             dp[i] = MAX_AMOUNT;
-            for(int j = 0; j < 100; ++j) {
-                if(i >= coins[j])
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            for(Integer j : coins) {
+                if(i >= j)
+                    dp[i] = Math.min(dp[i], dp[i - j] + 1);
             }
         }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
