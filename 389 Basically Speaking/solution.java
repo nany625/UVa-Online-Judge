e7ad22cf77f619dcd702a3593e7fb3405f33eeb5
nany625 <jimmy19980625@gmail.com>
2025-03-30 22:06:19 +0800

    Update solution.java

--- 389 Basically Speaking/solution.java
+++ 389 Basically Speaking/solution.java
@@ -9,25 +9,25 @@ public class Main {
         while((line = br.readLine()) != null) {
             String[] tokens = line.trim().split("\\s+");
             int num = Integer.parseInt(tokens[0], Integer.parseInt(tokens[1])), baseTo = Integer.parseInt(tokens[2]);
-            if(num == 0) {
+            if(num == 0)
                 output.append("      0\n");
-                continue;
-            }
-            for(int i = 6; i >= 0; --i) {
-                digits[i] = num % baseTo;
-                num /= baseTo;
-            }
-            if(num > 0)
-                output.append("  ERROR\n");
             else {
-                int i = -1;
-    			while(digits[++i] == 0)
-    			    output.append(' ');
-                while(i < 7) {
-    			    output.append(digits[i] > 9 ? (char)(digits[i] - 10 + 'A') : (char)(digits[i] + '0'));
-    			    ++i;
-    			}
-                output.append('\n');
+                for(int i = 6; i >= 0; --i) {
+                    digits[i] = num % baseTo;
+                    num /= baseTo;
+                }
+                if(num > 0)
+                    output.append("  ERROR\n");
+                else {
+                    int i = -1;
+        			while(digits[++i] == 0)
+        			    output.append(' ');
+                    while(i < 7) {
+        			    output.append(digits[i] > 9 ? (char)(digits[i] - 10 + 'A') : (char)(digits[i] + '0'));
+        			    ++i;
+        			}
+                    output.append('\n');
+                }
             }
         }
         System.out.print(output);
