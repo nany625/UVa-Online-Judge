33d6e16a068bcb0b997c006426b2d257d0dfe9ba
nany625 <jimmy19980625@gmail.com>
2025-03-30 21:33:37 +0800

    Update solution.c

--- 12970 Alcoholic Pilots/solution.c
+++ 12970 Alcoholic Pilots/solution.c
@@ -7,7 +7,7 @@ long GCD(long i, long j) {
 int main() {
     int cases = 0;
     long v1, d1, v2, d2;
-    while(scanf("%ld %ld %ld %ld", &v1, &d1, &v2, &d2) && v1 != 0 && d1 != 0 && v2 != 0 && d2 != 0) {
+    while(scanf("%ld %ld %ld %ld", &v1, &d1, &v2, &d2) && v1 != 0) {
         printf("Case #%d: %s\nAvg. arrival time: ", ++cases, d1 * v2 < d2 * v1 ? "You owe me a beer!" : "No beer for the captain.");
         long numerator = d1 * v2 + v1 * d2, denominator = v1 * v2 << 1;
         long temp = GCD(numerator, denominator);
