2cea2163230f70da111a623c2374720d05455af3
nany625 <jimmy19980625@gmail.com>
2025-03-30 18:59:16 +0800

    Update solution.java

--- 11764 Jumping Mario/solution.java
+++ 11764 Jumping Mario/solution.java
@@ -9,12 +9,17 @@ public class Main {
         StringBuilder output = new StringBuilder();
         for(int i = 1; i <= T; ++i) {
             st.nextToken();
-            String wall = br.readLine();
+            int N = (int)st.nval;
+            int[] heights = new int[N];
+            for(int j = 0; j < N; ++j) {
+                st.nextToken();
+                heights[j] = (int)st.nval;
+            }
             int highJumps = 0, lowJumps = 0;
-            for(int j = 2; j < wall.length(); j += 2) {
-                if(wall.charAt(j) > wall.charAt(j - 2))
+            for(int j = 1; j < N; ++j) {
+                if(heights[j] > heights[j - 1])
                     ++highJumps;
-                else if(wall.charAt(j) < wall.charAt(j - 2))
+                else if(heights[j] < heights[j - 1])
                     ++lowJumps;
             }
             output.append("Case ").append(i).append(": ").append(highJumps).append(' ').append(lowJumps).append('\n');
