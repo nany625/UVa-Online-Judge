c1597d6210417cb124eccebf90a9e3d62c86060b
nany625 <jimmy19980625@gmail.com>
2025-03-30 21:35:20 +0800

    Update solution.java

--- 12970 Alcoholic Pilots/solution.java
+++ 12970 Alcoholic Pilots/solution.java
@@ -5,17 +5,15 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
 		int cases = 0;
+        long v1;
 		StringBuilder output = new StringBuilder();
-		while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-		    long v1 = (long)st.nval;
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (v1 = (long)st.nval) != 0) {
 		    st.nextToken();
 		    long d1 = (long)st.nval;
 		    st.nextToken();
 		    long v2 = (long)st.nval;
 		    st.nextToken();
 		    long d2 = (long)st.nval;
-		    if(v1 == 0 && d1 == 0 && v2 == 0 && d2 == 0)
-		        break;
 		    output.append("Case #").append(++cases).append(": ").append(d1 * v2 < d2 * v1 ? "You owe me a beer!\nAvg. arrival time: " : "No beer for the captain.\nAvg. arrival time: ");
 		    long numerator = d1 * v2 + v1 * d2, denominator = v1 * v2 << 1;
             long temp = GCD(numerator, denominator);
