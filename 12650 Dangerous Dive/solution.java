1304ebe8913199ff2ae5922660e6fea15d9e714c
nany625 <jimmy19980625@gmail.com>
2025-03-30 21:20:54 +0800

    Update solution.java

--- 12650 Dangerous Dive/solution.java
+++ 12650 Dangerous Dive/solution.java
@@ -14,35 +14,35 @@ public class Main {
                 output.append("*\n");
                 while(R-- > 0)
                     st.nextToken();
-                continue;
-            }
-            if(R << 1 >= N) {
-                int[] returned = new int[R];
-                for(int i = 0; i < R; ++i) {
-                    st.nextToken();
-                    returned[i] = (int)st.nval;
-                }
-                Arrays.sort(returned);
-                for(int i = 1; i < returned[0]; ++i)
-                    output.append(i).append(' ');
-                for(int i = 0; i < R - 1; ++i) {
-                    for(int j = returned[i] + 1; j < returned[i + 1]; ++j)
-                        output.append(j).append(' ');
-                }
-                for(int i = returned[R - 1] + 1; i <= N; ++i)
-                    output.append(i).append(' ');
             } else {
-                boolean[] returned = new boolean[N];
-                while(R-- > 0) {
-                    st.nextToken();
-                    returned[(int)st.nval - 1] = true;
-                }
-                for(int i = 0; i < N; ++i) {
-                    if(!returned[i])
-                        output.append(i + 1).append(' ');
-                }
-    	    }
-		    output.append('\n');
+                if(R << 1 >= N) {
+                    int[] returned = new int[R];
+                    for(int i = 0; i < R; ++i) {
+                        st.nextToken();
+                        returned[i] = (int)st.nval;
+                    }
+                    Arrays.sort(returned);
+                    for(int i = 1; i < returned[0]; ++i)
+                        output.append(i).append(' ');
+                    for(int i = 0; i < R - 1; ++i) {
+                        for(int j = returned[i] + 1; j < returned[i + 1]; ++j)
+                            output.append(j).append(' ');
+                    }
+                    for(int i = returned[R - 1] + 1; i <= N; ++i)
+                        output.append(i).append(' ');
+                } else {
+                    boolean[] returned = new boolean[N];
+                    while(R-- > 0) {
+                        st.nextToken();
+                        returned[(int)st.nval - 1] = true;
+                    }
+                    for(int i = 0; i < N; ++i) {
+                        if(!returned[i])
+                            output.append(i + 1).append(' ');
+                    }
+        	    }
+    		    output.append('\n');
+            }
 		}
 		System.out.print(output);
 	}
