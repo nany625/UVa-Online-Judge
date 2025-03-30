3db849032c20fb58b075a51597c62e03eaeefe4d
nany625 <jimmy19980625@gmail.com>
2025-03-30 19:20:04 +0800

    Update solution.c

--- 11879 Multiple of 17/solution.c
+++ 11879 Multiple of 17/solution.c
@@ -4,7 +4,7 @@
 int main() {
     char n[102];
     while(scanf("%s", n) && n[0] != '0') {
-        int len = strlen(n), dividend = 0;
+        int dividend = 0, len = strlen(n);
         for(int i = 0; i < len; ++i)
             dividend = (10 * dividend + n[i] - '0') % 17;
         puts(dividend == 0 ? "1" : "0");
