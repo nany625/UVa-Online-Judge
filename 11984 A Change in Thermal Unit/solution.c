83089afc060986aa663339c0ad42f4076ce8a21c
nany625 <jimmy19980625@gmail.com>
2025-03-30 19:34:39 +0800

    Update solution.c

--- 11984 A Change in Thermal Unit/solution.c
+++ 11984 A Change in Thermal Unit/solution.c
@@ -6,7 +6,7 @@ int main() {
     for(int i = 1; i <= T; ++i) {
         int C, d;
         scanf("%d %d", &C, &d);
-        printf("Case %d: %.2lf\n", i, (C * 1.8 + d) * 5 / 9);
+        printf("Case %d: %.2f\n", i, (C * 1.8 + d) * 5 / 9);
     }
 	return 0;
 }
