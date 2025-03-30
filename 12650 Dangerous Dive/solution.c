19382f3f2480769fdd626bbfe141882e8147caed
nany625 <jimmy19980625@gmail.com>
2025-03-30 21:20:04 +0800

    Update solution.c

--- 12650 Dangerous Dive/solution.c
+++ 12650 Dangerous Dive/solution.c
@@ -14,35 +14,35 @@ int main() {
             puts("*");
             while(R--)
                 scanf("%d", &N);
-            continue;
-        }
-        if(R << 1 >= N) {
-            int returned[R];
-            for(int i = 0; i < R; ++i)
-                scanf("%d", &returned[i]);
-            qsort(returned, R, sizeof(int), compare);
-            for(int i = 1; i < returned[0]; ++i)
-                printf("%d ", i);
-            for(int i = 0; i < R - 1; ++i) {
-                for(int j = returned[i] + 1; j < returned[i + 1]; ++j)
-                    printf("%d ", j);
-            }
-            for(int i = returned[R - 1] + 1; i <= N; ++i)
-                printf("%d ", i);
         } else {
-            bool returned[N];
-            memset(returned, 0, sizeof(returned));
-            int num;
-            while(R--) {
-                scanf("%d", &num);
-                returned[num - 1] = true;
-            }
-            for(int i = 0; i < N; ++i) {
-                if(!returned[i])
-                    printf("%d ", i + 1);
-            }
-	    }
-	    putchar('\n');
+            if(R << 1 >= N) {
+                int returned[R];
+                for(int i = 0; i < R; ++i)
+                    scanf("%d", &returned[i]);
+                qsort(returned, R, sizeof(int), compare);
+                for(int i = 1; i < returned[0]; ++i)
+                    printf("%d ", i);
+                for(int i = 0; i < R - 1; ++i) {
+                    for(int j = returned[i] + 1; j < returned[i + 1]; ++j)
+                        printf("%d ", j);
+                }
+                for(int i = returned[R - 1] + 1; i <= N; ++i)
+                    printf("%d ", i);
+            } else {
+                bool returned[N];
+                memset(returned, 0, sizeof(returned));
+                int num;
+                while(R--) {
+                    scanf("%d", &num);
+                    returned[num - 1] = true;
+                }
+                for(int i = 0; i < N; ++i) {
+                    if(!returned[i])
+                        printf("%d ", i + 1);
+                }
+    	    }
+    	    putchar('\n');
+        }
     }
     return 0;
 }
