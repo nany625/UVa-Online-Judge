4f8c482f505a564ee62e705b83172fb22e27eedb
nany625 <jimmy19980625@gmail.com>
2025-03-30 22:05:31 +0800

    Update solution.c

--- 389 Basically Speaking/solution.c
+++ 389 Basically Speaking/solution.c
@@ -6,23 +6,23 @@ int main() {
 	int baseFrom, baseTo;
 	while(scanf("%s %d %d", number, &baseFrom, &baseTo) == 3) {
 		int num = strtol(number, NULL, baseFrom);
-		if(num == 0) {
+		if(num == 0) 
 		    puts("      0");
-		    continue;
+	    else {
+	        for(int i = 6; i >= 0; --i) {
+    		    int digit = num % baseTo;
+    			digits[i] = digit > 9 ? digit - 10 + 'A' : digit + '0';
+    			num /= baseTo;
+    		}
+    		if(num > 0)
+    			puts("  ERROR");
+    		else {
+    			int i = -1;
+    			while(digits[++i] == '0')
+    			    putchar(' ');
+    			puts(digits + i);
+    		}
 	    }
-		for(int i = 6; i >= 0; --i) {
-		    int digit = num % baseTo;
-			digits[i] = digit > 9 ? digit - 10 + 'A' : digit + '0';
-			num /= baseTo;
-		}
-		if(num > 0)
-			puts("  ERROR");
-		else {
-			int i = -1;
-			while(digits[++i] == '0')
-			    putchar(' ');
-			puts(digits + i);
-		}
 	}
 	return 0;
 }
