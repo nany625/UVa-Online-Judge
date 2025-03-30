c895ea6b5ea528abfff4df3f346d88152f632cf8
nany625 <jimmy19980625@gmail.com>
2025-03-30 19:17:55 +0800

    Update solution.java

--- 11849 CD/solution.java
+++ 11849 CD/solution.java
@@ -4,11 +4,9 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
+        int N;
 		StringBuilder output = new StringBuilder();
-		while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-		    int N = (int)st.nval;
-            if(N == 0)
-		        break;
+		while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0) {
 		    st.nextToken();
 		    int M = (int)st.nval;
 		    int[] Jack = new int[N], Jill = new int[M];
