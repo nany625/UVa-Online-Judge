000045c11068b58cc37a7f63d3e472f098152e8d
nany625 <jimmy19980625@gmail.com>
2025-03-31 12:21:24 +0800

    Update solution.c

--- 694 The Collatz Sequence/solution.c
+++ 694 The Collatz Sequence/solution.c
@@ -3,7 +3,7 @@
 int main() {
     long A;
     int cases = 0, L;
-    while(scanf("%ld %d", &A, &L) && A >= 0 && L >= 0) {
+    while(scanf("%ld %d", &A, &L) && A >= 0) {
         printf("Case %d: A = %ld, limit = %d, number of terms = ", ++cases, A, L);
         int terms = 0;
         while(A != 1 && A <= L) {
