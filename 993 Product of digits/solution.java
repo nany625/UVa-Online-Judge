c374dfdddfcc780af955636abdfc076663c195a5
nany625 <jimmy19980625@gmail.com>
2025-03-31 13:09:39 +0800

    Update solution.java

--- 993 Product of digits/solution.java
+++ 993 Product of digits/solution.java
@@ -11,29 +11,29 @@ public class Main {
         while(sets-- > 0) {
             st.nextToken();
             int N = (int)st.nval;
-            if(N == 0) {
+            if(N == 0)
                 output.append("0\n");
-			    continue;
-            }
-            ArrayList<Integer> factors = new ArrayList<>();
-    		int curr = 9;
-    		while(curr > 1) {
-    			if(N % curr == 0) {
-    				factors.add(curr);
-    				N /= curr;
-    			} else
-    				--curr;
-    		}
-    		if(N == 1) {
-    			if(factors.isEmpty())
-    				output.append("1\n");
-    			else {
-    				for(curr = factors.size() - 1; curr >= 0; --curr)
-    					output.append(factors.get(curr));
-    				output.append('\n');
-    			}
-    		} else
-    			output.append("-1\n");
+			else {
+			    ArrayList<Integer> factors = new ArrayList<>();
+        		int curr = 9;
+        		while(curr > 1) {
+        			if(N % curr == 0) {
+        				factors.add(curr);
+        				N /= curr;
+        			} else
+        				--curr;
+        		}
+        		if(N == 1) {
+        			if(factors.isEmpty())
+        				output.append("1\n");
+        			else {
+        				for(curr = factors.size() - 1; curr >= 0; --curr)
+        					output.append(factors.get(curr));
+        				output.append('\n');
+        			}
+        		} else
+        			output.append("-1\n");
+			}
         }
 	    System.out.print(output);
 	}
