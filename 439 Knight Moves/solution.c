7fa3cddca44e6f923888b56be6481a0c87a074ce
nany625 <jimmy19980625@gmail.com>
2025-03-31 11:10:26 +0800

    Update solution.c

--- 439 Knight Moves/solution.c
+++ 439 Knight Moves/solution.c
@@ -12,44 +12,42 @@ typedef struct {
 int main() {
     char buffer[7];
     while(fgets(buffer, sizeof(buffer), stdin)) {
-        if(strncmp(buffer, buffer + 3, 2) == 0) {
+        if(strncmp(buffer, buffer + 3, 2) == 0)
             printf("To get from %.2s to %.2s takes 0 knight moves.\n", buffer, buffer + 3);
-            continue;
-        }
-        Square **squares = (Square**)malloc(sizeof(Square*));
-        squares[0] = (Square*)malloc(sizeof(Square));
-        squares[0][0].row = buffer[1] - '1';
-        squares[0][0].col = buffer[0] - 'a';
-        int endRow = buffer[4] - '1', endCol = buffer[3] - 'a', currSize = 1, move = 1;
-        bool visited[8][8] = {}, found = false;
-        visited[squares[0][0].row][squares[0][0].col] = true;
-        while(!found) {
-            squares = (Square**)realloc(squares, (move + 1) * sizeof(Square*));
-            squares[move] = NULL;
-            int nextSize = 0;
-            for(int i = 0; i < currSize && !found; ++i) {
-                for(int j = 0; j < 8 && !found; ++j) {
-                    int newRow = squares[move - 1][i].row + dRow[j];
-                    int newCol = squares[move - 1][i].col + dCol[j];
-                    if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
-                        if(newRow == endRow && newCol == endCol) {
-                            printf("To get from %c%c to %c%c takes %d knight moves.\n", buffer[0], buffer[1], buffer[3], buffer[4], move);
-                            found = true;
-                        } else if(!visited[newRow][newCol]) {
-                            squares[move] = (Square*)realloc(squares[move], (nextSize + 1) * sizeof(Square));
-                            squares[move][nextSize].row = newRow;
-                            squares[move][nextSize++].col = newCol;
-                            visited[newRow][newCol] = true;
+        else {
+            Square *curr = (Square*)malloc(sizeof(Square));
+            curr[0] = (Square){buffer[1] - '1', buffer[0] - 'a'};
+            int endRow = buffer[4] - '1', endCol = buffer[3] - 'a', currSize = 1, move = 1;
+            bool visited[8][8] = {}, found = false;
+            visited[curr[0].row][curr[0].col] = true;
+            while(!found) {
+                Square *next = NULL;
+                int nextSize = 0;
+                for(int i = 0; i < currSize && !found; ++i) {
+                    for(int j = 0; j < 8 && !found; ++j) {
+                        int newRow = curr[i].row + dRow[j];
+                        int newCol = curr[i].col + dCol[j];
+                        if(newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+                            if(newRow == endRow && newCol == endCol) {
+                                printf("To get from %c%c to %c%c takes %d knight moves.\n", buffer[0], buffer[1], buffer[3], buffer[4], move);
+                                found = true;
+                            } else if(!visited[newRow][newCol]) {
+                                next = (Square*)realloc(next, (nextSize + 1) * sizeof(Square));
+                                next[nextSize++] = (Square){newRow, newCol};
+                                visited[newRow][newCol] = true;
+                            }
                         }
                     }
                 }
+                curr = (Square*)realloc(curr, nextSize * sizeof(Square));
+                for(int i = 0; i < nextSize; ++i)
+                    curr[i] = next[i];
+                currSize = nextSize;
+                ++move;
+                free(next);
             }
-            currSize = nextSize;
-            ++move;
+            free(curr);
         }
-        for(int i = 0; i < move; ++i)
-            free(squares[i]);
-        free(squares);
     }
     return 0;
 }
