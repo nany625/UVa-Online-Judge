0827994696fd46d79f62c0a912fe143fe0e3a381
nany625 <jimmy19980625@gmail.com>
2025-03-31 13:08:40 +0800

    Update solution.c

--- 993 Product of digits/solution.c
+++ 993 Product of digits/solution.c
@@ -7,31 +7,31 @@ int main() {
 	while(sets--) {
 		int N;
 		scanf("%d", &N);
-		if(N == 0) {
+		if(N == 0)
 			puts("0");
-			continue;
+		else {
+		    int *digits = NULL, size = 0, curr = 9;
+    		while(curr > 1) {
+    			if(N % curr == 0) {
+    				digits = (int*)realloc(digits, (size + 1) * sizeof(int));
+    				digits[size++] = curr;
+    				N /= curr;
+    			} else
+    				--curr;
+    		}
+    		if(N == 1) {
+    			if(size == 0)
+    				puts("1");
+    			else {
+    				do {
+    				    putchar(digits[--size] + '0');
+    				} while(size > 0);
+    				putchar('\n');
+    			}
+    		} else
+    			puts("-1");
+            free(digits);
 		}
-		int *digits = NULL, size = 0, curr = 9;
-		while(curr > 1) {
-			if(N % curr == 0) {
-				digits = (int*)realloc(digits, (size + 1) * sizeof(int));
-				digits[size++] = curr;
-				N /= curr;
-			} else
-				--curr;
-		}
-		if(N == 1) {
-			if(size == 0)
-				puts("1");
-			else {
-				do {
-				    putchar(digits[--size] + '0');
-				} while(size > 0);
-				putchar('\n');
-			}
-		} else
-			puts("-1");
-        free(digits);
 	}
 	return 0;
 }
