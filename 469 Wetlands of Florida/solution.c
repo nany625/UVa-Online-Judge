8a18e1b9e4ebe7534c45bf9adc5a416cbeaf6fd1
nany625 <jimmy19980625@gmail.com>
2025-03-31 11:30:20 +0800

    Update solution.c

--- 469 Wetlands of Florida/solution.c
+++ 469 Wetlands of Florida/solution.c
@@ -9,8 +9,8 @@ char grid[99][100];
 bool visited[99][99];
 
 void dfs(int row, int rowLimit, int col, int colLimit, int *area) {
-    ++(*area);
     visited[row][col] = true;
+    ++(*area);
     for(int i = 0; i < 8; ++i) {
         int newRow = row + dRow[i], newCol = col + dCol[i];
         if(newRow >= 0 && newRow < rowLimit && newCol >= 0 && newCol < colLimit && !visited[newRow][newCol] && grid[newRow][newCol] == grid[row][col])
@@ -20,9 +20,7 @@ void dfs(int row, int rowLimit, int col, int colLimit, int *area) {
 
 int main() {
     int T;
-    scanf("%d", &T);
-    getchar();
-    getchar();
+    scanf("%d  ", &T);
     int n = 0;
     char buffer[101];
     while(T--) {
