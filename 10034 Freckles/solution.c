e7e7832e7a9058e57d14948f1ff8c5303ae7da0a
nany625 <jimmy19980625@gmail.com>
2025-04-22 23:24:37 +0800

    Update solution.c

--- 10034 Freckles/solution.c
+++ 10034 Freckles/solution.c
@@ -55,7 +55,7 @@ int main() {
                 ++count;
             }
         }
-        printf("%.2lf\n", length);
+        printf("%.2f\n", length);
         if(cases)
             putchar('\n');
     }
