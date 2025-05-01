8dd62ef14b8b908aeb7d71f6342771a376446a4b
nany625 <jimmy19980625@gmail.com>
2025-05-02 01:41:44 +0800

    Update solution.c

--- 12621 On a Diet/solution.c
+++ 12621 On a Diet/solution.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX_SUM 498
 
-void findSubset(int *nums, int size, int target, int index, int currentSum, int *min) {
-    if(currentSum >= target) {
-        *min = *min < currentSum ? *min : currentSum;
-        return;
+int subsetSum(int *array, int size, int target) {
+    bool subsetSum[MAX_SUM + 1];
+    subsetSum[0] = true;
+    memset(subsetSum + 1, 0, MAX_SUM * sizeof(bool));
+    for(int i = 0; i < size; ++i) {
+        for(int j = MAX_SUM; j >= array[i]; --j)
+            subsetSum[j] |= subsetSum[j - array[i]];
     }
-    for(int i = index; i < size; ++i) {
-        findSubset(nums, size, target, i + 1, currentSum + nums[i], min);
+    for(int i = target; i <= MAX_SUM; ++i) {
+        if(subsetSum[i])
+            return i;
     }
+    return -1;
 }
 
 int main() {
@@ -16,15 +24,17 @@ int main() {
     while(t--) {
         int n, p;
         scanf("%d %d", &n, &p);
+        n /= 10;
         int calories[p];
-        for(int i = 0; i < p; ++i)
+        for(int i = 0; i < p; ++i) {
             scanf("%d", &calories[i]);
-        int min = 1e9;
-        findSubset(calories, p, n, 0, 0, &min);
-        if(min == 1e9)
+            calories[i] /= 10;
+        }
+        int ans = subsetSum(calories, p, n);
+        if(ans == -1)
             puts("NO SOLUTION");
         else
-            printf("%d\n", min);
+            printf("%d0\n", ans);
     }
 	return 0;
 }
