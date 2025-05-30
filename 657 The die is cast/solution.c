bb815189ca7ea669c3c4cde80b470d6ea44bbb24
nany625 <jimmy19980625@gmail.com>
2025-05-31 04:51:27 +0800

    Update solution.c

--- 657 The die is cast/solution.c
+++ 657 The die is cast/solution.c
@@ -4,7 +4,6 @@
 
 int dRow[] = {1, -1, 0, 0}, dCol[] = {0, 0, 1, -1}, count[6];
 char grid[50][51];
-bool visited[50][50];
 
 void dfsDots(int row, int col, int h, int w) {
     grid[row][col] = '*';
@@ -23,13 +22,13 @@ void dfs(int row, int col, int h, int w, int *dots) {
         dfsDots(row, col, h, w);
         dfs(row, col, h, w, dots);
     }
+    grid[row][col] = '.';
     for(int i = 0; i < 4; ++i) {
         int newRow = row + dRow[i], newCol = col + dCol[i];
         if(newRow >= 0 && newRow < h && newCol >= 0 && newCol < w) {
-            if(grid[newRow][newCol] == '*' && !visited[newRow][newCol]) {
-                visited[newRow][newCol] = true;
+            if(grid[newRow][newCol] == '*')
                 dfs(newRow, newCol, h, w, dots);
-            } else if(grid[newRow][newCol] == 'X') {
+            else if(grid[newRow][newCol] == 'X') {
                 ++(*dots);
                 dfsDots(newRow, newCol, h, w);
                 dfs(newRow, newCol, h, w, dots);
@@ -44,7 +43,6 @@ int main() {
         printf("Throw %d\n", ++Throws);
         for(int i = 0; i < h; ++i)
             scanf("%s", grid[i]);
-        memset(visited, 0, sizeof(visited));
         for(int i = 0; i < h; ++i) {
             for(int j = 0; j < w; ++j) {
                 if(grid[i][j] != '.') {
