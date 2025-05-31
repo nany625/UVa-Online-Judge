2c634bd618bafe1468ddce0da315f7e6e24a4df4
nany625 <jimmy19980625@gmail.com>
2025-05-31 22:33:43 +0800

    Create solution.c

---
+++ 10168 Summation of Four Primes/solution.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define MAXN 9999993
+
+bool isComposite[(MAXN >> 1) + 1];
+int *primes, size, ans[4];
+
+void eulerSieve() {
+    for(int n = 3; n <= MAXN; n += 2) {
+        if(!isComposite[n >> 1]) {
+            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
+            primes[size++] = n;
+        }
+        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAXN; ++i) {
+            isComposite[temp >> 1] = true;
+            if(n % primes[i] == 0)
+                break;
+        }
+    }
+}
+
+int compare(const void *a, const void *b) {
+    return *(int*)a - *(int*)b;
+}
+
+int main() {
+    eulerSieve();
+    int N;
+    while(scanf("%d", &N) == 1) {
+        if(N < 8)
+            puts("Impossible.");
+        else if(N & 1) {
+            if(!isComposite[N - 6 >> 1])
+                printf("2 2 2 %d\n", N - 6);
+            else {
+                N -= 5;
+                int i = 0;
+                while(isComposite[primes[i] >> 1] || isComposite[N - primes[i] >> 1])
+                    ++i;
+                ans[0] = 2;
+                ans[1] = 3;
+                ans[2] = primes[i];
+                ans[3] = N - primes[i];
+                qsort(ans, 4, sizeof(int), compare);
+                printf("2 %d %d %d\n", ans[1], ans[2], ans[3]);
+            }
+        } else {
+            if(N == 8)
+                puts("2 2 2 2");
+            else {
+                N -= 4;
+                int i = 0;
+                while(isComposite[primes[i] >> 1] || isComposite[N - primes[i] >> 1])
+                    ++i;
+                printf("2 2 %d %d\n", primes[i], N - primes[i]);
+            }
+        }
+    }
+    free(primes);
+	return 0;
+}
