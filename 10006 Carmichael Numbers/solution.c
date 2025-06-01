5f40c8c4d5d328141f376cc901a3455778526e46
nany625 <jimmy19980625@gmail.com>
2025-06-02 04:23:29 +0800

    Update solution.c

--- 10006 Carmichael Numbers/solution.c
+++ 10006 Carmichael Numbers/solution.c
@@ -1,3 +1,4 @@
+// #解法一(正規演算法)
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -53,5 +54,29 @@ int main() {
         }
     }
     free(primes);
+	return 0;
+}
+
+// #解法二(捷徑)
+#include <stdio.h>
+#include <stdbool.h>
+#define MAXN 64999
+
+bool isCarmichael[MAXN + 1];
+int ans[] = {
+    561, 1105, 1729, 2465, 2821, 6601, 8911, 10585, 15841, 29341,
+    41041, 46657, 52633, 62745, 63973
+};
+
+int main() {
+    for(int i = 0; i < 15; ++i)
+        isCarmichael[ans[i]] = true;
+    int n;
+    while(scanf("%d", &n) && n != 0) {
+        if(isCarmichael[n])
+            printf("The number %d is a Carmichael number.\n", n);
+        else
+            printf("%d is normal.\n", n);
+    }
 	return 0;
 }
