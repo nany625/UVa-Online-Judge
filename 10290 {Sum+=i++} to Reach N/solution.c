cb84d1b35d9e6572c7e57af6a80f1eea0a5a6dd8
nany625 <jimmy19980625@gmail.com>
2025-06-03 10:05:27 +0800

    Update solution.c

--- 10290 {Sum+=i++} to Reach N/solution.c
+++ 10290 {Sum+=i++} to Reach N/solution.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <stdbool.h>
-#define MAX_NUM 750000
+#define MAX_NUM 30000000
 
 bool isComposite[(MAX_NUM >> 1) + 1];
 int *primes, size;
@@ -24,20 +25,28 @@ int main() {
     eulerSieve();
     long N;
     while(scanf("%ld", &N) == 1) {
-        while((N & 1) == 0)
-            N >>= 1;
-        int count = 1;
-        for(int i = 0; i < size && N > 1; ++i) {
-            int term = 0;
-            while(N % primes[i] == 0) {
-                ++term;
-                N /= primes[i];
+        if(N == 0)
+            puts("1");
+        else {
+            double temp = sqrt((N << 3) + 1);
+            while((N & 1) == 0)
+                N >>= 1;
+            int count = 1, limit = sqrt(N);
+            for(int i = 0; i < size && primes[i] <= limit; ++i) {
+                if(N % primes[i] == 0) {
+                    int term = 0;
+                    do {
+                        ++term;
+                        N /= primes[i];
+                    } while(N % primes[i] == 0);
+                    count *= (term + 1);
+                    limit = sqrt(N);
+                }
             }
-            count *= (term + 1);
+            if(N > 1)
+                count <<= 1;
+            printf("%d\n", count + (temp == (int)temp));
         }
-        if(N > 1)
-            count <<= 1;
-        printf("%d\n", count);
     }
     free(primes);
     return 0;
