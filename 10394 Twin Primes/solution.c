dad4fec110403e069e5adc1e669bd3b7a295398a
nany625 <jimmy19980625@gmail.com>
2025-06-04 16:13:38 +0800

    Update solution.c

--- 10394 Twin Primes/solution.c
+++ 10394 Twin Primes/solution.c
@@ -6,29 +6,24 @@
 bool isComposite[(MAX_NUM >> 1) + 1];
 int *primes, size, twinPrimes[100000] = {3}, count = 1;
 
+void solve(int n) {
+    if(!isComposite[n >> 1]) {
+        primes = (int*)realloc(primes, (size + 1) * sizeof(int));
+        primes[size++] = n;
+    }
+    for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
+        isComposite[temp >> 1] = true;
+        if(n % primes[i] == 0)
+            break;
+    }
+}
+
 void eulerSieve() {
-    for(int n = 5; count < 100000; n += 4) {
-        if(!isComposite[n >> 1]) {
-            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
-            primes[size++] = n;
-        }
-        for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp >> 1] = true;
-            if(n % primes[i] == 0)
-                break;
-        }
-        n += 2;
-        if(!isComposite[n >> 1]) {
-            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
-            primes[size++] = n;
-        }
-        for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp >> 1] = true;
-            if(n % primes[i] == 0)
-                break;
-        }
-        if(!isComposite[(n >> 1) - 1] && !isComposite[n >> 1])
-            twinPrimes[count++] = n - 2;
+    for(int n = 5; count < 100000; n += 6) {
+        solve(n);
+        solve(n + 2);
+        if(!isComposite[n >> 1] && !isComposite[(n >> 1) + 1])
+            twinPrimes[count++] = n;
     }
 }
 
