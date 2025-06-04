4a5f28b5c5279dd5effd2ac01e570b8d66da0e25
nany625 <jimmy19980625@gmail.com>
2025-06-05 05:16:45 +0800

    Update solution.c

--- 1210 Sum of Consecutive Prime Numbers/solution.c
+++ 1210 Sum of Consecutive Prime Numbers/solution.c
@@ -3,7 +3,7 @@
 #include <stdbool.h>
 #define MAX_NUM 10000
 
-bool isComposite[MAX_NUM + 1];
+bool isComposite[(MAX_NUM >> 1) + 1];
 short *primes, count[MAX_NUM + 1];
 int size = 1;
 
@@ -15,7 +15,7 @@ void eulerSieve() {
             primes = (short*)realloc(primes, (size + 1) * sizeof(short));
             primes[size++] = n;
         }
-        for(int i = 1, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+        for(int i = 1, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
             isComposite[temp >> 1] = true;
             if(n % primes[i] == 0)
                 break;
