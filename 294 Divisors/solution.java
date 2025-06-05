3fd69302c744ee8877b25b00bc474c2d90973363
nany625 <jimmy19980625@gmail.com>
2025-06-05 15:08:18 +0800

    Update solution.java

--- 294 Divisors/solution.java
+++ 294 Divisors/solution.java
@@ -46,7 +46,7 @@ public class Main {
         for(short n = 3; n <= MAX_NUM; n += 2) {
             if(!isComposite[n >> 1])
                 primes.add(n);
-            for(int i = 1, temp; i < primes.size() && (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
+            for(int i = 1, temp; (temp = primes.get(i) * n) <= MAX_NUM; ++i) {
                 isComposite[temp >> 1] = true;
                 if(n % primes.get(i) == 0)
                     break;
