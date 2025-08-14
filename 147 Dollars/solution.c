a7bdbd9a9478d7b4828cf325d28da9932f4cb5bd
nany625 <jimmy19980625@gmail.com>
2025-08-14 16:39:54 +0800

    Update solution.c

--- 147 Dollars/solution.c
+++ 147 Dollars/solution.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
-long dp[6001];
+long dp[6001] = {1};
 int coins[] = {1, 2, 4, 10, 20, 40, 100, 200, 400, 1000, 2000};
 
 int main() {
-    dp[0] = 1;
     for(int i = 0; i < 11; ++i) {
         for(int j = coins[i]; j <= 6000; ++j)
             dp[j] += dp[j - coins[i]];
