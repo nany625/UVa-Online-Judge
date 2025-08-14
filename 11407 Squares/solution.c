eeaf30fd9accb99f5621425fc25a9a4c048d5454
nany625 <jimmy19980625@gmail.com>
2025-08-14 11:28:54 +0800

    Update solution.c

--- 11407 Squares/solution.c
+++ 11407 Squares/solution.c
@@ -8,10 +8,8 @@ int main() {
         coins[n - 1] = n * n;
     for(int i = 1; i <= MAX_AMOUNT; ++i) {
         dp[i] = MAX_AMOUNT;
-        for(int j = 0; j < 100; ++j) {
-            if(i >= coins[j])
-                dp[i] = dp[i] < dp[i - coins[j]] + 1 ? dp[i] : dp[i - coins[j]] + 1;
-        }
+        for(int j = 0; j < 100 && i >= coins[j]; ++j)
+            dp[i] = dp[i] < dp[i - coins[j]] + 1 ? dp[i] : dp[i - coins[j]] + 1;
     }
     int t;
     scanf("%d", &t);
