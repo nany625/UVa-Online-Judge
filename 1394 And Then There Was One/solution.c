4546c026fe6e6480f9f79c04e4063573339c427c
nany625 <jimmy19980625@gmail.com>
2025-08-15 23:59:26 +0800

    Update solution.c

--- 1394 And Then There Was One/solution.c
+++ 1394 And Then There Was One/solution.c
@@ -1,3 +1,21 @@
+// #解法一
+#include <stdio.h>
+
+int josephus(int n, int k) {
+    int ans = 0;
+    for(int i = 2; i <= n; ++i)
+        ans = (ans + k) % i;
+    return ans;
+}
+
+int main() {
+    int n, k, m;
+    while(scanf("%d %d %d", &n, &k, &m) && n != 0)
+        printf("%d\n", (josephus(n - 1, k) + m) % n + 1);
+    return 0;
+}
+
+// #解法二
 #include <stdio.h>
 
 int josephus(int n, int k) {
