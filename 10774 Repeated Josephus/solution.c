d1b5659c14bea84354d602c990a1842168c1c892
nany625 <jimmy19980625@gmail.com>
2025-08-15 23:56:28 +0800

    Update solution.c

--- 10774 Repeated Josephus/solution.c
+++ 10774 Repeated Josephus/solution.c
@@ -1,3 +1,30 @@
+// #解法一
+#include <stdio.h>
+
+int josephus(int n, int k) {
+    int ans = 0;
+    for(int i = 2; i <= n; ++i)
+        ans = (ans + k) % i;
+    return ans;
+}
+
+int main() {
+    int cases;
+    scanf("%d", &cases);
+    for(int i = 1; i <= cases; ++i) {
+        int n;
+        scanf("%d", &n);
+        int rep = 0, survivor;
+        while(n != (survivor = josephus(n, 2) + 1)) {
+            n = survivor;
+            ++rep;
+        }
+        printf("Case %d: %d %d\n", i, rep, n);
+    }
+    return 0;
+}
+
+// #解法二
 #include <stdio.h>
 
 int josephus(int n, int k) {
