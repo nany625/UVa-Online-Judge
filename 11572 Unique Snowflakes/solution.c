31e2d28188584f6d7bdafeff75b80978e12213d4
nany625 <jimmy19980625@gmail.com>
2025-08-17 00:05:36 +0800

    Create solution.c

---
+++ 11572 Unique Snowflakes/solution.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define MAXN 100000
+
+int main() {
+    int cases;
+    scanf("%d", &cases);
+    while(cases--) {
+        int n;
+        scanf("%d", &n);
+        int snowflake[MAXN + 1] = {}, ans = 0, top = 1;
+        for(int i = 1; i <= n; ++i) {
+            int num;
+            scanf("%d", &num);
+            if(snowflake[num] != 0 && top <= snowflake[num]) {
+                ans = ans > i - top ? ans : i - top;
+                top = snowflake[num] + 1;
+            }
+            snowflake[num] = i;
+        }
+        printf("%d\n", ans > n + 1 - top ? ans : n + 1 - top);
+    }
+    return 0;
+}
