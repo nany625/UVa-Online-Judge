23e806431c6d3d580c1b931cf80427eed0541f8f
nany625 <jimmy19980625@gmail.com>
2025-08-21 15:58:43 +0800

    Create solution.c

---
+++ 369 Combinations/solution.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+unsigned long ans[101][101] = {};
+
+int main() {
+    for(int i = 1; i <= 100; ++i)
+        ans[i][1] = i;
+    for(int i = 2; i <= 100; ++i) {
+        for(int j = i; j <= 100; ++j)
+            ans[j][i] = ans[j - 1][i - 1] + ans[j - 1][i];
+    }
+    int N, M;
+    while(scanf("%d %d", &N, &M) && N != 0)
+        printf("%d things taken %d at a time is %lu exactly.\n", N, M, ans[N][M]);
+    return 0;
+}
