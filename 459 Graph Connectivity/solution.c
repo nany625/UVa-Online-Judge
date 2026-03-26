ec06c68cce443ddf23c57cf44915bf4af5623400
nany625 <jimmy19980625@gmail.com>
2026-03-27 01:30:28 +0800

    Update solution.c

--- 459 Graph Connectivity/solution.c
+++ 459 Graph Connectivity/solution.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int root[26];
+int root[26], rank[26];
+
+void init(int n) {
+    for(int i = 0; i < n; ++i)
+        rank[root[i] = i] = 0;
+}
 
 int find(int x) {
 	return root[x] == x ? x : (root[x] = find(root[x]));
@@ -13,13 +18,17 @@ int main() {
     while(cases--) {
         fgets(line, sizeof(line), stdin);
         int subgraphs = line[0] - 'A' + 1;
-        for(int n = 0; n < subgraphs; ++n)
-            root[n] = n;
+        init(subgraphs);
         while(fgets(line, sizeof(line), stdin) && line[0] != '\n') {
             int root1 = find(line[0] - 'A'), root2 = find(line[1] - 'A');
             if(root1 != root2) {
                 --subgraphs;
-                root[root2] = root1;
+                if(rank[root1] > rank[root2])
+                    root[root2] = root1;
+                else if(rank[root1] < rank[root2])
+                    root[root1] = root2;
+                else
+                    ++rank[root[root2] = root1];
             }
         }
         printf("%d\n", subgraphs);
