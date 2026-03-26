70a9f39f1ee5cc5c911a693d4bc05d982c1be2d7
nany625 <jimmy19980625@gmail.com>
2026-03-27 01:32:55 +0800

    Update solution.java

--- 459 Graph Connectivity/solution.java
+++ 459 Graph Connectivity/solution.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Main {
-    static int[] root = new int[26];
+    static int[] root = new int[26], rank = new int[26];
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
@@ -11,14 +11,18 @@ public class Main {
 		StringBuilder output = new StringBuilder();
 		while(cases-- > 0) {
     		int subgraphs = br.readLine().charAt(0) - 'A' + 1;
-    		for(int n = 0; n < subgraphs; ++n)
-                root[n] = n;
+    		init(subgraphs);
             String s;
             while((s = br.readLine()) != null && !s.isEmpty()) {
                 int root1 = find(s.charAt(0) - 'A'), root2 = find(s.charAt(1) - 'A');
                 if(root1 != root2) {
                     --subgraphs;
-                    root[root2] = root1;
+                    if(rank[root1] > rank[root2])
+                        root[root2] = root1;
+                    else if(rank[root1] < rank[root2])
+                        root[root1] = root2;
+                    else
+                        ++rank[root[root2] = root1];
                 }
             }
             output.append(subgraphs).append('\n');
@@ -27,6 +31,11 @@ public class Main {
 		}
 		System.out.print(output);
 	}
+	
+	static void init(int n) {
+        for(int i = 0; i < n; ++i)
+            rank[root[i] = i] = 0;
+    }
 
     static int find(int x) {
     	return root[x] == x ? x : (root[x] = find(root[x]));
