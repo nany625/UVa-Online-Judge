8946054110a488ea4b1d8fe741d93c4572eb5cbb
nany625 <jimmy19980625@gmail.com>
2026-03-27 02:08:27 +0800

    Update solution.c

--- 793 Network Connections/solution.c
+++ 793 Network Connections/solution.c
@@ -2,8 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+void init(int *root, int *rank, int n) {
+    for(int i = 1; i <= n; ++i)
+		rank[root[i] = i] = 0;
+}
+
 int find(int *root, int x) {
 	return root[x] == x ? x : (root[x] = find(root, root[x]));
+}
+
+void unite(int *root, int *rank, int rootX, int rootY) {
+    if(rootX != rootY) {
+        if(rank[rootX] > rank[rootY])
+            root[rootY] = rootX;
+        else if(rank[rootX] < rank[rootY])
+            root[rootX] = rootY;
+        else
+            ++rank[root[rootY] = rootX];
+    }
 }
 
 int main() {
@@ -14,9 +30,8 @@ int main() {
     while(cases--) {
         int computers;
         scanf("%d ", &computers);
-        int root[computers + 1];
-        for(int i = 1; i <= computers; ++i)
-			root[i] = i;
+        int root[computers + 1], rank[computers + 1];
+        init(root, rank, computers);
 	    int succ = 0, unsucc = 0;
 	    while(getline(&buffer, &bufsize, stdin) != -1 && buffer[0] != '\n') {
 	        char *token = strtok(buffer, " ");
@@ -28,7 +43,7 @@ int main() {
             int rootI = find(root, computeri);
 			int rootJ = find(root, computerj);
 			if(command == 'c')
-			    root[rootI] = root[rootJ];
+			    unite(root, rank, rootI, rootJ);
 			else if(command == 'q')
 			    root[rootI] == root[rootJ] ? ++succ : ++unsucc;
 	    }
