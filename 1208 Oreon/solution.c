c67c8937078731174fb8a208213fb8f3efbe2ba5
nany625 <jimmy19980625@gmail.com>
2026-03-27 04:25:54 +0800

    Update solution.c

--- 1208 Oreon/solution.c
+++ 1208 Oreon/solution.c
@@ -5,6 +5,24 @@ typedef struct {
     int u, v, w;
 } Edge;
 
+void init(int *root, int *rank, int V) {
+    for(int n = 0; n < V; ++n)
+        rank[root[n] = n] = 0;
+}
+
+int find(int *root, int x) {
+    return root[x] == x ? x : (root[x] = find(root, root[x]));
+}
+
+void unite(int *root, int *rank, int rootX, int rootY) {
+    if(rank[rootX] > rank[rootY])
+        root[rootY] = rootX;
+    else if(rank[rootX] < rank[rootY])
+        root[rootX] = rootY;
+    else
+        ++rank[root[rootY] = rootX];
+}
+
 int compare(const void *a, const void *b) {
 	Edge *e1 = (Edge*)a;
 	Edge *e2 = (Edge*)b;
@@ -15,10 +33,6 @@ int compare(const void *a, const void *b) {
     return e1->v > e2->v;
 }
 
-int find(int *root, int x) {
-    return root[x] == x ? x : (root[x] = find(root, root[x]));
-}
-
 int main() {
     int cases;
     scanf("%d", &cases);
@@ -26,10 +40,10 @@ int main() {
         printf("Case %d:\n", i);
         int cities;
         scanf("%d", &cities);
-        int root[cities], size = 0;
+        int root[cities], rank[cities],size = 0;
+        init(root, rank, cities);
         Edge *edges = NULL;
         for(int j = 0; j < cities; ++j) {
-            root[j] = j;
             for(int k = 0; k < cities; ++k) {
                 int w;
                 if(k < cities - 1)
@@ -48,7 +62,7 @@ int main() {
             int root1 = find(root, edges[j].u), root2 = find(root, edges[j].v);
             if(root1 != root2) {
                 printf("%c-%c %d\n", edges[j].u + 'A', edges[j].v + 'A', edges[j].w);
-                root[root2] = root1;
+                unite(root, rank, root1, root2);
                 ++count;
             }
         }
