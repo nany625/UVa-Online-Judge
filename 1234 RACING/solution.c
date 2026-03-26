3664c8b54d4f3360af2ad3c17fa78e60534574d1
nany625 <jimmy19980625@gmail.com>
2026-03-27 04:36:25 +0800

    Update solution.c

--- 1234 RACING/solution.c
+++ 1234 RACING/solution.c
@@ -8,10 +8,24 @@ typedef struct {
 } Edge;
 
 Edge edges[MAXE];
-int root[MAXV + 1];
+int root[MAXV + 1], rank[MAXV + 1];
+
+void init(int V) {
+    for(int n = 1; n <= V; ++n)
+        rank[root[n] = n] = 0;
+}
 
 int find(int x) {
 	return root[x] == x ? x : (root[x] = find(root[x]));
+}
+
+void unite(int rootX, int rootY) {
+    if(rank[rootX] > rank[rootY])
+        root[rootY] = rootX;
+    else if(rank[rootX] < rank[rootY])
+        root[rootX] = rootY;
+    else
+        ++rank[root[rootY] = rootX];
 }
 
 int compare(const void *a, const void *b) {
@@ -27,13 +41,12 @@ int main() {
 	    for(int i = 0; i < m; ++i)
 	        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
 	    qsort(edges, m, sizeof(Edge), compare);
-	    for(int i = 1; i <= n; ++i)
-	        root[i] = i;
+	    init(n);
 	    int cost = 0;
 	    for(int i = 0; i < m; ++i) {
 	        int root1 = find(edges[i].u), root2 = find(edges[i].v);
 	        if(root1 != root2)
-	            root[root2] = root1;
+	            unite(root1, root2);
 	        else
 	            cost += edges[i].w;
 	    }
