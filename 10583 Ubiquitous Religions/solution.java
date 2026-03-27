e37b08c653e568127d6f5a520cf9af7d805dc870
nany625 <jimmy19980625@gmail.com>
2026-03-27 14:41:51 +0800

    Update solution.java

--- 10583 Ubiquitous Religions/solution.java
+++ 10583 Ubiquitous Religions/solution.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 public class Main {
     static int MAXN = 50000;
-    static int[] root = new int[MAXN + 1];
+    static int[] root = new int[MAXN + 1], rank = new int[MAXN + 1];
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
@@ -14,8 +14,7 @@ public class Main {
                 break;
             st.nextToken();
             int m = (int)st.nval;
-            for(int i = 1; i <= n; ++i)
-                root[i] = i;
+            init(n);
             while(m-- > 0) {
                 st.nextToken();
                 int rootI = find((int)st.nval);
@@ -23,7 +22,7 @@ public class Main {
                 int rootJ = find((int)st.nval);
                 if(rootI != rootJ) {
                     --n;
-                    root[rootJ] = rootI;
+                    unite(rootI, rootJ);
                 }
             }
             output.append("Case ").append(++cases).append(": ").append(n).append('\n');
@@ -31,7 +30,21 @@ public class Main {
         System.out.print(output);
 	}
 	
-	static int find(int x) {
+	static void init(int n) {
+        for(int i = 1; i <= n; ++i)
+            rank[root[i] = i] = 0;
+    }
+    
+    static int find(int x) {
     	return root[x] == x ? x : (root[x] = find(root[x]));
+    }
+    
+    static void unite(int rootX, int rootY) {
+        if(rank[rootX] > rank[rootY])
+            root[rootY] = rootX;
+        else if(rank[rootX] < rank[rootY])
+            root[rootX] = rootY;
+        else
+            ++rank[root[rootY] = rootX];
     }
 }
