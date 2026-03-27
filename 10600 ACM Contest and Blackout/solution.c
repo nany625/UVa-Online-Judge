6c036403d9c726e2dc19f0b578bcc82667b18dca
nany625 <jimmy19980625@gmail.com>
2026-03-27 14:58:40 +0800

    Update solution.c

--- 10600 ACM Contest and Blackout/solution.c
+++ 10600 ACM Contest and Blackout/solution.c
@@ -9,15 +9,24 @@ typedef struct {
 } Edge;
 
 Edge edges[MAXV * (MAXV - 1) >> 1];
-int root[MAXV + 1];
+int root[MAXV + 1], rank[MAXV + 1];
 
 void init(int n) {
     for(int i = 1; i <= n; ++i)
-        root[i] = i;
+        rank[root[i] = i] = 0;
 }
 
 int find(int x) {
     return root[x] == x ? x : (root[x] = find(root[x]));
+}
+
+void unite(int rootX, int rootY) {
+    if(rank[rootX] > rank[rootY])
+        root[rootY] = rootX;
+    else if(rank[rootX] < rank[rootY])
+        root[rootX] = rootY;
+    else
+        ++rank[root[rootY] = rootX];
 }
 
 int compare(const void *a, const void *b) {
@@ -30,17 +39,17 @@ int main() {
     while(T--) {
         int N, M;
         scanf("%d %d", &N, &M);
+        init(N);
         for(int i = 0; i < M; ++i)
             scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
         qsort(edges, M, sizeof(Edge), compare);
-        init(N);
         int edgeID[N - 1], cost1 = 0, count = 0;
         bool used[M];
         memset(used, 0, sizeof(used));
         for(int i = 0; count < N - 1; ++i) {
             int root1 = find(edges[i].u), root2 = find(edges[i].v);
             if(root1 != root2) {
-                root[root2] = root1;
+                unite(root1, root2);
                 cost1 += edges[i].w;
                 edgeID[count++] = i;
                 used[i] = true;
@@ -52,7 +61,7 @@ int main() {
             for(int j = 0; j < count; ++j) {
                 if(j != i) {
                     int root1 = find(edges[edgeID[j]].u), root2 = find(edges[edgeID[j]].v);
-                    root[root2] = root1;
+                    unite(root1, root2);
                 }
             }
             int temp = cost1 - edges[edgeID[i]].w;
