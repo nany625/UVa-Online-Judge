9702f895a6f6ec7331f35d1d3f1fdd8871888166
nany625 <jimmy19980625@gmail.com>
2026-03-27 14:37:53 +0800

    Update solution.c

--- 10583 Ubiquitous Religions/solution.c
+++ 10583 Ubiquitous Religions/solution.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
 #define MAXN 50000
 
-int root[MAXN + 1];
+int root[MAXN + 1], rank[MAXN + 1];
+
+void init(int n) {
+    for(int i = 1; i <= n; ++i)
+        rank[root[i] = i] = 0;
+}
 
 int find(int x) {
 	return root[x] == x ? x : (root[x] = find(root[x]));
 }
 
+void unite(int rootX, int rootY) {
+    if(rank[rootX] > rank[rootY])
+        root[rootY] = rootX;
+    else if(rank[rootX] < rank[rootY])
+        root[rootX] = rootY;
+    else
+        ++rank[root[rootY] = rootX];
+}
+
 int main() {
     int cases = 0, n, m;
     while(scanf("%d %d", &n, &m) && n != 0 && m != 0) {
-        for(int i = 1; i <= n; ++i)
-            root[i] = i;
+        init(n);
         while(m--) {
             int i, j;
             scanf("%d %d", &i, &j);
             int rootI = find(i), rootJ = find(j);
-            n -= rootI != rootJ;
-            root[rootJ] = rootI;
+            if(rootI != rootJ) {
+                --n;
+                unite(rootI, rootJ);
+            }
         }
         printf("Case %d: %d\n", ++cases, n);
     }
