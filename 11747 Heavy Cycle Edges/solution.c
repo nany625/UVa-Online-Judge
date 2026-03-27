d9f88e42b3fcb4ecbf7ec0c335d1d3031a872bcb
nany625 <jimmy19980625@gmail.com>
2026-03-27 16:30:13 +0800

    Update solution.c

--- 11747 Heavy Cycle Edges/solution.c
+++ 11747 Heavy Cycle Edges/solution.c
@@ -9,10 +9,24 @@ typedef struct {
 } Edge;
 
 Edge edges[MAXE];
-int root[MAXV];
+int root[MAXV], rank[MAXV];
+
+void init(int V) {
+    for(int n = 0; n < V; ++n)
+        rank[root[n] = n] = 0;
+}
 
 int find(int x) {
 	return root[x] == x ? x : (root[x] = find(root[x]));
+}
+
+void unite(int rootX, int rootY) {
+    if(rank[rootX] > rank[rootY])
+        root[rootY] = rootX;
+    else if(rank[rootX] < rank[rootY])
+        root[rootX] = rootY;
+    else
+        ++rank[root[rootY] = rootX];
 }
 
 int compare(const void *a, const void *b) {
@@ -22,8 +36,7 @@ int compare(const void *a, const void *b) {
 int main() {
 	int n, m;
 	while(scanf("%d %d", &n, &m) && n != 0) {
-		for(int i = 0; i < n; ++i)
-			root[i] = i;
+		init(n);
 		for(int i = 0; i < m; ++i)
 			scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
 		qsort(edges, m, sizeof(Edge), compare);
@@ -31,7 +44,7 @@ int main() {
 		for(int i = 0; i < m; ++i) {
 			int root1 = find(edges[i].u), root2 = find(edges[i].v);
 			if(root1 != root2)
-				root[root2] = root1;
+				unite(root1, root2);
 			else {
 				if(!isForest)
 					putchar(' ');
