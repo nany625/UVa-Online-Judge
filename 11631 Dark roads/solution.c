2b47719b46dcf15752505af5cc5668fcd49b48ee
nany625 <jimmy19980625@gmail.com>
2026-03-27 15:24:32 +0800

    Update solution.c

--- 11631 Dark roads/solution.c
+++ 11631 Dark roads/solution.c
@@ -8,10 +8,24 @@ typedef struct {
 } Edge;
 
 Edge edges[MAXE];
-int root[MAXV];
+int root[MAXV], rank[MAXV];
+
+void init(int V) {
+    for(int n = 0; n < V; ++n)
+        rank[root[n] = n] = 0;
+}
 
 int find(int x) {
 	return root[x] == x ? x : (root[x] = find(root[x]));
+}
+
+void unite(int rootX, int rootY) {
+    if(rank[rootX] > rank[rootY])
+        root[rootY] = rootX;
+    else if(rank[rootX] < rank[rootY])
+        root[rootX] = rootY;
+    else
+        ++rank[root[rootY] = rootX];
 }
 
 int compare(const void *a, const void *b) {
@@ -21,16 +35,15 @@ int compare(const void *a, const void *b) {
 int main() {
 	int m, n;
 	while(scanf("%d %d", &m, &n) && m != 0) {
+        init(m);
 	    for(int i = 0; i < n; ++i)
 	        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
 	    qsort(edges, n, sizeof(Edge), compare);
-	    for(int i = 0; i < m; ++i)
-	        root[i] = i;
 	    int save = 0;
 	    for(int i = 0; i < n; ++i) {
 	        int root1 = find(edges[i].u), root2 = find(edges[i].v);
 	        if(root1 != root2)
-	            root[root2] = root1;
+	            unite(root1, root2);
 	        else
 	            save += edges[i].w;
 	    }
