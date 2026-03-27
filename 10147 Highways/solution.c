70fc290060efc374e57caae58936fa16bcb2ed16
nany625 <jimmy19980625@gmail.com>
2026-03-27 14:06:11 +0800

    Update solution.c

--- 10147 Highways/solution.c
+++ 10147 Highways/solution.c
@@ -11,14 +11,28 @@ typedef struct {
 } Edge;
 
 Edge edges[MAXV * (MAXV - 1) >> 1];
-int root[MAXV + 1];
+int root[MAXV + 1], rank[MAXV + 1];
 
-int dist(Coordinate c1, Coordinate c2) {
-    return (c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y);
+void init(int V) {
+    for(int n = 1; n <= V; ++n)
+        rank[root[n] = n] = 0;
 }
 
 int find(int x) {
 	return root[x] == x ? x : (root[x] = find(root[x]));
+}
+
+void unite(int rootX, int rootY) {
+    if(rank[rootX] > rank[rootY])
+        root[rootY] = rootX;
+    else if(rank[rootX] < rank[rootY])
+        root[rootX] = rootY;
+    else
+        ++rank[root[rootY] = rootX];
+}
+
+int dist(Coordinate c1, Coordinate c2) {
+    return (c1.x - c2.x) * (c1.x - c2.x) + (c1.y - c2.y) * (c1.y - c2.y);
 }
 
 int compare(const void *a, const void *b) {
@@ -37,11 +51,10 @@ int main() {
 	while(cases--) {
 	    int N;
 	    scanf("%d", &N);
+        init(N);
 	    Coordinate towns[N];
-	    for(int i = 0; i < N; ++i) {
+	    for(int i = 0; i < N; ++i)
 	        scanf("%d %d", &towns[i].x, &towns[i].y);
-	        root[i + 1] = i + 1;
-	    }
 	    int M;
 	    scanf("%d", &M);
 	    int count = 0;
@@ -50,7 +63,7 @@ int main() {
 	        scanf("%d %d", &town1, &town2);
 	        int root1 = find(town1), root2 = find(town2);
 	        if(root1 != root2) {
-	            root[root2] = root1;
+	            unite(root1, root2);
 	            ++count;
 	        }
 	    }
@@ -67,7 +80,7 @@ int main() {
     	        int root1 = find(edges[i].u), root2 = find(edges[i].v);
     	        if(root1 != root2) {
     	            printf("%d %d\n", edges[i].u, edges[i].v);
-    	            root[root2] = root1;
+    	            unite(root1, root2);
     	            ++count;
     	        }
     	    }
