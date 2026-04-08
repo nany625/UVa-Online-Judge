4726a24749645ed6e0601c25047fbee1fb69cdc3
nany625 <jimmy19980625@gmail.com>
2026-04-09 05:21:46 +0800

    Update solution.c

--- 10290 {Sum+=i++} to Reach N/solution.c
+++ 10290 {Sum+=i++} to Reach N/solution.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <stdbool.h>
 #define MAX_NUM 30000000
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[(MAX_NUM >> 1) + 1];
+unsigned int mark[(MAX_NUM >> 6) + 1];
 int *primes, size;
 
 void eulerSieve() {
     for(int n = 3; n <= MAX_NUM; n += 2) {
-        if(!isComposite[n >> 1]) {
+        if(!GET(n >> 1)) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
         for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp >> 1] = true;
+            SET(temp >> 1);
             if(n % primes[i] == 0)
                 break;
         }
