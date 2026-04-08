0a95f7dc4a6be0636b803f063c1f92ea9967da1a
nany625 <jimmy19980625@gmail.com>
2026-04-09 05:29:40 +0800

    Update solution.c

--- 10311 Goldbach and Euler/solution.c
+++ 10311 Goldbach and Euler/solution.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MAXN 100000000
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[(MAXN >> 1) + 1];
+unsigned int mark[(MAXN >> 6) + 1];
 int *primes, size;
 
 void eulerSieve() {
     for(int n = 3; n <= MAXN; n += 2) {
-        if(!isComposite[n >> 1]) {
+        if(!GET(n >> 1)) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
         for(int i = 0, temp; (temp = primes[i] * n) <= MAXN; ++i) {
-            isComposite[temp >> 1] = true;
+            SET(temp >> 1);
             if(n % primes[i] == 0)
                 break;
         }
@@ -28,14 +29,14 @@ int main() {
             printf("%d is not the sum of two primes!\n", n);
         else {
             if(n & 1) {
-                if(isComposite[n - 2 >> 1])
+                if(GET(n - 2 >> 1))
                     printf("%d is not the sum of two primes!\n", n);
                 else
                     printf("%d is the sum of 2 and %d.\n", n, n - 2);
             } else {
                 int temp = n - 1 >> 1;
                 temp -= !(temp & 1);
-                while(temp >= 3 && (isComposite[temp >> 1] || isComposite[n - temp >> 1]))
+                while(temp >= 3 && (GET(temp >> 1) || GET(n - temp >> 1)))
                     temp -= 2;
                 if(temp >= 3)
                     printf("%d is the sum of %d and %d.\n", n, temp, n - temp);
