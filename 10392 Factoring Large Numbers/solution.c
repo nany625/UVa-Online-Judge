53cb5849580fd7f58b4e99e6bc8b2de084cc3dd6
nany625 <jimmy19980625@gmail.com>
2026-04-09 05:32:48 +0800

    Update solution.c

--- 10392 Factoring Large Numbers/solution.c
+++ 10392 Factoring Large Numbers/solution.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <stdbool.h>
 #define MAX_NUM 1000000
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[(MAX_NUM >> 1) + 1];
+unsigned int mark[(MAX_NUM >> 6) + 1];
 int *primes, size = 1;
 
 void eulerSieve() {
     primes = (int*)malloc(sizeof(int));
     primes[0] = 2;
     for(int n = 3; n <= MAX_NUM; n += 2) {
-        if(!isComposite[n >> 1]) {
+        if(!GET(n >> 1)) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
         for(int i = 1, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp >> 1] = true;
+            SET(temp >> 1);
             if(n % primes[i] == 0)
                 break;
         }
