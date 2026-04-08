e102401c35bc8508a9c8c400ee5fa85de0ba26c0
nany625 <jimmy19980625@gmail.com>
2026-04-09 06:21:23 +0800

    Update solution.c

--- 10852 Less Prime/solution.c
+++ 10852 Less Prime/solution.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MAX_NUM 5003
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[(MAX_NUM >> 1) + 1];
+unsigned int mark[(MAX_NUM >> 6) + 1];
 short *primes;
 int size;
 
 void eulerSieve() {
 	for(short n = 3; n <= MAX_NUM; n += 2) {
-		if(!isComposite[n >> 1]) {
+		if(!GET(n >> 1)) {
 		    primes = (short*)realloc(primes, (size + 1) * sizeof(short));
 			primes[size++] = n;
 		}
 		for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-			isComposite[temp >> 1] = true;
+			SET(temp >> 1);
 			if(n % primes[i] == 0)
 				break;
 		}
@@ -31,7 +32,7 @@ int main() {
         N >>= 1;
         ++N;
         N += !(N & 1);
-        while(isComposite[N >> 1])
+        while(GET(N >> 1))
             N += 2;
         printf("%hd\n", N);
     }
