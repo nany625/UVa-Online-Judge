8ea767c9a863418fad3e76a8cc49f3716257980c
nany625 <jimmy19980625@gmail.com>
2026-04-09 06:35:29 +0800

    Update solution.c

--- 10871 Primed Subsequence/solution.c
+++ 10871 Primed Subsequence/solution.c
@@ -3,19 +3,23 @@
 #include <math.h>
 #include <stdbool.h>
 #define MAX_NUM 10000
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[MAX_NUM + 1] = {true, true};
+unsigned int mark[(MAX_NUM >> 5) + 1];
 short *primes;
 int size;
 
 void eulerSieve() {
+    SET(0);
+    SET(1);
     for(short n = 2; n <= MAX_NUM; ++n) {
-        if(!isComposite[n]) {
+        if(!GET(n)) {
             primes = (short*)realloc(primes, (size + 1) * sizeof(short));
             primes[size++] = n;
         }
         for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp] = true;
+            SET(temp);
             if(n % primes[i] == 0)
                 break;
         }
@@ -48,7 +52,7 @@ int main() {
         while(antiPrimed && length <= n) {
             for(int i = 0; i <= n - length && antiPrimed; ++i) {
                 numbers[1][i] += numbers[0][i + length - 1];
-                if((numbers[1][i] <= MAX_NUM && !isComposite[numbers[1][i]]) || (numbers[1][i] > MAX_NUM && isPrime(numbers[1][i]))) {
+                if((numbers[1][i] <= MAX_NUM && !GET(numbers[1][i])) || (numbers[1][i] > MAX_NUM && isPrime(numbers[1][i]))) {
                     printf("Shortest primed subsequence is length %d:", length);
                     for(int j = i; j < i + length; ++j)
                         printf(" %d", numbers[0][j]);
