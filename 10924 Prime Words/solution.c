2c3dc18932b7d8680ba6469e07b5dc4e1cd48997
nany625 <jimmy19980625@gmail.com>
2026-04-09 06:42:29 +0800

    Update solution.c

--- 10924 Prime Words/solution.c
+++ 10924 Prime Words/solution.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdbool.h>
 #define MAX_NUM 1040
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[MAX_NUM + 1];
+unsigned int mark[(MAX_NUM >> 5) + 1];
 
 void eratosthenesSieve() {
     for(short n = 2; n <= MAX_NUM; ++n) {
-        if(n <= 32 && !isComposite[n]) {
+        if(n <= 32 && !GET(n)) {
             for(int i = n * n; i <= MAX_NUM; i += n)
-                isComposite[i] = true;
+                SET(i);
         }
     }
 }
@@ -26,7 +27,7 @@ int main() {
             else if(islower(L[i]))
                 sum += L[i] - 'a' + 1;
         }
-        puts(isComposite[sum] ? "It is not a prime word." : "It is a prime word.");
+        puts(GET(sum) ? "It is not a prime word." : "It is a prime word.");
     }
     return 0;
 }
