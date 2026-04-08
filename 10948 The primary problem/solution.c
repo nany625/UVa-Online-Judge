ee4f068c478c4384e8ac112497a3a6858b9f6568
nany625 <jimmy19980625@gmail.com>
2026-04-09 06:47:29 +0800

    Update solution.c

--- 10948 The primary problem/solution.c
+++ 10948 The primary problem/solution.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MAX_NUM 1000000
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[(MAX_NUM >> 1) + 1];
+unsigned int mark[(MAX_NUM >> 6) + 1];
 int *primes, size;
 
 void eulerSieve() {
     for(int n = 3; n <= MAX_NUM; n += 2) {
-        if(!isComposite[n >> 1]) {
+        if(!GET(n >> 1)) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
-        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp >> 1] = true;
+        for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
+            SET(temp >> 1);
             if(n % primes[i] == 0)
                 break;
         }
@@ -28,14 +29,14 @@ int main() {
         if(N == 4)
             puts("2+2");
         else if(N & 1) {
-            if(!isComposite[N - 2 >> 1])
+            if(!GET(N - 2 >> 1))
                 printf("2+%d\n", N - 2);
             else
                 puts("NO WAY!");
         } else {
             int i = 3, j = N - 3;
             while(i <= j) {
-                if(!isComposite[i >> 1] && !isComposite[j >> 1])
+                if(!GET(i >> 1) && !GET(j >> 1))
                     break;
                 i += 2;
                 j -= 2;
