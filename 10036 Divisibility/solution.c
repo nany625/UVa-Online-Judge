8e0cfc8cb32e9b917d66686e1f771cdb7c17ecfe
nany625 <jimmy19980625@gmail.com>
2026-04-10 06:24:41 +0800

    Update solution.c

--- 10036 Divisibility/solution.c
+++ 10036 Divisibility/solution.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
-bool dp[2][100];
+unsigned int dp[2][4];
+
+int get(int row, int num) {
+    return dp[row][num >> 5] & (1u << (num & 31));
+}
+
+void set(int row, int num) {
+    dp[row][num >> 5] |= 1u << (num & 31);
+}
 
 int main() {
     int M;
@@ -15,25 +21,19 @@ int main() {
         int num;
         scanf("%d", &num);
         num = (num % K + K) % K;
-        dp[0][num] = true;
+        set(0, num);
         for(int i = 1; i < N; ++i) {
             scanf("%d", &num);
             num = (num % K + K) % K;
-            if(i & 1) {
-            	memset(dp[1], 0, sizeof(dp[1]));
-            	for(int j = 0; j < K; ++j) {
-	                if(dp[0][j])
-	                    dp[1][(j + num) % K] = dp[1][(j + K - num) % K] = true;
+            memset(dp[i & 1], 0, sizeof(dp[i & 1]));
+            for(int j = 0; j < K; ++j) {
+	            if(get(!(i & 1), j)) {
+	                set(i & 1, (j + num) % K);
+	                set(i & 1, (j + K - num) % K);
 	            }
-            } else {
-            	memset(dp[0], 0, sizeof(dp[0]));
-            	for(int j = 0; j < K; ++j) {
-	                if(dp[1][j])
-	                    dp[0][(j + num) % K] = dp[0][(j + K - num) % K] = true;
-	            }
-            }
+	        }
         }
-        puts(dp[(N - 1) & 1][0] ? "Divisible" : "Not divisible");
+        puts(get((N - 1) & 1, 0) ? "Divisible" : "Not divisible");
     }
 	return 0;
 }
