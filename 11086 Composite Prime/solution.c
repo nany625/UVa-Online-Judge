8ac7310b2f86d58b47654e51f19b3d974cb676da
nany625 <jimmy19980625@gmail.com>
2026-04-09 08:29:31 +0800

    Update solution.c

--- 11086 Composite Prime/solution.c
+++ 11086 Composite Prime/solution.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MAX_NUM 524288
 #define MAX_N 1048576
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[(MAX_NUM >> 1) + 1], isCompositePrime[MAX_N + 1];
+unsigned int mark[(MAX_NUM >> 6) + 1], isCompositePrime[(MAX_N >> 5) + 1];
 int *primes, size = 1;
 
 void eulerSieve() {
     primes = (int*)malloc(sizeof(int));
     primes[0] = 2;
     for(int n = 3; n <= MAX_NUM; n += 2) {
-        if(!isComposite[n >> 1]) {
+        if(!GET(n >> 1)) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
         for(int i = 1, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp >> 1] = true;
+            SET(temp >> 1);
             if(n % primes[i] == 0)
                 break;
         }
@@ -27,7 +28,7 @@ int main() {
     eulerSieve();
     for(int i = 0; i < size; ++i) {
         for(int j = 0, temp; j < size && (temp = primes[i] * primes[j]) <= MAX_N; ++j)
-            isCompositePrime[temp] = true;
+            isCompositePrime[temp >> 5] |= (1u << (temp & 31));
     }
     int N;
     while(scanf("%d", &N) == 1) {
@@ -35,7 +36,8 @@ int main() {
         while(N--) {
             int num;
             scanf("%d", &num);
-            count += isCompositePrime[num];
+            if(isCompositePrime[num >> 5] & (1u << (num & 31)))
+                ++count;
         }
         printf("%d\n", count);
     }
