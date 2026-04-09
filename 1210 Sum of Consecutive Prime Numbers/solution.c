1b864056b5b3c3f46bf6dbfdd4fa49fefc90fe59
nany625 <jimmy19980625@gmail.com>
2026-04-09 10:30:05 +0800

    Update solution.c

--- 1210 Sum of Consecutive Prime Numbers/solution.c
+++ 1210 Sum of Consecutive Prime Numbers/solution.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MAX_NUM 10000
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[(MAX_NUM >> 1) + 1];
+unsigned int mark[(MAX_NUM >> 6) + 1];
 short *primes, count[MAX_NUM + 1];
 int size = 1;
 
@@ -11,12 +12,12 @@ void eulerSieve() {
     primes = (short*)malloc(sizeof(short));
     primes[0] = 2;
     for(short n = 3; n <= MAX_NUM; n += 2) {
-        if(!isComposite[n >> 1]) {
+        if(!GET(n >> 1)) {
             primes = (short*)realloc(primes, (size + 1) * sizeof(short));
             primes[size++] = n;
         }
         for(int i = 1, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp >> 1] = true;
+            SET(temp >> 1);
             if(n % primes[i] == 0)
                 break;
         }
@@ -31,7 +32,7 @@ int main() {
     }
     int num;
     while(scanf("%d", &num) && num != 0)
-        printf("%d\n", count[num]);
+        printf("%hd\n", count[num]);
     free(primes);
 	return 0;
 }
