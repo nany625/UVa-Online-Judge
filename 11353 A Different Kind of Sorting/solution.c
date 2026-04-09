54f8e438bd1c5a7d256bff053a28973e336fc34e
nany625 <jimmy19980625@gmail.com>
2026-04-09 08:50:15 +0800

    Update solution.c

--- 11353 A Different Kind of Sorting/solution.c
+++ 11353 A Different Kind of Sorting/solution.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MAX_NUM 2000000
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
 typedef struct {
     int n, pfCount;  
 } Answer;
 
-bool isComposite[MAX_NUM + 1];
+unsigned int mark[(MAX_NUM >> 5) + 1];
 int *primes, size;
 Answer answers[MAX_NUM + 1];
 
 void eulerSieve() {
     answers[1].n = 1;
     for(int n = 2; n <= MAX_NUM; ++n) {
-        if(!isComposite[n]) {
+        if(!GET(n)) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
             answers[n].pfCount = 1;
         }
         for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp] = true;
+            SET(temp);
             answers[temp].pfCount = answers[primes[i]].pfCount + answers[n].pfCount;
             if(n % primes[i] == 0)
                 break;
