3bcaa89da50e4965a59df0b929b0957822a5b444
nany625 <jimmy19980625@gmail.com>
2026-04-09 09:26:20 +0800

    Update solution.c

--- 543 Goldbach's Conjecture/solution.c
+++ 543 Goldbach's Conjecture/solution.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #define MAX_NUM 999995
+#define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
+#define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-bool isComposite[(MAX_NUM >> 1) + 1];
+unsigned int mark[(MAX_NUM >> 6) + 1];
 int *primes, size;
 
 void eulerSieve() {
     for(int n = 3; n <= MAX_NUM; n += 2) {
-        if(!isComposite[n >> 1]) {
+        if(!GET(n >> 1)) {
             primes = (int*)realloc(primes, (size + 1) * sizeof(int));
             primes[size++] = n;
         }
         for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
-            isComposite[temp >> 1] = true;
+            SET(temp >> 1);
             if(n % primes[i] == 0)
                 break;
         }
@@ -25,7 +26,7 @@ int main() {
     int n;
     while(scanf("%d", &n) && n != 0) {
         int i = 0;
-        while(isComposite[n - primes[i] >> 1])
+        while(GET(n - primes[i] >> 1))
             ++i;
         printf("%d = %d + %d\n", n, primes[i], n - primes[i]);
     }
