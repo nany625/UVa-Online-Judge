44e2d631dc1bf309f853dca1272f2b0f80b381ba
nany625 <jimmy19980625@gmail.com>
2026-04-09 11:14:59 +0800

    Update solution.java

--- 1730 Sum of MSLCM/solution.java
+++ 1730 Sum of MSLCM/solution.java
@@ -1,25 +1,25 @@
 import java.io.*;
 
 public class Main {
-    static int MAXN = 20000000;
-    static long[] ans = new long[MAXN + 1];
 	public static void main(String[] args) throws IOException {
-	    init();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StreamTokenizer st = new StreamTokenizer(br);
 		int N;
         StringBuilder output = new StringBuilder();
-        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0)
-            output.append(ans[N] + N - 1).append('\n');
+        while(st.nextToken() == StreamTokenizer.TT_NUMBER && (N = (int)st.nval) != 0) {
+            int limit = (int)Math.sqrt(N);
+            long ans = -1;
+            long[] quotients = new long[limit + 1];
+            for(int i = 1; i <= limit; ++i) {
+                quotients[i] = N / i;
+                ans += i * quotients[i];
+            }
+            for(int i = 1; i < limit; ++i)
+                ans += i * (quotients[i] + quotients[i + 1] + 1) * (quotients[i] - quotients[i + 1]) >> 1;
+            if(limit != quotients[limit])
+                ans += limit * (quotients[limit] + N / (limit + 1) + 1) * (quotients[limit] - N / (limit + 1)) >> 1;
+            output.append(ans).append('\n');
+        }
 		System.out.print(output);
 	}
-	
-	static void init() {
-        for(int n = 2; n <= MAXN; ++n) {
-            for(int i = n; i <= MAXN; i += n)
-                ans[i] += n;
-        }
-        for(int n = 3; n <= MAXN; ++n)
-            ans[n] += ans[n - 1];
-    }
 }
