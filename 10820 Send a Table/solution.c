9de44cb4c242a079f81b89b4a2a28bab381487ec
nany625 <jimmy19980625@gmail.com>
2026-04-10 08:56:28 +0800

    Create solution.c

---
+++ 10820 Send a Table/solution.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define MAXN 50000
+
+int ans[MAXN + 1] = {0, 1};
+
+void eratosthenesSieve() {
+    for(int n = 2; n <= MAXN; ++n) {
+        if(ans[n] == n) {
+            for(int i = n; i <= MAXN; i += n)
+                ans[i] -= ans[i] / n;
+        }
+    }
+}
+
+int main() {
+    for(int n = 2; n <= MAXN; ++n)
+        ans[n] = n;
+    eratosthenesSieve();
+    for(int n = 2; n <= MAXN; ++n)
+        ans[n] = (ans[n] << 1) + ans[n - 1];
+    int N;
+    while(scanf("%d", &N) && N != 0)
+        printf("%d\n", ans[N]);
+	return 0;
+}
