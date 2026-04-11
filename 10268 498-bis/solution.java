d1605ced9532e14d8c961c1be8f4ccea742c9e80
nany625 <jimmy19980625@gmail.com>
2026-04-11 21:10:11 +0800

    Update solution.java

--- 10268 498-bis/solution.java
+++ 10268 498-bis/solution.java
@@ -4,16 +4,15 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StreamTokenizer st = new StreamTokenizer(br);
+        String line;
         StringBuilder output = new StringBuilder();
-        while(st.nextToken() == StreamTokenizer.TT_NUMBER) {
-            int x = (int)st.nval;
-            String line = br.readLine();
-            StringTokenizer strtok = new StringTokenizer(line);
-            int n = strtok.countTokens();
+        while((line = br.readLine()) != null) {
+            int x = Integer.parseInt(line);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = st.countTokens();
             long result = 0;
             for(int i = 0; i < n - 1; ++i)
-                result = result * x + Long.parseLong(strtok.nextToken()) * (n - 1 - i);
+                result = result * x + Long.parseLong(st.nextToken()) * (n - 1 - i);
             output.append(result).append('\n');
         }
         System.out.print(output);
