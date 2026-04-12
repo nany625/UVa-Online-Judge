c1f3f1fc1372dba7fe6b47c1497621ee660ee536
nany625 <jimmy19980625@gmail.com>
2026-04-13 04:20:25 +0800

    Create solution.cpp

---
+++ 11286 Conformity/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<short> courses(5);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    while(cin >> n && n != 0) {
+        unordered_map<long, int> combinations;
+        while(n--) {
+            for(int i = 0; i < 5; ++i)
+                cin >> courses[i];
+            sort(courses.begin(), courses.end());
+            long temp = 0;
+            for(short c : courses)
+                temp = 1000 * temp + c;
+            ++combinations[temp];
+        }
+        int max = 0, students = 0;
+        for(auto it : combinations) {
+            if(max < it.second)
+                students = max = it.second;
+            else if(max == it.second)
+                students += max;
+        }
+        cout << students << '\n';
+    }
+    return 0;
+}
