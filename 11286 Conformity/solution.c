f18f2b1228c488a9ce1e1102bd87694c68132e23
nany625 <jimmy19980625@gmail.com>
2026-04-13 04:43:01 +0800

    Update solution.c

--- 11286 Conformity/solution.c
+++ 11286 Conformity/solution.c
@@ -1,11 +1,5 @@
-// #解法一
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct {
-    long froshes;
-    short popularity;
-} Combination;
 
 short courses[5];
 
@@ -21,155 +15,40 @@ void InsertionSort() {
     }
 }
 
-int binarySearch(Combination *array, short size, long key) {
-    int left = 0, right = size - 1;
-    while(left <= right) {
-        int mid = left + (right - left >> 1);
-        if(array[mid].froshes == key)
-            return mid;
-        if(array[mid].froshes < key)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return left;
+int compare(const void *a, const void *b) {
+    return *(long*)a > *(long*)b;
 }
 
 int main() {
 	int n;
 	while(scanf("%d", &n) && n != 0) {
-	    Combination *combinations = NULL;
-		short size = 0;
-		while(n--) {
-		    for(int i = 0; i < 5; ++i)
-		        scanf("%hd", &courses[i]);
+	    long combinations[n];
+		for(int i = 0; i < n; ++i) {
+		    for(int j = 0; j < 5; ++j)
+		        scanf("%hd", &courses[j]);
 		    InsertionSort();
-		    long temp = 0;
-		    for(int i = 0; i < 5; ++i)
-		        temp += 1000 * temp + courses[i];
-		    int pos = binarySearch(combinations, size, temp);
-		    if(pos < size && combinations[pos].froshes == temp)
-		        ++combinations[pos].popularity;
+		    combinations[i] = 0;
+		    for(int j = 0; j < 5; ++j)
+		        combinations[i] = 1000 * combinations[i] + courses[j];
+		}
+		qsort(combinations, n, sizeof(long), compare);
+		int max = 1, students = 0, count = 1;
+		for(int i = 1; i < n; ++i) {
+		    if(combinations[i] == combinations[i - 1])
+		        ++count;
 		    else {
-		        combinations = (Combination*)realloc(combinations, (size + 1) * sizeof(Combination));
-		        for(int i = size++; i > pos; --i)
-                    combinations[i] = combinations[i - 1];
-                combinations[pos] = (Combination){temp, 1};
+		        if(max < count)
+    		        students = max = count;
+    		    else if(max == count)
+    		        students += max;
+    		    count = 1;
 		    }
 		}
-		short max = 0, students = 0;
-		for(int i = 0; i < size; ++i) {
-		    if(max < combinations[i].popularity)
-		        students = max = combinations[i].popularity;
-		    else if(max == combinations[i].popularity)
-		        students += max;
-		}
-		printf("%hd\n", students);
-		free(combinations);
+		if(max < count)
+        	students = max = count;
+        else if(max == count)
+            students += max;
+		printf("%d\n", students);
 	}
 	return 0;
-}
-
-// #解法二(by ChatGPT 我暫時不會寫)
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
-
-#define HASH_SIZE 100003
-
-typedef struct Combination {
-    long froshes;
-    short popularity;
-    struct Combination *next;
-} Combination;
-
-unsigned long hash(long key) {
-    return (unsigned long)key % HASH_SIZE;
-}
-
-void addCombination(Combination **hashTable, long key) {
-    unsigned long index = hash(key);
-    Combination *entry = hashTable[index];
-
-    while (entry != NULL) {
-        if (entry->froshes == key) {
-            entry->popularity++;
-            return;
-        }
-        entry = entry->next;
-    }
-
-    entry = (Combination *)malloc(sizeof(Combination));
-    if (entry == NULL) {
-        perror("Failed to allocate memory");
-        exit(EXIT_FAILURE);
-    }
-
-    entry->froshes = key;
-    entry->popularity = 1;
-    entry->next = hashTable[index];
-    hashTable[index] = entry;
-}
-
-void InsertionSort(short *array) {
-    for (int i = 1; i < 5; ++i) {
-        int j = i - 1;
-        short insertionNum = array[i];
-        while (j >= 0 && insertionNum < array[j]) {
-            array[j + 1] = array[j];
-            --j;
-        }
-        array[j + 1] = insertionNum;
-    }
-}
-
-int main() {
-    short courses[5];
-    int n;
-
-    while (scanf("%d", &n) && n != 0) {
-        Combination *hashTable[HASH_SIZE] = {0};
-
-        while (n--) {
-            for (int i = 0; i < 5; ++i)
-                scanf("%hd", &courses[i]);
-            InsertionSort(courses);
-
-            long temp = 0;
-            for (int i = 0; i < 5; ++i)
-                temp = 1000 * temp + courses[i];
-
-            addCombination(hashTable, temp);
-        }
-
-        // 計算 max 和 students
-        short max = 0;
-        int students = 0;
-        for (int i = 0; i < HASH_SIZE; i++) {
-            Combination *entry = hashTable[i];
-            while (entry != NULL) {
-                if (entry->popularity > max) {
-                    students = max = entry->popularity;
-                } else if (entry->popularity == max) {
-                    students += max;
-                }
-                entry = entry->next;
-            }
-        }
-
-        printf("%hd\n", students);
-
-        // 釋放記憶體
-        for (int i = 0; i < HASH_SIZE; i++) {
-            Combination *entry = hashTable[i];
-            while (entry != NULL) {
-                Combination *prev = entry;
-                entry = entry->next;
-                free(prev);
-            }
-        }
-    }
-
-    return 0;
 }
