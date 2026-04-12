479faffcaf70e9b0453ea9501c8cbd22f0887462
nany625 <jimmy19980625@gmail.com>
2026-04-13 06:24:41 +0800

    Create solution.cpp

---
+++ 10049 Self-describing Sequence/solution.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> f;
+
+int binarySearch(int key) {
+    int left = 0, right = f.size() - 2;
+    while(left <= right) {
+        int mid = left + (right - left >> 1);
+        if(f[mid] == key)
+            return mid + 1;
+        if(f[mid] < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return left;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    f.push_back(1);
+    int pos = 0;
+    while(f.back() <= 2000000000)
+        f.push_back(f.back() + (pos += f.size() >= f[pos]));
+    int n;
+    while(cin >> n && n != 0)
+        cout << binarySearch(n) << '\n';
+    return 0;
+}
