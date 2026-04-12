22ccfd7d1a154267167860ebd5a62a114a58c4b4
nany625 <jimmy19980625@gmail.com>
2026-04-13 05:19:52 +0800

    Update solution.java

--- 11286 Conformity/solution.java
+++ 11286 Conformity/solution.java
@@ -5,29 +5,37 @@ public class Main {
     static short[] courses = new short[5];;
     public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StreamTokenizer st = new StreamTokenizer(br);
+	    StreamTokenizer st = new StreamTokenizer(br);
 	    int n;
 	    StringBuilder output = new StringBuilder();
 	    while(st.nextToken() == StreamTokenizer.TT_NUMBER && (n = (int)st.nval) != 0) {
-	        Map<Long, Short> combinations = new HashMap<>();
-    		while(n-- > 0) {
-    	        for(int i = 0; i < 5; ++i) {
-    	            st.nextToken();
-    	            courses[i] = (short)st.nval;
-    	        }
-    	        Arrays.sort(courses);
-    	        long temp = 0;
-    	        for(Short s : courses)
-    	            temp = 1000 * temp + s;
-    	        combinations.put(temp, (short)(combinations.getOrDefault(temp, (short)0) + 1));
+	        long[] combinations = new long[n];
+    		for(int i = 0; i < n; ++i) {
+    		    for(int j = 0; j < 5; ++j) {
+    		        st.nextToken();
+    		        courses[j] = (short)st.nval;
+    		    }
+    		    Arrays.sort(courses);
+    		    for(int j = 0; j < 5; ++j)
+    		        combinations[i] = 1000 * combinations[i] + courses[j];
     		}
-    		short max = 0, students = 0;
-    		for(Short s : combinations.values()) {
-    		    if(max < s)
-    		        students = max = s;
-    		    else if(max == s)
-    		        students += max;
+    		Arrays.sort(combinations);
+    		int max = 1, students = 0, count = 1;
+    		for(int i = 1; i < n; ++i) {
+    		    if(combinations[i] == combinations[i - 1])
+    		        ++count;
+    		    else {
+    		        if(max < count)
+        		        students = max = count;
+        		    else if(max == count)
+        		        students += max;
+        		    count = 1;
+    		    }
     		}
+    		if(max < count)
+            	students = max = count;
+            else if(max == count)
+                students += max;
     	    output.append(students).append('\n');
 	    }
 	    System.out.print(output);
