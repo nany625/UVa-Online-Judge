8a8321d33a945013486cf1515e20d7412da2fbb6
nany625 <jimmy19980625@gmail.com>
2026-04-13 13:47:25 +0800

    Update solution.c

--- 11353 A Different Kind of Sorting/solution.c
+++ 11353 A Different Kind of Sorting/solution.c
@@ -1,45 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_NUM 2000000
+#define MAX_CNT 20
 #define GET(n) (mark[(n) >> 5] & (1u << ((n) & 31)))
 #define SET(n) (mark[(n) >> 5] |= (1u << ((n) & 31)))
 
-typedef struct {
-    int n, pfCount;  
-} Answer;
-
 unsigned int mark[(MAX_NUM >> 5) + 1];
-int *primes, size;
-Answer answers[MAX_NUM + 1];
+int *primes, primeSize, pf[MAX_NUM + 1], **bucket, size[MAX_CNT + 1], order[MAX_NUM];
 
 void eulerSieve() {
-    answers[1].n = 1;
     for(int n = 2; n <= MAX_NUM; ++n) {
         if(!GET(n)) {
-            primes = (int*)realloc(primes, (size + 1) * sizeof(int));
-            primes[size++] = n;
-            answers[n].pfCount = 1;
+            primes = (int*)realloc(primes, (primeSize + 1) * sizeof(int));
+            primes[primeSize++] = n;
+            pf[n] = 1;
         }
         for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
             SET(temp);
-            answers[temp].pfCount = answers[primes[i]].pfCount + answers[n].pfCount;
+            pf[temp] = 1 + pf[n];
             if(n % primes[i] == 0)
                 break;
         }
-        answers[n].n = n;
     }
-}
-
-int compare(const void *a, const void *b) {
-    return ((Answer*)a)->pfCount > ((Answer*)b)->pfCount;
 }
 
 int main() {
     eulerSieve();
-    qsort(answers, MAX_NUM + 1, sizeof(Answer), compare);
+    bucket = (int**)malloc((MAX_CNT + 1) * sizeof(int*));
+    for(int i = 0; i <= MAX_CNT; ++i)
+        bucket[i] = NULL;
+    for(int i = 1; i <= MAX_NUM; ++i) {
+        bucket[pf[i]] = (int*)realloc(bucket[pf[i]], (size[pf[i]] + 1) * sizeof(int));
+        bucket[pf[i]][size[pf[i]]++] = i;
+    }
+    int idx = 0;
+    for(int i = 0; i <= MAX_CNT; ++i) {
+        for(int j = 0; j < size[i]; ++j)
+            order[idx++] = bucket[i][j];
+    }
     int cases = 0, n;
     while(scanf("%d", &n) && n != 0)
-        printf("Case %d: %d\n", ++cases, answers[n].n);
+        printf("Case %d: %d\n", ++cases, order[n - 1]);
     free(primes);
+    for(int i = 0; i <= MAX_CNT; ++i)
+        free(bucket[i]);
+    free(bucket);
 	return 0;
 }
