707d6b6fe4a22d7a9c20745b2eb1e3528043c33b
nany625 <jimmy19980625@gmail.com>
2026-04-14 04:47:50 +0800

    Update solution.java

--- 11448 Who said crisis?/solution.java
+++ 11448 Who said crisis?/solution.java
@@ -1,16 +1,15 @@
 import java.io.*;
+import java.util.*;
 import java.math.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StreamTokenizer st = new StreamTokenizer(br);
-        st.nextToken();
-        int n = (int)st.nval;
+        int n = Integer.parseInt(br.readLine());
         StringBuilder output = new StringBuilder();
         while(n-- > 0) {
-            String[] tokens = br.readLine().split("\\s+");
-            output.append(new BigInteger(tokens[0]).subtract(new BigInteger(tokens[1]))).append('\n');
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            output.append(new BigInteger(st.nextToken()).subtract(new BigInteger(st.nextToken()))).append('\n');
         }
         System.out.print(output);
     }
