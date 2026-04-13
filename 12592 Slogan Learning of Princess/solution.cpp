5ae2c37d844cb40bffd98ee70e53f8a63477498f
nany625 <jimmy19980625@gmail.com>
2026-04-14 07:34:23 +0800

    Create solution.cpp

---
+++ 12592 Slogan Learning of Princess/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    cin.ignore();
+    unordered_map<string, string> slogans;
+    string first, second;
+    while(N--) {
+        getline(cin, first);
+        getline(cin, second);
+        slogans[first] = second;
+    }
+    int Q;
+    cin >> Q;
+    cout << Q;
+    cin.ignore();
+    while(Q--) {
+        getline(cin, first);
+        cout << slogans[first];
+    }
+    return 0;
+}
