e3782143d0e884726875161952d3175cd9c6a02e
nany625 <jimmy19980625@gmail.com>
2026-04-13 13:31:10 +0800

    Update solution.cpp

--- 11353 A Different Kind of Sorting/solution.cpp
+++ 11353 A Different Kind of Sorting/solution.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 const int MAX_NUM = 2000000;
+const int MAX_CNT = 20;
 unsigned int mark[(MAX_NUM >> 5) + 1];
-vector<int> primes;
-vector<pair<int, int>> answers(MAX_NUM + 1);
+vector<int> primes, pf(MAX_NUM + 1), order;
+vector<vector<int>> bucket(MAX_CNT + 1);
 
 bool get(int n) {
     return mark[n >> 5] & (1u << (n & 31));
@@ -15,19 +16,17 @@ void setBit(int n) {
 }
 
 void eulerSieve() {
-    answers[1].first = 1;
     for(int n = 2; n <= MAX_NUM; ++n) {
         if(!get(n)) {
             primes.push_back(n);
-            answers[n].second = 1;
+            pf[n] = 1;
         }
         for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
             setBit(temp);
-            answers[temp].second = answers[primes[i]].second + answers[n].second;
+            pf[temp] = 1 + pf[n];
             if(n % primes[i] == 0)
                 break;
         }
-        answers[n].first = n;
     }
 }
 
@@ -35,11 +34,14 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     eulerSieve();
-    stable_sort(answers.begin(), answers.end(), [](const pair<int, int> &a, const pair<int, int> &b) {
-        return a.second < b.second;
-    });
+    for(int i = 1; i <= MAX_NUM; ++i)
+        bucket[pf[i]].push_back(i);
+    for(int i = 0; i <= MAX_CNT; ++i) {
+        for(int num : bucket[i])
+            order.push_back(num);
+    }
     int cases = 0, n;
     while(cin >> n && n != 0)
-        cout << "Case " << ++cases << ": " << answers[n].first << '\n';
+        cout << "Case " << ++cases << ": " << order[n - 1] << '\n';
     return 0;
 }
