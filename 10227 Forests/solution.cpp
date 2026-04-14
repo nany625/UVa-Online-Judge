4746f1435e22b62d9af497991ce3b6868c65c354
nany625 <jimmy19980625@gmail.com>
2026-04-15 05:20:58 +0800

    Update solution.cpp

--- 10227 Forests/solution.cpp
+++ 10227 Forests/solution.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int MAXT = 99;
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -11,19 +13,16 @@ int main() {
     while(cases--) {
         int P, T;
         cin >> P >> T;
-        vector<string> hear(P);
-        for(auto s : hear)
-            s.assign(T, '0');
+        cin.ignore();
+        vector<bitset<MAXT>> hear(P);
         string line;
         while(getline(cin, line) && !line.empty()) {
             istringstream iss(line);
             int i, j;
             iss >> i >> j;
-            hear[i - 1][j - 1] = '1';
+            hear[i - 1].set(j - 1);
         }
-        unordered_set<string> opinions;
-        for(string s : hear)
-            opinions.insert(s);
+        unordered_set<bitset<MAXT>> opinions(hear.begin(), hear.end());
         cout << opinions.size() << '\n';
         if(cases)
             cout << '\n';
