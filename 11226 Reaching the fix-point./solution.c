93cc1b71cac6b41abd3e241ad264bda484605aeb
nany625 <jimmy19980625@gmail.com>
2026-04-16 16:18:03 +0800

    Update solution.c

--- 11226 Reaching the fix-point./solution.c
+++ 11226 Reaching the fix-point./solution.c
@@ -13,7 +13,7 @@ void eulerSieve() {
             lsopf[n] = 1;
         } else
             lsopf[n] = 1 + lsopf[sopf[n]];
-        for(int i = 0, temp; i < size && (temp = primes[i] * n) <= MAX_NUM; ++i) {
+        for(int i = 0, temp; (temp = primes[i] * n) <= MAX_NUM; ++i) {
             sopf[temp] = sopf[primes[i]] + sopf[n];
             if(n % primes[i] == 0)
                 break;
