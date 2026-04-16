ab9b6b7c2c7d559dbf8406d86a1903ab7efd147c
nany625 <jimmy19980625@gmail.com>
2026-04-17 03:43:00 +0800

    Create solution.cpp

---
+++ 11908 Skyscraper/solution.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    for(int i = 1; i <= T; ++i) {
+        int N;
+        cin >> N;
+        vector<tuple<int, int, int>> ad(N);
+        for(int j = 0; j < N; ++j) {
+            cin >> get<0>(ad[j]) >> get<1>(ad[j]) >> get<2>(ad[j]);
+            get<1>(ad[j]) += get<0>(ad[j]);
+        }
+        sort(ad.begin(), ad.end(), [](auto &a, auto &b) {
+            return get<1>(a) < get<1>(b);
+        });
+        vector<int> ends(N);
+        for(int j = 0; j < N; ++j)
+            ends[j] = get<1>(ad[j]);
+        vector<int> dp(N);
+        dp[0] = get<2>(ad[0]);
+        for(int j = 1; j < N; ++j) {
+            int idx = upper_bound(ends.begin(), ends.begin() + j, get<0>(ad[j])) - ends.begin() - 1;
+            if(idx >= 0)
+                dp[j] = max(dp[j - 1], dp[idx] + get<2>(ad[j]));
+            else
+                dp[j] = max(dp[j - 1], get<2>(ad[j]));
+        }
+        cout << "Case " << i << ": " << dp[N - 1] << '\n';
+    }
+    return 0;
+}
