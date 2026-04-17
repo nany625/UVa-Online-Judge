3cd9766135f6d1e418f3620e34c655e231bb6e7d
nany625 <jimmy19980625@gmail.com>
2026-04-18 05:57:31 +0800

    Update solution.cpp

--- 10100 Longest Match/solution.cpp
+++ 10100 Longest Match/solution.cpp
@@ -47,3 +47,57 @@ int main() {
     }
     return 0;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> words1, words2;
+
+void getWords(vector<string> words, string line) {
+    string temp;
+    for(int i = 0; i < line.length(); ++i) {
+        if(isalnum(line[i]))
+            temp.push_back(line[i]);
+        else {
+            if(!temp.empty()) {
+                words.push_back(temp);
+                temp.clear();
+            }
+        }
+    }
+    if(!temp.empty())
+        words.push_back(temp);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int cases = 0;
+    string line1, line2;
+    while(getline(cin, line1) && getline(cin, line2)) {
+        if(line1.empty() || line2.empty()) {
+            cout << setw(2) << ++cases << ". Blank!\n";
+            continue;
+        }
+        
+        getWords(words1, line1);
+        getWords(words2, line2);
+        vector<int> dp(words2.size() + 1);
+        for(int i = 0; i < words1.size(); ++i) {
+            int prev = 0;
+            for(int j = 1; j <= words2.size(); ++j) {
+                int temp = dp[j];
+                if(words1[i] == words2[j - 1]) {
+                    dp[j] = prev + 1;
+                } else {
+                    dp[j] = max(dp[j], dp[j - 1]);
+                }
+                prev = temp;
+            }
+        }
+        cout << setw(2) << ++cases << ". Length of longest match: " << dp[words2.size()] << '\n';
+        words1.clear();
+        words2.clear();
+    }
+    return 0;
+}
