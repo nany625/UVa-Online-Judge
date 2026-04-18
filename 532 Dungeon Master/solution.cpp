169387572f4893828790dbe5465db8f284c40ce3
nany625 <jimmy19980625@gmail.com>
2026-04-18 18:40:41 +0800

    Update solution.cpp

--- 532 Dungeon Master/solution.cpp
+++ 532 Dungeon Master/solution.cpp
@@ -2,42 +2,31 @@
 using namespace std;
 
 const int MAX = 30;
-const int INF = 1e9;
 vector<vector<string>> maze(MAX, vector<string>(MAX));
 vector<vector<vector<int>>> times(MAX, vector<vector<int>>(MAX, vector<int>(MAX)));
-tuple<int, int, int> endss;
+vector<vector<int>> dir = {{-1, 1, 0, 0, 0, 0}, {0, 0, -1, 1, 0, 0}, {0, 0, 0, 0, -1, 1}};
 
-void bfs(queue<tuple<int, int, int>> q, int L, int R, int C) {
-    while(!q.empty() && q.front() != endss) {
+void bfs(queue<tuple<int, int, int>>& q, int L, int R, int C) {
+    while(!q.empty()) {
         int x = get<0>(q.front());
         int y = get<1>(q.front());
         int z = get<2>(q.front());
-        if(x > 0 && maze[x - 1][y][z] != '#' && times[x - 1][y][z] == INF) {
-            times[x - 1][y][z] = times[x][y][z] + 1;
-            q.push(make_tuple(x - 1, y, z));
-        }
-        if(x + 1 < L && maze[x + 1][y][z] != '#' && times[x + 1][y][z] == INF) {
-            times[x + 1][y][z] = times[x][y][z] + 1;
-            q.push(make_tuple(x + 1, y, z));
-        }
-        if(y > 0 && maze[x][y - 1][z] != '#' && times[x][y - 1][z] == INF) {
-            times[x][y - 1][z] = times[x][y][z] + 1;
-            q.push(make_tuple(x, y - 1, z));
-        }
-        if(y + 1 < R && maze[x][y + 1][z] != '#' && times[x][y + 1][z] == INF) {
-            times[x][y + 1][z] = times[x][y][z] + 1;
-            q.push(make_tuple(x, y + 1, z));
-        }
-        if(z > 0 && maze[x][y][z - 1] != '#' && times[x][y][z - 1] == INF) {
-            times[x][y][z - 1] = times[x][y][z] + 1;
-            q.push(make_tuple(x, y, z - 1));
-        }
-        if(z + 1 < C && maze[x][y][z + 1] != '#' && times[x][y][z + 1] == INF) {
-            times[x][y][z + 1] = times[x][y][z] + 1;
-            q.push(make_tuple(x, y, z + 1));
+        for(int i = 0; i < 6; ++i) {
+            int nx = x + dir[0][i];
+            int ny = y + dir[1][i];
+            int nz = z + dir[2][i];
+            if(nx >= 0 && nx < L && ny >= 0 && ny < R && nz >= 0 && nz < C && maze[nx][ny][nz] != '#' && times[nx][ny][nz] == -1) {
+                times[nx][ny][nz] = times[x][y][z] + 1;
+                if(maze[nx][ny][nz] == 'E') {
+                    cout << "Escaped in " << times[nx][ny][nz] << " minute(s).\n";
+                    return;
+                }
+                q.emplace(nx, ny, nz);
+            }
         }
         q.pop();
     }
+    cout << "Trapped!\n";
 }
 
 int main() {
@@ -53,20 +42,15 @@ int main() {
 	    for(int i = 0; i < L; ++i) {
 	        for(int j = 0; j < R; ++j) {
 	            for(int k = 0; k < C; ++k) {
-	                times[i][j][k] = INF;
 	                if(maze[i][j][k] == 'S') {
-	                    q.push(make_tuple(i, j, k));
+	                    q.emplace(i, j, k);
 	                    times[i][j][k] = 0;
-	                } else if(maze[i][j][k] == 'E')
-	                    endss = make_tuple(i, j, k);
+	                } else
+	                    times[i][j][k] = -1;
 	            }
 	        }
 	    }
 	    bfs(q, L, R, C);
-	    if(times[get<0>(endss)][get<1>(endss)][get<2>(endss)] == INF)
-	        cout << "Trapped!\n";
-	    else
-	        cout << "Escaped in " << times[get<0>(endss)][get<1>(endss)][get<2>(endss)] << " minute(s).\n";
     }
     return 0;
 }
