c47a98ce56a1ee14963b1a4cacbfd63d648c5920
nany625 <jimmy19980625@gmail.com>
2026-04-18 09:57:06 +0800

    Create solution.cpp

---
+++ 10910 Marks Distribution/solution.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 70;
+vector<vector<int>> ans(MAX, vector<int>(MAX));
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    for(int i = 1; i < MAX; ++i) {
+        ans[i][0] = 1;
+        for(int j = 1; j < MAX; ++j)
+            ans[i][j] = ans[i - 1][j] + ans[i][j - 1];
+    }
+    for(int i = 1; i < MAX; ++i) {
+        for(int j = 1; j < MAX; ++j)
+            ans[i][j] += ans[i][j - 1];
+    }
+    int K;
+    cin >> K;
+    while(K--) {
+        int N, T, P;
+        cin >> N >> T >> P;
+        if(T - N * P < 0)
+            cout << "0\n";
+        else
+            cout << ans[N - 1][T - N * P] << '\n';
+    }
+    return 0;
+}
