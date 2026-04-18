26f146abaf821d6f0987d2fbf2d5723856bcadc0
nany625 <jimmy19980625@gmail.com>
2026-04-19 04:43:58 +0800

    Update solution.cpp

--- 10910 Marks Distribution/solution.cpp
+++ 10910 Marks Distribution/solution.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 const int MAX = 70;
-vector<vector<int>> ans(MAX + 1, vector<int>(MAX + 1));
+vector<vector<int>> ans(MAX + 1, vector<int>(MAX));
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     for(int i = 1; i <= MAX; ++i) {
         ans[i][0] = 1;
-        for(int j = 1; j <= MAX; ++j)
+        for(int j = 1; j < MAX; ++j)
             ans[i][j] = ans[i - 1][j] + ans[i][j - 1];
     }
     int K;
