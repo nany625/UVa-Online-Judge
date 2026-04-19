5c89a2a44fd10ef3a17d48c79baf060ec495312f
nany625 <jimmy19980625@gmail.com>
2026-04-19 09:31:27 +0800

    Create solution.cpp

---
+++ 10308 Roads in the North/solution.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXE = 10000;
+vector<vector<pair<int, int>>> roads(MAXE + 1);
+bitset<MAXE + 1> visited;
+int ans, S;
+
+void init() {
+    for(int i = 0; i <= MAXE; ++i)
+        roads[i].clear();
+    visited.reset();
+    ans = 0;
+}
+
+void addRoads(string line) {
+    istringstream iss(line);
+    int u, v, w;
+    iss >> u >> v >> w;
+    roads[u].emplace_back(v, w);
+    roads[v].emplace_back(u, w);
+}
+
+void dfs(int V, int dist) {
+    visited[V] = 1;
+    for(int i = 0; i < roads[V].size(); ++i) {
+        if(!visited[roads[V][i].first])
+            dfs(roads[V][i].first, dist + roads[V][i].second);
+    }
+    if(ans < dist) {
+        ans = dist;
+        S = V;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string line;
+    int u, v, w;
+    while(getline(cin, line) && !line.empty()) {
+        init();
+        addRoads(line);
+        while(getline(cin, line) && !line.empty())
+            addRoads(line);
+        dfs(1, 0);
+        visited.reset();
+        dfs(S, 0);
+        cout << ans << '\n';
+    }
+    return 0;
+}
