59d9fb76916fc9ffdef691ad6481ca6a5db08223
nany625 <jimmy19980625@gmail.com>
2026-04-20 08:44:21 +0800

    Create solution.cpp

---
+++ 11077 Find the Permutations/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 21;
+vector<vector<unsigned long>> dp(MAXN + 1, vector<unsigned long>(MAXN));
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    for(int i = 1; i <= MAXN; ++i) {
+        dp[i][0] = 1;
+        for(int j = 1; j < i; ++j)
+            dp[i][j] = dp[i - 1][j - 1] * (i - 1) + dp[i - 1][j];
+    }
+    int N, K;
+    while(cin >> N >> K && N != 0)
+        cout << dp[N][K] << '\n';
+    return 0;
+}
