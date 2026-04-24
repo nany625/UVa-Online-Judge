a456952920732fa1ca269f4cb1f57c3f46b24f4a
nany625 <jimmy19980625@gmail.com>
2026-04-24 09:19:20 +0800

    Create solution.cpp

---
+++ 10986 Sending email/solution.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 20000;
+vector<vector<pair<int, int>>> adj(MAXN);
+
+int dijkstra(int n, int S, int T) {
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    vector<int> dist(n, INT_MAX);
+    dist[S] = 0;
+    pq.emplace(0, S);
+    do {
+        auto [d, u] = pq.top();
+        pq.pop();
+        if(d > dist[u])
+            continue;
+        for(auto [v, w] : adj[u]) {
+            if(dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                pq.emplace(dist[v], v);
+            }
+        }
+    } while(!pq.empty());
+    return dist[T];
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    for(int i = 1; i <= N; ++i) {
+        int n, m, S, T;
+        cin >> n >> m >> S >> T;
+        for(int j = 0; j < n; ++j)
+            adj[j].clear();
+        while(m--) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            adj[u].emplace_back(v, w);
+            adj[v].emplace_back(u, w);
+        }
+        cout << "Case #" << i << ": ";
+        int ans = dijkstra(n, S, T);
+        if(ans == INT_MAX)
+            cout << "unreachable\n";
+        else
+            cout << ans << '\n';
+    }
+    return 0;
+}
