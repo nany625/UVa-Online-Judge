ff19089c10c84b39cfd3c78bd22bed569a3b2602
nany625 <jimmy19980625@gmail.com>
2026-04-25 00:21:58 +0800

    Update solution.cpp

--- 10049 Self-describing Sequence/solution.cpp
+++ 10049 Self-describing Sequence/solution.cpp
@@ -3,20 +3,6 @@ using namespace std;
 
 vector<int> f;
 
-int binarySearch(int key) {
-    int left = 0, right = f.size() - 2;
-    while(left <= right) {
-        int mid = left + (right - left >> 1);
-        if(f[mid] == key)
-            return mid + 1;
-        if(f[mid] < key)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-    return left;
-}
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
