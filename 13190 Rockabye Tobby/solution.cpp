4770a7a2f68477335485588e50cf9dacd235a106
nany625 <jimmy19980625@gmail.com>
2026-04-26 22:38:18 +0800

    Create solution.cpp

---
+++ 13190 Rockabye Tobby/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while(T--) {
+        int n, k;
+        cin >> n >> k;
+        vector<pair<string, int>> medicine(n);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        for(int i = 0; i < n; ++i) {
+            cin >> medicine[i].first >> medicine[i].second;
+            pq.emplace(medicine[i].second, i);
+        }
+        while(k--) {
+            auto [t, num] = pq.top();
+            cout << t << ' ' << medicine[num].first << '\n';
+            pq.emplace(t + medicine[num].second, num);
+            pq.pop();
+        }
+    }
+    return 0;
+}
