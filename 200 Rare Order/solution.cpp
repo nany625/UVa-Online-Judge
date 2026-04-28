342ae5d9cc1dff1d0afa01870db7d8417aae28cc
nany625 <jimmy19980625@gmail.com>
2026-04-29 07:07:12 +0800

    Create solution.cpp

---
+++ 200 Rare Order/solution.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 26;
+vector<vector<int>> adj(MAXN);
+vector<int> indeg(MAXN);
+unordered_map<char, int> charNum;
+vector<char> charDict;
+
+vector<int> topo_sort() {
+    queue<int> q;
+    for(int i = 0; i < charDict.size(); ++i) {
+        if(indeg[i] == 0)
+            q.push(i);
+    }
+    vector<int> topo;
+    do {
+        int u = q.front();
+        q.pop();
+        topo.push_back(u);
+        for(int v : adj[u]) {
+            if(--indeg[v] == 0)
+                q.push(v);
+        }
+    } while(!q.empty());
+    return topo;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    string pre, nxt;
+    cin >> pre;
+    charNum[pre[0]] = 0;
+    charDict.push_back(pre[0]);
+    int id = 1;
+    while(cin >> nxt && nxt[0] != '#') {
+        int mn = min(pre.length(), nxt.length());
+        for(int i = 0; i < mn; ++i) {
+            if(pre[i] != nxt[i]) {
+                if(!charNum.count(pre[i])) {
+                    charNum[pre[i]] = id++;
+                    charDict.push_back(pre[i]);
+                }
+                if(!charNum.count(nxt[i])) {
+                    charNum[nxt[i]] = id++;
+                    charDict.push_back(nxt[i]);
+                }
+                int u = charNum[pre[i]];
+                int v = charNum[nxt[i]];
+                adj[u].push_back(v);
+                ++indeg[v];
+                break;
+            }
+        }
+        pre = nxt;
+    }
+    vector<int> topo = topo_sort();
+    for(int x : topo)
+        cout << charDict[x];
+    cout << '\n';
+    return 0;
+}
