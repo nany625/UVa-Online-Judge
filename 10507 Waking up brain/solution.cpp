7ce4502d09263cfc1349d2517c3a4666b08673c0
nany625 <jimmy19980625@gmail.com>
2026-04-29 05:33:58 +0800

    Create solution.cpp

---
+++ 10507 Waking up brain/solution.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 26;
+vector<vector<int>> adj(MAXN);
+vector<int> indeg(MAXN);
+
+int topo_sort(int n, string dirSti) {
+    vector<int> q;
+    for(int i = 0; i < dirSti.length(); ++i) {
+        indeg[dirSti[i] - 'A'] = 0;
+        q.push_back(dirSti[i] - 'A');
+    }
+    vector<int> topo;
+    int year = 0;
+    do {
+        topo = q;
+        q.clear();
+        for(int u : topo) {
+            for(int v : adj[u]) {
+                if(--indeg[v] == 0) {
+                    q.push_back(v);
+                    --n;
+                }
+            }
+        }
+        if(!q.empty())
+            ++year;
+    } while(!q.empty());
+    if(n == 0)
+        return year;
+    return INT_MAX;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int N;
+    while(cin >> N) {
+        for(int i = 0; i < MAXN; ++i)
+            adj[i].clear();
+        fill(indeg.begin(), indeg.end(), 3);
+        int M;
+        cin >> M;
+        string dirSti;
+        cin >> dirSti;
+        string conn;
+        while(M--) {
+            cin >> conn;
+            adj[conn[0] - 'A'].push_back(conn[1] - 'A');
+            adj[conn[1] - 'A'].push_back(conn[0] - 'A');
+        }
+        int ans = topo_sort(N - dirSti.length(), dirSti);
+        if(ans == INT_MAX)
+            cout << "THIS BRAIN NEVER WAKES UP\n";
+        else
+            cout << "WAKE UP IN, " << ans << ", YEARS\n";
+    }
+    return 0;
+}
