8fc537254150b333838b4a566fc3f29485a993ef
nany625 <jimmy19980625@gmail.com>
2026-04-30 00:07:03 +0800

    Create solution.cpp

---
+++ 10450 World Cup Noise/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<long> ans(51);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ans[0] = 1;
+    ans[1] = 2;
+    for(int n = 2; n < 51; ++n)
+        ans[n] = ans[n - 2] + ans[n - 1];
+    int scenarios;
+    cin >> scenarios;
+    for(int i = 1; i <= scenarios; ++i) {
+        int n;
+        cin >> n;
+        cout << "Scenario #" << i << ":\n" << ans[n] << "\n\n";
+    }
+    return 0;
+}
