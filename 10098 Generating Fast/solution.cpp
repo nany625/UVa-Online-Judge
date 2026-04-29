2022c2e5be516ec2ced123356de76946c0c083f0
nany625 <jimmy19980625@gmail.com>
2026-04-29 23:13:17 +0800

    Optimize permutation generation using next_permutation
    Replaced backtracking approach with next_permutation for generating permutations.

--- 10098 Generating Fast/solution.cpp
+++ 10098 Generating Fast/solution.cpp
@@ -1,28 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bitset<10> used;
 string str;
-vector<char> path(10);
-
-void backtrack(int n, int depth) {
-    if(depth == n) {
-        for(int i = 0; i < n; ++i)
-            cout << path[i];
-        cout << '\n';
-        return;
-    }
-    for(int i = 0; i < n; ++i) {
-        if(used[i])
-            continue;
-        if(i > 0 && str[i - 1] == str[i] && !used[i - 1])
-            continue;
-        used[i] = 1;
-        path[depth] = str[i];
-        backtrack(n, depth + 1);
-        used[i] = 0;
-    }
-}
 
 int main() {
     ios::sync_with_stdio(false);
@@ -32,8 +11,9 @@ int main() {
     while(n--) {
         cin >> str;
         sort(str.begin(), str.end());
-        used.reset();
-        backtrack(str.length(), 0);
+        do {
+            cout << str << '\n';
+        } while(next_permutation(str.begin(), str.end()));
         cout << '\n';
     }
     return 0;
