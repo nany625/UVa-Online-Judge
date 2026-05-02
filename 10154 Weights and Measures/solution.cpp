815f0ec3c51229a30fc736f4b769ade01a73c6dd
nany625 <jimmy19980625@gmail.com>
2026-05-03 01:57:41 +0800

    Update solution.cpp

--- 10154 Weights and Measures/solution.cpp
+++ 10154 Weights and Measures/solution.cpp
@@ -1,3 +1,4 @@
+// #解法一
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -23,5 +24,31 @@ int main() {
         }
     }
     cout << pq.size() << '\n';
+    return 0;
+}
+
+//#解法二
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<pair<int, int>> turtle;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int w, s;
+    while(cin >> w >> s)
+        turtle.emplace_back(s, w);
+    sort(turtle.begin(), turtle.end());
+    vector<int> dp = {0};
+    for(auto [s, w] : turtle) {
+        if(dp.back() + w <= s)
+            dp.push_back(dp.back() + w);
+        for(int i = dp.size() - 2; i >= 0; --i) {
+            if(dp[i] + w <= s)
+                dp[i + 1] = min(dp[i + 1], dp[i] + w);
+        }
+    }
+    cout << dp.size() - 1 << '\n';
     return 0;
 }
