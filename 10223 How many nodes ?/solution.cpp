8676b07c1e4d0b5cdec114bb93e433773d9cd18b
nany625 <jimmy19980625@gmail.com>
2026-05-08 23:47:41 +0800

    Refactor ans initialization and update loop

--- 10223 How many nodes ?/solution.cpp
+++ 10223 How many nodes ?/solution.cpp
@@ -1,12 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<long> ans(20);
+array<long, 20> ans = {0, 1};
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    ans[1] = 1;
     for(int n = 2; n < 20; ++n)
         ans[n] = (ans[n - 1] << 1) * ((n << 1) - 1) / (n + 1);
     int n;
